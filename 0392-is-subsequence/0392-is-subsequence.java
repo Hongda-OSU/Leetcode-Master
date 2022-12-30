f3aa4c7e9dfb68e1b28a5d53d0e17f57f13578b8
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2022-12-30 11:08:44 -0500

    Time: 1 ms (92.06%), Space: 40.5 MB (71.68%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,9 +3,7 @@ class Solution {
         if (s.length() == 0) return true;
         int p = 0;
         for (int i = 0; i < t.length(); i++) {
-            char c1 = s.charAt(p);
-            char c2 = t.charAt(i);
-            if (c1 == c2) p++;
+            if (s.charAt(p) == t.charAt(i)) p++;
             if (p == s.length()) return true;
         }
         return false;
