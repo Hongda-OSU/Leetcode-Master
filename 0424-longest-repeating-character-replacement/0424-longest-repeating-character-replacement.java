26880870d30ba999564c3e63342016cd4921dfd1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-09 14:57:23 -0500

    Time: 6 ms (86.90%), Space: 42.2 MB (80.57%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,15 +1,15 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int len = s.length();
-        int[] alphaMapping = new int[26];
-        // maxLength = largest count of a single, unique character in the current window
+        int[] sMapping = new int[26];
+        // maxLength: largest count of a sigle unique character in the current window
         int left = 0, maxLength = 0, mostFreqLetter = 0;
         for (int right = 0; right < len; right++) {
-            alphaMapping[s.charAt(right) - 'A']++;
-            mostFreqLetter = Math.max(mostFreqLetter, alphaMapping[s.charAt(right) - 'A']);
-            int lettersToChange = (right - left + 1) - mostFreqLetter;
-            if (lettersToChange > k) {
-                alphaMapping[s.charAt(left) - 'A']--;
+            sMapping[s.charAt(right) - 'A']++;
+            mostFreqLetter = Math.max(mostFreqLetter, sMapping[s.charAt(right) - 'A']);
+            int letterToChange = (right - left + 1) - mostFreqLetter;
+            if (letterToChange > k) {
+                sMapping[s.charAt(left) - 'A']--;
                 left++;
             }
             maxLength = Math.max(maxLength, right - left + 1);
