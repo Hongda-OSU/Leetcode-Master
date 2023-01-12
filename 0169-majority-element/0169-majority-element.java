f9477334e43691f839cfe948dddea257b248cc34
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-12 14:27:26 -0500

    Time: 8 ms (55.88%), Space: 47.3 MB (67.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,8 +3,8 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) 
             map.put(num, map.getOrDefault(num, 0) + 1);
-        for (int i : nums)
-            if (map.get(i) > nums.length / 2) return i;
+        for (int key : map.keySet())
+            if (map.get(key) > nums.length / 2) return key;
         return 0;
     }
 }
