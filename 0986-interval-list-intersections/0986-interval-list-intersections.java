4c4ad056d40ddc92ccde63bceb83d34bf177de42
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-16 16:43:49 -0500

    Time: 3 ms (93.15%), Space: 43.3 MB (56.95%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -5,9 +5,12 @@ class Solution {
         if (l1 == 0 || l2 == 0) return new int[0][0];
         int left = 0, right = 0, i = 0, j = 0;
         while (i < l1 && j < l2) {
+            // left will always be maximum of two intervals
             left = Math.max(firstList[i][0], secondList[j][0]);
+            // right will always be minimum of two intervals
             right = Math.min(firstList[i][1], secondList[j][1]);
             if (left <= right) list.add(new int[]{left, right});
+            // Move to next interval
             if (firstList[i][1] == right) i++;
             if (secondList[j][1] == right) j++;
         }
