5c2852860ecee2d931ffe30a40bf371632dca52c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-17 16:13:48 -0500

    Time: 130 ms (53.05%), Space: 59 MB (63.09%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+        // symmetric like "aa" is also palindrome
+        int pairs = 0, symmetrics = 0;
+        for (String word : words) {
+            String reverse = new StringBuilder(word).reverse().toString();
+            if (map.getOrDefault(reverse, 0) > 0) {
+                pairs++;
+                map.merge(reverse, -1, (a, b) -> a + b);
+                symmetrics -= word.charAt(0) == word.charAt(1) ? 1 : 0;
+            } else {
+                map.merge(word, 1, (a, b) -> a + b);
+                symmetrics += word.charAt(0) == word.charAt(1) ? 1 : 0;
+            } 
+        }
+        return 4 * pairs + (symmetrics > 0 ? 2 : 0);
+    }
+}
