8ba6d7a8aa9fd938fefefc98c1762dfb7ba16e24
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-17 17:55:54 -0500

    Time: 1 ms (100.00%), Space: 50.6 MB (67.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,11 +2,14 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
         for (int i = 0, temp = 1; i < nums.length; i++) {
-            // get the prev num in slot
+            // get the prev nums multiplication in slot
+            // ex: [1, 2, 3, 4] -> result: [1, 1, 2, 6]
             result[i] = temp;
             temp *= nums[i];
         }
         for (int j = nums.length - 1, temp = 1; j >= 0; j--) {
+            // get the next nums multiplication in slot
+            // result: [1, 1, 2, 6] -> [ ..., 4 * 3 * 2 = 12, 6 * 1 = 6]
             result[j] *= temp;
             temp *= nums[j];
         }
