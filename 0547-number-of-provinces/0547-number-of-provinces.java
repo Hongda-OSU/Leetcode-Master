44040ee55376a02ebdca77962c677b439bde243b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-18 14:31:10 -0500

    Time: 1 ms (99.81%), Space: 43 MB (88.81%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int numProvinces = 0;
+        boolean[] visited = new boolean[isConnected.length];
+        for (int i = 0; i < isConnected.length; i++) {
+            if (visited[i] == false) {
+                dfs(isConnected, visited, i);
+                numProvinces++;
+            }
+        }
+        return numProvinces;
+    }
+    
+    public void dfs(int[][] isConnected, boolean[] visited, int i) {
+        // since it's an n * n matrix, then i is row, j is column, imax == jmax
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && visited[j] == false) {
+                visited[j] = true;
+                dfs(isConnected, visited, j);
+            }
+        }
+    }
+}
