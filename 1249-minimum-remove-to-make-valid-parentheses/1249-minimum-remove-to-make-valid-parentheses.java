c8e461eec91e621c197e09f2da35b922b2fd6e74
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-26 15:39:53 -0500

    Time: 59 ms (26.28%), Space: 55.4 MB (5.19%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isAlphabetic(ch))
+                continue;
+            if (ch == '(') {
+                stack.push(i);
+            } else {
+                if (!stack.isEmpty() && s.charAt(stack.peek()) == '(')
+                    stack.pop();
+                else
+                    stack.push(i);
+            } 
+        }
+        StringBuilder sb = new StringBuilder();
+        HashSet<Integer> set = new HashSet<>(stack);
+        for (int i = 0; i < s.length(); i++) {
+            if (!set.contains(i))
+                sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
