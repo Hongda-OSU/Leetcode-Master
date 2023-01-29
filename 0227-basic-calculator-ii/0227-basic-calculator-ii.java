62506b558ed633541f3203fa567b3f96fde2eeff
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-29 15:27:12 -0500

    Time: 7 ms (99.30%), Space: 43.3 MB (81.26%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,26 +1,30 @@
 class Solution {
     public int calculate(String s) {
         if (s == null || s.isEmpty()) return 0;
-        int currentNumber = 0, lastNumber = 0, result = 0;
-        char operation = '+';
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (Character.isDigit(ch))
-                currentNumber = (currentNumber * 10) + (ch - '0');
-            if (!Character.isDigit(ch) && !Character.isWhitespace(ch) || i == s.length() - 1) {
-                if (operation == '+' || operation == '-') {
-                    result += lastNumber;
-                    lastNumber = operation == '+' ? currentNumber : -currentNumber;
-                } else if (operation == '*') {
-                    lastNumber = lastNumber * currentNumber;
-                } else if (operation == '/') {
-                    lastNumber = lastNumber / currentNumber;
+        int num = 0, tmp = 0, res = 0;
+        char op = '+';
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                tmp = tmp * 10 + c - '0';
+            } else if (c != ' ') {
+				//process the numerical value of string so far; based on what 'op' we have before it
+                num = cal(num, tmp, op);
+                if (c == '+' || c == '-') {
+                    res += num;
+                    num = 0;
                 }
-                operation = ch;
-                currentNumber = 0;
+				// reset
+                tmp = 0;
+                op = c;
             }
         }
-        result += lastNumber;
-        return result;
+        return res + cal(num, tmp, op);
+    }
+    
+    public int cal(int num, int tmp, char op) {
+        if (op == '+') return num + tmp;
+        else if (op == '-') return num - tmp;
+        else if (op == '*') return num * tmp;
+        else    return num / tmp;
     }
 }
