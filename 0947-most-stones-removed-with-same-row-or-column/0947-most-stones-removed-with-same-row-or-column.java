3c658088cdd120d781adb7b2066cf890e6bc2d52
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-30 13:55:14 -0500

    Time: 67 ms (28.02%), Space: 42.7 MB (66.67%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        if (n <= 1) return 0;
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < n; i++) {
+            int[] u = stones[i];
+            for (int j = 0; j < n; j++) {
+                if (j == i) continue;
+                int[] v = stones[j];
+                if (u[0] == v[0] || u[1] == v[1])
+                    graph[i].add(j);
+            }
+        }
+        boolean[] visited = new boolean[n];
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            dfs(graph, visited, i);
+            result++;
+        }
+        return n - result;
+    }
+    
+    public void dfs(List<Integer>[] graph, boolean[] visited, int start) {
+        visited[start] = true;
+        List<Integer> neighbors = graph[start];
+        for (int neighbor : neighbors) {
+            if (visited[neighbor])
+                continue;
+            dfs(graph, visited, neighbor);
+        }
+    }
+}
