acd60ed3dabc974335563b3ee1e55635ec3c13f2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-01-31 14:20:40 -0500

    Time: 2 ms (68.23%), Space: 41.9 MB (87.42%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -2,14 +2,15 @@ class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         boolean[] seen = new boolean[rooms.size()];
         seen[0] = true;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
+        Stack<Integer> visitedRooms = new Stack<>();
+        visitedRooms.push(0);
         int count = 1; // room 0 is opened
-        while (!stack.isEmpty()) {
-            int key = stack.pop();
-            for (int room : rooms.get(key)) {
+        while (!visitedRooms.isEmpty()) {
+            int visitedRoom = visitedRooms.pop();
+            // get all the keys in that visited room, which also represent the room going to be visited
+            for (int room : rooms.get(visitedRoom)) {
                 if (!seen[room]) {
-                    stack.push(room); // key i to room i added
+                    visitedRooms.push(room); // key i to room i added
                     seen[room] = true;
                     count++;
                 }
