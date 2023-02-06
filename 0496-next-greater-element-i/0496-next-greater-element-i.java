40006ef5ef429ad0f83bff4ea5353c53334f8fa7
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-02-06 15:42:51 -0500

    Time: 3 ms (87.40%), Space: 42.4 MB (58.85%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>(); //LIFO
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) 
+                // store num (key) next great number (value)
+                map.put(stack.pop(), num);
+            stack.add(num);
+        }
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        } 
+        return result;
+    }
+}
