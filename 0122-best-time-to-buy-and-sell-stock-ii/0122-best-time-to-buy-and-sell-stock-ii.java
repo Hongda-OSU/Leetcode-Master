0259482f90ea0234a5d53f6a5f8c2cd7a0e4e0b0
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-02-08 16:44:52 -0500

    Time: 1 ms (86.97%), Space: 42.4 MB (57.13%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,6 +2,9 @@ class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
         for (int i = 1; i < prices.length; i++) {
+            // Checking if we can profit with previous day's price.
+            // If yes, then we buy on previous day and sell on current day.
+            // Add all such profits to get the total profit.
             if (prices[i] > prices[i - 1]) {
                 maxProfit += prices[i] - prices[i - 1];
             }
