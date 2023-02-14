3f85ebbd1bd4452451b1a549d410ef899c0e76ac
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-02-14 17:25:36 -0500

    Time: 2 ms (83.55%), Space: 42.4 MB (86.89%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int closedIsland(int[][] grid) {
+        int row = grid.length, col = grid[0].length;
+        int result = 0;
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (grid[r][c] == 0) {
+                    if (dfs(grid, r, c))
+                        result++;
+                }
+            }
+        }
+        return result;
+    }
+    
+    public boolean dfs(int[][] grid, int i, int j) {
+        int row = grid.length;
+        int col = grid[0].length;
+        // go beyond of the border is not a good idea
+        if (i < 0 || j < 0 || i >= row || j >= col) return false;
+        // okay we hit the wall but at least we are okay
+        if (grid[i][j] == 1 || grid[i][j] == 2) return true;
+        // visit this cell, mark it as visited by changing the value to 2
+        grid[i][j] = 2;
+        
+        boolean up = dfs(grid, i + 1, j);
+        boolean down = dfs(grid, i - 1, j);
+        boolean left = dfs(grid, i, j - 1);
+        boolean right = dfs(grid, i, j + 1);
+        
+        return up && down && left && right;
+    }
+}
