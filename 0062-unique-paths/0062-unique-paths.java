216a305ec4f6dec0ca58430bd563d58257bb59a1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-02-15 14:45:33 -0500

    Time: 0 ms (100.00%), Space: 39.1 MB (59.37%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -3,7 +3,7 @@ class Solution {
         int[][] dp = new int[m][n];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(i==0||j==0)
+                if(i == 0 || j == 0)
                     dp[i][j] = 1;
                 else
                     dp[i][j] = dp[i][j-1] + dp[i-1][j];
