cfc3fe5e287d3b0a48c355fa7217dd6d60ba2d06
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-02-27 16:19:21 -0500

    Time: 36 ms (48.16%), Space: 56.6 MB (35.53%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -2,25 +2,25 @@ class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         if (n == 1)
             return Collections.singletonList(0);
-        List<Set<Integer>> adj = new ArrayList<>();
+        List<Set<Integer>> neighbors = new ArrayList<>();
         for (int i = 0; i < n; i++)
-            adj.add(new HashSet<>());
+            neighbors.add(new HashSet<>());
         for (int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+            neighbors.get(edge[0]).add(edge[1]);
+            neighbors.get(edge[1]).add(edge[0]);
         }
         List<Integer> leaves = new ArrayList<>();
         for (int i = 0; i < n; i++) 
-            if (adj.get(i).size() == 1)
+            if (neighbors.get(i).size() == 1)
                 leaves.add(i);
         while (n > 2) {
             n -= leaves.size();
             List<Integer> newLeaves = new ArrayList<>();
-            for (int i : leaves) {
-                int j = adj.get(i).iterator().next();
-                adj.get(j).remove(i);
-                if (adj.get(j).size() == 1)
-                    newLeaves.add(j);
+            for (int leaf : leaves) {
+                int neighbor = neighbors.get(leaf).iterator().next();
+                neighbors.get(neighbor).remove(leaf);
+                if (neighbors.get(neighbor).size() == 1)
+                    newLeaves.add(neighbor);
             }
             leaves = newLeaves;
         }
