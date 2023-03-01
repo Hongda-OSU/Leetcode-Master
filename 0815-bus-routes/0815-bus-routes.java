9999cc7e81c8f78702fcc45354369b7abc23d251
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-01 15:14:49 -0500

    Time: 46 ms (79.69%), Space: 68.1 MB (53.22%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.java
+++ 0815-bus-routes/0815-bus-routes.java
@@ -1,36 +1,36 @@
 class Solution {
-    public int numBusesToDestination(int[][] routes, int S, int T) {
+    public int numBusesToDestination(int[][] routes, int source, int target) {
         HashSet<Integer> visited = new HashSet<>();
-       Queue<Integer> q = new LinkedList<>();
-       HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-       int ret = 0; 
-        
-       if (S==T) return 0; 
-        
-       for(int i = 0; i < routes.length; i++){
-            for(int j = 0; j < routes[i].length; j++){
+        Queue<Integer> q = new LinkedList<>();
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        int result = 0;
+        if (source == target)
+            return 0;
+        for (int i = 0; i < routes.length; i++) {
+            for (int j = 0; j < routes[i].length; j++) {
                 ArrayList<Integer> buses = map.getOrDefault(routes[i][j], new ArrayList<>());
                 buses.add(i);
-                map.put(routes[i][j], buses);                
-            }       
+                map.put(routes[i][j], buses);
+            }
         }
-                
-       q.offer(S); 
-       while (!q.isEmpty()) {
-           int len = q.size();
-           ret++;
-           for (int i = 0; i < len; i++) {
-               int cur = q.poll();
-               ArrayList<Integer> buses = map.get(cur);
-               for (int bus: buses) {
-                    if (visited.contains(bus)) continue;
+        q.offer(source);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            result++;
+            for (int i = 0; i < size; i++) {
+                int curr = q.poll();
+                ArrayList<Integer> buses = map.get(curr);
+                for (int bus : buses) {
+                    if (visited.contains(bus))
+                        continue;
                     visited.add(bus);
                     for (int j = 0; j < routes[bus].length; j++) {
-                        if (routes[bus][j] == T) return ret;
-                        q.offer(routes[bus][j]);  
-                   }
-               }
-           }
+                        if (routes[bus][j] == target)
+                            return result;
+                        q.offer(routes[bus][j]);
+                    }
+                }
+            }
         }
         return -1;
     }
