2a9d0c532fc0e85bcc24fe74d89d55a5f6adf57c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-01 15:50:38 -0500

    Time: 6 ms (54.34%), Space: 43.3 MB (22.87%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.java
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.java
@@ -1,35 +1,85 @@
 class Solution {
     public int earliestAcq(int[][] logs, int n) {
-        Arrays.sort(logs, (a, b) -> a[0] - b[0]);
-        int[] group = new int[n], size = new int[n];
-        for (int i = 0; i < n; i++) {
-            group[i] = i;
-            size[i] = 1;
-        }
+
+        // First, we need to sort the events in chronological order.
+        Arrays.sort(logs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] log1, int[] log2) {
+                Integer tsp1 = new Integer(log1[0]);
+                Integer tsp2 = new Integer(log2[0]);
+                return tsp1.compareTo(tsp2);
+            }
+        });
+
+        // Initially, we treat each individual as a separate group.
+        int groupCount = n;
+        UnionFind uf = new UnionFind(n);
+
         for (int[] log : logs) {
-            int f1 = find(group, log[1]);
-            int f2 = find(group, log[2]);
-            if (f1 != f2) {
-                union(size, group, f1, f2);
-                if (--n == 1)
-                    return log[0];
+            int timestamp = log[0], friendA = log[1], friendB = log[2];
+
+            // We merge the groups along the way.
+            if (uf.union(friendA, friendB)) {
+                groupCount -= 1;
+            }
+
+            // The moment when all individuals are connected to each other.
+            if (groupCount == 1) {
+                return timestamp;
             }
         }
+
+        // There are still more than one groups left,
+        //  i.e. not everyone is connected.
         return -1;
     }
-    
-    public void union(int[] size, int[] group, int i, int j) {
-        if (size[i] < size[j])
-            union(size, group, j, i);
-        group[j] = i;
-        size[i] += size[j];
-    }
-    
-    public int find(int[] group, int x) {
-        while (group[x] != x) {
-            group[x] = group[group[x]];
-            x = group[x];
+}
+
+class UnionFind {
+    private int[] group;
+    private int[] rank;
+
+    public UnionFind(int size) {
+        this.group = new int[size];
+        this.rank = new int[size];
+        for (int person = 0; person < size; ++person) {
+            this.group[person] = person;
+            this.rank[person] = 0;
         }
-        return group[x];
     }
-} 
+
+    /** Return the id of group that the person belongs to. */
+    public int find(int person) {
+        if (this.group[person] != person)
+            this.group[person] = this.find(this.group[person]);
+        return this.group[person];
+    }
+
+    /**
+     * If it is necessary to merge the two groups that x, y belong to.
+     * @return true: if the groups are merged.
+     */
+    public boolean union(int a, int b) {
+        int groupA = this.find(a);
+        int groupB = this.find(b);
+        boolean isMerged = false;
+
+        // The two people share the same group.
+        if (groupA == groupB)
+            return isMerged;
+
+        // Otherwise, merge the two groups.
+        isMerged = true;
+        // Merge the lower-rank group into the higher-rank group.
+        if (this.rank[groupA] > this.rank[groupB]) {
+            this.group[groupB] = groupA;
+        } else if (this.rank[groupA] < this.rank[groupB]) {
+            this.group[groupA] = groupB;
+        } else {
+            this.group[groupA] = groupB;
+            this.rank[groupB] += 1;
+        }
+
+        return isMerged;
+    }
+}
