49943c908364d9695d887f9eb8051bb215dafabb
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-01 15:01:42 -0500

    Time: 6 ms (72.53%), Space: 43 MB (90.74%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+        for (int[] flight : flights) 
+            adj.get(flight[0]).add(new int[]{flight[1], flight[2]});
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{src, 0});
+        int[] minCost = new int[n];
+        Arrays.fill(minCost, Integer.MAX_VALUE);
+        int stops = 0;
+        while (!q.isEmpty() && stops <= k) {
+            int size = q.size();
+            while (size-- > 0) {
+                int[] curr = q.poll();
+                for (int[] neighbor : adj.get(curr[0])) {
+                    int price = neighbor[1], destination = neighbor[0];
+                    if (price + curr[1] >= minCost[destination])
+                        continue;
+                    minCost[destination] = price + curr[1];
+                    q.offer(new int[]{destination, minCost[destination]});
+                }
+            }
+            stops++;
+        }
+        return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
+    }
+}
