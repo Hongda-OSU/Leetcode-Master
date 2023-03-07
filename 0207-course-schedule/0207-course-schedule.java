a084b0f0c1ff5f35d4cca450a0dc4a92af244c1b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-07 16:02:21 -0500

    Time: 24 ms (9.87%), Space: 42.8 MB (56.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,16 +1,18 @@
 class Solution {
+    public  boolean[] memo;
+    
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         // using dfs
+        memo = new boolean[numCourses];
         boolean[] waitList = new boolean[numCourses];
-        boolean[] memo = new boolean[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (!canFinishThisCourse(i, prerequisites, waitList, memo))
+            if (!canFinishThisCourse(i, prerequisites, waitList))
                 return false;
         }
         return true;
     }
     
-    public boolean canFinishThisCourse(int course, int[][] prerequisites, boolean[] waitList, boolean[] memo) {
+    public boolean canFinishThisCourse(int course, int[][] prerequisites, boolean[] waitList) {
         if (memo[course])
             return true;
         if (waitList[course])
@@ -18,7 +20,7 @@ class Solution {
         waitList[course] = true;
         for (int[] pre : prerequisites) {
             if (pre[0] == course) {
-                if (!canFinishThisCourse(pre[1], prerequisites, waitList, memo))
+                if (!canFinishThisCourse(pre[1], prerequisites, waitList))
                     return false;
             }
         }
