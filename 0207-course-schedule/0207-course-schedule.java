7f4cf48b643ad0678c4d87a9d286edf5bb2cac37
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-07 16:03:10 -0500

    Time: 2 ms (99.68%), Space: 42.8 MB (56.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,29 +1,36 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // using dfs
-        boolean[] waitList = new boolean[numCourses];
-        boolean[] memo = new boolean[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            if (!canFinishThisCourse(i, prerequisites, waitList, memo))
-                return false;
-        }
-        return true;
-    }
-    
-    public boolean canFinishThisCourse(int course, int[][] prerequisites, boolean[] waitList, boolean[] memo) {
-        if (memo[course])
-            return true;
-        if (waitList[course])
-            return false;
-        waitList[course] = true;
-        for (int[] pre : prerequisites) {
-            if (pre[0] == course) {
-                if (!canFinishThisCourse(pre[1], prerequisites, waitList, memo))
+   public boolean canFinish(int numCourses, int[][] prerequisites) {
+            ArrayList[] graph = new ArrayList[numCourses];
+            for(int i=0;i<numCourses;i++)
+                graph[i] = new ArrayList();
+                
+            boolean[] visited = new boolean[numCourses];
+            boolean[] dp = new boolean[numCourses];
+            for(int i=0; i<prerequisites.length;i++){
+                graph[prerequisites[i][1]].add(prerequisites[i][0]);
+            }
+
+            for(int i=0; i<numCourses; i++){
+                if(!dfs(graph,visited,i, dp))
                     return false;
             }
+            return true;
         }
-        waitList[course] = false;
-        memo[course] = true;
-        return true;
-    }
+
+        private boolean dfs(ArrayList[] graph, boolean[] visited, int course, boolean[] dp){
+            if(visited[course])
+                return dp[course];
+            else
+                visited[course] = true;
+
+            for(int i=0; i<graph[course].size();i++){
+                if(!dfs(graph,visited,(int)graph[course].get(i), dp)) {
+                    dp[course] = false;
+                    return false;
+                }
+            }
+            
+            dp[course] = true;
+            return true;
+        }
 }
