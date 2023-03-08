2d4791a325648e5886aa1209fea6f0ef1fe908c7
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-08 16:02:34 -0500

    Time: 39 ms (48.51%), Space: 48.4 MB (51.09%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
         int[] result = new int[2];
-        int minX = Integer.MAX_VALUE, maxY = Integer.MIN_VALUE, minRange = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE, minRange = Integer.MAX_VALUE;
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         for (int i = 0; i < nums.size(); i++) {
-            if (nums.get(i).get(0) > maxY)
-                maxY = nums.get(i).get(0);
+            if (nums.get(i).get(0) > max)
+                max = nums.get(i).get(0);
             pq.offer(new int[] {nums.get(i).get(0), i, 0});
         }
         while (!pq.isEmpty()) {
             int[] min = pq.poll();
             int i = min[1], j = min[2];
-            if (maxY - min[0] < minRange) {
-                minRange = maxY - min[0];
+            if (max - min[0] < minRange) {
+                minRange = max - min[0];
                 result[0] = min[0];
-                result[1] = maxY;
+                result[1] = max;
             }
             if (i < nums.size() && j + 1 < nums.get(i).size()) {
                 pq.offer(new int[] {nums.get(i).get(j + 1), i, j + 1});
-                if (nums.get(i).get(j + 1) > maxY)
-                    maxY = nums.get(i).get(j + 1);
+                if (nums.get(i).get(j + 1) > max)
+                    max = nums.get(i).get(j + 1);
             } else 
                 break;
         }
