04750674f2a78d73736b487d10feba6ba26a233b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-11 16:53:03 -0500

    Time: 0 ms (100.00%), Space: 63.8 MB (66.83%) - LeetHub

--- 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.java
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.java
@@ -1,16 +1,13 @@
 class Solution {
     public int smallestCommonElement(int[][] mat) {
          int n = mat.length, m = mat[0].length;
-    for (int j = 0; j < m; ++j) {
-        boolean found = true;
-        for (int i = 1; i < n && found; ++i) {
-            found = Arrays.binarySearch(mat[i], mat[0][j]) >= 0;
+        for (int i = 0; i < m; i++) {
+            boolean found = true;
+            for (int j = 1; j < n && found; j++) 
+                found = Arrays.binarySearch(mat[j], mat[0][i]) >= 0;
+            if (found)
+                return mat[0][i];
         }
-        if (found) {
-            return mat[0][j];
-        }
-    }
-    return -1;
-
+        return -1;
     }
 }
