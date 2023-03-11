2c3c4c6874c9000c595b708d81cfd1f62abc4405
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-11 16:39:58 -0500

    Time: 1 ms (99.99%), Space: 43.3 MB (18.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int reachable = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i > reachable) return false;
-            reachable = Math.max(reachable, i + nums[i]);
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
         }
-        return true;
+        return lastPos == 0;
     }
 }
