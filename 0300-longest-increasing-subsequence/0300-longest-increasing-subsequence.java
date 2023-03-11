95ea820b8864c80438b95dfe54875f1a0559deae
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-11 16:33:35 -0500

    Time: 2 ms (99.74%), Space: 42.5 MB (28.95%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,31 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        int max = 1;
-        for (int i = 0; i < nums.length; i++) {
-            dp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                // count at position i, the number of dp that is increasing
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    max = Math.max(max, dp[i]);
-                } 
+       if (nums == null || nums.length == 0)
+           return 0;
+        int n = nums.length, len = 0;
+        int[] increasing = new int[n];
+        increasing[len++] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > increasing[len - 1]) {
+                increasing[len++] = nums[i];
+            } else {
+                int position = findPosition(increasing, 0, len - 1, nums[i]);
+                increasing[position] = nums[i];
             }
-        } 
-        return max;
+        }
+        return len;
+    }
+    
+    public int findPosition(int[] nums, int low, int high, int target) {
+        while (low <= high) {
+            int pivot = (low + high) >>> 1;
+            if (nums[pivot] == target)
+                return pivot;
+            else if (nums[pivot] > target)
+                high = pivot - 1;
+            else 
+                low = pivot + 1;
+        }
+        return low;
     }
 }
