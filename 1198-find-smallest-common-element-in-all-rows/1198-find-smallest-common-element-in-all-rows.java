8549eebf95be84f49f6c7689eb70f25bc3aaf35a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-11 16:50:42 -0500

    Time: 2 ms (63.41%), Space: 63.9 MB (59.02%) - LeetHub

--- 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.java
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.java
@@ -1,20 +1,22 @@
 class Solution {
     public int smallestCommonElement(int[][] mat) {
-        int row = mat.length, col = mat[0].length;
-        int[] indexes = new int[row];
-        int i = 0;
-        while (i < row - 1 && indexes[i] < col && indexes[i + 1] < col) {
-            int curr = mat[i][indexes[i]], next = mat[i + 1][indexes[i + 1]];
-            if (curr == next)
-                i++;
-            else if (curr > next) 
-                indexes[i + 1]++;
-            else {
-                for (int j = i; j >= 0; j--)
-                    indexes[j]++;
-                i = 0;
+        int n = mat.length, m = mat[0].length;
+    int pos[] = new int[n], cur_max = 0, cnt = 0;
+    while (true) {
+        for (int i = 0; i < n; ++i) {
+            while (pos[i] < m && mat[i][pos[i]] < cur_max) {
+                ++pos[i];
+            }
+            if (pos[i] >= m) {
+                return -1;
+            }
+            if (mat[i][pos[i]] != cur_max) {
+                cnt = 1;
+                cur_max = mat[i][pos[i]];
+            } else if (++cnt == n) {
+                return cur_max;
             }
         }
-        return i == row - 1 && indexes[i] < col ? mat[i][indexes[i]] : -1;
+    }
     }
 }
