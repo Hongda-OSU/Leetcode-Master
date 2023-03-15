b29a1e092983b7ed38ae2089153089e5daeb52b6
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-15 18:24:06 -0400

    Time: 0 ms (100.00%), Space: 42.9 MB (28.17%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int pivot = indexOfLastPeak(nums) - 1;
+        if (pivot != -1) {
+            int nextPrefix = lastIndexOfGreater(nums, nums[pivot]);
+            swap(nums, pivot, nextPrefix);
+        }
+        reverseSuffix(nums, pivot + 1);
+    }
+    
+    public int indexOfLastPeak(int[] nums) {
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i])
+                return i;
+        }
+        return 0;
+    }
+    
+    public int lastIndexOfGreater(int[] nums, int threshold) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (threshold < nums[i])
+                return i;
+        }
+        return -1;
+    }
+    
+    public void reverseSuffix(int[] nums, int start) {
+        int end = nums.length - 1;
+        while (start < end)
+            swap(nums, start++, end--);
+    }
+    
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
