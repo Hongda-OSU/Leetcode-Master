e192da9fc478dc81cd981f392e9e2c37dcd19b39
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-15 19:14:31 -0400

    Time: 8 ms (72.86%), Space: 42.1 MB (14.94%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,40 +1,31 @@
-class Solution {
+public class Solution {
     public void solveSudoku(char[][] board) {
-        if (board == null || board.length == 0)
-            return;
-        solve(board);
+        dfs(board,0);
     }
-    
-    public boolean solve(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == '.') {
-                    for (char ch = '1'; ch <= '9'; ch++) {
-                        if (isValid(board, i, j, ch)) {
-                            board[i][j] = ch;
-                            if (solve(board))
-                                return true;
-                            else
-                                board[i][j] = '.';
-                        }
-                    }
-                    return false;
-                }
+    private boolean dfs(char[][] board, int d) {
+        if (d==81) return true; //found solution
+        int i=d/9, j=d%9;
+        if (board[i][j]!='.') return dfs(board,d+1);//prefill number skip
+        
+        boolean[] flag=new boolean[10];
+        validate(board,i,j,flag);
+        for (int k=1; k<=9; k++) {
+            if (flag[k]) {
+                board[i][j]=(char)('0'+k);
+                if (dfs(board,d+1)) return true;
             }
         }
-        return true;
+        board[i][j]='.'; //if can not solve, in the wrong path, change back to '.' and out
+        return false;
     }
-    
-    public boolean isValid(char[][] board, int row, int col, char ch) {
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] != '.' && board[i][col] == ch)
-                return false;
-            if (board[row][i] != '.' && board[row][i] == ch)
-                return false;
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.' && 
-               board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch)
-                return false;
+    private void validate(char[][] board, int i, int j, boolean[] flag) {
+        Arrays.fill(flag,true);
+        for (int k=0; k<9; k++) {
+            if (board[i][k]!='.') flag[board[i][k]-'0']=false;
+            if (board[k][j]!='.') flag[board[k][j]-'0']=false;
+            int r=i/3*3+k/3;
+            int c=j/3*3+k%3;
+            if (board[r][c]!='.') flag[board[r][c]-'0']=false;
         }
-        return true;
     }
-} 
+}
