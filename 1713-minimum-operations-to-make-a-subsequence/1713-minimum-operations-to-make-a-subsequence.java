531f6f30a942491e95c82334175b2aec40939dd9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-16 14:30:07 -0400

    Time: 178 ms (6.94%), Space: 61.4 MB (38.89%) - LeetHub

--- 1713-minimum-operations-to-make-a-subsequence/1713-minimum-operations-to-make-a-subsequence.java
+++ 1713-minimum-operations-to-make-a-subsequence/1713-minimum-operations-to-make-a-subsequence.java
@@ -1,26 +1,22 @@
 class Solution {
-    public int minOperations(int[] target, int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < target.length; i++)
-            map.put(target[i], i);
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int num : arr) {
-            if (!map.containsKey(num))
-                continue;
-            if (list.size() == 0 || map.get(num) > list.get(list.size() - 1)) {
-                list.add(map.get(num));
-                continue;
-            }
-            int left = 0, right = list.size() - 1;
-            while (left < right) {
-                int pivot = (left + right) >>> 1;
-                if (list.get(pivot) < map.get(num))
-                    left = pivot + 1;
-                else
-                    right = pivot;
-            }
-            list.set(left, map.get(num));
+    public int minOperations(int[] target, int[] A) {
+        Map<Integer, Integer> h = new HashMap<>();
+        for (int i = 0; i < target.length; i++) {
+            h.put(target[i], i);
         }
-        return target.length - list.size();
+        TreeSet<Integer> stack = new TreeSet<>();
+        for (int a : A) {
+            if (!h.containsKey(a)) {
+                continue;
+            }
+            if (stack.isEmpty() || stack.last() < h.get(a)) {
+                stack.add(h.get(a));
+                continue;
+            }
+            int key = stack.ceiling(h.get(a));
+            stack.remove(key);
+            stack.add(h.get(a));
+        }
+        return target.length - stack.size();
     }
 }
