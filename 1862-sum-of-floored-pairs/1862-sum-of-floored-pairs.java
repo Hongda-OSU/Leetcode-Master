035d6a98282d6cd2e77d06be15b254b128dbc021
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-17 16:26:14 -0400

    Time: 66 ms (85.29%), Space: 55.9 MB (50.00%) - LeetHub

---
+++ 1862-sum-of-floored-pairs/1862-sum-of-floored-pairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int MAX = (int)1e5;
+    public int MODULUS = (int)1e9 + 7;
+    
+    public int sumOfFlooredPairs(int[] nums) {
+        int[] counts = new int[MAX + 1];
+        for (int num : nums)
+            ++counts[num];
+        for (int i = 1; i <= MAX; i++) 
+            counts[i] += counts[i - 1];
+        long total = 0;
+        for (int i = 1; i <= MAX; i++) {
+            if (counts[i] > counts[i - 1]) {
+                long sum = 0;
+                for (int j = 1; i * j <= MAX; j++) {
+                    int lower = i * j - 1;
+                    int upper = i * (j + 1) -1;
+                    sum += (counts[Math.min(MAX, upper)] - counts[lower]) * (long)j;
+                }
+                total = (total + (sum % MODULUS) * (counts[i] - counts[i - 1])) % MODULUS;
+            }
+        }
+        return (int)total;
+    }
+}
