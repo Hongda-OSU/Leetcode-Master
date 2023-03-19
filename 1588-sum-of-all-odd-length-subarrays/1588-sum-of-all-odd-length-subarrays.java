e2053c046a04e0fbf51ed1961186a20762bc874b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-19 19:24:20 -0400

    Time: 0 ms (100.00%), Space: 40.4 MB (35.20%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,13 +1,14 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
         int n = arr.length, sum = 0;
-        for (int left = 0; left < n; left++) {
-            int tmpSum = 0;
-            for (int right = left; right < n; right++) {
-                tmpSum += arr[right];
-                // odd length window
-                sum += (right - left + 1) % 2 == 1 ? tmpSum : 0;
-            }
+        for (int i = 0; i < n; i++) {
+            int start = n - i;
+            int end = i + 1;
+            int target = start * end;
+            int odd = target / 2;
+            if (target % 2 == 1)
+                odd++;
+            sum += odd * arr[i];
         }
         return sum;
     }
