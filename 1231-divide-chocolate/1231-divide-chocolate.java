8034539364d2fa2e086e5bc8512ca6ca4d26469e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-20 19:47:21 -0400

    Time: 5 ms (82.17%), Space: 43.1 MB (36.94%) - LeetHub

--- 1231-divide-chocolate/1231-divide-chocolate.java
+++ 1231-divide-chocolate/1231-divide-chocolate.java
@@ -1,44 +1,27 @@
 class Solution {
-    public int maximizeSweetness(int[] sweetness, int K) {
-		// Minimum and maximum level of sweetness to use in binary search.
-        int l = 0;
-        int hi = 0;
-        
-        for (int s : sweetness) {
-            hi += s;
+    public int maximizeSweetness(int[] sweetness, int k) {
+        int low = 0, high = 0;
+        for (int sweet : sweetness) 
+            high += sweet;
+        while (low <= high) {
+            int level = (low + high) >>> 1;
+            if (canCutIntoK(sweetness, level, k + 1))
+                low = level + 1;
+            else 
+                high = level - 1;
         }
-        
-        while (l <= hi) {
-		    // hypothesize that we can cut into at least `k + 1`  parts, each has `level` or more sum of sweetness.
-            int level = (l + hi) / 2;
-            
-			// Test if we can cut into `k + 1`  or more parts
-            if (canCutIntoK(sweetness, level, K + 1)) {
-				// hypothesis was correct, try again with greater level of sweetness
-                l = level + 1;
-            } else {
-				// hypothesis was false, try again with smaller level of sweetness
-                hi = level -1;
-            }
-        }
-        
-		// `l` increases by 1 after every correct sweet level, so the final correct answer must be `l - 1`
-        return l - 1;
+        return low - 1;
     }
     
-    boolean canCutIntoK(int[] sweetness, int level, int K) {
-        int cnt = 0;
-        int curr = 0;
-        
+    public boolean canCutIntoK(int[] sweetness, int level, int k) {
+        int count = 0, curr = 0;
         for (int i = 0; i < sweetness.length; i++) {
             curr += sweetness[i];
-            
             if (curr >= level) {
                 curr = 0;
-                cnt++;
+                count++;
             }
         }
-        
-        return cnt >= K;
+        return count >= k;
     }
 }
