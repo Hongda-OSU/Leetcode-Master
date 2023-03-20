953f25060e41f62d5b7d8187837e5977697f88a3
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-20 19:33:58 -0400

    Time: 0 ms (100.00%), Space: 40.5 MB (79.83%) - LeetHub

---
+++ 1427-perform-string-shifts/1427-perform-string-shifts.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String stringShift(String s, int[][] shift) {
+        int direction = 0;
+        for (int[] pair : shift) {
+            if (pair[0] == 0)
+                direction -= pair[1];
+            else 
+                direction += pair[1];
+        }
+        return shift(s, direction);
+    }
+    
+    public String shift(String str, int direction) {
+        if (direction == 0)
+            return str;
+        direction = direction % str.length();
+        if (direction < 0)
+            direction = str.length() - Math.abs(direction);
+        char[] result = new char[str.length()];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            int idx = (i + direction) % str.length();
+            result[idx] = str.charAt(i);
+        }
+         for (char c : result) sb.append(c);
+        return sb.toString();
+    }
+}
