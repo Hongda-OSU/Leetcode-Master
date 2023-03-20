0074619f9f42ddbd4dd476d9aa7c712e564f33c9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-20 19:39:02 -0400

    Time: 1 ms (100.00%), Space: 41.1 MB (45.76%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -3,16 +3,16 @@ class Solution {
        int[] count = new int[256];
         for (char ch : s.toCharArray())
             count[ch]++;
-        boolean bool = false;
+        boolean extra = false;
         int len = 0;
         for (int c : count) {
             if (c % 2 == 0)
                 len += c;
             else {
                 len += c - 1;
-                bool = true;
+                extra = true;
             }
         }
-        return bool ? len + 1 : len;
+        return extra ? len + 1 : len;
     }
 }
