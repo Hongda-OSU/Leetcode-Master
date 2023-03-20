87a7620d2aea54fb7b8c12ed126a79d56c82aff2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-19 20:36:29 -0400

    Time: 0 ms (100.00%), Space: 81 MB (35.79%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,19 +1,43 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int startCol = 0, endCol = mat[0].length - 1;
-        while (startCol <= endCol) {
-            int maxRow = 0, midCol = (startCol + endCol) >>> 1;
-            for (int row = 0; row < mat.length; row++)
-                maxRow = mat[row][midCol] >= mat[maxRow][midCol] ? row : maxRow;
-            boolean leftBigger = midCol - 1 >= startCol && mat[maxRow][midCol - 1] > mat[maxRow][midCol];
-            boolean rightBigger = midCol + 1 <= endCol && mat[maxRow][midCol + 1] > mat[maxRow][midCol];
-            if (!leftBigger && !rightBigger)
-                return new int[]{maxRow, midCol};
-            else if (rightBigger)
-                startCol = midCol + 1;
+        int startRow = 0;
+        int endRow = mat.length -1;
+        while(startRow <= endRow) {
+            int midRow = startRow  + (endRow - startRow)/2;
+            int maxElementInmidRow = findMaxElementPosition(mat[midRow] , mat[midRow].length);
+            if(midRow == 0) {
+                if(mat[midRow][maxElementInmidRow] > mat[midRow +1][maxElementInmidRow]) {
+                    return new int[]{midRow,maxElementInmidRow};
+                }
+            }
+            if(midRow == mat.length-1) {
+                if(mat[midRow][maxElementInmidRow] > mat[midRow -1][maxElementInmidRow]) {
+                    return new int[]{midRow,maxElementInmidRow};
+                }
+            }
+            else if(midRow >0  && midRow < mat.length ) {
+                if(mat[midRow][maxElementInmidRow] > mat[midRow +1][maxElementInmidRow] && mat[midRow][maxElementInmidRow] > mat[midRow -1][maxElementInmidRow] )
+                    return new int[]{midRow,maxElementInmidRow};
+                
+            }
+            if(mat[midRow][maxElementInmidRow] < mat[midRow+1][maxElementInmidRow]) {
+                startRow = midRow+1;
+            }
             else
-                endCol = midCol - 1;
+                endRow = midRow -1;
         }
-        return null;
-    } 
+        return new int[]{-1,-1};
+    }
+    
+    public int findMaxElementPosition(int[] arr, int n) {
+        int max = Integer.MIN_VALUE;
+        int indx =0;
+        for(int i = 0;i<n;i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+                indx =i;
+            }
+        }
+        return indx;
+    }
 }
