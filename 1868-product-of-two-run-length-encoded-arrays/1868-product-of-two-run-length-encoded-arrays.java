71429ac7974e94fb794186f58273a8ea95e68e21
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-21 15:46:20 -0400

    Time: 36 ms (35.04%), Space: 88 MB (82.05%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.java
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.java
@@ -1,35 +1,29 @@
 class Solution {
     public List<List<Integer>> findRLEArray(int[][] encoded1, int[][] encoded2) {
-    if (encoded1 == null || encoded2 == null && encoded1.length == 0 || encoded2.length == 0) return new ArrayList<>();
-    List<List<Integer>> result = new ArrayList<>();
-    int i = 0, j = 0;
-    int[] first = encoded1[i], second = encoded2[j];
-    while (i < encoded1.length && j < encoded2.length) {
-        if (first[1] < second[1]) {
-            addToResult(result, first[0] * second[0], first[1]);
-            second[1] -= first[1];      // Reduce frequency of more frequent one.
-            first = encoded1[++i];      // Less frequent one is fully used up. Can't be out of bounds because the two fully encoded arrays have an equal length.
-    
-        } else if (first[1] > second[1]) {
-            addToResult(result, first[0] * second[0], second[1]);
-            first[1] -= second[1];      
-            second = encoded2[++j];    
-            
-        } else {
-            addToResult(result, first[0] * second[0], first[1]);
-            first = ++i < encoded1.length ? encoded1[i] : null; 
-            second = ++j < encoded2.length ? encoded2[j] : null;
+        int p1 = 0, p2 = 0, idx1 = 0, idx2 = 0;
+        List<List<Integer>> result = new ArrayList<>();
+        while (idx1 < encoded1.length && idx2 < encoded2.length) {
+            int freq1 = encoded1[idx1][1] - p1;
+            int freq2 = encoded2[idx2][1] - p2;
+            int freq = Math.min(freq1, freq2);
+            p1 += freq; p2 += freq;
+            int prod = encoded1[idx1][0] * encoded2[idx2][0];
+            int size = result.size();
+            // if current product is same as the most recent one in the result, concat it
+            if (size > 0 && result.get(size - 1).get(0) == prod) {
+                freq += result.get(size - 1).get(1);
+                result.remove(size - 1);
+            }
+            result.add(Arrays.asList(prod, freq));
+            if (p1 == encoded1[idx1][1]) {
+                idx1++;
+                p1 = 0;
+            }
+            if (p2 == encoded2[idx2][1]) {
+                idx2++;
+                p2 = 0;
+            }
         }
+        return result;
     }
-    return result;
-}
-
-private void addToResult(List<List<Integer>> result, int value, int frequency) {
-    if (result.size() == 0 || result.get(result.size() - 1).get(0) != value) {
-        result.add(Arrays.asList(value, frequency));
-    } else {
-        List<Integer> last = result.remove(result.size() - 1);
-        result.add(Arrays.asList(last.get(0), last.get(1) + frequency));
-    }
-}
 }
