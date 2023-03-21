935bfe2e0cb8509b0c6f0845a6ca1a8f07719d8b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-21 15:26:50 -0400

    Time: 22 ms (75.78%), Space: 42.2 MB (76.71%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,25 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() == 0) return "";
-        int left = 0, right = 0, len = s.length();
-        boolean[][] dp = new boolean[len][len];
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j >= 0; j--) {
-                boolean startEqEnd = s.charAt(i) == s.charAt(j);
-                if (i == j) 
-                    //If the same char: 'a' is palindrome
-                    dp[i][j] = true;
-                else if (i - j == 1)
-                    //If length 2: 'ab' is palindrome when 'a' == 'b'
-                    dp[i][j] = startEqEnd;
-                else if (startEqEnd && dp[i - 1][j + 1]) 
-                    //Otherwise: string is palindrome if s(i) == s(j) and substring s(j + 1, i - 1) is palindrome
-                    dp[i][j] = true;
-                
-                if (dp[i][j] && i - j > right - left) {
-                    right = i;
-                    left = j;
-                }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int left = i, right = i;
+            while (left >= 0 && s.charAt(left) == ch) 
+                left--;
+            while (right < s.length() && s.charAt(right) == ch)
+                right++;
+            while (left >= 0 && right < s.length()) {
+                if (s.charAt(left) != s.charAt(right))
+                    break;
+                left--;
+                right++;
+            }
+            left += 1;
+            if (end - start < right - left) {
+                start = left;
+                end = right;
             }
         }
-        return s.substring(left, right + 1);
+        return s.substring(start, end);
     }
 }
