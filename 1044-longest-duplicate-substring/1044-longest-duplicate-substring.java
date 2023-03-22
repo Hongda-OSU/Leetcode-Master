e20660ee603c3ed3cc92f2706dd82f8d5e11e442
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-22 16:28:57 -0400

    Time: 187 ms (83.41%), Space: 50.4 MB (78.17%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.java
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.java
@@ -1,62 +1,48 @@
 class Solution {
-    public String longestDupSubstring(String S) {
-        String ans = "";
-        
-        int left = 1;
-        int right = S.length()-1;
-        
-        while(left <= right){
-            int m = left + (right - left)/2;
-            
-            String dup = getDup(m,S);
-            
-            if(dup != null){
-                ans = dup;
-                left = m+1;
-            }else{
-                right = m-1;
+    public String longestDupSubstring(String s) {
+        String result = "";
+        int left = 1, right = s.length() - 1;
+        while (left <= right) {
+            int pivot = left + (right - left) / 2;
+            String dup = getDup(pivot, s);
+            if (dup != null) {
+                result = dup;
+                left = pivot + 1;
+            } else {
+                right = pivot - 1;
             }
         }
-        
-        return ans;
+        return result;
     }
     
-    private String getDup(int size, String s){
-        long H = hash(s.substring(0,size));
-		
-        HashSet<Long> set = new HashSet();
-        set.add(H);
-
+    public String getDup(int size, String str) {
+        long h = hash(str.substring(0, size));
+        HashSet<Long> set = new HashSet<>();
+        set.add(h);
         long pow = 1;
-        for(int i=1;i<size;i++)
-            pow = (pow * 31);
-        
-		int n = s.length();
-        for(int i=size;i < n;i++){
-            H = nextHash(pow, H, s.charAt(i-size), s.charAt(i));
-            if(!set.add(H)){
-                return s.substring(i-size+1, i+1);
-            }
+        for (int i = 1; i < size; i++) 
+            pow = pow * 31;
+        int n = str.length();
+        for (int i = size; i < n; i++) {
+            h = nextHash(pow, h, str.charAt(i - size), str.charAt(i));
+            if (!set.add(h))
+                return str.substring(i - size + 1, i + 1);
         }
-        
         return null;
     }
     
-    private long hash(String s){
-        long h = 0;
-        long a = 1;
-        
-		int n = s.length();
-        for(int k=n;k>=1;k--){
-            char ch = s.charAt(k-1);
+    public long hash(String str) {
+        long h = 0, a = 1;
+        int n = str.length();
+        for (int k = n; k >= 1; k--) {
+            char ch = str.charAt(k - 1);
             h += (ch - 'a' + 1) * a;
-            a = (a*31);
+            a = a * 31;
         }
-        
         return h;
     }
     
-    private long nextHash(long pow,long hash,char left,char right){
+    public long nextHash(long pow, long hash, char left, char right) {
         return (hash - (left - 'a' + 1) * pow) * 31 + (right - 'a' + 1);
     }
 }
