23ea65f4d70fb5ef4e31e8ef4c9939612bb4eaeb
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-22 15:44:31 -0400

    Time: 99 ms (23.35%), Space: 43.5 MB (23.25%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.java
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.java
@@ -1,27 +1,19 @@
-public class Solution {
+class Solution {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
-        if(s.length() < 1) return 0;
-        HashMap<Character,Integer> index = new HashMap<Character,Integer>();
-        int lo = 0;
-        int hi = 0;
-        int maxLength = 0;
-        while(hi < s.length()) {
-            if(index.size() <= 2) {
-                char c = s.charAt(hi);
-                index.put(c, hi);
-                hi++;
+        int n = s.length();
+        if (n < 3)
+            return n;
+        int left = 0, right = 0, max= 2;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while (right < n) {
+            map.put(s.charAt(right), right++);
+            if (map.size() == 3) {
+                int idx = Collections.min(map.values());
+                map.remove(s.charAt(idx));
+                left = idx + 1;
             }
-            if(index.size() > 2) {
-                int leftMost = s.length();
-                for(int i : index.values()) {
-                    leftMost = Math.min(leftMost,i);
-                }
-                char c = s.charAt(leftMost);
-                index.remove(c);
-                lo = leftMost+1;
-            }
-            maxLength = Math.max(maxLength, hi-lo);
+            max = Math.max(max, right - left);
         }
-        return maxLength;
+        return max;
     }
 }
