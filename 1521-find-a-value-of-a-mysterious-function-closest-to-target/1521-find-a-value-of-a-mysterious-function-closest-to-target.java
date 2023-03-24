951d22e22ae2c172febc02345e1b22d32bff0478
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-24 16:22:11 -0400

    Time: 168 ms (44.12%), Space: 62.2 MB (29.41%) - LeetHub

--- 1521-find-a-value-of-a-mysterious-function-closest-to-target/1521-find-a-value-of-a-mysterious-function-closest-to-target.java
+++ 1521-find-a-value-of-a-mysterious-function-closest-to-target/1521-find-a-value-of-a-mysterious-function-closest-to-target.java
@@ -1,30 +1,16 @@
 class Solution {
-   public int closestToTarget(int[] arr, int target) {
-    int n = arr.length, min = Integer.MAX_VALUE;
-
-    // ands[i]: unique AND values of arr[i...]
-    // the size of ands[i] is at most ceil(log(arr[i])), i.e. arr[i].bitCount()
-    Set<Integer>[] ands = new Set[n];
-    for (int i = 0; i < n; i++) {
-        ands[i] = new HashSet<>();
-    }
-
-    ands[n - 1].add(arr[n - 1]);
-
-    // computes ands[i] from ands[i + 1]
-    for (int i = n - 2; i >= 0; i--) {   
-        ands[i].add(arr[i]);
-        for (int v: ands[i + 1]) {
-            ands[i].add(arr[i] & v);
+    public int closestToTarget(int[] arr, int target) {
+        int m = arr.length, result = Integer.MAX_VALUE;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            Set<Integer> tmp = new HashSet<>();
+            tmp.add(arr[i]);
+            for (int num : set)
+                tmp.add(num & arr[i]);
+            for (int num : tmp) 
+                result = Math.min(result, Math.abs(num - target));
+            set = tmp;
         }
+        return result;
     }
-
-    for (var a : ands) {
-        for (int v : a) {
-            min = Math.min(min, Math.abs(v - target));
-        }
-    }
-
-    return min;
-}
 }
