a1bb043e5089a1918f5f95a5b41a22703256597f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-24 15:24:39 -0400

    Time: 0 ms (100.00%), Space: 41.8 MB (74.97%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null)
+            return head;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = new Node(curr.val, next, null);
+            curr = next;
+        }
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) 
+                curr.next.random = curr.random.next;
+            curr = curr.next.next;
+        }
+        curr = head;
+        Node copyHead = head.next;
+        while (curr != null) {
+            Node next = curr.next.next;
+            Node copy = curr.next;
+            curr.next = next;
+            if (next != null)
+                copy.next = next.next;
+            curr = next;
+        }
+        return copyHead;
+    }
+}
