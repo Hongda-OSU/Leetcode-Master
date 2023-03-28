aeb866436189306b35a5bdb4056fe1fa84b98f50
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-28 14:56:48 -0400

    Time: 0 ms (100.00%), Space: 42.4 MB (76.72%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -4,15 +4,15 @@ class Solution {
         int len = nums.length;
         for(int i = 0; i < len; i++) {
             max = Math.max(product *= nums[i], max);
-            if (nums[i] == 0) product = 1;
+            if (nums[i] == 0) 
+                product = 1;
         }
-
         product = 1;
         for(int i = len - 1; i >= 0; i--) {
             max = Math.max(product *= nums[i], max);
-            if (nums[i] == 0) product = 1;
+            if (nums[i] == 0) 
+                product = 1;
         }
-
         return max;
     }
 }
