aeee2fab0e2ceb07ccb3308863b1d15acc99a782
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-29 16:42:45 -0400

    Time: 2 ms (89.15%), Space: 43 MB (43.97%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,47 +1,40 @@
 class Solution {
-    List<List<String>> result = new ArrayList<>();
+    public List<List<String>> result;
+    
     public List<List<String>> solveNQueens(int n) {
+        result = new ArrayList<>();
         boolean[] visited = new boolean[n];
-        //2*n-1个斜对角线
-        boolean[] dia1 = new boolean[2*n-1];
-        boolean[] dia2 = new boolean[2*n-1];
-        
-        fun(n, new ArrayList<String>(),visited,dia1,dia2,0);
-        
+        boolean[] diagonal1 = new boolean[2 * n - 1];
+        boolean[] diagonal2 = new boolean[2 * n - 1];
+        dfs(n, new ArrayList<String>(), visited, diagonal1, diagonal2, 0);
         return result;
     }
     
-    private void fun(int n,List<String> list,boolean[] visited,boolean[] dia1,boolean[] dia2,int rowIndex){
-        if(rowIndex == n){
-            result.add(new ArrayList<String>(list));
+    public void dfs(int n, List<String> list, boolean[] visited, boolean[] diagonal1, boolean[] diagonal2, int row) {
+        if (row == n) {
+            result.add(new ArrayList<>(list));
             return;
         }
-        
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             //这一行、正对角线、反对角线都不能再放了，如果发现是true，停止本次循环
-            if(visited[i] || dia1[rowIndex+i] || dia2[rowIndex-i+n-1])
+            if (visited[i] || diagonal1[row + i] || diagonal2[row - i + n - 1])
                 continue;
-            
             //init一个长度为n的一维数组，里面初始化为'.'
-            char[] charArray = new char[n];
-            Arrays.fill(charArray,'.');
-            
-            charArray[i] = 'Q';
-            String stringArray = new String(charArray);
-            list.add(stringArray);
+            char[] arr = new char[n];
+            Arrays.fill(arr, '.');
+            arr[i] = 'Q';
+            String str = new String(arr);
+            list.add(str);
             visited[i] = true;
-            dia1[rowIndex+i] = true;
-            dia2[rowIndex-i+n-1] = true;
-
-            fun(n,list,visited,dia1,dia2,rowIndex+1);
-
+            diagonal1[row + i] = true;
+            diagonal2[row - i + n - 1] = true;
+            dfs(n, list, visited, diagonal1, diagonal2, row + 1);
             //reset 不影响回溯的下个目标
-            list.remove(list.size()-1);
-            charArray[i] = '.';
+            list.remove(list.size() - 1);
+            arr[i] = '.';
             visited[i] = false;
-            dia1[rowIndex+i] = false;
-            dia2[rowIndex-i+n-1] = false;
+            diagonal1[row + i] = false;
+            diagonal2[row - i + n - 1] = false;
         }
     }
-    
 }
