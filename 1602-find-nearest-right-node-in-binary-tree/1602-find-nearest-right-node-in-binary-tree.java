c4aa5e4f127fe42530e6d73d2ce07a41add78611
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-29 14:58:55 -0400

    Time: 13 ms (17.46%), Space: 54.2 MB (50.00%) - LeetHub

---
+++ 1602-find-nearest-right-node-in-binary-tree/1602-find-nearest-right-node-in-binary-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode findNearestRightNode(TreeNode root, TreeNode u) {
+        if (root == null)
+            return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(null);
+        TreeNode curr = null;
+        while (!queue.isEmpty()) {
+            curr = queue.poll();
+            if (curr != null) {
+                if (curr == u)
+                    return queue.poll();
+                if (curr.left != null)
+                    queue.offer(curr.left);
+                if (curr.right != null)
+                    queue.offer(curr.right);
+            } else {
+                if (!queue.isEmpty())
+                    queue.offer(null);
+            }
+        }
+        return null;
+    }
+}
