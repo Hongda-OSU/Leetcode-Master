e4db1850fe14a90030a7df856238a1540f98301e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-30 18:24:47 -0400

    Time: 0 ms (100.00%), Space: 42.8 MB (28.82%) - LeetHub

--- 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.java
+++ 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.java
@@ -23,17 +23,25 @@ class Node {
 
 class Solution {
     public int diameter(Node root) {
-        if (root == null || root.children.size() == 0)
+        if (root == null || root.children.size() == 0) {
             return 0;
+        }
+
         int[] maxDiameter = new int[1];
         diameterHelper(root, maxDiameter);
         return maxDiameter[0];
     }
-    
-    public int diameterHelper(Node root, int[] maxDiameter) {
-        if (root == null)
+
+    private int diameterHelper(Node root, int[] maxDiameter) {
+        if (root.children.size() == 0) {
             return 0;
-        int maxHeight1 = 0, maxHeight2 = 0;
+        }
+
+        // Setting below maximums to -1 helps in the case if there is only one child
+        // node of this root node.
+        int maxHeight1 = -1;
+        int maxHeight2 = -1;
+
         for (Node child : root.children) {
             int childHeight = diameterHelper(child, maxDiameter);
             if (childHeight > maxHeight1) {
@@ -43,7 +51,8 @@ class Solution {
                 maxHeight2 = childHeight;
             }
         }
-        maxDiameter[0] = Math.max(maxDiameter[0], maxHeight1 + maxHeight2);
+
+        maxDiameter[0] = Math.max(maxDiameter[0], maxHeight1 + maxHeight2 + 2);
         return maxHeight1 + 1;
     }
 }
