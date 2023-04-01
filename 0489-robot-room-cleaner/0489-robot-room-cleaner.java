6e0c3500863e1e2dab77a82ce7acef1771426c8c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-03-31 21:44:37 -0400

    Time: 14 ms (27.68%), Space: 43.2 MB (33.60%) - LeetHub

--- 0489-robot-room-cleaner/0489-robot-room-cleaner.java
+++ 0489-robot-room-cleaner/0489-robot-room-cleaner.java
@@ -17,31 +17,22 @@
  */
 
 class Solution {
-    
-    private static final int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    public int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     
     public void cleanRoom(Robot robot) {
         clean(robot, 0, 0, 0, new HashSet<>());
     }
     
-    private void clean(Robot robot, int x, int y, int curDirection, Set<String> visited) {
-        // Cleans current cell.
+    public void clean(Robot robot, int x, int y, int curDirection, Set<String> visited) {
         robot.clean();
         visited.add(x + " " + y);
-        
-        for (int nDirection = curDirection; 
-             nDirection < curDirection + 4; 
-             nDirection++) {
-            int nx = directions[nDirection % 4][0] + x;
-            int ny = directions[nDirection % 4][1] + y;
-            if (!visited.contains(nx + " " + ny) && robot.move()) {
-                clean(robot, nx, ny, nDirection % 4, visited);
-            }
-            // Changed orientation.
+        for (int dir = curDirection; dir < curDirection + 4; dir++) {
+            int cx = directions[dir % 4][0] + x;
+            int cy = directions[dir % 4][1] + y;
+            if (!visited.contains(cx + " " + cy) && robot.move())
+                clean(robot, cx, cy, dir % 4, visited);
             robot.turnRight();
         }
-        
-        // Moves backward one step while maintaining the orientation.
         robot.turnRight();
         robot.turnRight();
         robot.move();
