3a8db85888021e6ebb7a5fd6b4dea7dda11d2383
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-01 17:02:56 -0400

    Time: 2 ms (100.00%), Space: 43.7 MB (36.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,23 +1,21 @@
 class Solution {
-    public double findMedianSortedArrays(int[] A, int[] B) {
-	    int m = A.length, n = B.length;
-	    int l = (m + n + 1) / 2;
-	    int r = (m + n + 2) / 2;
-	    return (getkth(A, 0, B, 0, l) + getkth(A, 0, B, 0, r)) / 2.0;
-	}
-
-public double getkth(int[] A, int aStart, int[] B, int bStart, int k) {
-	if (aStart > A.length - 1) return B[bStart + k - 1];            
-	if (bStart > B.length - 1) return A[aStart + k - 1];                
-	if (k == 1) return Math.min(A[aStart], B[bStart]);
-	
-	int aMid = Integer.MAX_VALUE, bMid = Integer.MAX_VALUE;
-	if (aStart + k/2 - 1 < A.length) aMid = A[aStart + k/2 - 1]; 
-	if (bStart + k/2 - 1 < B.length) bMid = B[bStart + k/2 - 1];        
-	
-	if (aMid < bMid) 
-	    return getkth(A, aStart + k/2, B, bStart,       k - k/2);// Check: aRight + bLeft 
-	else 
-	    return getkth(A, aStart,       B, bStart + k/2, k - k/2);// Check: bRight + aLeft
-}
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int p1 = 0, p2 = 0;
+        int m1 = 0, m2 = 0;
+        int l1 = nums1.length, l2 = nums2.length;
+        for (int i = 0; i <= (l1 + l2) >>> 1; i++) {
+            m1 = m2;
+            if (p1 == l1)
+                m2 = nums2[p2++];
+            else if (p2 == l2)
+                m2 = nums1[p1++];
+            else if (nums1[p1] < nums2[p2])
+                m2 = nums1[p1++];
+            else 
+                m2 = nums2[p2++];
+        }
+        if ((l1 + l2) % 2 == 0)
+            return (double) (m1 + m2) / 2;
+        return m2;
+    }
 }
