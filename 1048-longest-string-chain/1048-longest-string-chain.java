230d5de9bdbac8dd49bab492f994b90df00e3a8c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-02 16:10:02 -0400

    Time: 39 ms (72.73%), Space: 43.3 MB (13.59%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,17 +1,26 @@
+
 class Solution {
     public int longestStrChain(String[] words) {
-         Map<String, Integer> dp = new HashMap<>();
-        Arrays.sort(words, (a, b)->a.length() - b.length());
-        int res = 0;
+        Map<String, Integer> dp = new HashMap<>();
+
+        // Sorting the list in terms of the word length.
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        int longestWordSequenceLength = 1;
+
         for (String word : words) {
-            int best = 0;
-            for (int i = 0; i < word.length(); ++i) {
-                String prev = word.substring(0, i) + word.substring(i + 1);
-                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
+            int presentLength = 1;
+            // Find all possible predecessors for the current word by removing one letter at a time.
+            for (int i = 0; i < word.length(); i++) {
+                StringBuilder temp = new StringBuilder(word);
+                temp.deleteCharAt(i);
+                String predecessor = temp.toString();
+                int previousLength = dp.getOrDefault(predecessor, 0);
+                presentLength = Math.max(presentLength, previousLength + 1);
             }
-            dp.put(word, best);
-            res = Math.max(res, best);
+            dp.put(word, presentLength);
+            longestWordSequenceLength = Math.max(longestWordSequenceLength, presentLength);
         }
-        return res;
+        return longestWordSequenceLength;
     }
 }
