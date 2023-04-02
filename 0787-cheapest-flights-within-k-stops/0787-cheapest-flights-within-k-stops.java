dae361c4bb82d2b241c83af562318e3fa81d799c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-02 15:34:55 -0400

    Time: 7 ms (44.02%), Space: 44.5 MB (7.86%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,38 +1,21 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, List<int[]>> adj = new HashMap<>();
-        for (int[] i : flights)
-            adj.computeIfAbsent(i[0], value -> new ArrayList<>()).add(new int[] { i[1], i[2] });
-
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] { src, 0 });
-        int stops = 0;
-
-        while (stops <= k && !q.isEmpty()) {
-            int sz = q.size();
-            // Iterate on current level.
-            while (sz-- > 0) {
-                int[] temp = q.poll();
-                int node = temp[0];
-                int distance = temp[1];
-
-                if (!adj.containsKey(node))
-                    continue;
-                // Loop over neighbors of popped node.
-                for (int[] e : adj.get(node)) {
-                    int neighbour = e[0];
-                    int price = e[1];
-                    if (price + distance >= dist[neighbour])
-                        continue;
-                    dist[neighbour] = price + distance;
-                    q.offer(new int[] { neighbour, dist[neighbour] });
+        int[][] dp = new int[k+2][n];
+        for(int i=0; i<=k+1; i++){
+            Arrays.fill(dp[i],Integer.MAX_VALUE);    
+        }
+        //fly from src to scr cost 0
+        for(int i=0; i<=k+1; i++){
+            dp[i][src] = 0;    
+        }
+        
+        for(int i=1; i<=k+1; i++){
+            for(int[] f: flights){
+                if(dp[i-1][f[0]]!=Integer.MAX_VALUE){
+                    dp[i][f[1]] = Math.min(dp[i][f[1]],dp[i-1][f[0]]+f[2]);
                 }
             }
-            stops++;
         }
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        return dp[k+1][dst] == Integer.MAX_VALUE ? -1 : dp[k+1][dst];
     }
 }
