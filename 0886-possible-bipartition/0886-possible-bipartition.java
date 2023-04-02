36723ccc93a1c55adcf5997fc8c9dbeb8ae8f2e5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-02 15:23:51 -0400

    Time: 14 ms (81.03%), Space: 51.5 MB (12.60%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,38 +1,36 @@
 class Solution {
-    public boolean possibleBipartition(int n, int[][] dislikes) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
+    public boolean possibleBipartition(int N, int[][] dislikes) {        
+        List<Integer>[] graph = new List[N + 1];  
+
+        for (int i = 1; i <= N; ++i) graph[i] = new ArrayList<>();        
+
         for (int[] dislike : dislikes) {
-            int a = dislike[0], b = dislike[1];
-            adj.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
-            adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
+            graph[dislike[0]].add(dislike[1]);
+            graph[dislike[1]].add(dislike[0]);
         }
-        int[] color = new int[n + 1];
-        Arrays.fill(color, -1); // 0 stands for red and 1 stands for blue.
-        for (int i = 1; i <= n; i++) {
-            if (color[i] == -1)
-                if (!bfs(i, adj, color))
-                    return false;
+
+        Integer[] colors = new Integer[N + 1];
+
+        for (int i = 1; i <= N; ++i) {
+            // If the connected component that node i belongs to hasn't been colored yet then try coloring it.
+            if (colors[i] == null && !dfs(graph, colors, i, 1)) return false;
         }
-        return true;
+        return true;   
     }
-    
-    public boolean bfs(int people, Map<Integer, List<Integer>> adj, int[] color) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(people);
-        color[people] = 0; // Start with marking source as `RED`.
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            if (!adj.containsKey(node))
-                continue;
-            for (int neighbor : adj.get(node)) {
-                if (color[neighbor] == color[node])
-                    return false;
-                if (color[neighbor] == -1) {
-                    color[neighbor] = 1 - color[node];
-                    q.add(neighbor);
-                }
+
+    private boolean dfs(List<Integer>[] graph, Integer[] colors, int currNode, int currColor) {
+        colors[currNode] = currColor;
+
+        // Color all uncolored adjacent nodes.
+        for (Integer adjacentNode : graph[currNode]) {
+
+            if (colors[adjacentNode] == null) {
+                if (!dfs(graph, colors, adjacentNode, currColor * -1)) return false;     
+
+            } else if (colors[adjacentNode] == currColor) {
+                return false;                                     
             }
         }
-        return true;
+        return true;        
     }
 }
