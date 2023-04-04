84be436c750decd3a3c59ef26a4c9f79d89dbf94
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-04 16:38:17 -0400

    Time: 11 ms (15.39%), Space: 54.6 MB (27.78%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,15 +1,29 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int r=matrix.length;
-        if(r==0) return 0;
-        int c=matrix[0].length,edge=0;
-        int[][] dp=new int[r+1][c+1];
-        for(int i=1;i<=r;i++)
-            for(int j=1;j<=c;j++) {
-                if(matrix[i-1][j-1]=='0') continue;
-                dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]));
-                edge=Math.max(edge,dp[i][j]);
+        
+        if(matrix.length == 0) return 0;
+        
+        int[][] memo = new int[matrix.length][matrix[0].length];
+        int max = 0;
+        int currMax = 0;
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = 0; j<matrix[0].length; j++){
+                currMax = findsquare(matrix,i,j,memo);
+                if(max < currMax)
+                    max = currMax;
             }
-        return edge*edge;
+        }
+        return max*max;
+    }
+    
+    private int findsquare(char[][] matrix, int i, int j, int[][] dp){
+        
+        if(i >= matrix.length || j >= matrix[0].length || matrix[i][j] == '0')
+            return 0;
+        if(dp[i][j] != 0) return dp[i][j];
+        
+        return dp[i][j]  =  1 + Math.min(findsquare(matrix, i+1, j+1,dp),
+                                Math.min(findsquare(matrix, i+1,j,dp),
+                                         findsquare(matrix, i, j+1, dp)));
     }
 }
