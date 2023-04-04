14d375798e9453303956315ea2694a93a75cfcce
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-04 16:37:36 -0400

    Time: 8 ms (26.96%), Space: 53.3 MB (93.52%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,35 +1,15 @@
-public class Solution {
+class Solution {
     public int maximalSquare(char[][] matrix) {
-        
-        if(matrix == null || matrix.length == 0) return 0;
-        
-        int result = 0;
-        int[][] count = new int[matrix.length][matrix[0].length];
-        
-        for(int i = 0; i < matrix.length; i ++) {
-            count[i][0] = matrix[i][0] == '0' ? 0 : 1;
-            result = Math.max(result, count[i][0]);
-        }
-        
-        for(int i = 0; i < matrix[0].length; i ++) {
-            count[0][i] = matrix[0][i] == '0' ? 0 : 1;
-            result = Math.max(result, count[0][i]);
-        }
-        
-        
-        for(int i = 1; i < matrix.length; i++) {
-            for(int j = 1; j < matrix[0].length; j++) {
-                
-                if(matrix[i][j] == '0') {
-                    count[i][j] = 0;
-                    continue;
-                }
-                
-                count[i][j] = Math.min(Math.min(count[i - 1][j - 1], count[i - 1][j]), 
-                                                                      count[i][j - 1]) + 1;
-                result = Math.max(result, count[i][j]);
+        int r=matrix.length;
+        if(r==0) return 0;
+        int c=matrix[0].length,edge=0;
+        int[][] dp=new int[r+1][c+1];
+        for(int i=1;i<=r;i++)
+            for(int j=1;j<=c;j++) {
+                if(matrix[i-1][j-1]=='0') continue;
+                dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]));
+                edge=Math.max(edge,dp[i][j]);
             }
-        }
-        return result * result;
+        return edge*edge;
     }
 }
