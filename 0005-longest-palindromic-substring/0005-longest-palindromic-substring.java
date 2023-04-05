f1c8a9fe8d506c3c68a60f6eba1f79a7e4d01ff5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-04 21:01:06 -0400

    Time: 260 ms (19.04%), Space: 52.6 MB (5.32%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,25 +1,54 @@
-public class Solution {
-private int lo, maxLen;
-
-public String longestPalindrome(String s) {
-	int len = s.length();
-	if (len < 2)
-		return s;
-	
-    for (int i = 0; i < len-1; i++) {
-     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
-     	extendPalindrome(s, i, i+1); //assume even length.
+class Solution {
+    
+    Boolean[][] memo ;
+    
+    public String longestPalindrome(String s) {
+        
+        int len = s.length();
+        
+        int maxLen = 0;
+        String res = "";
+        memo = new Boolean[len][len];
+        
+        for (int left = 0; left < len; left++){
+            for (int right = left; right < len; right++){
+                
+             if (s.charAt(left) == s.charAt(right) && isPalin(s,left + 1, right -1)){
+                if (maxLen < right - left + 1) {
+                    maxLen = right - left + 1;
+                    res = s.substring(left, right + 1);
+                    }
+                }   
+            }
+        }
+        
+        return res;
     }
-    return s.substring(lo, lo + maxLen);
+    
+    
+    public boolean isPalin(String s, int left, int right){
+        
+        if (left >= right){
+            return true;
+        }
+        
+        if (memo[left][right] != null){
+            return memo[left][right] ;
+        }
+        
+        if (s.charAt(left) != s.charAt(right)) {
+            memo[left][right] = false;
+            return memo[left][right];
+        }
+        
+        if (right - left <= 2){
+            memo[left][right] = true;
+            return memo[left][right];
+        }
+        
+        memo[left][right] = isPalin(s,left + 1, right - 1);
+        return memo[left][right];
+        
+    }
+    
 }
-
-private void extendPalindrome(String s, int j, int k) {
-	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-		j--;
-		k++;
-	}
-	if (maxLen < k - j - 1) {
-		lo = j + 1;
-		maxLen = k - j - 1;
-	}
-}}
