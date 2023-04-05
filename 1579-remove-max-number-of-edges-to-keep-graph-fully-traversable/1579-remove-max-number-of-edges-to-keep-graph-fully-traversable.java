4c6b0a46ba9a15ff8838b88ec8172cf249550ac2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-05 18:52:07 -0400

    Time: 13 ms (78.91%), Space: 119.3 MB (12.92%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,57 +1,67 @@
+class UF {
+    int[] parent;
+    
+    public UF(int n) {
+        parent = new int[n];
+        for (int i = 1; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    public boolean union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py) {
+            return false;
+        }
+        parent[px] = py;
+        return true;
+    }
+    
+    private int find(int x) {
+        return parent[x] = x == parent[x] ? x : find(parent[x]);
+    }
+}
+
 class Solution {
     public int maxNumEdgesToRemove(int n, int[][] edges) {
-        Arrays.sort(edges, (a, b) -> b[0] - a[0]);
-        int[] parentAlice = new int[n + 1];
-        int[] parentBob = new int[n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            parentAlice[i] = i;
-            parentBob[i] = i;
-        }
-        int mergeAlice = 1, mergeBob = 1, removeEdge = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int res = 0;
+        UF uf = new UF(n + 1);
         for (int[] edge : edges) {
-            int category = edge[0], u = edge[1], v = edge[2];
-            if (category == 3) {
-                boolean alice = union(u, v, parentAlice);
-                boolean bob = union(u, v, parentBob);
-                if (alice == true)
-                    mergeAlice++;
-                if (bob == true)
-                    mergeBob++;
-                if (alice == false && bob == false)
-                    removeEdge++;
-            } else if (category == 2) {
-                boolean bob = union(u, v, parentBob);
-                if (bob == true)
-                    mergeBob++;
-                else
-                    removeEdge++;
-            } else {
-                boolean alice = union(u, v, parentAlice);
-                if (alice == true)
-                    mergeAlice++;
-                else
-                    removeEdge++;
+            if (edge[0] == 3) {
+                if (uf.union(edge[1], edge[2])) {
+                    cnt1++;
+                    cnt2++;
+                }
+                else {
+                    res++;
+                }
             }
         }
-        if (mergeAlice != n || mergeBob != n)
-            return -1;
-        return removeEdge;
-    }
-    
-    public boolean union(int u, int v, int[] parent) {
-        int fu = find(u, parent), fv = find(v, parent);
-        if (fu != fv) {
-            parent[fu] = fv;
-            return true;
-        } else 
-            return false;
-    }
-    
-    public int find(int i, int[] parent) {
-        if (parent[i] == i)
-            return i;
-        int tmp = find(parent[i], parent);
-        parent[i] = tmp;
-        return tmp;
+        int[] parentcpy = uf.parent.clone();
+        for (int[] edge : edges) {
+            if (edge[0] == 1) {
+                if (uf.union(edge[1], edge[2])) {
+                    cnt1++;
+                }
+                else {
+                    res++;
+                }
+            }
+        }
+        uf.parent = parentcpy;
+        for (int[] edge : edges) {
+            if (edge[0] == 2) {
+                if (uf.union(edge[1], edge[2])) {
+                    cnt2++;
+                }
+                else {
+                    res++;
+                }
+            }
+        }
+        return cnt1 == n - 1 && cnt2 == n - 1 ? res : -1;
     }
 }
