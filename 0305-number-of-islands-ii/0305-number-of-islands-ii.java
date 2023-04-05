3806614e85a930e149a8420959e7cfa2519a9b72
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-05 18:47:06 -0400

    Time: 10 ms (48.61%), Space: 45.8 MB (72.78%) - LeetHub

--- 0305-number-of-islands-ii/0305-number-of-islands-ii.java
+++ 0305-number-of-islands-ii/0305-number-of-islands-ii.java
@@ -1,35 +1,26 @@
 class Solution {
     public List<Integer> numIslands2(int m, int n, int[][] positions) {
         List<Integer> result = new ArrayList<>();
-        if (positions == null || positions.length == 0) {
+        if (m == 0 || n == 0 || positions == null || positions.length == 0 || positions[0].length == 0)
             return result;
-        }
-        int[] root = new int[m * n];
-        Arrays.fill(root, -1);
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        UnionFind uf = new UnionFind(m * n);
         int count = 0;
-        int[] xDelta = {1, -1, 0, 0};
-        int[] yDelta = {0, 0, 1, -1};
         for (int[] position : positions) {
-            
-            int x = position[0];
-            int y = position[1];
-            int index = x * n + y;
-            if (root[index] != -1) {    // duplicate position
+            int x = position[0], y = position[1];
+            int p = x * n + y;
+            if (uf.parent[p] != -1) {
                 result.add(count);
                 continue;
             }
+            uf.parent[p] = p;
             count++;
-            root[index] = index;
-            for (int i = 0; i < 4; i++) {
-                int r = x + xDelta[i];
-                int c = y + yDelta[i];
-                if (isValid(m, n, r, c, root)) {
-                    int neighborIndex = r * n + c;
-                    int neighborRoot = findRoot(root, neighborIndex);
-                    if (neighborRoot != index) {
-                        root[neighborRoot] = index;
+            for (int[] dir : dirs) {
+                int r = x + dir[0], c = y + dir[1];
+                if (isValid(uf, m, n, r, c)) {
+                    int q = r * n + c;
+                    if (uf.union(p, q)) 
                         count--;
-                    }
                 }
             }
             result.add(count);
@@ -37,18 +28,43 @@ class Solution {
         return result;
     }
     
-    private boolean isValid(int m, int n, int r, int c, int[] root) {
-        if (r < 0 || c < 0 || r >= m || c >= n || root[r * n + c] == -1) {
+    private boolean isValid(UnionFind uf, int m, int n, int r, int c) {
+        if (r < 0 || c < 0 || r >= m || c >= n || uf.parent[r * n + c] == -1) {
             return false;
         }
         return true;
     }
+}
+
+class UnionFind {
+    public int[] parent, rank;
     
-    private int findRoot(int[] root, int index) {
-        while (index != root[index]) {
-            root[index] = root[root[index]];
-            index = root[index];
+    public UnionFind(int n) {
+        this.parent = new int[n];
+        this.rank = new int[n];
+        Arrays.fill(parent, -1);
+    }
+    
+    public boolean union(int p, int q) {
+        int f1 = find(p), f2 = find(q);
+        if (f1 == f2)
+            return false;
+        if (rank[f1] < rank[f2]) 
+            parent[f1] = f2;
+        else if (rank[f1] > rank[f2])
+            parent[f2] = f1;
+        else {
+            parent[f2] = f1;
+            rank[f1]++;
         }
-        return index;
+        return true;
+    }
+    
+    public int find(int x) {
+        while (x != parent[x]) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
     }
 }
