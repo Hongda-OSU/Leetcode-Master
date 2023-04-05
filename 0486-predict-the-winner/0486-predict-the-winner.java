e18df4dc46a2d414f2d3d317f1383d9ccc8c4cc4
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-05 17:11:13 -0400

    Time: 0 ms (100.00%), Space: 39.4 MB (97.98%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        if (nums == null)
+            return false;
+        int n = nums.length;
+        if ((n & 1) == 0)
+            return true;
+        int[] dp = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j < n; j++) {
+                if (i == j) 
+                    dp[i] = nums[i];
+                else
+                    dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        return dp[n - 1] >= 0;
+    }
+}
