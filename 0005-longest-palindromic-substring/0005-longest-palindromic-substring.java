07fd00ca633efb975419fd94c8af21cfc6c23912
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-04 21:01:51 -0400

    Time: 23 ms (69.58%), Space: 42.1 MB (86.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,54 +1,25 @@
 class Solution {
-    
-    Boolean[][] memo ;
-    
     public String longestPalindrome(String s) {
-        
-        int len = s.length();
-        
-        int maxLen = 0;
-        String res = "";
-        memo = new Boolean[len][len];
-        
-        for (int left = 0; left < len; left++){
-            for (int right = left; right < len; right++){
-                
-             if (s.charAt(left) == s.charAt(right) && isPalin(s,left + 1, right -1)){
-                if (maxLen < right - left + 1) {
-                    maxLen = right - left + 1;
-                    res = s.substring(left, right + 1);
-                    }
-                }   
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int left = i, right = i;
+            while (left >= 0 && s.charAt(left) == ch) 
+                left--;
+            while (right < s.length() && s.charAt(right) == ch)
+                right++;
+            while (left >= 0 && right < s.length()) {
+                if (s.charAt(left) != s.charAt(right))
+                    break;
+                left--;
+                right++;
+            }
+            left += 1;
+            if (end - start < right - left) {
+                start = left;
+                end = right;
             }
         }
-        
-        return res;
+        return s.substring(start, end);
     }
-    
-    
-    public boolean isPalin(String s, int left, int right){
-        
-        if (left >= right){
-            return true;
-        }
-        
-        if (memo[left][right] != null){
-            return memo[left][right] ;
-        }
-        
-        if (s.charAt(left) != s.charAt(right)) {
-            memo[left][right] = false;
-            return memo[left][right];
-        }
-        
-        if (right - left <= 2){
-            memo[left][right] = true;
-            return memo[left][right];
-        }
-        
-        memo[left][right] = isPalin(s,left + 1, right - 1);
-        return memo[left][right];
-        
-    }
-    
 }
