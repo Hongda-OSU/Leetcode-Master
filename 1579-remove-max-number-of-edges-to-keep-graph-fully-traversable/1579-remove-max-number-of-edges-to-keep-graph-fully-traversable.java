669d406cab17ca3a845155d1ed990c30120f1e41
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-05 18:51:06 -0400

    Time: 40 ms (25.17%), Space: 103.9 MB (31.29%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,61 +1,73 @@
 class Solution {
     public int maxNumEdgesToRemove(int n, int[][] edges) {
-        Arrays.sort(edges, (a, b) -> (b[0] - a[0]));
-        int[] roots1 = new int[n + 1];
-        int[] roots2 = new int[n + 1];
-        for(int i = 1; i <= n; i++) {
-            roots1[i] = i;
-            roots2[i] = i;
-        }
-        int n1 = n, n2 = n; // Number of components for two players.
-        int res = 0;
-        for(int[] e : edges) {
-            if(e[0] == 1) {
-                int root_a = find(e[1], roots1);
-                int root_b = find(e[2], roots1);
-                if(root_a == root_b) { // If roots are the same, then this is a redundant edge and can be removed.
-                    res++;
-                } else {
-                    roots1[root_a] = root_b; // If roots are different, we connect two different components.
-                    n1--;
-                }
-            } else if(e[0] == 2) {
-                int root_a = find(e[1], roots2);
-                int root_b = find(e[2], roots2);
-                if(root_a == root_b) {
-                    res++;
-                } else {
-                    roots2[root_a] = root_b;
-                    n2--;
-                }
-            } else {
-                int root_a1 = find(e[1], roots1);
-                int root_b1 = find(e[2], roots1);
-                int root_a2 = find(e[1], roots2);
-                int root_b2 = find(e[2], roots2);
-                if(root_a1 != root_b1) {
-                    roots1[root_a1] = root_b1;
-                    n1--;
-                }
-                if(root_a2 != root_b2) {
-                    roots2[root_a2] = root_b2;
-                    n2--;
-                }
-                if(root_a1 == root_b1 && root_a2 == root_b2) {
-                    res++;
-                }
+        Arrays.sort(edges, (a, b) -> b[0] - a[0]);
+        
+        int edgeAdd = 0;
+        
+        UnionFind alice = new UnionFind(n);
+        UnionFind bob = new UnionFind(n);
+        
+        for (int[] edge : edges) {
+            int type = edge[0];
+            int a = edge[1];
+            int b = edge[2];
+            
+            switch (type) {
+                case 3:
+                    if (alice.unite(a, b) | bob.unite(a, b)) {
+                        edgeAdd++;
+                    }
+                    break;
+                case 2:
+                    if (bob.unite(a, b)) {
+                        edgeAdd++;
+                    }
+                    break;
+                case 1:
+                    if (alice.unite(a, b)) {
+                        edgeAdd++;
+                    } 
+                    break;
             }
         }
-        if(n1 != 1 || n2 != 1) return -1; // If total number of components is not one for either players, return -1.
-        return res;
+        
+        return (alice.united() && bob.united()) ? edges.length - edgeAdd : -1;
     }
     
-    private int find(int i, int[] roots) {
-        int j = i;
-        while(roots[i] != i) {
-            i = roots[i];
+    private class UnionFind {
+        int[] component;
+        int distinctComponents;
+        
+        public UnionFind(int n) {
+            component = new int[n+1];
+            for (int i = 0; i <= n; i++) {
+                component[i] = i;
+            }
+            distinctComponents = n;
         }
-        roots[j] = i;
-        return i;
+        // unite. For example, if previously we have component {0, 4, 4, 4, 4, 6, 7, 7}, then invoke this method with a=1, b=5, then after invoke, {0, 4, 4, 4, 5, 7, 7, 7}
+        private boolean unite(int a, int b) {
+            if (findComponent(a) != findComponent(b)) {
+                component[findComponent(a)] = b;
+                distinctComponents--;
+                return true;
+            }
+            
+            return false;
+        }
+        
+        // find and change component
+        // for example, if previously we have component:{0, 2, 3, 4, 4, 6, 7, 7}, then after invoke this method with a=1, the component become {0, 4, 4, 4, 4, 6, 7, 7}
+        private int findComponent(int a) {
+            if (component[a] != a) {
+                component[a] = findComponent(component[a]);
+            }
+            return component[a];
+        }
+        
+        private boolean united() {
+            return distinctComponents == 1;
+        }
+        
     }
 }
