e914945a873bfbc7d4d71693024dd5d64d433f1b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-05 18:50:28 -0400

    Time: 42 ms (20.41%), Space: 103.6 MB (32.65%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,65 +1,61 @@
 class Solution {
     public int maxNumEdgesToRemove(int n, int[][] edges) {
-        Arrays.sort(edges, (a, b) -> Integer.compare(b[0], a[0]));
-        
-        DSU alice = new DSU(n + 1);
-        DSU bob = new DSU(n + 1);
-        int removedEdges = 0, totalEdges = 0;
-        
+        Arrays.sort(edges, (a, b) -> (b[0] - a[0]));
+        int[] roots1 = new int[n + 1];
+        int[] roots2 = new int[n + 1];
+        for(int i = 1; i <= n; i++) {
+            roots1[i] = i;
+            roots2[i] = i;
+        }
+        int n1 = n, n2 = n; // Number of components for two players.
+        int res = 0;
         for(int[] e : edges) {
-            if(e[0] == 3) {
-                boolean isEdgeValidForAlice = alice.union(e[1], e[2]);
-                boolean isEdgeValidForBob = bob.union(e[1], e[2]);
-                
-                if(isEdgeValidForAlice) totalEdges++;//made the edge 
-                if(isEdgeValidForBob) totalEdges++;//made the edge
-                
-                if(!isEdgeValidForAlice || !isEdgeValidForBob) removedEdges++;//remove the edge if it's not valid
-            } else if (e[0] == 1) {
-                boolean isEdgeValidForAlice = alice.union(e[1], e[2]);
-                if(isEdgeValidForAlice) 
-                    totalEdges++;
-                else
-                    removedEdges++;
+            if(e[0] == 1) {
+                int root_a = find(e[1], roots1);
+                int root_b = find(e[2], roots1);
+                if(root_a == root_b) { // If roots are the same, then this is a redundant edge and can be removed.
+                    res++;
+                } else {
+                    roots1[root_a] = root_b; // If roots are different, we connect two different components.
+                    n1--;
+                }
+            } else if(e[0] == 2) {
+                int root_a = find(e[1], roots2);
+                int root_b = find(e[2], roots2);
+                if(root_a == root_b) {
+                    res++;
+                } else {
+                    roots2[root_a] = root_b;
+                    n2--;
+                }
             } else {
-                boolean isEdgeValidForBob = bob.union(e[1], e[2]);
-                if(isEdgeValidForBob)  
-                    totalEdges++;   
-                else
-                    removedEdges++;
+                int root_a1 = find(e[1], roots1);
+                int root_b1 = find(e[2], roots1);
+                int root_a2 = find(e[1], roots2);
+                int root_b2 = find(e[2], roots2);
+                if(root_a1 != root_b1) {
+                    roots1[root_a1] = root_b1;
+                    n1--;
+                }
+                if(root_a2 != root_b2) {
+                    roots2[root_a2] = root_b2;
+                    n2--;
+                }
+                if(root_a1 == root_b1 && root_a2 == root_b2) {
+                    res++;
+                }
             }
         }
-        
-        if(totalEdges != 2 * n - 2) return -1;//n - 1 for alice and n - 1 for bob
-                
-        return removedEdges;      
-    }
-}
-
-class DSU {
-    private int parent[];
-    
-    DSU(int n) {
-        parent = new int[n];
-        for(int i = 0; i < n; i++)
-            parent[i] = i;
+        if(n1 != 1 || n2 != 1) return -1; // If total number of components is not one for either players, return -1.
+        return res;
     }
     
-    public int find(int x) {
-        while(x != parent[x]) {
-            parent[x] = parent[parent[x]];//path compression
-            x = parent[x];
+    private int find(int i, int[] roots) {
+        int j = i;
+        while(roots[i] != i) {
+            i = roots[i];
         }
-        return x;
-    }
-    
-    public boolean union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-            
-        if(rootX == rootY) return false;
-        
-        parent[rootX] = rootY;
-        return true;
+        roots[j] = i;
+        return i;
     }
 }
