90a994d412be13ff5cdde6378d775c26ba6b1f0a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-05 17:33:34 -0400

    Time: 29 ms (73.50%), Space: 44.1 MB (40.14%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minCut(String s) {
+        int n = s.length(), min;
+        int[] cut = new int[n];
+        boolean[][] palindrome = new boolean[n][n];
+        for (int j = 0; j < n; j++) {
+            min = j;
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 3 || palindrome[i + 1][j - 1])) {
+                    palindrome[i][j] = true;
+                    min = i == 0 ? 0 : Math.min(min, cut[i - 1] + 1);
+                }
+            }
+            cut[j] = min;
+        }
+        return cut[n - 1];
+    }
+}
