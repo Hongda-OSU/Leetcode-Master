12f780000088620b9c0983a56843822e0b232b56
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-06 16:59:41 -0400

    Time: 13 ms (94.55%), Space: 73.5 MB (19.80%) - LeetHub

--- 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.java
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.java
@@ -1,47 +1,43 @@
 class Solution {
     public List<Integer> shortestDistanceColor(int[] colors, int[][] queries) {
-        // initialization
-        List<Integer> queryResults = new ArrayList<>();
-        Map<Integer, List<Integer>> hashmap = new HashMap<>();
+        // initializations
+        int n = colors.length;
+        int[] rightmost = {0, 0, 0};
+        int[] leftmost = {n - 1, n - 1, n - 1};
 
-        for (int i = 0; i < colors.length; i++) {
-            hashmap.putIfAbsent(colors[i], new ArrayList<Integer>());
-            hashmap.get(colors[i]).add(i);
+        int[][] distance = new int[3][n];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < n; j++) {
+                distance[i][j] = -1;
+            }
         }
 
-        // for each query, apply binary search
+        // looking forward
+        for (int i = 0; i < n; i++) {
+            int color = colors[i] - 1;
+            for (int j = rightmost[color]; j < i + 1; j++) {
+                distance[color][j] = i - j;
+            }
+            rightmost[color] = i + 1;
+        }
+
+        // looking backward
+        for (int i = n - 1; i > -1; i--) {
+            int color = colors[i] - 1;
+            for (int j = leftmost[color]; j > i - 1; j--) {
+                if (distance[color][j] == -1 || distance[color][j] > j - i) {
+                    distance[color][j] = j - i;
+                }
+            }
+            leftmost[color] = i - 1;
+        }
+
+        List<Integer> queryResults = new ArrayList<>();
         for (int i = 0; i < queries.length; i++) {
-            int target = queries[i][0], color = queries[i][1];
-            if (!hashmap.containsKey(color)) {
-                queryResults.add(-1);
-                continue;
-            }
-
-            List<Integer> indexList = hashmap.get(color);
-            int insert = Collections.binarySearch(indexList, target);
-
-            // due to its nature, we need to convert the returning values
-            // from Collections.binarySearch
-            // more details: https://docs.oracle.com/en/java/javase/12/docs/api/java.base/java/util/Collections.html#binarySearch(java.util.List,T)
-            if (insert < 0) {
-                insert = (insert + 1) * -1;
-            }
-
-            // Handling cases when:
-            // - the target index is smaller than all elements in the indexList
-            // - the target index is larger than all elements in the indexList
-            // - the target index sits between the left and right boundaries
-            if (insert == 0) {
-                queryResults.add(indexList.get(insert) - target);
-            } else if (insert == indexList.size()) {
-                queryResults.add(target - indexList.get(insert - 1));
-            } else {
-                int leftNearest = target - indexList.get(insert - 1);
-                int rightNearest = indexList.get(insert) - target;
-                queryResults.add(Math.min(leftNearest, rightNearest));
-            }
-
+            queryResults.add(distance[queries[i][1] - 1][queries[i][0]]);
         }
         return queryResults;
+
     }
 }
