9ad57ac99a9b5ca34566930cf083c6a91c8a7c7b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-06 16:15:47 -0400

    Time: 14 ms (65.64%), Space: 44.4 MB (53.30%) - LeetHub

--- 0562-longest-line-of-consecutive-one-in-matrix/0562-longest-line-of-consecutive-one-in-matrix.java
+++ 0562-longest-line-of-consecutive-one-in-matrix/0562-longest-line-of-consecutive-one-in-matrix.java
@@ -1,16 +1,22 @@
 class Solution {
+
+    private int[][] dir = {{ -1, 0}, {0, -1}, { -1, -1}, { -1, 1}};
+
     public int longestLine(int[][] M) {
-        if (M.length==0) return 0;
-        int m=M.length, n=M[0].length, res=0;
-        int[][][] dp= new int[m+1][n+2][4];
-        for (int i=0; i<m; i++)
-            for (int j=0; j<n; j++)
-                if (M[i][j]==1){
-                    res=Math.max(res, dp[i+1][j+1][0]=dp[i][j+1][0]+1);//update row
-                    res=Math.max(res, dp[i+1][j+1][1]=dp[i+1][j][1]+1);//update column
-                    res=Math.max(res, dp[i+1][j+1][2]=dp[i]  [j][2]+1);//update diagnoal
-                    res=Math.max(res, dp[i+1][j+1][3]=dp[i][j+2][3]+1);//update antidiagonal
+        if (M.length == 0 || M[0].length == 0) return 0;
+        int m = M.length, n = M[0].length;
+
+        int max = 0;
+        int[][][] dp = new int[m + 1][n + 2][4];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (M[i - 1][j - 1] == 0) continue;
+                for (int k = 0; k < 4; k++) {
+                    dp[i][j][k] = dp[i + dir[k][0]][j + dir[k][1]][k] + 1;
+                    max = Math.max(max, dp[i][j][k]);
                 }
-        return res;
+            }
+        }
+        return max;
     }
 }
