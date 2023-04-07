ccf84a7df32662fd718e338dd3fffde7f17d37a0
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-07 16:48:15 -0400

    Time: 1 ms (51.29%), Space: 39.6 MB (36.65%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,14 +1,12 @@
 class Solution {
-   public int integerBreak(int n) {
-        if(n == 2)
-            return 1;
-        else if(n == 3)
-            return 2;
-        else if(n%3 == 0)
-            return (int)Math.pow(3, n/3);
-        else if(n%3 == 1)
-            return 2 * 2 * (int) Math.pow(3, (n - 4) / 3);
-        else 
-            return 2 * (int) Math.pow(3, n/3);
+    public int integerBreak(int n) {
+        int dp[] = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * Math.max(i - j, dp[i - j]));
+            }
+        }
+        return dp[n];
     }
 }
