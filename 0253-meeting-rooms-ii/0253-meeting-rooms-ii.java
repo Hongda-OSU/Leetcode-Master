d1c519566b65f598b0d58864c0f387d10b31fb5e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-07 17:51:26 -0400

    Time: 7 ms (71.32%), Space: 42.5 MB (55.27%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0)
+            return 0;
+        int rooms = 0;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(intervals.length, (a, b) -> a[1] - b[1]);
+        pq.offer(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = pq.poll();
+            if (intervals[i][0] >= interval[1])
+                interval[1] = intervals[i][1];
+            else
+                pq.offer(intervals[i]);
+            pq.offer(interval);
+        }
+        return pq.size();
+    }
+}
