41a5f1ae7e629ff8713bc9858a4d4b3b03318e6c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-08 15:33:17 -0400

    Time: 6 ms (69.46%), Space: 43.2 MB (15.08%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,23 +1,35 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<Integer> set = new HashSet<>();
-        return dfs(s, 0, wordDict, set);
+        // create the memoization array to save results and avoid repeat computations
+        Boolean[] canBreak = new Boolean[s.length()];
+
+        // convert the list into set for faster lookup
+        Set<String> wordSet = new HashSet<>(wordDict);
+
+        // recursion with memoization
+        return helper(s, 0, wordSet, canBreak);
     }
-    
-    public boolean dfs(String s, int idx, List<String> dict, Set<Integer> set) {
-        if (idx == s.length())
-            return true;
-        if (set.contains(idx))
-            return false;
-        for (int i = idx + 1; i <= s.length(); i++) {
-            String str = s.substring(idx, i);
-            if (dict.contains(str)) 
-                if (dfs(s, i, dict, set))
-                    return true;
-                else 
-                    set.add(i);
+
+    private boolean helper(String s, int startIdx, Set<String> wordSet, Boolean[] canBreak) {
+        // in case we've reached the end of string, return true
+        if (startIdx == s.length()) return true;
+        // else if we've already computed on current substring before
+        if (canBreak[startIdx] != null) return canBreak[startIdx]; // auto-unboxing
+
+        boolean res = false;
+        // iterate through all indices after startIdx, explore every possible word
+        for (int i = startIdx + 1; i <= s.length(); i++) {
+            String currWord = s.substring(startIdx, i);
+            // skip if this is not a word in the input dictionary
+            if (!wordSet.contains(currWord)) continue;
+            // recursively call upon the rest of string
+            if (helper(s, i, wordSet, canBreak)) {
+                res = true;
+                break;
+            }
         }
-        set.add(idx);
-        return false;
+        // add result to memo and return the result
+        canBreak[startIdx] = res;
+        return res;
     }
 }
