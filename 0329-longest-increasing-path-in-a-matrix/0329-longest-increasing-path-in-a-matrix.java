268aa0b0562c7a017399bee4a56898637a65ba6f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-08 16:23:33 -0400

    Time: 10 ms (56.55%), Space: 43.3 MB (20.66%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,30 +1,29 @@
 class Solution {
-    public static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-public int longestIncreasingPath(int[][] matrix) {
-    if(matrix.length == 0) return 0;
-    int m = matrix.length, n = matrix[0].length;
-    int[][] cache = new int[m][n];
-    int max = 1;
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            int len = dfs(matrix, i, j, m, n, cache);
-            max = Math.max(max, len);
+    public int longestIncreasingPath(int[][] matrix) {
+        int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        int max = 0;
+        for(int[] d : dp) Arrays.fill(d, -1);
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                max = Math.max(max,dfs(matrix, i, j, dir, dp));
+            }
         }
-    }   
-    return max;
-}
-
-public int dfs(int[][] matrix, int i, int j, int m, int n, int[][] cache) {
-    if(cache[i][j] != 0) return cache[i][j];
-    int max = 1;
-    for(int[] dir: dirs) {
-        int x = i + dir[0], y = j + dir[1];
-        if(x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) continue;
-        int len = 1 + dfs(matrix, x, y, m, n, cache);
-        max = Math.max(max, len);
+        return max;
     }
-    cache[i][j] = max;
-    return max;
-}
+    
+    public int dfs(int[][] arr, int sr, int sc, int[][] dir, int[][] dp){
+        if(dp[sr][sc] != -1) return dp[sr][sc];
+        int count = 0;
+        for(int d = 0 ; d < dir.length ; d++){
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+            if(r < arr.length && c < arr[0].length && r >= 0 && c >= 0 && arr[r][c] > arr[sr][sc]){
+                count = Math.max(count,dfs(arr, r, c, dir, dp));
+            }
+        }
+        return dp[sr][sc] = count + 1;
+    }
 }
