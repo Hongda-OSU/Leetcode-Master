4a7a07e1fd769aeb8bddfb95aeff9e3509f2ec33
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-08 16:34:19 -0400

    Time: 10 ms (56.55%), Space: 42.2 MB (97.57%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,29 +1,32 @@
 class Solution {
+    public static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    
     public int longestIncreasingPath(int[][] matrix) {
-        int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int[][] dp = new int[n][m];
-        int max = 0;
-        for(int[] d : dp) Arrays.fill(d, -1);
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                max = Math.max(max,dfs(matrix, i, j, dir, dp));
+        if (matrix.length == 0)
+            return 0;
+        int m = matrix.length, n = matrix[0].length, max = 1;
+        int[][] cache = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int len = dfs(matrix, i, j, m, n, cache);
+                max = Math.max(max, len);
             }
         }
         return max;
     }
     
-    public int dfs(int[][] arr, int sr, int sc, int[][] dir, int[][] dp){
-        if(dp[sr][sc] != -1) return dp[sr][sc];
-        int count = 0;
-        for(int d = 0 ; d < dir.length ; d++){
-            int r = sr + dir[d][0];
-            int c = sc + dir[d][1];
-            if(r < arr.length && c < arr[0].length && r >= 0 && c >= 0 && arr[r][c] > arr[sr][sc]){
-                count = Math.max(count,dfs(arr, r, c, dir, dp));
-            }
+    public int dfs(int[][] matrix, int row, int col, int rows, int cols, int[][] cache) {
+        if (cache[row][col] != 0)
+            return cache[row][col];
+        int max = 1;
+        for (int[] dir : dirs) {
+            int x = row + dir[0], y = col + dir[1];
+            if (x < 0 || x >= rows || y < 0 || y >= cols || matrix[x][y] <= matrix[row][col])
+                continue;
+            int len = 1 + dfs(matrix, x, y, rows, cols, cache);
+            max = Math.max(max, len);
         }
-        return dp[sr][sc] = count + 1;
-    }
+        cache[row][col] = max;
+        return max;
+    } 
 }
