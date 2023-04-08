686ca47bbec20c512fae39d2598235029df5849c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-08 17:01:29 -0400

    Time: 142 ms (46.63%), Space: 61.9 MB (49.84%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,25 +1,25 @@
 class MedianFinder {
-    private PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
-    private PriorityQueue<Integer> min = new PriorityQueue<>();
-    
+    private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
+private PriorityQueue<Integer> large = new PriorityQueue<>();
+private boolean even = true;
 
-    public MedianFinder() {
-        
+public double findMedian() {
+    if (even)
+        return (small.peek() + large.peek()) / 2.0;
+    else
+        return small.peek();
+}
+
+public void addNum(int num) {
+    if (even) {
+        large.offer(num);
+        small.offer(large.poll());
+    } else {
+        small.offer(num);
+        large.offer(small.poll());
     }
-    
-    public void addNum(int num) {
-        min.offer(num);
-        max.offer(min.poll());
-        if (min.size() < max.size())
-            min.offer(max.poll());
-    }
-    
-    public double findMedian() {
-        if (min.size() == max.size())
-            return (min.peek() + max.peek()) / 2.0;
-        else
-            return min.peek();
-    }
+    even = !even;
+}
 }
 
 /**
