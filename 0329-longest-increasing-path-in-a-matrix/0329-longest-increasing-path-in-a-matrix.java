28b3102cde615d7cf44f9756f615a10970c26959
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-08 16:21:03 -0400

    Time: 10 ms (56.55%), Space: 42.9 MB (50.93%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,41 +1,30 @@
 class Solution {
-    public int longestIncreasingPath(int[][] matrix) {
-        if (matrix == null || matrix.length == 0)
-            return 0;
-        int rows = matrix.length, cols = matrix[0].length, maxLength = 0;
-        int[][] dp = new int[rows + 1][cols + 1];
-        for (int[] row : dp) 
-            Arrays.fill(row, -1);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                maxLength = Math.max(maxLength, dfs(matrix, dp, i, j, Integer.MIN_VALUE));
-            }
+    public static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+public int longestIncreasingPath(int[][] matrix) {
+    if(matrix.length == 0) return 0;
+    int m = matrix.length, n = matrix[0].length;
+    int[][] cache = new int[m][n];
+    int max = 1;
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            int len = dfs(matrix, i, j, m, n, cache);
+            max = Math.max(max, len);
         }
-        return maxLength;
+    }   
+    return max;
+}
+
+public int dfs(int[][] matrix, int i, int j, int m, int n, int[][] cache) {
+    if(cache[i][j] != 0) return cache[i][j];
+    int max = 1;
+    for(int[] dir: dirs) {
+        int x = i + dir[0], y = j + dir[1];
+        if(x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) continue;
+        int len = 1 + dfs(matrix, x, y, m, n, cache);
+        max = Math.max(max, len);
     }
-    
-    public int dfs(int[][] matrix, int[][] dp, int row, int col, int compareWith) {
-        int rows = matrix.length, cols = matrix[0].length, result = 0;
-        boolean safe = isSafe(matrix, row, col, compareWith);
-        if (safe) {
-            if (dp[row][col] != -1)
-                return dp[row][col];
-            int temp = matrix[row][col];
-            matrix[row][col] = -1;
-            int d = dfs(matrix, dp, row + 1, col, temp);
-            int u = dfs(matrix, dp, row - 1, col, temp);
-            int r = dfs(matrix, dp, row, col + 1, temp);
-            int l = dfs(matrix, dp, row, col - 1, temp);
-            result = 1 + Math.max(Math.max(d, u), Math.max(r, l));
-            matrix[row][col] = temp;
-            dp[row][col] = result;
-        }
-        return result;
-    }
-    
-    public boolean isSafe(int[][] matrix, int row, int col, int compareWith) {
-        if (row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length && matrix[row][col] > compareWith && compareWith != -1)
-            return true;
-        return false;
-    }
+    cache[i][j] = max;
+    return max;
+}
 }
