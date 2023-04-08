d04eb620bb4c0a81108aaad36c7e036b23d1620e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-08 15:38:46 -0400

    Time: 14 ms (9.07%), Space: 42.9 MB (27.75%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,35 +1,18 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        // create the memoization array to save results and avoid repeat computations
-        Boolean[] canBreak = new Boolean[s.length()];
-
-        // convert the list into set for faster lookup
-        Set<String> wordSet = new HashSet<>(wordDict);
-
-        // recursion with memoization
-        return helper(s, 0, wordSet, canBreak);
-    }
-
-    private boolean helper(String s, int startIdx, Set<String> wordSet, Boolean[] canBreak) {
-        // in case we've reached the end of string, return true
-        if (startIdx == s.length()) return true;
-        // else if we've already computed on current substring before
-        if (canBreak[startIdx] != null) return canBreak[startIdx]; // auto-unboxing
-
-        boolean res = false;
-        // iterate through all indices after startIdx, explore every possible word
-        for (int i = startIdx + 1; i <= s.length(); i++) {
-            String currWord = s.substring(startIdx, i);
-            // skip if this is not a word in the input dictionary
-            if (!wordSet.contains(currWord)) continue;
-            // recursively call upon the rest of string
-            if (helper(s, i, wordSet, canBreak)) {
-                res = true;
-                break;
+        if (s == null || s.length() == 0)
+            return false;
+        int n = s.length();
+        boolean[] dp = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                String str = s.substring(j, i + 1);
+                if (wordDict.contains(str) && (j == 0 || dp[j - 1])) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        // add result to memo and return the result
-        canBreak[startIdx] = res;
-        return res;
+        return dp[n - 1];
     }
 }
