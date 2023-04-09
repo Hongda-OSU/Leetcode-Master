94897ccded532998680456421400354110db489f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-09 17:20:31 -0400

    Time: 18 ms (29.89%), Space: 44.8 MB (16.57%) - LeetHub

--- 0759-employee-free-time/0759-employee-free-time.java
+++ 0759-employee-free-time/0759-employee-free-time.java
@@ -14,23 +14,24 @@ class Interval {
 */
 
 class Solution {
-      public List<Interval> employeeFreeTime(List<List<Interval>> avails) {
-
-        List<Interval> result = new ArrayList<>();
-
-        PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> a.start - b.start);
-        avails.forEach(e -> pq.addAll(e));
-
-        Interval temp = pq.poll();
-        while(!pq.isEmpty()) {
-            if(temp.end < pq.peek().start) { // no intersect
-                result.add(new Interval(temp.end, pq.peek().start));
-                temp = pq.poll(); // becomes the next temp interval
-            }else { // intersect or sub merged
-                temp = temp.end < pq.peek().end ? pq.peek() : temp;
-                pq.poll();
+     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> schedule.get(a[0]).get(a[1]).start - schedule.get(b[0]).get(b[1]).start);
+        for (int i = 0; i < schedule.size(); i++) {
+            pq.add(new int[] {i, 0});
+        }
+        List<Interval> res = new ArrayList<>();
+        int prev = schedule.get(pq.peek()[0]).get(pq.peek()[1]).start;
+        while (!pq.isEmpty()) {
+            int[] index = pq.poll();
+            Interval interval = schedule.get(index[0]).get(index[1]);
+            if (interval.start > prev) {
+                res.add(new Interval(prev, interval.start));
+            }
+            prev = Math.max(prev, interval.end);
+            if (schedule.get(index[0]).size() > index[1] + 1) {
+                pq.add(new int[] {index[0], index[1] + 1});
             }
         }
-        return result;
+        return res;
     }
 }
