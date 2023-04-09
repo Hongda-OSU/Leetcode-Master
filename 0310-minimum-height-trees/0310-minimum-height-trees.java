d765a1aac8ab7578abbe1c915cf7918e15ba8152
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-09 16:49:36 -0400

    Time: 25 ms (77.89%), Space: 52.6 MB (77.02%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,50 +1,46 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-    List<Integer>[] edgeList = new List[n];
-    for (int i = 0; i < n; i++){
-        edgeList[i] = new ArrayList<>();
-    }
-    for(int[] edge : edges){
-        edgeList[edge[0]].add(edge[1]);
-        edgeList[edge[1]].add(edge[0]);
-    }
-    
-    int[] prev = new int[n];
-    Arrays.fill(prev, -1);
-    int p0 = bfs(edgeList, 0, prev);
-
-    prev = new int[n];
-    Arrays.fill(prev, -1);
-    int p1 = bfs(edgeList, p0, prev);
-    
-    List<Integer> path = new ArrayList<>();
-    int i = p1;
-    while (i != p0) {
-        path.add(i);
-        i = prev[i];
-    }
-    path.add(p0);
-
-    List<Integer> res = new ArrayList<>();
-    res.add(path.get(path.size() / 2));
-    if (path.size() % 2 == 0) res.add(path.get(path.size() / 2 - 1));
-    return res;
-}
-
-private int bfs(List<Integer>[] edgeList, int start, int[] prev) {
-    Queue<Integer> q = new LinkedList<>();
-    q.add(start);
-    prev[start] = start;
-    int ret = start;
-    while (!q.isEmpty()) {
-        ret = q.poll();
-        for (int i : edgeList[ret]) {
-            if (prev[i] == -1) {
-                q.add(i);
-                prev[i] = ret;
-            } 
+        List<Integer>[] edgeList = new ArrayList[n];
+        for (int i = 0; i < n; i++)
+            edgeList[i] = new ArrayList<>();
+        for (int[] edge : edges) {
+            edgeList[edge[0]].add(edge[1]);
+            edgeList[edge[1]].add(edge[0]);
         }
+        int[] prev = new int[n];
+        Arrays.fill(prev, -1);
+        int p1 = bfs(edgeList, 0, prev);
+        prev = new int[n];
+        Arrays.fill(prev, -1);
+        int p2 = bfs(edgeList, p1, prev);
+        List<Integer> path = new ArrayList<>();
+        int i = p2;
+        while (i != p1) {
+            path.add(i);
+            i = prev[i];
+        }
+        path.add(p1);
+        List<Integer> result = new ArrayList<>();
+        result.add(path.get(path.size() / 2));
+        if (path.size() % 2 == 0)
+            result.add(path.get(path.size() / 2 - 1));
+        return result;
     }
-    return ret;
-}
+    
+    public int bfs(List<Integer>[] edgeList, int start, int[] prev) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        prev[start] = start;
+        int result = start;
+        while (!queue.isEmpty()) {
+            result = queue.poll();
+            for (int edge : edgeList[result]) {
+                if (prev[edge] == -1) {
+                    queue.add(edge);
+                    prev[edge] = result;
+                }
+            }
+        }
+        return result;
+    } 
 }
