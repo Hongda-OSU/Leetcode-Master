e99721f24c96db6d6a15c9b36f5c2c2b70ed2318
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-10 17:10:48 -0400

    Time: 11 ms (25.51%), Space: 41.4 MB (88.07%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
@@ -1,25 +1,35 @@
-class Solution {
-  public int findPaths(int m, int n, int N, int x, int y) {
-    int M = 1000000000 + 7;
-    int dp[][] = new int[m][n];
-    dp[x][y] = 1;
-    int count = 0;
-    for (int moves = 1; moves <= N; moves++) {
-      int[][] temp = new int[m][n];
-      for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-          if (i == m - 1) count = (count + dp[i][j]) % M;
-          if (j == n - 1) count = (count + dp[i][j]) % M;
-          if (i == 0) count = (count + dp[i][j]) % M;
-          if (j == 0) count = (count + dp[i][j]) % M;
-          temp[i][j] = (
-              ((i > 0 ? dp[i - 1][j] : 0) + (i < m - 1 ? dp[i + 1][j] : 0)) % M +
-              ((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % M
-          ) % M;
+
+
+public class Solution {
+    public int findPaths(int m, int n, int N, int i, int j) {
+        if (N <= 0) return 0;
+        
+        final int MOD = 1000000007;
+        int[][] count = new int[m][n];
+        count[i][j] = 1;
+        int result = 0;
+        
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        for (int step = 0; step < N; step++) {
+            int[][] temp = new int[m][n];
+            for (int r = 0; r < m; r++) {
+                for (int c = 0; c < n; c++) {
+                    for (int[] d : dirs) {
+                        int nr = r + d[0];
+                        int nc = c + d[1];
+                        if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                            result = (result + count[r][c]) % MOD;
+                        }
+                        else {
+                            temp[nr][nc] = (temp[nr][nc] + count[r][c]) % MOD;
+                        }
+                    }
+                }
+            }
+            count = temp;
         }
-      }
-      dp = temp;
+        
+        return result;
     }
-    return count;
-  }
 }
