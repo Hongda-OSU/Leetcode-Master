12bb9c537dd77e6016cf20a923eef9e84d210cce
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-10 18:00:02 -0400

    Time: 7 ms (93.98%), Space: 58.8 MB (46.60%) - LeetHub

--- 0361-bomb-enemy/0361-bomb-enemy.java
+++ 0361-bomb-enemy/0361-bomb-enemy.java
@@ -1,24 +1,22 @@
-public class Solution {
+class Solution {
     public int maxKilledEnemies(char[][] grid) {
-        int m = grid.length;
-        int n = m != 0 ? grid[0].length : 0;
-        int result = 0;
-        int rowhits = 0;
-        int[] colhits = new int[n];
+        int m = grid.length, n = m != 0 ? grid[0].length : 0;
+        int result = 0, rowHits = 0;
+        int[] colHits = new int[n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (j == 0 || grid[i][j - 1] == 'W') {
-                    rowhits = 0;
-                    for (int k = j; k < n && grid[i][k] != 'W'; k++)
-                        rowhits += grid[i][k] == 'E' ? 1 : 0;
+                    rowHits = 0;
+                    for (int k = j; k < n && grid[i][k] != 'W'; k++) 
+                        rowHits += grid[i][k] == 'E' ? 1 : 0;
                 }
                 if (i == 0 || grid[i - 1][j] == 'W') {
-                    colhits[j] = 0;
+                    colHits[j] = 0;
                     for (int k = i; k < m && grid[k][j] != 'W'; k++)
-                        colhits[j] += grid[k][j] == 'E' ? 1 : 0;
+                        colHits[j] += grid[k][j] == 'E' ? 1 : 0;
                 }
                 if (grid[i][j] == '0')
-                    result = Math.max(result, rowhits + colhits[j]);
+                    result = Math.max(result, rowHits + colHits[j]);
             }
         }
         return result;
