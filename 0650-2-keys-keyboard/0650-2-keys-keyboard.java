eabe66b43c9e0ada212f91b58c078a236f12f960
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-10 17:48:43 -0400

    Time: 44 ms (18.04%), Space: 39.8 MB (38.36%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,17 +1,16 @@
 class Solution {
-   public int minSteps(int n) {
-        int[] dp = new int[n+1];
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = i;
-            for (int j = i-1; j > 1; j--) {
-                if (i % j == 0) {
-                    dp[i] = dp[j] + (i/j);
-                    break;
-                }
-                
-            }
+   public static int minSteps(int n) {
+    int[] dp = new int[n + 1];
+		
+    for (int k = 2; k <= n; k++) {
+        dp[k] = Integer.MAX_VALUE;
+				
+        for (int i = 1; i < k; i++) {
+            if (k % i != 0) continue;
+            dp[k] = Math.min(dp[k], dp[i] + k / i);
         }
-        return dp[n];
     }
+        
+    return dp[n];
+}
 }
