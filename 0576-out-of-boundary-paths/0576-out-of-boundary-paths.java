34658e0fd3310790da5df6a6ca53f03a524a5eb7
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-10 17:13:12 -0400

    Time: 4 ms (100.00%), Space: 41.7 MB (68.31%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
@@ -1,25 +1,20 @@
 class Solution {
+    Integer[][][] memo;
+    int m, n;
+    int[] DIR = new int[]{0, 1, 0, -1, 0};
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memo = new Integer[m][n][maxMove+1];
+        this.m = m; this.n = n;
+        return dp(startRow, startColumn, maxMove);
+    }
+    int dp(int r, int c, int maxMove) {
+        if (r < 0 || r == m || c < 0 || c == n) return 1; // Out of bound -> Count 1 way
+        if (memo[r][c][maxMove] != null) return memo[r][c][maxMove];
         if (maxMove == 0) return 0;
-        int[][] dpCurr = new int[m+2][n+2], dpLast = new int[m+2][n+2];
-        for (int i = 1; i <= m; i++) {
-            dpCurr[i][1]++;
-            dpCurr[i][n]++;
+        int ans = 0;
+        for (int i = 0; i < 4; ++i) {
+            ans = (ans + dp(r + DIR[i], c + DIR[i + 1], maxMove - 1)) % 1_000_000_007;
         }
-        for (int j = 1; j <= n; j++) {
-            dpCurr[1][j]++;
-            dpCurr[m][j]++;
-        }
-        int ans = dpCurr[startRow+1][startColumn+1];
-        for (int d = 1; d < maxMove; d++) {
-            int[][] temp = dpCurr;
-            dpCurr = dpLast;
-            dpLast = temp;
-            for (int i = 1; i <= m; i++)
-                for (int j = 1; j <= n; j++)
-                    dpCurr[i][j] = (int)(((long)dpLast[i-1][j] + dpLast[i+1][j] + dpLast[i][j-1] + dpLast[i][j+1]) % 1000000007L);
-            ans = (ans + dpCurr[startRow+1][startColumn+1]) % 1000000007;
-        }
-        return ans;
+        return memo[r][c][maxMove] = ans;
     }
 }
