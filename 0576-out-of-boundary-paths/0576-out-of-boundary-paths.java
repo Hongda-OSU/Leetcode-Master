1bb2d14e44c1e5762c0a597b8643eb124636c4a1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-10 17:11:57 -0400

    Time: 7 ms (38.68%), Space: 39.4 MB (100.00%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
@@ -1,35 +1,25 @@
-class Solution 
-{
-    int mod=(int)1e9+7;
-    int[][] dir={{ 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 }};
-    public int findPaths(int n, int m, int k, int r, int c) 
-    {
-        int[][][]dpp=new int[n+1][m+1][k+1];
-        for (int[][] dp : dpp)
-            for (int[] d : dp)
-                Arrays.fill(d, -1);
-        
-         return findPaths(n, m, k, r, c, dpp);
-    }
-    public int findPaths(int n, int m, int K, int sr, int sc,int[][][]dp)
-    {
-        if(sr<0||sc<0|| sr == n || sc == m)
-            return 1;
-        if (K == 0)
-            return 0;
-
-        if (dp[sr][sc][K] != -1)
-            return dp[sr][sc][K];
-        int count = 0;
-
-        for (int[] d : dir) {
-            int r = sr + d[0];
-            int c = sc + d[1];
-
-            count = (count + findPaths(n, m, K - 1, r, c, dp)) % mod;
-
+class Solution {
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        if (maxMove == 0) return 0;
+        int[][] dpCurr = new int[m+2][n+2], dpLast = new int[m+2][n+2];
+        for (int i = 1; i <= m; i++) {
+            dpCurr[i][1]++;
+            dpCurr[i][n]++;
         }
-
-        return dp[sr][sc][K] = count;
+        for (int j = 1; j <= n; j++) {
+            dpCurr[1][j]++;
+            dpCurr[m][j]++;
+        }
+        int ans = dpCurr[startRow+1][startColumn+1];
+        for (int d = 1; d < maxMove; d++) {
+            int[][] temp = dpCurr;
+            dpCurr = dpLast;
+            dpLast = temp;
+            for (int i = 1; i <= m; i++)
+                for (int j = 1; j <= n; j++)
+                    dpCurr[i][j] = (int)(((long)dpLast[i-1][j] + dpLast[i+1][j] + dpLast[i][j-1] + dpLast[i][j+1]) % 1000000007L);
+            ans = (ans + dpCurr[startRow+1][startColumn+1]) % 1000000007;
+        }
+        return ans;
     }
 }
