33039cc333e277cb2587b1912ccf5ab6c61004ad
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-10 16:56:13 -0400

    Time: 3 ms (94.49%), Space: 41.1 MB (77.05%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.java
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -1,44 +1,56 @@
 class Solution {
+    
+    private Map<Character, List<Character>> reverseAdjList = new HashMap<>();
+    private Map<Character, Boolean> seen = new HashMap<>();
+    private StringBuilder output = new StringBuilder();
+    
     public String alienOrder(String[] words) {
-        // using bfs
-        Map<Character, List<Character>> adjList = new HashMap<>();
-        Map<Character, Integer> counts = new HashMap<>();
+        
+        // Step 0: Put all unique letters into reverseAdjList as keys.
         for (String word : words) {
-            for (char ch : word.toCharArray()) {
-                counts.put(ch, 0);
-                adjList.put(ch, new ArrayList<>());
+            for (char c : word.toCharArray()) {
+                reverseAdjList.putIfAbsent(c, new ArrayList<>());
             }
         }
+        
+        // Step 1: Find all edges and add reverse edges to reverseAdjList.
         for (int i = 0; i < words.length - 1; i++) {
             String word1 = words[i];
             String word2 = words[i + 1];
-            if (word1.length() > word2.length() && word1.startsWith(word2))
+            // Check that word2 is not a prefix of word1.
+            if (word1.length() > word2.length() && word1.startsWith(word2)) {
                 return "";
+            }
+            // Find the first non match and insert the corresponding relation.
             for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
                 if (word1.charAt(j) != word2.charAt(j)) {
-                    adjList.get(word1.charAt(j)).add(word2.charAt(j));
-                    counts.put(word2.charAt(j), counts.get(word2.charAt(j)) + 1);
+                    reverseAdjList.get(word2.charAt(j)).add(word1.charAt(j));
                     break;
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
-        Queue<Character> queue = new LinkedList<>();
-        for (Character ch : counts.keySet()) {
-            if (counts.get(ch).equals(0))
-                queue.add(ch);
+        
+        // Step 2: DFS to build up the output list.
+        for (Character c : reverseAdjList.keySet()) {
+            boolean result = dfs(c);
+            if (!result) return "";
         }
-        while (!queue.isEmpty()) {
-            Character ch = queue.remove();
-            sb.append(ch);
-            for (Character next : adjList.get(ch)) {
-                counts.put(next, counts.get(next) - 1);
-                if (counts.get(next).equals(0))
-                    queue.add(next);
-            }
-        }
-        if (sb.length() < counts.size())
-            return "";
-        return sb.toString();
+        
+        return output.toString();
     }
+    
+    // Return true iff no cycles detected.
+    private boolean dfs(Character c) {
+        if (seen.containsKey(c)) {
+            return seen.get(c); // If this node was grey (false), a cycle was detected.
+        }
+        seen.put(c, false);
+        for (Character next : reverseAdjList.get(c)) {
+            boolean result = dfs(next);
+            if (!result) return false;
+        }
+        seen.put(c, true);
+        output.append(c);
+        return true;
+    }    
 }
