5ea6a32b88a4cad57619f8e3c618d66292efd989
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-10 16:54:28 -0400

    Time: 6 ms (94.75%), Space: 43.3 MB (91.08%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.java
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -1,44 +1,33 @@
 class Solution {
-    public int minimumSemesters(int N, int[][] relations) {
-        List<List<Integer>> graph = new ArrayList<>(N + 1);
-        for (int i = 0; i < N + 1; ++i) {
-            graph.add(new ArrayList<Integer>());
-        }
-        for (int[] relation : relations) {
+    public int minimumSemesters(int n, int[][] relations) {
+        List<List<Integer>> graph = new ArrayList<>(n + 1);
+        for (int i = 0; i < n + 1; i++) 
+            graph.add(new ArrayList<>());
+        for (int[] relation : relations)
             graph.get(relation[0]).add(relation[1]);
-        }
-        int[] visited = new int[N + 1];
-
+        int[] visited = new int[n + 1];
         int maxLength = 1;
-        for (int node = 1; node < N + 1; node++) {
-            int length = dfs(node, graph, visited);
-            // we meet a cycle!
-            if (length == -1) {
+        for (int i = 0; i < n + 1; i++) {
+            int length = dfs(i, graph, visited);
+            if (length == -1)
                 return -1;
-            }
-            maxLength = Math.max(length, maxLength);
+            maxLength = Math.max(maxLength, length);
         }
         return maxLength;
     }
-
-    private int dfs(int node, List<List<Integer>> graph, int[] visited) {
-        // return the longest path (inclusive)
-        if (visited[node] != 0) {
+    
+    public int dfs(int node,List<List<Integer>> graph, int[] visited) {
+        if (visited[node] != 0)
             return visited[node];
-        } else {
-            // mark as visiting
+        else
             visited[node] = -1;
-        }
         int maxLength = 1;
         for (int endNode : graph.get(node)) {
             int length = dfs(endNode, graph, visited);
-            // we meet a cycle!
-            if (length == -1) {
+            if (length == -1)
                 return -1;
-            }
-            maxLength = Math.max(length + 1, maxLength);
+            maxLength = Math.max(maxLength, length + 1);
         }
-        // mark as visited
         visited[node] = maxLength;
         return maxLength;
     }
