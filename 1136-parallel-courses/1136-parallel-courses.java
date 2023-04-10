c350c19746b18cd47b4b8c4e182507dced804c2b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-10 16:49:52 -0400

    Time: 6 ms (94.75%), Space: 43.4 MB (85.17%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.java
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -7,57 +7,39 @@ class Solution {
         for (int[] relation : relations) {
             graph.get(relation[0]).add(relation[1]);
         }
-        // check if the graph contains a cycle
         int[] visited = new int[N + 1];
-        for (int node = 1; node < N + 1; node++) {
-            // if has cycle, return -1
-            if (dfsCheckCycle(node, graph, visited) == -1) {
-                return -1;
-            }
-        }
 
-        // if no cycle, return the longest path
-        int[] visitedLength = new int[N + 1];
         int maxLength = 1;
         for (int node = 1; node < N + 1; node++) {
-            int length = dfsMaxPath(node, graph, visitedLength);
+            int length = dfs(node, graph, visited);
+            // we meet a cycle!
+            if (length == -1) {
+                return -1;
+            }
             maxLength = Math.max(length, maxLength);
         }
         return maxLength;
     }
 
-    private int dfsCheckCycle(int node, List<List<Integer>> graph, int[] visited) {
-        // return -1 if has a cycle
-        // return 1 if does not have any cycle
+    private int dfs(int node, List<List<Integer>> graph, int[] visited) {
+        // return the longest path (inclusive)
         if (visited[node] != 0) {
             return visited[node];
         } else {
             // mark as visiting
             visited[node] = -1;
         }
-        for (int endNode : graph.get(node)) {
-            if (dfsCheckCycle(endNode, graph, visited) == -1) {
-                // we meet a cycle!
-                return -1;
-            }
-        }
-        // mark as visited
-        visited[node] = 1;
-        return 1;
-    }
-
-    private int dfsMaxPath(int node, List<List<Integer>> graph, int[] visitedLength) {
-        // return the longest path (inclusive)
-        if (visitedLength[node] != 0) {
-            return visitedLength[node];
-        }
         int maxLength = 1;
         for (int endNode : graph.get(node)) {
-            int length = dfsMaxPath(endNode, graph, visitedLength);
+            int length = dfs(endNode, graph, visited);
+            // we meet a cycle!
+            if (length == -1) {
+                return -1;
+            }
             maxLength = Math.max(length + 1, maxLength);
         }
-        // store it
-        visitedLength[node] = maxLength;
+        // mark as visited
+        visited[node] = maxLength;
         return maxLength;
     }
 }
