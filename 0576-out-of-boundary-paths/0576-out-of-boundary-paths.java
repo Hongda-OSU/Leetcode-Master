3c81d575843fd2163f8286ec61f608e5e9d0eee2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-10 17:11:14 -0400

    Time: 4 ms (100.00%), Space: 41.3 MB (88.07%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
@@ -1,35 +1,35 @@
+class Solution 
+{
+    int mod=(int)1e9+7;
+    int[][] dir={{ 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 }};
+    public int findPaths(int n, int m, int k, int r, int c) 
+    {
+        int[][][]dpp=new int[n+1][m+1][k+1];
+        for (int[][] dp : dpp)
+            for (int[] d : dp)
+                Arrays.fill(d, -1);
+        
+         return findPaths(n, m, k, r, c, dpp);
+    }
+    public int findPaths(int n, int m, int K, int sr, int sc,int[][][]dp)
+    {
+        if(sr<0||sc<0|| sr == n || sc == m)
+            return 1;
+        if (K == 0)
+            return 0;
 
+        if (dp[sr][sc][K] != -1)
+            return dp[sr][sc][K];
+        int count = 0;
 
-public class Solution {
-    public int findPaths(int m, int n, int N, int i, int j) {
-        if (N <= 0) return 0;
-        
-        final int MOD = 1000000007;
-        int[][] count = new int[m][n];
-        count[i][j] = 1;
-        int result = 0;
-        
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        for (int step = 0; step < N; step++) {
-            int[][] temp = new int[m][n];
-            for (int r = 0; r < m; r++) {
-                for (int c = 0; c < n; c++) {
-                    for (int[] d : dirs) {
-                        int nr = r + d[0];
-                        int nc = c + d[1];
-                        if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
-                            result = (result + count[r][c]) % MOD;
-                        }
-                        else {
-                            temp[nr][nc] = (temp[nr][nc] + count[r][c]) % MOD;
-                        }
-                    }
-                }
-            }
-            count = temp;
+        for (int[] d : dir) {
+            int r = sr + d[0];
+            int c = sc + d[1];
+
+            count = (count + findPaths(n, m, K - 1, r, c, dp)) % mod;
+
         }
-        
-        return result;
+
+        return dp[sr][sc][K] = count;
     }
 }
