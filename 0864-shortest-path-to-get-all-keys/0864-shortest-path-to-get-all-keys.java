87937bc3cd78a5f18432d47c18f46ca106c6e436
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-11 16:10:22 -0400

    Time: 15 ms (100.00%), Space: 43.2 MB (75.97%) - LeetHub

--- 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
+++ 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
@@ -1,87 +1,76 @@
 class Solution {
-    
-    private int[] xd = new int[]{0, 0, 1, -1};
-    private int[] yd = new int[]{1, -1, 0, 0};
-    
-    private class Node {
-        int x;
-        int y;
-        int ks;
-        public Node(int x, int y, int ks) {
-            this.x = x;
-            this.y = y;
-            this.ks = ks;
-        }
-    }
+    private int[] dx = new int[]{0, 0, 1, -1};
+    private int[] dy = new int[]{1, -1, 0, 0};
     
     public int shortestPathAllKeys(String[] grid) {
-        int m = grid.length;
-        int n = grid[0].length();
-        char[][] chars = new char[m][n];
-        int x = -1, y = -1;
-        int k = 0;
+        int m = grid.length, n = grid[0].length();
+        char[][] arr = new char[m][n];
+        int x = -1, y = -1, k = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                char c = grid[i].charAt(j);
-                chars[i][j] = c;
-                if (c == '@') {
+                char ch = grid[i].charAt(j);
+                arr[i][j] = ch;
+                if (ch == '@') {
                     x = i;
                     y = j;
-                }
-                else if (c >= 'a' && c <= 'f') {
+                } else if (ch >= 'a' && ch <= 'f') {
                     k++;
                 }
             }
         }
-        int keys = 0;
-        for (int i = 0; i < k; i++) {
+        int keys = 0, count = 0;
+        for (int i = 0; i < k; i++)
             keys = addKey(keys, 'a' + i);
-        }
-        Queue<Node> queue = new LinkedList();
+        Queue<Node> queue = new LinkedList<>();
         boolean[][][] visited = new boolean[m][n][keys + 1];
         queue.offer(new Node(x, y, 0));
         visited[x][y][0] = true;
-        int cnt = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 Node curr = queue.poll();
-                if (curr.ks == keys) {
-                    return cnt;
-                }
+                if (curr.keys == keys)
+                    return count;
                 for (int j = 0; j < 4; j++) {
-                    x = curr.x + xd[j];
-                    y = curr.y + yd[j];
-                    if (x < 0 || x >= m || y < 0 || y >= n || chars[x][y] == '#') {
+                    x = curr.x + dx[j];
+                    y = curr.y + dy[j];
+                    if (x < 0 || x >= m || y < 0 || y >= n || arr[x][y] == '#') 
                         continue;
-                    }
-                    char c = chars[x][y];
-                    int ks = addKey(curr.ks, c);
-                    if (visited[x][y][ks]) {
+                    char ch = arr[x][y];
+                    int ks = addKey(curr.keys, ch);
+                    if (visited[x][y][ks])
                         continue;
-                    }
-                    if (c >= 'A' && c <= 'F' && !unlock(curr.ks, c)) {
+                    if (ch >= 'A' && ch <= 'F' && !unlock(curr.keys, ch))
                         continue;
-                    }
                     visited[x][y][ks] = true;
                     queue.offer(new Node(x, y, ks));
                 }
             }
-            cnt++;
+            count++;
         }
         return -1;
     }
     
-    public int addKey(int keys, int c) {
-        if (c >= 'a' && c <= 'f') {
-            int index = c - 'a';
-            return keys | (1 << index);
+    public int addKey(int keys, int ch) {
+        if (ch >= 'a' && ch <= 'f') {
+            int idx = ch - 'a';
+            return keys | (1 << idx);
         }
         return keys;
     }
     
-    public boolean unlock(int keys, int c) {
-        int index = c - 'A';
-        return (keys & (1 << index)) > 0;
+    public boolean unlock(int keys, int ch) {
+        int idx = ch - 'a';
+        return (keys & (1 << idx)) > 0;
+    }
+}
+
+class Node {
+    public int x, y, keys;
+    
+    public Node(int x, int y, int keys) {
+        this.x = x;
+        this.y = y;
+        this.keys = keys;
     }
 }
