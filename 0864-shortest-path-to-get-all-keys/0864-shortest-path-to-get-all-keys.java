556f039169d1e6db65ba2186a581a9cd13467cc2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-11 15:35:18 -0400

    Time: 126 ms (70.07%), Space: 50.2 MB (62.59%) - LeetHub

--- 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
+++ 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
@@ -1,60 +1,63 @@
 class Solution {
+   int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public int shortestPathAllKeys(String[] grid) {
-        int x = -1, y = -1, m = grid.length, n = grid[0].length(), max = -1;
+        int m = grid.length, n = grid[0].length();
+        int si = -1, sj = -1, k = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                char ch = grid[i].charAt(j);
-                if (ch == '@') {
-                    x = i;
-                    y = j;
+                char c = grid[i].charAt(j);
+                if (c == '@') {
+                    si = i;
+                    sj = j;
                 }
-                if (ch >= 'a' && ch <= 'f')
-                    max = Math.max(ch - 'a' + 1, max);
+                if (isKey(c)) k++;
             }
         }
-        State start = new State(x, y, 0);
-        Queue<State> queue = new LinkedList<>();
+        Node start = new Node(si, sj, 0);
+        Queue<Node> q = new LinkedList<>();
+        q.offer(start);
         Set<String> visited = new HashSet<>();
-        visited.add(0 + " " + x + " " + y);
-        queue.offer(start);
-        int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int steps = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size-- > 0) {
-                State curr = queue.poll();
-                if (curr.keys == (1 << max) - 1)
-                    return steps;
-                for (int[] dir : dirs) {
-                    int i = curr.i + dir[0], j = curr.j + dir[1];
-                    int keys = curr.keys;
-                    if (i >= 0 && i < m && j >= 0 && j < n) {
-                        char ch = grid[i].charAt(j);
-                        if (ch == '#')
-                            continue;
-                        if (ch >= 'a' && ch <= 'f')
-                            keys |= 1 << (ch - 'a');
-                        if (ch >= 'A' && ch <= 'F' && ((keys >> (ch - 'A')) & 1) == 0)
-                            continue;
-                        if (!visited.contains(keys + " " + i + " " + j)) {
-                            visited.add(keys + " " + i + " " + j);
-                            queue.offer(new State(i, j, keys));
-                        }
-                    }
+        visited.add(si + " " +  sj + " " + 0);
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                Node cur = q.poll();
+                if (cur.key == (1 << k) - 1) return level;
+                for (int[] d : dirs) {
+                    int x = cur.i + d[0];
+                    int y = cur.j + d[1];
+                    int key = cur.key;
+                    if (!isValid(grid, x, y, m, n)) continue;
+                    char c = grid[x].charAt(y);
+                    if (isKey(c)) key |= (1 << (c - 'a'));
+                    if (isLock(c) && (key >> (c - 'A') & 1) == 0) continue;
+                    if (visited.add(x + " " + y + " " + key)) q.offer(new Node(x, y, key));
                 }
-            } 
-            steps++;
+            }
+            level++;
         }
         return -1;
     }
-}
 
-class State {
-    public int i, j, keys;
-    
-    public State(int i, int j, int keys) {
-        this.i = i;
-        this.j = j;
-        this.keys = keys;
+    private boolean isLock(char c) {
+        return c >= 'A' && c <= 'F';
+    }
+
+    private boolean isKey(char c) {
+        return c >= 'a' && c <= 'f';
+    }
+
+    private boolean isValid(String[] grid, int i, int j, int m, int n) {
+        return i >= 0 && i < m && j >= 0 && j < n && grid[i].charAt(j) != '#';
+    }
+
+    class Node {
+        int i, j, key;
+        public Node(int i, int j, int key) {
+            this.i = i;
+            this.j = j;
+            this.key = key;
+        }
     }
 }
