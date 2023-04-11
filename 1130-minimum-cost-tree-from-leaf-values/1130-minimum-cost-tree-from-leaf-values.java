bb0073cf76fbe3f0f5ca527f388eb096340a5b63
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-11 16:39:54 -0400

    Time: 1 ms (98.00%), Space: 40.1 MB (77.67%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.java
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.java
@@ -1,18 +1,31 @@
 class Solution {
-     public int mctFromLeafValues(int[] A) {
-        int res = 0;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(Integer.MAX_VALUE);
-        for (int a : A) {
-            while (stack.peek() <= a) {
-                int mid = stack.pop();
-                res += mid * Math.min(stack.peek(), a);
-            }
-            stack.push(a);
-        }
-        while (stack.size() > 2) {
-            res += stack.pop() * stack.peek();
-        }
-        return res;
+    public int mctFromLeafValues(int[] arr) {
+          if(arr == null || arr.length < 2){
+              return 0;
+          }
+          
+          int res = 0;
+          Stack<Integer> stack = new Stack<>();          
+          for(int num : arr){
+              
+             // while num is bigger than peek(), pop and calculate
+             while(!stack.isEmpty() && stack.peek() <= num){
+                 int mid = stack.pop();
+                 if(stack.isEmpty()) 
+                     res += mid * num;
+                 else
+                    res += mid * Math.min(stack.peek(), num);
+             }
+             
+             stack.push(num); // if num is smaller, push into stack
+         }
+         
+         // if there are values left in the stack, they sure will be mutiplied anyway
+         // and added to the result. 
+         while(stack.size() > 1){ // > 1 because we have a peek() after pop() below
+             res += stack.pop() * stack.peek();
+         }
+         
+         return res;
     }
 }
