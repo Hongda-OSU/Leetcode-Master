612b12dc84e48f54d98de8777dcfebef3c685c47
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-12 16:17:31 -0400

    Time: 29 ms (89.41%), Space: 44 MB (91.76%) - LeetHub

--- 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.java
+++ 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.java
@@ -1,28 +1,28 @@
 class Solution {
-	private final Map<String, Boolean> map = new HashMap<>();
-	public String longestWord(String[] words) {
-		Set<String> dictionary = new HashSet<>();
-		for(String word : words) {
-			dictionary.add(word);
-		}
-		//sort array by length decending and lexicographically order 
-		Arrays.sort(words, (a,b) -> a.length()!=b.length() ? b.length() - a.length() : a.compareTo(b));
-		for(String s : words) {
-			if(dfs(s, 0, dictionary)) return s;
-		}
-		return "";
-	}
-
-private boolean dfs(String input, int index, Set<String> dictionary) {
-    if(map.containsKey(input)) return map.get(input);
-    if(index==input.length()) {
-        map.put(input, true);
-        return true;
+    private Map<String, Boolean> map;
+    
+    public String longestWord(String[] words) {
+        map = new HashMap<>();
+        Set<String> dictionary = new HashSet<>();
+        for (String word : words) 
+            dictionary.add(word);
+        Arrays.sort(words, (a, b) -> a.length() != b.length() ? b.length() - a.length() : a.compareTo(b));
+        for (String word : words) {
+            if (dfs(word, 0, dictionary))
+                return word;
+        }
+        return "";
     }
-    //move to next index to search longer prefix
-    if(dictionary.contains(input.substring(0,index+1))) {
-        return dfs(input, index+1, dictionary);
-    } 
-    return false;
-}
+    
+    private boolean dfs(String word, int idx, Set<String> dictionary) {
+        if (map.containsKey(word))
+            return map.get(word);
+        if (idx == word.length()) {
+            map.put(word, true);
+            return true;
+        }
+        if (dictionary.contains(word.substring(0, idx + 1))) 
+            return dfs(word, idx + 1, dictionary);
+        return false;
+    }
 }
