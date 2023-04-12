53bce378f04b683cafd7fe0ca104740d6f093066
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-12 15:49:09 -0400

    Time: 60 ms (88.21%), Space: 110.9 MB (92.84%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,31 +1,30 @@
 class LRUCache {
-    LinkedNodeList list;
-    Map<Integer, Node> map;
-    Integer cap;
+    private LinkedNodeList list;
+    private Map<Integer, Node> map;
+    private Integer cap;
+
     public LRUCache(int capacity) {
         list = new LinkedNodeList();
-        map = new HashMap(capacity);
+        map = new HashMap<>();
         cap = capacity;
     }
     
     public int get(int key) {
         Node node = map.get(key);
-        if(node == null){
+        if (node == null)
             return -1;
-        }
         list.moveToHead(node);
         return node.val;
     }
     
     public void put(int key, int value) {
         Node node = map.get(key);
-        if(node != null){
+        if (node != null) {
             list.moveToHead(node);
             node.val = value;
-
-        }else{
-             Node newNode = new Node(key, value);
-            if(map.size() == cap){
+        } else {
+            Node newNode = new Node(key, value);
+            if (map.size() == cap) {
                 Node tail = list.getTail();
                 map.remove(tail.key);
                 list.removeTail();
@@ -36,53 +35,50 @@ class LRUCache {
     }
 }
 
-class LinkedNodeList{
-    Node dummyHead;
-    Node dummyTail;
+class LinkedNodeList {
+    public Node head, tail;
     
-    LinkedNodeList(){
-        dummyHead = new Node(0,0);
-        dummyTail = new Node(0,0);
-        dummyHead.next = dummyTail;
-        dummyTail.prev = dummyHead;
+    public LinkedNodeList() {
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
     }
     
-    void moveToHead(Node node){
+    public void moveToHead(Node node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
         addToHead(node);
     }
     
-    void addToHead(Node node){
-        Node tmp = dummyHead.next ;
-        dummyHead.next = node;
-        node.next = tmp;
-        node.prev = dummyHead;
-        tmp.prev = node;
+    public void addToHead(Node node) {
+        Node temp = head.next;
+        head.next = node;
+        node.next = temp;
+        node.prev = head;
+        temp.prev = node;
     }
     
-    void removeTail(){
-        Node newTail = dummyTail.prev.prev;
-        newTail.next = dummyTail;
-        dummyTail.prev = newTail;
+    public void removeTail() {
+        Node newTail = tail.prev.prev;
+        newTail.next = tail;
+        tail.prev = newTail;
     }
     
-    Node getTail(){
-        return dummyTail.prev;
+    public Node getTail() {
+        return tail.prev;
     }
-    
-}
+} 
 
- class Node{
-        int key;
-        int val;
-        Node next;
-        Node prev;
-        Node(int key, int value){
-            this.key = key;
-            this.val =value;
-        }
-}
+class Node {
+    public int key, val;
+    public Node prev, next;
+    
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+} 
 
 /**
  * Your LRUCache object will be instantiated and called as such:
