b54cdcfcce1e756c763c29973a8e4f489dbe1fd6
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-12 15:28:04 -0400

    Time: 106 ms (45.36%), Space: 55.8 MB (59.29%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,28 +1,73 @@
-class UndergroundSystem {
-    Map<Integer, Pair<String, Integer>> checkInMap = new HashMap<>();  // Uid - {StationName, Time}
-    Map<String, Pair<Double, Integer>> routeMap = new HashMap<>(); // RouteName - {TotalTime, Count}
+class Passenger {
+    int checkinTime;
+    int checkoutTime;
+    String checkinLocation;
+    String checkoutLocation;
 
-    public UndergroundSystem() {}
+    public Passenger(String checkinLocation, int checkinTime) {
+        this.checkinLocation = checkinLocation;
+        this.checkinTime = checkinTime;
+    }
+
+    void checkout(String checkoutLocation, int checkoutTime) {
+        this.checkoutLocation = checkoutLocation;
+        this.checkoutTime = checkoutTime;
+    }
+
+}
+
+class Route {
+    String startStation;
+    String endStation;
+    int totalNumberOfTrips;
+    long totalTimeSpentInTrips;
+
+    public Route(String startStation, String endStation) {
+        this.startStation = startStation;
+        this.endStation = endStation;
+    }
+
+    double getAverageTime() {
+        return (double) totalTimeSpentInTrips / totalNumberOfTrips;
+    }
+
+    void addTrip(int startTime, int endTime) {
+        totalTimeSpentInTrips += endTime - startTime;
+        totalNumberOfTrips++;
+    }
+}
+
+class UndergroundSystem {
+
+    Map<Integer, Passenger> currentPassengerMap;
+    Map<String, Route> routeMap;
+
+    public UndergroundSystem() {
+        currentPassengerMap = new HashMap<>();
+        routeMap = new HashMap<>();
+    }
 
     public void checkIn(int id, String stationName, int t) {
-        checkInMap.put(id, new Pair<>(stationName, t));
+        if (!currentPassengerMap.containsKey(id)) {
+            Passenger passenger = new Passenger(stationName, t);
+            currentPassengerMap.put(id, passenger);
+        }
     }
 
     public void checkOut(int id, String stationName, int t) {
-        Pair<String, Integer> checkIn = checkInMap.get(id);
-        checkInMap.remove(id); // Remove after using it which will not make HashTable big
-
-        String routeName = checkIn.getKey() + "_" + stationName;
-        int totalTime = t - checkIn.getValue();
-
-        Pair<Double, Integer> route = routeMap.getOrDefault(routeName, new Pair<>(0.0, 0));
-        routeMap.put(routeName, new Pair<>(route.getKey() + totalTime, route.getValue() + 1));
+        if (currentPassengerMap.containsKey(id)) {
+            Passenger passenger = currentPassengerMap.get(id);
+            passenger.checkout(stationName, t);
+            String routeKey = passenger.checkinLocation + "," + passenger.checkoutLocation;
+            Route route = routeMap.getOrDefault(routeKey, new Route(passenger.checkinLocation, passenger.checkoutLocation));
+            route.addTrip(passenger.checkinTime, passenger.checkoutTime);
+            routeMap.put(routeKey, route);
+            currentPassengerMap.remove(id);
+        }
     }
 
     public double getAverageTime(String startStation, String endStation) {
-        String routeName = startStation + "_" + endStation;
-        Pair<Double, Integer> trip = routeMap.get(routeName);
-        return trip.getKey() / trip.getValue();
+        return routeMap.get(startStation + "," + endStation).getAverageTime();
     }
 }
 
