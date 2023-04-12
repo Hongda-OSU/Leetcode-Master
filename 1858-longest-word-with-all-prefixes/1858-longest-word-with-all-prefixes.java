46402d4564386baaa0f01fd3d659fa95429b7ca8
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-12 16:13:19 -0400

    Time: 29 ms (89.41%), Space: 44.2 MB (88.24%) - LeetHub

--- 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.java
+++ 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.java
@@ -1,43 +1,28 @@
 class Solution {
-    private TrieNode root;
-    private String result = "";
-    
-    public String longestWord(String[] words) {
-        root = new TrieNode();
-        for (String word : words)
-            addWord(word);
-        for (String word : words)
-            searchPrefix(word);
-        return result;
+	private final Map<String, Boolean> map = new HashMap<>();
+	public String longestWord(String[] words) {
+		Set<String> dictionary = new HashSet<>();
+		for(String word : words) {
+			dictionary.add(word);
+		}
+		//sort array by length decending and lexicographically order 
+		Arrays.sort(words, (a,b) -> a.length()!=b.length() ? b.length() - a.length() : a.compareTo(b));
+		for(String s : words) {
+			if(dfs(s, 0, dictionary)) return s;
+		}
+		return "";
+	}
+
+private boolean dfs(String input, int index, Set<String> dictionary) {
+    if(map.containsKey(input)) return map.get(input);
+    if(index==input.length()) {
+        map.put(input, true);
+        return true;
+    }
+    //move to next index to search longer prefix
+    if(dictionary.contains(input.substring(0,index+1))) {
+        return dfs(input, index+1, dictionary);
     } 
-    
-    private void addWord(String word) {
-       TrieNode curr = root;
-        for (char ch : word.toCharArray()) {
-            if (curr.children[ch] == null)
-                curr.children[ch] = new TrieNode();
-            curr = curr.children[ch];
-        }
-        curr.isWord = true;
-    }
-    
-    private void searchPrefix(String word) {
-        TrieNode curr = root;
-        for (char ch : word.toCharArray()) {
-            curr = curr.children[ch];
-            if (!curr.isWord)
-                return;
-        }
-        if (result.length() < word.length() || result.length() == word.length() && result.compareTo(word) > 0)
-            result = word;
-    }
+    return false;
 }
- 
-class TrieNode {
-    public TrieNode[] children;
-    public boolean isWord;
-    
-    public TrieNode() {
-        this.children = new TrieNode[128];
-    }
 }
