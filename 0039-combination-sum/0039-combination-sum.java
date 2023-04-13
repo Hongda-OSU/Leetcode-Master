649ff71aabd16b6d788be0a9f2683551ec964f75
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 16:52:15 -0400

    Time: 6 ms (11.15%), Space: 43.5 MB (10.37%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,21 +1,24 @@
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        Stack<Integer> comb = new Stack<>();
-        find(candidates, target, comb, 0, result);
-        return result;
-    }
-    private void find(int[] candidates, int target, Stack<Integer> comb, int k, List<List<Integer>> result) {
-        for (int i = k; i < candidates.length; i++) {
-            if (candidates[i] == target) {
-                comb.push(candidates[i]);
-                result.add(new ArrayList<>(comb));
-                comb.pop();
-            } else if (candidates[i] < target) {
-                comb.push(candidates[i]);
-                find(candidates, target - candidates[i], comb, i, result);
-                comb.pop();
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] cands, int t) {
+        Arrays.sort(cands); // sort candidates to try them in asc order
+        List<List<List<Integer>>> dp = new ArrayList<>();
+        for (int i = 1; i <= t; i++) { // run through all targets from 1 to t
+            List<List<Integer>> newList = new ArrayList(); // combs for curr i
+            // run through all candidates <= i
+            for (int j = 0; j < cands.length && cands[j] <= i; j++) {
+                // special case when curr target is equal to curr candidate
+                if (i == cands[j]) newList.add(Arrays.asList(cands[j]));
+                // if current candidate is less than the target use prev results
+                else for (List<Integer> l : dp.get(i-cands[j]-1)) {
+                    if (cands[j] <= l.get(0)) {
+                        List cl = new ArrayList<>();
+                        cl.add(cands[j]); cl.addAll(l);
+                        newList.add(cl);
+                    }
+                }
             }
+            dp.add(newList);
         }
+        return dp.get(t-1);
     }
 }
