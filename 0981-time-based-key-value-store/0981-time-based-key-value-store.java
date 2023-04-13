da0648513aedafd26d21f8c34d45843a29ec5619
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 15:15:17 -0400

    Time: 155 ms (84.99%), Space: 116.4 MB (76.13%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,27 +1,48 @@
 class TimeMap {
+    private Map<String, List<Node>> map;
 
-    Map<String, TreeMap<Integer, String>> map;
-    /** Initialize your data structure here. */
     public TimeMap() {
-        map = new HashMap();
+        map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        map.putIfAbsent(key, new TreeMap());
-        map.get(key).put(timestamp, value);
+        map.putIfAbsent(key, new ArrayList<>());
+        map.get(key).add(new Node(value, timestamp));
     }
     
     public String get(String key, int timestamp) {
-        TreeMap<Integer, String> treeMap = map.get(key);
-        
-        if (treeMap == null) return "";
-        
-        Integer floorKey = treeMap.floorKey(timestamp);
-        
-        if (floorKey == null) return "";
-        return treeMap.get(floorKey);
+        List<Node> nodes = map.get(key);
+        if (nodes == null)
+            return "";
+        int left = 0, right = nodes.size() - 1;
+        while (right - left > 1) {
+            int mid = left + (right - left) / 2;
+            Node node = nodes.get(mid);
+            if (node.timestamp == timestamp) 
+                return node.value;
+            else if (node.timestamp < timestamp) 
+                left = mid;
+            else 
+                right = mid;
+        }
+        if (nodes.get(right).timestamp <= timestamp)
+            return nodes.get(right).value;
+        else if (nodes.get(left).timestamp <= timestamp)
+            return nodes.get(left).value;
+        return "";
     }
 }
+
+class Node {
+    public String value;
+    public int timestamp;
+    
+    public Node(String value, int timestamp) {
+        this.value = value;
+        this.timestamp = timestamp;
+    }
+}
+
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap obj = new TimeMap();
