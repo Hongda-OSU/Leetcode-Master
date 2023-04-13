db57e10fd1415e3240e8761459d315146b17b159
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 17:02:23 -0400

    Time: 45 ms (47.03%), Space: 42.9 MB (20.33%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,26 +1,32 @@
-public class Solution {
-/**
- * s[i] denotes the least number of square numbers that add up to n
- * initial s[i] as maximum integer
- * for i from 1 to n, 
- *      if i is perfect square, s[i]=1, 
- *      otherwise get the square root of the maximum perfect square smaller than i
- * for j from 1 to square root, 
- *      if(s[i-j*j]+1<s[i]) update s[i] as s[i-j*j]+1
- * 
- * */
-public int numSquares(int n) {
-    int[] s = new int[n+1];
-    for(int i=0;i<n+1;i++) s[i] = Integer.MAX_VALUE;
-    //note to me: no need to store a list of perfect squares, knowing the square root of the largest perfect square is sufficient
-    //List<Integer> squares = new ArrayList<Integer>();
-    for(int i = 1;i<n+1;i++){
-        int sqrt = (int) Math.sqrt(i);
-        if(i == sqrt*sqrt){s[i] = 1;continue;}
-        for(int j = 1;j<=sqrt;j++){
-            if(s[i-j*j]+1<s[i]) s[i] = s[i-j*j]+1;
-        }
+class Solution {
+  public int numSquares(int n) {
+
+    ArrayList<Integer> square_nums = new ArrayList<Integer>();
+    for (int i = 1; i * i <= n; ++i) {
+      square_nums.add(i * i);
     }
-    return s[n];
-}
+
+    Set<Integer> queue = new HashSet<Integer>();
+    queue.add(n);
+
+    int level = 0;
+    while (queue.size() > 0) {
+      level += 1;
+      Set<Integer> next_queue = new HashSet<Integer>();
+
+      for (Integer remainder : queue) {
+        for (Integer square : square_nums) {
+          if (remainder.equals(square)) {
+            return level;
+          } else if (remainder < square) {
+            break;
+          } else {
+            next_queue.add(remainder - square);
+          }
+        }
+      }
+      queue = next_queue;
+    }
+    return level;
+  }
 }
