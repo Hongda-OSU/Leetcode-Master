180c172dce334c190cf5704a25bd2abb9a03de20
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 17:02:15 -0400

    Time: 35 ms (76.28%), Space: 41.9 MB (67.15%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,34 +1,26 @@
 public class Solution {
+/**
+ * s[i] denotes the least number of square numbers that add up to n
+ * initial s[i] as maximum integer
+ * for i from 1 to n, 
+ *      if i is perfect square, s[i]=1, 
+ *      otherwise get the square root of the maximum perfect square smaller than i
+ * for j from 1 to square root, 
+ *      if(s[i-j*j]+1<s[i]) update s[i] as s[i-j*j]+1
+ * 
+ * */
 public int numSquares(int n) {
-    int[] dp = new int[n + 1];
-    for (int i = 1; i <= n; i++) {
-        dp[i] = Integer.MAX_VALUE;
-    }
-    
-    for (int i = 1; i <= n; i++) {
-        int sqrt = (int)Math.sqrt(i);
-        
-        // If the number is already a perfect square,
-        // then dp[number] can be 1 directly. This is
-        // just a optimization for this DP solution.
-        if (sqrt * sqrt == i) {
-            dp[i] = 1;
-            continue;                
-        }
-        
-        // To get the value of dp[n], we should choose the min
-        // value from:
-        //     dp[n - 1] + 1,
-        //     dp[n - 4] + 1,
-        //     dp[n - 9] + 1,
-        //     dp[n - 16] + 1
-        //     and so on...
-        for (int j = 1; j <= sqrt; j++) {
-            int dif = i - j * j;
-            dp[i] = Math.min(dp[i], (dp[dif] + 1));
+    int[] s = new int[n+1];
+    for(int i=0;i<n+1;i++) s[i] = Integer.MAX_VALUE;
+    //note to me: no need to store a list of perfect squares, knowing the square root of the largest perfect square is sufficient
+    //List<Integer> squares = new ArrayList<Integer>();
+    for(int i = 1;i<n+1;i++){
+        int sqrt = (int) Math.sqrt(i);
+        if(i == sqrt*sqrt){s[i] = 1;continue;}
+        for(int j = 1;j<=sqrt;j++){
+            if(s[i-j*j]+1<s[i]) s[i] = s[i-j*j]+1;
         }
     }
-    
-    return dp[n];
+    return s[n];
 }
 }
