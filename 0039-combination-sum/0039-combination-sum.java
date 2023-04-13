139ad1b0e08aafd7fdef7874d3206787789a9ec3
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 16:54:16 -0400

    Time: 1 ms (100.00%), Space: 43.1 MB (24.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,21 +1,44 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates); // sort to terminate early when target < 0
-        backtracking(0, target, candidates, new LinkedList<>());
-        return ans;
+    
+    List<List<Integer>> resultList = new ArrayList<>();
+    
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+            
+        getTargetCombination(arr, 0, target, new ArrayList<Integer>());
+        return resultList;
     }
-    void backtracking(int i, int target, int[] candidates, LinkedList<Integer> path) {
-        if (target == 0) {
-            ans.add((List<Integer>) path.clone());
+    
+    
+    public void getTargetCombination(int[] arr, int position, int currentTarget, List<Integer> result) {
+
+        /**
+         * Base case
+         * 1. If currentTarget is reaching to Zero
+         * 2. Current Position is equal to the length of the Array
+         */
+        if (currentTarget == 0) {
+            resultList.add(new ArrayList<>(result));
             return;
         }
-        if (i == candidates.length || target < candidates[i]) return;
-        
-        path.addLast(candidates[i]);
-        backtracking(i, target - candidates[i], candidates, path); // Choose ith candidate
-        path.removeLast();
-        
-        backtracking(i + 1, target, candidates, path); // Skip ith candidate
+        if (position == arr.length) {
+            return;
+        }
+
+        /**
+         * There are two cases
+         * 1. Pick the current value if the current value (i.e arr[position]) is less than or equal to the currentTarget
+         *    value then use the same attribute by passing the same position
+         *
+         *  2. Not picking up the current element by not reducing the currentTarget value and increasing the position
+         */
+        if (arr[position] <= currentTarget) {
+            result.add(arr[position]);
+            getTargetCombination(arr, position, currentTarget - arr[position], result);
+            // removing the last element because post adding of the value the call came back
+            result.remove(result.size() - 1);
+        }
+        // not picked
+        getTargetCombination(arr, position + 1, currentTarget, result);
     }
+    
 }
