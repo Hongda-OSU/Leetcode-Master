d82401acf0b77c5e7ab978d066f2b68ebbf059de
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 17:00:08 -0400

    Time: 44 ms (47.95%), Space: 42.5 MB (25.99%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,13 +1,32 @@
 class Solution {
-    public int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j <= Math.sqrt(i); j++) {
-                dp[i] = Math.min(dp[i], (dp[i - (j * j)] + 1));
-            }
-        }
-        return dp[n];
+  public int numSquares(int n) {
+
+    ArrayList<Integer> square_nums = new ArrayList<Integer>();
+    for (int i = 1; i * i <= n; ++i) {
+      square_nums.add(i * i);
     }
+
+    Set<Integer> queue = new HashSet<Integer>();
+    queue.add(n);
+
+    int level = 0;
+    while (queue.size() > 0) {
+      level += 1;
+      Set<Integer> next_queue = new HashSet<Integer>();
+
+      for (Integer remainder : queue) {
+        for (Integer square : square_nums) {
+          if (remainder.equals(square)) {
+            return level;
+          } else if (remainder < square) {
+            break;
+          } else {
+            next_queue.add(remainder - square);
+          }
+        }
+      }
+      queue = next_queue;
+    }
+    return level;
+  }
 }
