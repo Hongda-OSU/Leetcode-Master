d8263876102d58dd39ab6ec9714112eeda3c00a5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 16:58:33 -0400

    Time: 1 ms (100.00%), Space: 42.7 MB (76.33%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,44 +1,23 @@
 class Solution {
+    private List<List<Integer>> result = new ArrayList<>();
     
-    List<List<Integer>> resultList = new ArrayList<>();
-    
-    public List<List<Integer>> combinationSum(int[] arr, int target) {
-            
-        getTargetCombination(arr, 0, target, new ArrayList<Integer>());
-        return resultList;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        getTargetCombination(candidates, 0, target, new ArrayList<>());
+        return result;
     }
     
-    
-    public void getTargetCombination(int[] arr, int position, int currentTarget, List<Integer> result) {
-
-        /**
-         * Base case
-         * 1. If currentTarget is reaching to Zero
-         * 2. Current Position is equal to the length of the Array
-         */
-        if (currentTarget == 0) {
-            resultList.add(new ArrayList<>(result));
+    private void getTargetCombination(int[] candidates, int position, int target, List<Integer> list) {
+        if (target == 0) {
+            result.add(new ArrayList<>(list));
             return;
-        }
-        if (position == arr.length) {
+        } 
+        if (position == candidates.length)
             return;
+        if (candidates[position] <= target) {
+            list.add(candidates[position]);
+            getTargetCombination(candidates, position, target - candidates[position], list);
+            list.remove(list.size() - 1);
         }
-
-        /**
-         * There are two cases
-         * 1. Pick the current value if the current value (i.e arr[position]) is less than or equal to the currentTarget
-         *    value then use the same attribute by passing the same position
-         *
-         *  2. Not picking up the current element by not reducing the currentTarget value and increasing the position
-         */
-        if (arr[position] <= currentTarget) {
-            result.add(arr[position]);
-            getTargetCombination(arr, position, currentTarget - arr[position], result);
-            // removing the last element because post adding of the value the call came back
-            result.remove(result.size() - 1);
-        }
-        // not picked
-        getTargetCombination(arr, position + 1, currentTarget, result);
+        getTargetCombination(candidates, position + 1, target, list);
     }
-    
 }
