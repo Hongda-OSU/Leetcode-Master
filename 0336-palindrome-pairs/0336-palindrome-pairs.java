cf7b19af93f59ec8364ab15937e2003b2ef696ef
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 17:09:29 -0400

    Time: 914 ms (60.80%), Space: 223.1 MB (14.62%) - LeetHub

--- 0336-palindrome-pairs/0336-palindrome-pairs.java
+++ 0336-palindrome-pairs/0336-palindrome-pairs.java
@@ -1,72 +1,72 @@
 class Solution {
-
-    private List<String> allValidPrefixes(String word) {
-        List<String> validPrefixes = new ArrayList<>();
-        for (int i = 0; i < word.length(); i++) {
-            if (isPalindromeBetween(word, i, word.length() - 1)) {
-                validPrefixes.add(word.substring(0, i));
-            }
-        }
-        return validPrefixes;
+    private static class TrieNode {
+    TrieNode[] next;
+    int index;
+    List<Integer> list;
+    	
+    TrieNode() {
+    	next = new TrieNode[26];
+    	index = -1;
+    	list = new ArrayList<>();
     }
+}
+    
+public List<List<Integer>> palindromePairs(String[] words) {
+    List<List<Integer>> res = new ArrayList<>();
 
-    private List<String> allValidSuffixes(String word) {
-        List<String> validSuffixes = new ArrayList<>();
-        for (int i = 0; i < word.length(); i++) {
-            if (isPalindromeBetween(word, 0, i)) {
-                validSuffixes.add(word.substring(i + 1, word.length()));
-            }
-        }
-        return validSuffixes;
+    TrieNode root = new TrieNode();
+		
+    for (int i = 0; i < words.length; i++) {
+        addWord(root, words[i], i);
     }
-
-    // Is the prefix ending at i a palindrome?
-    private boolean isPalindromeBetween(String word, int front, int back) {
-        while (front < back) {
-            if (word.charAt(front) != word.charAt(back)) return false;
-            front++;
-            back--;
-        }
-        return true;
+		
+    for (int i = 0; i < words.length; i++) {
+        search(words, i, root, res);
     }
-
-    public List<List<Integer>> palindromePairs(String[] words) {
-        // Build a word -> original index mapping for efficient lookup.
-        Map<String, Integer> wordSet = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-            wordSet.put(words[i], i);
+    
+    return res;
+}
+    
+private void addWord(TrieNode root, String word, int index) {
+    for (int i = word.length() - 1; i >= 0; i--) {
+        int j = word.charAt(i) - 'a';
+				
+        if (root.next[j] == null) {
+            root.next[j] = new TrieNode();
         }
-
-        // Make a list to put all the palindrome pairs we find in.
-        List<List<Integer>> solution = new ArrayList<>();
-
-        for (String word : wordSet.keySet()) {
-
-            int currentWordIndex = wordSet.get(word);
-            String reversedWord = new StringBuilder(word).reverse().toString();
-
-            // Build solutions of case #1. This word will be word 1.
-            if (wordSet.containsKey(reversedWord)
-              && wordSet.get(reversedWord) != currentWordIndex) {
-                solution.add(Arrays.asList(currentWordIndex, wordSet.get(reversedWord)));
-            }
-
-            // Build solutions of case #2. This word will be word 2.
-            for (String suffix : allValidSuffixes(word)) {
-                String reversedSuffix = new StringBuilder(suffix).reverse().toString();
-                if (wordSet.containsKey(reversedSuffix)) {
-                    solution.add(Arrays.asList(wordSet.get(reversedSuffix), currentWordIndex));
-                }
-            }
-
-            // Build solutions of case #3. This word will be word 1.
-            for (String prefix : allValidPrefixes(word)) {
-                String reversedPrefix = new StringBuilder(prefix).reverse().toString();
-                if (wordSet.containsKey(reversedPrefix)) {
-                    solution.add(Arrays.asList(currentWordIndex, wordSet.get(reversedPrefix)));
-                }
-            }
+				
+        if (isPalindrome(word, 0, i)) {
+            root.list.add(index);
         }
-        return solution;
+				
+        root = root.next[j];
     }
+    	
+    root.list.add(index);
+    root.index = index;
+}
+    
+private void search(String[] words, int i, TrieNode root, List<List<Integer>> res) {
+    for (int j = 0; j < words[i].length(); j++) {	
+    	if (root.index >= 0 && root.index != i && isPalindrome(words[i], j, words[i].length() - 1)) {
+    	    res.add(Arrays.asList(i, root.index));
+    	}
+    		
+    	root = root.next[words[i].charAt(j) - 'a'];
+      	if (root == null) return;
+    }
+    	
+    for (int j : root.list) {
+    	if (i == j) continue;
+    	res.add(Arrays.asList(i, j));
+    }
+}
+    
+private boolean isPalindrome(String word, int i, int j) {
+    while (i < j) {
+    	if (word.charAt(i++) != word.charAt(j--)) return false;
+    }
+    	
+    return true;
+}
 }
