01b9a5f0625db6c0fe12f5e49ffa8ad7467b714d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 17:03:12 -0400

    Time: 34 ms (78.84%), Space: 53.3 MB (5.01%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,15 +1,29 @@
 public class Solution {
     public int numSquares(int n) {
-        int[] memo = new int[n + 1];
-        Arrays.fill(memo, Integer.MAX_VALUE);
-        memo[0] = 0;
-        int i = 0;
-        while (++i * i <= n) {
-            for (int j = i * i; j < memo.length; j++) {
-                memo[j] = Math.min(memo[j], memo[j - (i * i)] + 1);
+
+        ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+        queue.add(n);
+        int depth = 1, m = 1, tmp = 0;
+        
+        while(true){
+            if(m == 0){
+                depth++;
+                m = tmp;
+                tmp = 0;
+            }
+            
+            int cur = queue.remove();
+            m--;
+            
+            int l = (int) Math.sqrt(cur);
+            for(int i=l; i>0; i--){
+                int sq = i*i;
+                int delta = cur - sq;
+                if(delta == 0)
+                    return depth;
+                queue.add(delta);
+                tmp++;
             }
         }
-        return memo[n];
-        
     }
 }
