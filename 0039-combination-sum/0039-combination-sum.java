ba911bce2702128ad76a284dfa621f1b44183295
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 16:51:52 -0400

    Time: 2 ms (82.26%), Space: 43 MB (35.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,21 @@
 class Solution {
-   public List<List<Integer>> combinationSum(int[] candidates, int target) {
-	List<List<Integer>> list = new ArrayList<>();
-	combinationUtil(list, new ArrayList<>(), candidates, 0, target);
-	return list;
-}
-
-public void combinationUtil(List<List<Integer>> list, List<Integer> temp, int[] candidates, int start, int target) {
-	// base case
-	if(target == 0) {
-		list.add(new ArrayList<>(temp));
-		return;
-	}
-
-	// recursion step
-	for(int i = start; i < candidates.length; i++) {
-		if(target < candidates[i]) continue;
-
-		temp.add(candidates[i]);
-		combinationUtil(list, temp, candidates, i, target - candidates[i]);
-		temp.remove(temp.size() - 1);
-	}
-}
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Stack<Integer> comb = new Stack<>();
+        find(candidates, target, comb, 0, result);
+        return result;
+    }
+    private void find(int[] candidates, int target, Stack<Integer> comb, int k, List<List<Integer>> result) {
+        for (int i = k; i < candidates.length; i++) {
+            if (candidates[i] == target) {
+                comb.push(candidates[i]);
+                result.add(new ArrayList<>(comb));
+                comb.pop();
+            } else if (candidates[i] < target) {
+                comb.push(candidates[i]);
+                find(candidates, target - candidates[i], comb, i, result);
+                comb.pop();
+            }
+        }
+    }
 }
