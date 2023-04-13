87ca58f5dbf68be89b3ff00f87b631d03f24a5a0
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 16:52:38 -0400

    Time: 2 ms (82.26%), Space: 43.1 MB (35.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,21 @@
-public class Solution {
-    public List<List<Integer>> combinationSum(int[] cands, int t) {
-        Arrays.sort(cands); // sort candidates to try them in asc order
-        List<List<List<Integer>>> dp = new ArrayList<>();
-        for (int i = 1; i <= t; i++) { // run through all targets from 1 to t
-            List<List<Integer>> newList = new ArrayList(); // combs for curr i
-            // run through all candidates <= i
-            for (int j = 0; j < cands.length && cands[j] <= i; j++) {
-                // special case when curr target is equal to curr candidate
-                if (i == cands[j]) newList.add(Arrays.asList(cands[j]));
-                // if current candidate is less than the target use prev results
-                else for (List<Integer> l : dp.get(i-cands[j]-1)) {
-                    if (cands[j] <= l.get(0)) {
-                        List cl = new ArrayList<>();
-                        cl.add(cands[j]); cl.addAll(l);
-                        newList.add(cl);
-                    }
-                }
-            }
-            dp.add(newList);
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates); // sort to terminate early when target < 0
+        backtracking(0, target, candidates, new LinkedList<>());
+        return ans;
+    }
+    void backtracking(int i, int target, int[] candidates, LinkedList<Integer> path) {
+        if (target == 0) {
+            ans.add((List<Integer>) path.clone());
+            return;
         }
-        return dp.get(t-1);
+        if (i == candidates.length || target < candidates[i]) return;
+        
+        path.addLast(candidates[i]);
+        backtracking(i, target - candidates[i], candidates, path); // Choose ith candidate
+        path.removeLast();
+        
+        backtracking(i + 1, target, candidates, path); // Skip ith candidate
     }
 }
