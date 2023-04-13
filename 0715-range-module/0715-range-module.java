38ffb3906a688e37aeb441f0b1728a8b51317591
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 16:47:29 -0400

    Time: 55 ms (68.74%), Space: 51.1 MB (33.26%) - LeetHub

--- 0715-range-module/0715-range-module.java
+++ 0715-range-module/0715-range-module.java
@@ -1,34 +1,35 @@
 class RangeModule {
-    TreeMap<Integer, Integer> m = new TreeMap<>();
-    public RangeModule() {}
-    
-    public void addRange(int s, int e) { // s: start, e: end
-        // find overlap ranges, calc merged range, clear overlapped ranges, insert merged range
-        var L = m.floorEntry(s); // left possible overlap entry
-        var R = m.floorEntry(e); // right possible overlap entry
+    private TreeMap<Integer, Integer> map;
 
-        if (L != null && L.getValue() >= s) s = L.getKey(); // update overlap start
-        if (R != null && R.getValue() > e) e = R.getValue(); // update overlap end
-
-        m.subMap(s, e).clear(); // clear all overlapped entries
-        m.put(s, e); // save final merged entry
+    public RangeModule() {
+        map = new TreeMap<>();
     }
     
-    public boolean queryRange(int s, int e) {
-        var L = m.floorEntry(s);
-        return L != null && L.getValue() >= e; // if there exist a range: start <+ s, end >= e
+    public void addRange(int left, int right) {
+        Map.Entry<Integer,Integer> leftEntry = map.floorEntry(left), rightEntry = map.floorEntry(right);
+        if (leftEntry != null && leftEntry.getValue() >= left)
+            left = leftEntry.getKey();
+        if (rightEntry != null && rightEntry.getValue() > right)
+            right = rightEntry.getValue();
+        map.subMap(left, right).clear();
+        map.put(left, right);
     }
     
-    public void removeRange(int s, int e) {
-        var L = m.floorEntry(s); // left possible overlap entry
-        var R = m.floorEntry(e); // right possible overlap entry
-
-        if (L != null && L.getValue() > s) m.put(L.getKey(), s); // after removal, if anything left
-        if (R != null && R.getValue() > e) m.put(e, R.getValue()); // after removal, if anything left
-
-        m.subMap(s, e).clear(); // removal
+    public boolean queryRange(int left, int right) {
+        Map.Entry<Integer,Integer> leftEntry = map.floorEntry(left);
+        return leftEntry != null && leftEntry.getValue() >= right;
+    }
+    
+    public void removeRange(int left, int right) {
+        Map.Entry<Integer,Integer> leftEntry = map.floorEntry(left), rightEntry = map.floorEntry(right);
+        if (leftEntry != null && leftEntry.getValue() > left)
+            map.put(leftEntry.getKey(), left);
+        if (rightEntry != null && rightEntry.getValue() > right)
+            map.put(right, rightEntry.getValue());
+        map.subMap(left, right).clear();
     }
 }
+
 /**
  * Your RangeModule object will be instantiated and called as such:
  * RangeModule obj = new RangeModule();
