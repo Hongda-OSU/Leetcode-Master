1099600ebdd0d833b238b9fefeeb7c784ecfadb7
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 17:01:24 -0400

    Time: 34 ms (78.84%), Space: 42.5 MB (25.99%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,25 +1,34 @@
-class Solution {
-
-  protected boolean isSquare(int n) {
-    int sq = (int) Math.sqrt(n);
-    return n == sq * sq;
-  }
-
-  public int numSquares(int n) {
-    // four-square and three-square theorems.
-    while (n % 4 == 0)
-      n /= 4;
-    if (n % 8 == 7)
-      return 4;
-
-    if (this.isSquare(n))
-      return 1;
-    // enumeration to check if the number can be decomposed into sum of two squares.
-    for (int i = 1; i * i <= n; ++i) {
-      if (this.isSquare(n - i * i))
-        return 2;
+public class Solution {
+public int numSquares(int n) {
+    int[] dp = new int[n + 1];
+    for (int i = 1; i <= n; i++) {
+        dp[i] = Integer.MAX_VALUE;
     }
-    // bottom case of three-square theorem.
-    return 3;
-  }
+    
+    for (int i = 1; i <= n; i++) {
+        int sqrt = (int)Math.sqrt(i);
+        
+        // If the number is already a perfect square,
+        // then dp[number] can be 1 directly. This is
+        // just a optimization for this DP solution.
+        if (sqrt * sqrt == i) {
+            dp[i] = 1;
+            continue;                
+        }
+        
+        // To get the value of dp[n], we should choose the min
+        // value from:
+        //     dp[n - 1] + 1,
+        //     dp[n - 4] + 1,
+        //     dp[n - 9] + 1,
+        //     dp[n - 16] + 1
+        //     and so on...
+        for (int j = 1; j <= sqrt; j++) {
+            int dif = i - j * j;
+            dp[i] = Math.min(dp[i], (dp[dif] + 1));
+        }
+    }
+    
+    return dp[n];
+}
 }
