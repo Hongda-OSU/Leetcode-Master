47275695b65cb8fc0ce36967b0342b0aaf3f4308
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 17:00:44 -0400

    Time: 1 ms (100.00%), Space: 40.1 MB (92.56%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,32 +1,25 @@
 class Solution {
+
+  protected boolean isSquare(int n) {
+    int sq = (int) Math.sqrt(n);
+    return n == sq * sq;
+  }
+
   public int numSquares(int n) {
+    // four-square and three-square theorems.
+    while (n % 4 == 0)
+      n /= 4;
+    if (n % 8 == 7)
+      return 4;
 
-    ArrayList<Integer> square_nums = new ArrayList<Integer>();
+    if (this.isSquare(n))
+      return 1;
+    // enumeration to check if the number can be decomposed into sum of two squares.
     for (int i = 1; i * i <= n; ++i) {
-      square_nums.add(i * i);
+      if (this.isSquare(n - i * i))
+        return 2;
     }
-
-    Set<Integer> queue = new HashSet<Integer>();
-    queue.add(n);
-
-    int level = 0;
-    while (queue.size() > 0) {
-      level += 1;
-      Set<Integer> next_queue = new HashSet<Integer>();
-
-      for (Integer remainder : queue) {
-        for (Integer square : square_nums) {
-          if (remainder.equals(square)) {
-            return level;
-          } else if (remainder < square) {
-            break;
-          } else {
-            next_queue.add(remainder - square);
-          }
-        }
-      }
-      queue = next_queue;
-    }
-    return level;
+    // bottom case of three-square theorem.
+    return 3;
   }
 }
