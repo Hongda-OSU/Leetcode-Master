cae7431311b8f01a5bb97d5bded79a1f4538bcd4
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 16:50:36 -0400

    Time: 1 ms (100.00%), Space: 43.2 MB (17.13%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,34 +1,27 @@
 class Solution {
-
-    protected void backtrack(
-            int remain,
-            LinkedList<Integer> comb,
-            int start, int[] candidates,
-            List<List<Integer>> results) {
-
-        if (remain == 0) {
-            // make a deep copy of the current combination
-            results.add(new ArrayList<Integer>(comb));
-            return;
-        } else if (remain < 0) {
-            // exceed the scope, stop exploration.
-            return;
-        }
-
-        for (int i = start; i < candidates.length; ++i) {
-            // add the number into the combination
-            comb.add(candidates[i]);
-            this.backtrack(remain - candidates[i], comb, i, candidates, results);
-            // backtrack, remove the number from the combination
-            comb.removeLast();
+    
+     void findAllways(int[] candidates,List<List<Integer>> finalList,ArrayList<Integer> list,int i,int target){
+        if(target == 0)
+            finalList.add(new ArrayList<Integer>(list));
+        else{
+            for(int j=i;j<candidates.length;j++)
+            {
+                if(candidates[j]<=target){
+                list.add(candidates[j]);
+                findAllways(candidates,finalList,list,j,target-candidates[j]);
+                list.remove(list.size()-1);
+                }
+            }
         }
     }
-
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<List<Integer>>();
-        LinkedList<Integer> comb = new LinkedList<Integer>();
-
-        this.backtrack(target, comb, 0, candidates, results);
-        return results;
+    
+        List<List<Integer>> finalList = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        findAllways(candidates,finalList,list,0,target);
+        
+        return finalList;
     }
+    
 }
