975a5d11ffa223f5b0181231ed37e88431e2d23e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 15:06:30 -0400

    Time: 152 ms (89.63%), Space: 116.5 MB (75.34%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,41 +1,44 @@
-class Data {
-    String val;
-    int time;
-    Data(String val, int time) {
-        this.val = val;
-        this.time = time;
-    }
-}
 class TimeMap {
 
+    class Node {
+        String value;
+        int timestamp;
+        Node(String value, int timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+    
+    Map<String, List<Node>> map;
     /** Initialize your data structure here. */
-    Map<String, List<Data>> map;
     public TimeMap() {
-        map = new HashMap<String, List<Data>>();
+        map = new HashMap();
     }
     
     public void set(String key, String value, int timestamp) {
-        if (!map.containsKey(key)) map.put(key, new ArrayList<Data>());
-        map.get(key).add(new Data(value, timestamp));
+        map.putIfAbsent(key, new ArrayList());
+        map.get(key).add(new Node(value, timestamp));
     }
     
     public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) return "";
-        return binarySearch(map.get(key), timestamp);
-    }
-    
-    protected String binarySearch(List<Data> list, int time) {
-        int low = 0, high = list.size() - 1;
-        while (low < high) {
-            int mid = (low + high) >> 1;
-            if (list.get(mid).time == time) return list.get(mid).val;
-            if (list.get(mid).time < time) {
-                if (list.get(mid+1).time > time) return list.get(mid).val;
-                low = mid + 1;
+        List<Node> nodes = map.get(key);
+        if (nodes == null) return "";
+        
+        int left = 0, right = nodes.size() - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            Node node = nodes.get(mid);
+            if (node.timestamp == timestamp) {
+                return node.value;
+            } else if (node.timestamp < timestamp) {
+                left = mid;
+            } else {
+                right = mid;
             }
-            else high = mid -1;
         }
-        return list.get(low).time <= time ? list.get(low).val : "";
+        if (nodes.get(right).timestamp <= timestamp) return nodes.get(right).value;
+        else if (nodes.get(left).timestamp <= timestamp) return nodes.get(left).value;
+        return "";
     }
 }
 /**
