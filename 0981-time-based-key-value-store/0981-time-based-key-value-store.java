61b7c3ac109d6b7b8c8a9b7c02e42aac3d1b54d1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 15:05:56 -0400

    Time: 167 ms (52.41%), Space: 115.8 MB (90.05%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,32 +1,41 @@
+class Data {
+    String val;
+    int time;
+    Data(String val, int time) {
+        this.val = val;
+        this.time = time;
+    }
+}
 class TimeMap {
-    HashMap<String, TreeMap<Integer, String>> keyTimeMap;
-    
+
+    /** Initialize your data structure here. */
+    Map<String, List<Data>> map;
     public TimeMap() {
-        keyTimeMap = new HashMap<String, TreeMap<Integer, String>>();
+        map = new HashMap<String, List<Data>>();
     }
     
     public void set(String key, String value, int timestamp) {
-        if (!keyTimeMap.containsKey(key)) {
-            keyTimeMap.put(key, new TreeMap<Integer, String>());
-        }
-        
-        // Store '(timestamp, value)' pair in 'key' bucket.
-        keyTimeMap.get(key).put(timestamp, value);
+        if (!map.containsKey(key)) map.put(key, new ArrayList<Data>());
+        map.get(key).add(new Data(value, timestamp));
     }
     
     public String get(String key, int timestamp) {
-        // If the 'key' does not exist in map we will return empty string.
-        if (!keyTimeMap.containsKey(key)) {
-            return "";
+        if (!map.containsKey(key)) return "";
+        return binarySearch(map.get(key), timestamp);
+    }
+    
+    protected String binarySearch(List<Data> list, int time) {
+        int low = 0, high = list.size() - 1;
+        while (low < high) {
+            int mid = (low + high) >> 1;
+            if (list.get(mid).time == time) return list.get(mid).val;
+            if (list.get(mid).time < time) {
+                if (list.get(mid+1).time > time) return list.get(mid).val;
+                low = mid + 1;
+            }
+            else high = mid -1;
         }
-        
-        Integer floorKey = keyTimeMap.get(key).floorKey(timestamp);
-        // Return searched time's value, if exists.
-        if (floorKey != null) {
-            return keyTimeMap.get(key).get(floorKey);
-        }
-        
-        return "";
+        return list.get(low).time <= time ? list.get(low).val : "";
     }
 }
 /**
