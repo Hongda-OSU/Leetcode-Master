bff68e498a5afb3dee7ddb9769641ad24cc487bf
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 15:06:36 -0400

    Time: 170 ms (46.23%), Space: 118.1 MB (30.69%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,44 +1,25 @@
 class TimeMap {
 
-    class Node {
-        String value;
-        int timestamp;
-        Node(String value, int timestamp) {
-            this.value = value;
-            this.timestamp = timestamp;
-        }
-    }
-    
-    Map<String, List<Node>> map;
+    Map<String, TreeMap<Integer, String>> map;
     /** Initialize your data structure here. */
     public TimeMap() {
         map = new HashMap();
     }
     
     public void set(String key, String value, int timestamp) {
-        map.putIfAbsent(key, new ArrayList());
-        map.get(key).add(new Node(value, timestamp));
+        map.putIfAbsent(key, new TreeMap());
+        map.get(key).put(timestamp, value);
     }
     
     public String get(String key, int timestamp) {
-        List<Node> nodes = map.get(key);
-        if (nodes == null) return "";
+        TreeMap<Integer, String> treeMap = map.get(key);
         
-        int left = 0, right = nodes.size() - 1;
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            Node node = nodes.get(mid);
-            if (node.timestamp == timestamp) {
-                return node.value;
-            } else if (node.timestamp < timestamp) {
-                left = mid;
-            } else {
-                right = mid;
-            }
-        }
-        if (nodes.get(right).timestamp <= timestamp) return nodes.get(right).value;
-        else if (nodes.get(left).timestamp <= timestamp) return nodes.get(left).value;
-        return "";
+        if (treeMap == null) return "";
+        
+        Integer floorKey = treeMap.floorKey(timestamp);
+        
+        if (floorKey == null) return "";
+        return treeMap.get(floorKey);
     }
 }
 /**
