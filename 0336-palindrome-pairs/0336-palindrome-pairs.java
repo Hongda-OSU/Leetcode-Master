639bcffc59a5dbe62253656343b6bf10b9b4e9e7
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-13 17:10:36 -0400

    Time: 151 ms (94.92%), Space: 51.2 MB (90.47%) - LeetHub

--- 0336-palindrome-pairs/0336-palindrome-pairs.java
+++ 0336-palindrome-pairs/0336-palindrome-pairs.java
@@ -1,46 +1,53 @@
 class Solution {
     public List<List<Integer>> palindromePairs(String[] words) {
-    List<List<Integer>> ret = new ArrayList<>(); 
-    if (words == null || words.length < 2) return ret;
-    Map<String, Integer> map = new HashMap<String, Integer>();
-    for (int i=0; i<words.length; i++) map.put(words[i], i);
-    for (int i=0; i<words.length; i++) {
-        // System.out.println(words[i]);
-        for (int j=0; j<=words[i].length(); j++) { // notice it should be "j <= words[i].length()"
-            String str1 = words[i].substring(0, j);
-            String str2 = words[i].substring(j);
-            if (isPalindrome(str1)) {
-                String str2rvs = new StringBuilder(str2).reverse().toString();
-                if (map.containsKey(str2rvs) && map.get(str2rvs) != i) {
-                    List<Integer> list = new ArrayList<Integer>();
-                    list.add(map.get(str2rvs));
-                    list.add(i);
-                    ret.add(list);
-                    // System.out.printf("isPal(str1): %s\n", list.toString());
+        HashMap<String,Integer> wordMap = new HashMap<>();
+        Set<Integer> set = new TreeSet<>();
+        int n = words.length;
+        
+        for(int i=0;i<n;i++){
+            wordMap.put(words[i],i);
+            set.add(words[i].length());
+        }
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            int length = words[i].length();
+            
+            // if(length ==1){
+            //     if(wordMap.containsKey("")){
+            //         ans.add(Arrays.asList(i, wordMap.get("")));
+            //         ans.add(Arrays.asList(wordMap.get(""), i));
+            //     }
+            //     continue;
+            // }
+            String reverse= new StringBuilder(words[i]).reverse().toString();
+            if(wordMap.containsKey(reverse) && wordMap.get(reverse) != i)
+                ans.add(Arrays.asList(i,wordMap.get(reverse)));
+            
+            for(Integer k:set){
+                if(k==length)
+                    break;
+                if(isPalindrome(reverse,0,length-1-k)){
+                    String s1 = reverse.substring(length-k);
+                    if(wordMap.containsKey(s1))
+                        ans.add(Arrays.asList(i,wordMap.get(s1)));
                 }
-            }
-            if (isPalindrome(str2)) {
-                String str1rvs = new StringBuilder(str1).reverse().toString();
-                // check "str.length() != 0" to avoid duplicates
-                if (map.containsKey(str1rvs) && map.get(str1rvs) != i && str2.length()!=0) { 
-                    List<Integer> list = new ArrayList<Integer>();
-                    list.add(i);
-                    list.add(map.get(str1rvs));
-                    ret.add(list);
-                    // System.out.printf("isPal(str2): %s\n", list.toString());
+                
+                if(isPalindrome(reverse,k,length-1)){
+                    String s2 = reverse.substring(0,k);
+                    if(wordMap.containsKey(s2))
+                        ans.add(Arrays.asList(wordMap.get(s2),i));
                 }
             }
         }
+        return ans;
     }
-    return ret;
-}
-
-private boolean isPalindrome(String str) {
-    int left = 0;
-    int right = str.length() - 1;
-    while (left <= right) {
-        if (str.charAt(left++) !=  str.charAt(right--)) return false;
+    
+    private boolean isPalindrome(String s, int left, int right){
+        while(left<right)
+            if(s.charAt(left++)!=s.charAt(right--))
+                return false;
+        return true;
     }
-    return true;
-}
 }
