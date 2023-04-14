8bc71387f5bc62414eae1f59963b08d47632fc29
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-14 14:37:01 -0400

    Time: 2 ms (67.98%), Space: 52.7 MB (56.08%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,20 +1,25 @@
 class Solution {
-    public boolean isMonotonic(int[] arr) {
-        if (arr[arr.length-1] < arr[0])
-            reverse(arr);
-        
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < arr[i-1]) 
-                return false;
+    public boolean isMonotonic(int[] A) {
+        if (A.length == 1) return true;
+        if (A[0] < A[1]) return isIncreasing(A);
+        else if (A[0] > A[1]) return isDecreasing(A);
+        else {
+            if (isIncreasing(A)) return true;
+            return isDecreasing(A);
+        }
+    }
+    
+    public boolean isIncreasing(int[] A){
+        for (int i = 0; i < A.length-1; i++){
+            if (!(A[i] <= A[i+1])) return false;
         }
         return true;
     }
     
-    private void reverse(int[] arr) {
-        for (int i = 0; i < arr.length/2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[arr.length - 1 - i];
-            arr[arr.length - 1 - i] = temp;
+    public boolean isDecreasing(int[] A){
+        for (int i = 0; i < A.length-1; i++){
+            if (!(A[i] >= A[i+1])) return false;
         }
+        return true;
     }
 }
