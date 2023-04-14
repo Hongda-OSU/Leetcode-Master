cf60e9a8b3432fbd0c49ce9083b94ae93cfa703b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-14 14:36:43 -0400

    Time: 4 ms (33.71%), Space: 52.6 MB (77.40%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,10 +1,20 @@
 class Solution {
-   public boolean isMonotonic(int[] A) {
-        boolean inc = true, dec = true;
-        for (int i = 1; i < A.length; ++i) {
-            inc &= A[i - 1] <= A[i];
-            dec &= A[i - 1] >= A[i];
+    public boolean isMonotonic(int[] arr) {
+        if (arr[arr.length-1] < arr[0])
+            reverse(arr);
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i-1]) 
+                return false;
         }
-        return inc || dec;
+        return true;
+    }
+    
+    private void reverse(int[] arr) {
+        for (int i = 0; i < arr.length/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
     }
 }
