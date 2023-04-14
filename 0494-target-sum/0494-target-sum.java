9b24f21f2169647e9e16c2c1b9ba3f1b27d6d1b1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-14 15:09:56 -0400

    Time: 5 ms (79.67%), Space: 43 MB (19.11%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,21 +1,20 @@
 public class Solution {
-    public int findTargetSumWays(int[] nums, int S) {
-        int total = Arrays.stream(nums).sum();
-        int[] dp = new int[2 * total + 1];
-        dp[nums[0] + total] = 1;
-        dp[-nums[0] + total] += 1;
-        
-        for (int i = 1; i < nums.length; i++) {
-            int[] next = new int[2 * total + 1];
-            for (int sum = -total; sum <= total; sum++) {
-                if (dp[sum + total] > 0) {
-                    next[sum + nums[i] + total] += dp[sum + total];
-                    next[sum - nums[i] + total] += dp[sum + total];
+    public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0; 
+        for(int i: nums) sum+=i;
+        if(s>sum || s<-sum) return 0;
+        int[] dp = new int[2*sum+1];
+        dp[0+sum] = 1;
+        for(int i = 0; i<nums.length; i++){
+            int[] next = new int[2*sum+1];
+            for(int k = 0; k<2*sum+1; k++){
+                if(dp[k]!=0){
+                    next[k + nums[i]] += dp[k];
+                    next[k - nums[i]] += dp[k];
                 }
             }
             dp = next;
         }
-        
-        return Math.abs(S) > total ? 0 : dp[S + total];
+        return dp[sum+s];
     }
 }
