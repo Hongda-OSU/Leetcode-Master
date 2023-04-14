0ed5c21c5ab8814a18dd38d868fbdccaf799858b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-14 14:50:13 -0400

    Time: 68 ms (45.29%), Space: 43.3 MB (18.06%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,17 +1,9 @@
 class Solution {
-    public boolean repeatedSubstringPattern(String str) {
-	int l = str.length();
-	for(int i=l/2;i>=1;i--) {
-		if(l%i==0) {
-			int m = l/i;
-			String subS = str.substring(0,i);
-			StringBuilder sb = new StringBuilder();
-			for(int j=0;j<m;j++) {
-				sb.append(subS);
-			}
-			if(sb.toString().equals(str)) return true;
-		}
-	}
-	return false;
-}
+    public boolean repeatedSubstringPattern(String s) {
+         int size = s.length();
+        
+        String sFold = s.substring(1, size) + s.substring(0, size-1);
+        
+        return sFold.contains(s);
+    }
 }
