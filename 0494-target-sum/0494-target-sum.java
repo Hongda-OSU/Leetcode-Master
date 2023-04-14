a1657b2113ee9800d130304565b4c0bb439dc2a2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-14 15:08:50 -0400

    Time: 5 ms (79.67%), Space: 42.5 MB (32.15%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,25 +1,28 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        //Solution 1
-        int sum = 0;
-        for(int x : nums)
-            sum += x;
-        if(((sum - target) % 2 == 1) || (target > sum))
-            return 0;
-        
-        int n = nums.length;
-        int s2 = (sum - target)/2;
-        int[][] t = new int[n + 1][s2 + 1];
-        t[0][0] = 1;
-        
-        for(int i = 1; i < n + 1; i++) {
-            for(int j = 0; j < s2 + 1; j++) {
-                if(nums[i - 1] <= j)
-                    t[i][j] = t[i-1][j] + t[i - 1][j - nums[i - 1]];
-                else
-                    t[i][j] = t[i - 1][j];
-            }
-        }
-        return t[n][s2];
+    public int findTargetSumWays(int[] nums, int S) {
+    int sum = 0;
+    S = Math.abs(S);
+    for(int i = 0; i < nums.length; i++)
+        sum += nums[i];
+	// Invalid S, just return 0
+    if( S > sum || (sum + S) % 2 != 0 )
+        return 0;
+
+    int dp[][] = new int[(sum + S) / 2 + 1][nums.length + 1];
+    dp[0][0] = 1;
+    for(int i = 0; i < nums.length; i++) { // empty knapsack must be processed specially
+        if( nums[i] == 0 )
+            dp[0][i + 1] = dp[0][i] * 2;
+        else
+            dp[0][i + 1] = dp[0][i];
     }
+    for(int i = 1; i < dp.length; i++) {
+        for(int j = 0; j < nums.length; j++) {
+            dp[i][j + 1] += dp[i][j];
+            if( nums[j] <= i )
+                dp[i][j + 1] += dp[i - nums[j]][j];
+        }
+    }
+    return dp[(sum + S) / 2][nums.length];
+}
 }
