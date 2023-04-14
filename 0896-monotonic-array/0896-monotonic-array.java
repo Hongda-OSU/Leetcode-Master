e940fbd9b5b55d5afed1b1baa58090e78f70d3ae
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-14 14:37:12 -0400

    Time: 2 ms (67.98%), Space: 52.4 MB (84.86%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,25 +1,15 @@
 class Solution {
     public boolean isMonotonic(int[] A) {
-        if (A.length == 1) return true;
-        if (A[0] < A[1]) return isIncreasing(A);
-        else if (A[0] > A[1]) return isDecreasing(A);
-        else {
-            if (isIncreasing(A)) return true;
-            return isDecreasing(A);
+        int store = 0;
+        for (int i = 0; i < A.length - 1; ++i) {
+            int c = Integer.compare(A[i], A[i+1]);
+            if (c != 0) {
+                if (c != store && store != 0)
+                    return false;
+                store = c;
+            }
         }
-    }
-    
-    public boolean isIncreasing(int[] A){
-        for (int i = 0; i < A.length-1; i++){
-            if (!(A[i] <= A[i+1])) return false;
-        }
-        return true;
-    }
-    
-    public boolean isDecreasing(int[] A){
-        for (int i = 0; i < A.length-1; i++){
-            if (!(A[i] >= A[i+1])) return false;
-        }
+
         return true;
     }
 }
