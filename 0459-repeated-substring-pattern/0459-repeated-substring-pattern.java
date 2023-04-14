2b0313909b929269b0e72d6d793c4176349884ee
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-14 15:03:55 -0400

    Time: 7 ms (96.86%), Space: 43.2 MB (26.69%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,31 +1,30 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-
-        if(s.length()==1) return false;
-        if(s.length()==2) return s.substring(0,1).equals(s.substring(1));
-
-        int size = s.length();
-        for(int i=2;i<=size/2;i++){
-            if(size%i==0){
-                String str = s.substring(0,size/i);
+        int n = s.length();
+        if (n == 1)
+            return false;
+        if (n == 2)
+            return s.substring(0, 1).equals(s.substring(1));
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                String str = s.substring(0, n / i);
                 int count = 0;
-
-                for(int j=size/i;j<size;j+=size/i){
-                    if(s.substring(j,j+size/i).equals(str)){
+                for (int j = n / i; j < n; j += n / i) {
+                    if (s.substring(j, j + n / i).equals(str))
                         count++;
-                    }else{
-                        count=0;
+                    else {
+                        count = 0;
                         break;
                     }
                 }
-
-                if(count==i-1) return true;
+                if (count == i - 1)
+                    return true;
             }
         }
-
         int a = s.charAt(0);
-        for(char ch : s.toCharArray()){
-            if(a!=ch) return false;
+        for (char ch : s.toCharArray()) {
+            if (a != ch)
+                return false;
         }
         return true;
     }
