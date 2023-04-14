52375e12e0d48a04cadb632fcd8856abcb4b6614
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-14 15:08:19 -0400

    Time: 5 ms (79.67%), Space: 41.6 MB (57.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,26 +1,25 @@
-public class Solution {
-    public int findTargetSumWays(int[] nums, int S) {
-        if (nums == null || nums.length == 0){
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        //Solution 1
+        int sum = 0;
+        for(int x : nums)
+            sum += x;
+        if(((sum - target) % 2 == 1) || (target > sum))
             return 0;
-        }
-        return helper(nums, 0, 0, S, new HashMap<>());
-    }
-    private int helper(int[] nums, int index, int sum, int S, Map<String, Integer> map){
-        String encodeString = index + "->" + sum;
-        if (map.containsKey(encodeString)){
-            return map.get(encodeString);
-        }
-        if (index == nums.length){
-            if (sum == S){
-                return 1;
-            }else {
-                return 0;
+        
+        int n = nums.length;
+        int s2 = (sum - target)/2;
+        int[][] t = new int[n + 1][s2 + 1];
+        t[0][0] = 1;
+        
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 0; j < s2 + 1; j++) {
+                if(nums[i - 1] <= j)
+                    t[i][j] = t[i-1][j] + t[i - 1][j - nums[i - 1]];
+                else
+                    t[i][j] = t[i - 1][j];
             }
         }
-        int curNum = nums[index];
-        int add = helper(nums, index + 1, sum - curNum, S, map);
-        int minus = helper(nums, index + 1, sum + curNum, S, map);
-        map.put(encodeString, add + minus);
-        return add + minus;
+        return t[n][s2];
     }
 }
