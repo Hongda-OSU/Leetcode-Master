e0f5544cc99cf5eaa7ef177d30500c13f7a10865
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-15 19:36:33 -0400

    Time: 50 ms (28.09%), Space: 40.4 MB (28.65%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
@@ -1,18 +1,11 @@
 class Solution {
-     public int twoEggDrop(int n) {
-        int eggs = 2;
-        return drop(n, eggs, new int[n + 1][eggs + 1]);
-    }
-
-    int drop(int floors, int eggs, int[][] dp) {
-        if (eggs == 1 || floors <= 1)
-            return floors;
-        if (dp[floors][eggs] > 0)
-            return dp[floors][eggs];
-        int min = Integer.MAX_VALUE;
-        for (int f = 1; f <= floors; f++)
-            min = Math.min(min, 1 + Math.max(drop(f - 1, eggs - 1, dp), drop(floors - f, eggs, dp)));
-        dp[floors][eggs] = min;
-        return min;
+    private int[] dp = new int[1001];
+    
+    public int twoEggDrop(int n) {
+        if (dp[n] == 0) {
+            for (int i = 1; i <= n; i++)
+                dp[n] = Math.min(dp[n] == 0 ? n : dp[n], 1 + Math.max(i - 1, twoEggDrop(n - i)));
+        }
+        return dp[n];
     }
 }
