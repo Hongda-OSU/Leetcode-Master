0366e8e6ecd8855e824d054edfdd04b5d7256faa
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-15 18:10:29 -0400

    Time: 2 ms (99.80%), Space: 42.2 MB (64.24%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,31 +1,30 @@
 class Solution {
-    public int evalRPN(String[] a) {
-  Stack<Integer> stack = new Stack<Integer>();
-  
-  for (int i = 0; i < a.length; i++) {
-    switch (a[i]) {
-      case "+":
-        stack.push(stack.pop() + stack.pop());
-        break;
-          
-      case "-":
-        stack.push(-stack.pop() + stack.pop());
-        break;
-          
-      case "*":
-        stack.push(stack.pop() * stack.pop());
-        break;
-
-      case "/":
-        int n1 = stack.pop(), n2 = stack.pop();
-        stack.push(n2 / n1);
-        break;
-          
-      default:
-        stack.push(Integer.parseInt(a[i]));
+    public int evalRPN(String[] tokens) {
+ int[] ls = new int[tokens.length/2+1];
+    int index = 0;
+    for (String token : tokens) {
+        switch (token) {
+            case "+":
+                ls[index - 2] = ls[index - 2] + ls[index - 1];
+                index--;
+                break;
+            case "-":
+                ls[index - 2] = ls[index - 2] - ls[index - 1];
+                index--;
+                break;
+            case "*":
+                ls[index - 2] = ls[index - 2] * ls[index - 1];
+                index--;
+                break;
+            case "/":
+                ls[index - 2] = ls[index - 2] / ls[index - 1];
+                index--;
+                break;
+            default:
+                ls[index++] = Integer.parseInt(token);
+                break;
+        }
     }
-  }
-  
-  return stack.pop();
+    return ls[0];
 }
 }
