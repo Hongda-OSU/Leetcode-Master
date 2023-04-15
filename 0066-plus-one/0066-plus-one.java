e5f112680e405e2aeb92e6f94334871816cc3e6c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-15 18:52:29 -0400

    Time: 0 ms (100.00%), Space: 41.1 MB (38.20%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,24 +1,15 @@
 class Solution {
   public int[] plusOne(int[] digits) {
-    int n = digits.length;
+    for (int i = digits.length - 1; i >= 0; i--) {
+	if (digits[i] < 9) {
+		digits[i]++;
+		return digits;
+	}
+	digits[i] = 0;
+}
 
-    // move along the input array starting from the end
-    for (int idx = n - 1; idx >= 0; --idx) {
-      // set all the nines at the end of array to zeros
-      if (digits[idx] == 9) {
-        digits[idx] = 0;
-      }
-      // here we have the rightmost not-nine
-      else {
-        // increase this rightmost not-nine by 1
-        digits[idx]++;
-        // and the job is done
-        return digits;
-      }
-    }
-    // we're here because all the digits are nines
-    digits = new int[n + 1];
-    digits[0] = 1;
-    return digits;
+digits = new int[digits.length + 1];
+digits[0] = 1;
+return digits;
   }
 }
