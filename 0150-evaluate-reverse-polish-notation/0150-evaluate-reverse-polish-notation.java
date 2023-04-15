af1623ce954929809dc25d92ffd3830db141437a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-15 18:09:15 -0400

    Time: 74 ms (5.19%), Space: 42.2 MB (64.24%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,22 +1,49 @@
 class Solution {
+    
+    private static final Map<String, BiFunction<Integer, Integer, Integer>> OPERATIONS = new HashMap<>();
+    
+    // Ensure this only gets done once for ALL test cases.
+    static {
+        OPERATIONS.put("+", (a, b) -> a + b);
+        OPERATIONS.put("-", (a, b) -> a - b);
+        OPERATIONS.put("*", (a, b) -> a * b);
+        OPERATIONS.put("/", (a, b) -> a / b);
+    }
+    
     public int evalRPN(String[] tokens) {
-        Deque<Integer> dq = new LinkedList<>();
-        for (String token : tokens) {
-            if (token.equals("*") || token.equals("/") || token.equals("+") || token.equals("-")) {
-                int right = dq.removeFirst();
-                int left = dq.removeFirst();
-                if (token.equals("*"))
-                    dq.addFirst(left * right);
-                else if (token.equals("/"))
-                    dq.addFirst(left / right);
-                else if (token.equals("+"))
-                    dq.addFirst(left + right);
-                else
-                    dq.addFirst(left - right);
-            } else {
-                dq.addFirst(Integer.valueOf(token));
+        
+        int currentPosition = 0;
+        int length = tokens.length; // We need to keep track of this ourselves.
+        
+        while (length > 1) {
+            
+            // Move the position pointer to the next operator token.
+            while (!OPERATIONS.containsKey(tokens[currentPosition])) {
+                currentPosition++;
             }
+            
+            // Extract the operation and numbers to apply operation too.
+            String operation = tokens[currentPosition];
+            int number1 = Integer.parseInt(tokens[currentPosition - 2]);
+            int number2 = Integer.parseInt(tokens[currentPosition - 1]);
+            
+            // Calculate the result to overwrite the operator with.
+            BiFunction<Integer, Integer, Integer> operator = OPERATIONS.get(operation);
+            int value = operator.apply(number1, number2);
+            tokens[currentPosition] = Integer.toString(value);
+            
+            // Delete numbers and point pointers correctly.
+            delete2AtIndex(tokens, currentPosition - 2, length);
+            currentPosition--;
+            length -= 2;
+        } 
+        
+        return Integer.parseInt(tokens[0]);
+    }
+    
+    private void delete2AtIndex(String[] tokens, int d, int length) {
+        for (int i = d; i < length - 2; i++) {
+            tokens[i] = tokens[i + 2];
         }
-        return dq.peekFirst();
     }
 }
