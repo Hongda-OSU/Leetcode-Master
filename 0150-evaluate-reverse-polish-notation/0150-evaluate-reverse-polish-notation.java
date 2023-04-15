cd7e4d264849c817216dba54dffa6418c1e5f9ca
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-15 18:09:36 -0400

    Time: 9 ms (15.48%), Space: 42.5 MB (33.70%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -11,39 +11,25 @@ class Solution {
     }
     
     public int evalRPN(String[] tokens) {
-        
-        int currentPosition = 0;
-        int length = tokens.length; // We need to keep track of this ourselves.
-        
-        while (length > 1) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
             
-            // Move the position pointer to the next operator token.
-            while (!OPERATIONS.containsKey(tokens[currentPosition])) {
-                currentPosition++;
+            if (!OPERATIONS.containsKey(token)) {
+                stack.push(Integer.valueOf(token));
+                continue;
             }
             
-            // Extract the operation and numbers to apply operation too.
-            String operation = tokens[currentPosition];
-            int number1 = Integer.parseInt(tokens[currentPosition - 2]);
-            int number2 = Integer.parseInt(tokens[currentPosition - 1]);
-            
-            // Calculate the result to overwrite the operator with.
-            BiFunction<Integer, Integer, Integer> operator = OPERATIONS.get(operation);
-            int value = operator.apply(number1, number2);
-            tokens[currentPosition] = Integer.toString(value);
-            
-            // Delete numbers and point pointers correctly.
-            delete2AtIndex(tokens, currentPosition - 2, length);
-            currentPosition--;
-            length -= 2;
-        } 
-        
-        return Integer.parseInt(tokens[0]);
-    }
-    
-    private void delete2AtIndex(String[] tokens, int d, int length) {
-        for (int i = d; i < length - 2; i++) {
-            tokens[i] = tokens[i + 2];
+            int number2 = stack.pop();
+            int number1 = stack.pop();
+            BiFunction<Integer, Integer, Integer> operation;
+            operation = OPERATIONS.get(token);
+            int result = operation.apply(number1, number2);
+            stack.push(result);
         }
+        
+        return stack.pop();
+        
     }
 }
