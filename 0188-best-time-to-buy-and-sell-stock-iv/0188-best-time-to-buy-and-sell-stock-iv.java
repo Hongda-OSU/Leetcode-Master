317549c619f92272c29f214b17ab75f4988cfa06
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-15 20:04:19 -0400

    Time: 9 ms (37.46%), Space: 44.9 MB (17.12%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,25 +1,54 @@
-class Solution {
+public class Solution {
     public int maxProfit(int k, int[] prices) {
-        int len = prices.length;
-        if (k >= len / 2) return quickSolve(prices);
-        
-        int[][] t = new int[k + 1][len];
-        for (int i = 1; i <= k; i++) {
-            int tmpMax =  -prices[0];
-            for (int j = 1; j < len; j++) {
-                t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax);
-                tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);
+        int n = prices.length;
+
+        // solve special cases
+        if (n <= 0 || k <= 0) {
+            return 0;
+        }
+
+        if (2 * k > n) {
+            int res = 0;
+            for (int i = 1; i < n; i++) {
+                res += Math.max(0, prices[i] - prices[i - 1]);
+            }
+            return res;
+        }
+
+        // dp[i][used_k][ishold] = balance
+        // ishold: 0 nothold, 1 hold
+        int[][][] dp = new int[n][k + 1][2];
+
+        // initialize the array with -inf
+        // we use -1e9 here to prevent overflow
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= k; j++) {
+                dp[i][j][0] = -1000000000;
+                dp[i][j][1] = -1000000000;
             }
         }
-        return t[k][len - 1];
-    }
-    
 
-    private int quickSolve(int[] prices) {
-        int len = prices.length, profit = 0;
-        for (int i = 1; i < len; i++)
-            // as long as there is a price gap, we gain a profit.
-            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
-        return profit;
+        // set starting value
+        dp[0][0][0] = 0;
+        dp[0][1][1] = -prices[0];
+
+        // fill the array
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= k; j++) {
+                // transition equation
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                // you can't hold stock without any transaction
+                if (j > 0) {
+                    dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int j = 0; j <= k; j++) {
+            res = Math.max(res, dp[n - 1][j][0]);
+        }
+
+        return res;
     }
 }
