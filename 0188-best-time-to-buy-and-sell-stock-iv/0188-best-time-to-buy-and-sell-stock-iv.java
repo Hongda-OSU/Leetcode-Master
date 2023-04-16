69e07dd34c8fc8d672f790b98ecab9781cf4f580
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-15 20:04:43 -0400

    Time: 3 ms (71.38%), Space: 42.6 MB (41.42%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,54 +1,27 @@
-public class Solution {
-    public int maxProfit(int k, int[] prices) {
-        int n = prices.length;
-
-        // solve special cases
-        if (n <= 0 || k <= 0) {
-            return 0;
-        }
-
-        if (2 * k > n) {
-            int res = 0;
-            for (int i = 1; i < n; i++) {
-                res += Math.max(0, prices[i] - prices[i - 1]);
-            }
-            return res;
-        }
-
-        // dp[i][used_k][ishold] = balance
-        // ishold: 0 nothold, 1 hold
-        int[][][] dp = new int[n][k + 1][2];
-
-        // initialize the array with -inf
-        // we use -1e9 here to prevent overflow
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                dp[i][j][0] = -1000000000;
-                dp[i][j][1] = -1000000000;
-            }
-        }
-
-        // set starting value
-        dp[0][0][0] = 0;
-        dp[0][1][1] = -prices[0];
-
-        // fill the array
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                // transition equation
-                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
-                // you can't hold stock without any transaction
-                if (j > 0) {
-                    dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
-                }
-            }
-        }
-
-        int res = 0;
-        for (int j = 0; j <= k; j++) {
-            res = Math.max(res, dp[n - 1][j][0]);
-        }
-
-        return res;
+class Solution {
+public int maxProfit(int k, int[] prices) {
+	int n = prices.length;
+	if (n <= 1)
+		return 0;
+	
+	//if k >= n/2, then you can make maximum number of transactions.
+	if (k >=  n/2) {
+		int maxPro = 0;
+		for (int i = 1; i < n; i++) {
+			if (prices[i] > prices[i-1])
+				maxPro += prices[i] - prices[i-1];
+		}
+		return maxPro;
+	}
+	
+    int[][] dp = new int[k+1][n];
+    for (int i = 1; i <= k; i++) {
+    	int localMax = dp[i-1][0] - prices[0];
+    	for (int j = 1; j < n; j++) {
+    		dp[i][j] = Math.max(dp[i][j-1],  prices[j] + localMax);
+    		localMax = Math.max(localMax, dp[i-1][j] - prices[j]);
+    	}
     }
+    return dp[k][n-1];
+}
 }
