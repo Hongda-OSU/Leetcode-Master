7a9198ff38aa5b1b6de2368148bfdf9223bdc4e8
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-16 14:56:13 -0400

    Time: 12 ms (99.07%), Space: 49.5 MB (100.00%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,29 +1,15 @@
 class Solution {
-    class Job implements Comparable<Job>{
-        int start;
-        int end;
-        int profit;
-        Job(int start, int end, int profit) {
-            this.start = start;
-            this.end = end;
-            this.profit = profit;
-        }
-        public int compareTo(Job otherJob) {
-            return this.start - otherJob.start;
-        }
-    }
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         int n = startTime.length;
         Job[] jobs = new Job[n];
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++)
             jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
-        }
         Arrays.sort(jobs);
         int[] dp = new int[n];
-        dp[n-1] = jobs[n-1].profit;
-        for (int i=n-2; i >=0; i--) {
-            dp[i] = Math.max(jobs[i].profit, dp[i+1]);
-            for (int j=i+1; j < n; j++) {
+        dp[n - 1] = jobs[n - 1].profit;
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i] = Math.max(jobs[i].profit, dp[i + 1]);
+            for (int j = i + 1; j < n; j++) {
                 if (jobs[i].end <= jobs[j].start) {
                     dp[i] = Math.max(dp[i], jobs[i].profit + dp[j]);
                     break;
@@ -31,5 +17,19 @@ class Solution {
             }
         }
         return dp[0];
+    }
+}
+
+class Job implements Comparable<Job> {
+    public int start, end, profit;
+    
+    public Job(int start, int end, int profit) {
+        this.start = start;
+        this.end = end;
+        this.profit = profit;
+    }
+    
+    public int compareTo(Job other) {
+        return this.start - other.start;
     }
 }
