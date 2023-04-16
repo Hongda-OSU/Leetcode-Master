18612b682b5a060100ab13e7ab9b4fa77ea42180
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-16 14:43:31 -0400

    Time: 9 ms (95.12%), Space: 80.4 MB (80.49%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,27 +1,47 @@
 class Solution {
-    public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, (e1, e2) -> (e1[0] == e2[0] ? e1[1]-e2[1] : e1[0]-e2[0]));
-        return maxValue(events, 0, k, 0, new int[k+1][events.length]);
+      public int maxValue(int[][] events, int k) {
+        int max = 0; 
+        if (k == 1) {
+            // just return the highest value
+            for(int [] event : events) 
+                max = Math.max(max, event[2]);
+           
+            return max;
+        }
+       
+        // sort the events based on start time
+        Arrays.sort(events, (a,b) -> Integer.compare(a[0], b[0]));
+        int size = events.length;
+       
+        int[][] cache = new int[size + 1][k + 1];
+       
+	   // if you want to create a recursive method, you can break off the rest of the code into a DFS method
+	   
+	   // looping through each event from the back
+        for (int i = size - 1; i >= 0; i--) {
+            // find the next available event (next event's start must be after current event's end)
+            int next = binarySearch(events, events[i][1], i + 1, size);
+            
+            for (int j = 1; j <= k; j++) {
+                // we have 2 choices: attend current event OR skip current & attend the next available event
+                cache[i][j] = Math.max(cache[i + 1][j], // current event
+                                       cache[next][j - 1] + events[i][2]); // last event + next available event
+            }
+        }
+        return cache[0][k];
     }
-    
-    private int maxValue(int[][] events, int index, int remainingEvents, int lastEventEndDay, int[][] dp) {
-		// Return 0 if no events are left or maximum choice is reached
-		if (index >= events.length || remainingEvents == 0)
-            return 0;
-			
-        // An Event cannot be picked if the previous event has not completed before current event
-        if (lastEventEndDay >= events[index][0])
-            return maxValue(events, index+1, remainingEvents, lastEventEndDay, dp);
-        
-		// Return the value if the solution is already available
-        if (dp[remainingEvents][index] != 0)
-            return dp[remainingEvents][index];
-                
-		// There are 2 choices that we can make,
-			// SKIP this meeting or PICK this meeting
-        return dp[remainingEvents][index] = Math.max(
-            maxValue(events, index+1, remainingEvents, lastEventEndDay, dp), // skip
-            maxValue(events, index+1, remainingEvents-1, events[index][1], dp) + events[index][2] // pick
-        );
+
+    // search for next available event that starts after current event ends
+    private int binarySearch(int[][] events, int targetEnd, int lo, int hi) {
+        while (lo < hi) {
+            int mid = (hi - lo) / 2 + lo;
+            if (targetEnd >= events[mid][0]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
     }
+
 }
