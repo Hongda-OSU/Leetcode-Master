f998cfff686647576d7718f4b1461d998d71d50f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-17 14:19:09 -0400

    Time: 1 ms (100.00%), Space: 39.5 MB (84.40%) - LeetHub

--- 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.java
+++ 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.java
@@ -1,13 +1,14 @@
 class Solution {
-    public int numberOfWays(int num_people) {
-        long M = 1_000_000_007;
-        long[] cache = new long[num_people+1];
-        cache[0] = 1;
-        for (int i = 2; i <= num_people; i += 2) {
-            for (int j = 2; j <= i; j += 2) {
-                cache[i] = (cache[i] + (cache[j-2]*cache[i-j])) % M;
-            }
+    public int numberOfWays(int numPeople) {
+        long[] arr = new long[numPeople / 2 + 2];
+        arr[1] = 1;
+        long mod = (long)1e9 + 7, result = 1;
+        for (int i = 2; i < numPeople / 2 + 2; i++) 
+            arr[i] = mod - mod / i * arr[(int)mod % i] % mod;
+        for (int i = 1; i <= numPeople / 2; i++) {
+            result = result * (i + numPeople / 2) % mod;
+            result = result * arr[i] % mod;
         }
-        return (int)cache[num_people];
+        return (int)(result * arr[numPeople / 2 + 1] % mod);
     }
 }
