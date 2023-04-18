773f6d1d0fe022d4e708abadf685c0e09c8c9a16
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-18 15:16:52 -0400

    Time: 7 ms (98.95%), Space: 86 MB (98.15%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,17 +1,35 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-         Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
-        int result = 0;
-        int prev = intervals[0][1];
-        for (int i = 1; i < intervals.length; i++) {
-            // if current start smaller than previous end,
-            // remove current
-            if (intervals[i][0] < prev) {
-                result++;
-            } else {
-                prev = intervals[i][1];
-            }    
+         int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int[] arr: intervals) {
+            min = Math.min(min, arr[0]);
+            max = Math.max(max, arr[1]);
         }
-        return result;
+        int[] map = new int[max + 1 - min];
+        Arrays.fill(map, -1);
+        for (int[] arr: intervals) {
+            int index = arr[1] - min;
+            if (map[index] == -1) {
+                map[index] = arr[0] - min;
+            } else if (map[index] < arr[0] - min) {
+                map[index] = arr[0] - min;
+            }
+        }
+        int nonOverlapCount = 0;
+        int preEnd = -1;
+        for (int i = 0; i < map.length; i++) {
+            if (map[i] == -1) continue;
+            if (preEnd == -1) {
+                nonOverlapCount++;
+                preEnd = i;
+            } else {
+                if (preEnd <= map[i]) {
+                    nonOverlapCount++;  
+                    preEnd = i;                  
+                } // otherwise, use previous node              
+            }
+        }
+        return intervals.length - nonOverlapCount;
     }
 }
