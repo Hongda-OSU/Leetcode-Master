2dbf9b930843e3cd7d371b97a91d9a8eb88d2c63
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-18 14:36:22 -0400

    Time: 19 ms (71.03%), Space: 49.8 MB (44.27%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,42 +1,25 @@
 class Solution {
+    public int numDistinct(String S, String T) {
+    // array creation
+    int[][] mem = new int[T.length()+1][S.length()+1];
+
+    // filling the first row: with 1s
+    for(int j=0; j<=S.length(); j++) {
+        mem[0][j] = 1;
+    }
     
-    public int numDistinct(String s, String t) {
-
-        int M = s.length();
-        int N = t.length();
-        
-        int[] dp = new int[N];
-        
-        int prev = 1;
-        
-        // Iterate over the strings in reverse so as to
-        // satisfy the way we've modeled our recursive solution
-        for (int i = M - 1; i >= 0; i--) {
-                
-                // At each step we start with the last value in
-                // the row which is always 1. Notice how we are
-                // starting the loop from N - 1 instead of N like
-                // in the previous solution.
-                prev = 1;
-
-                for (int j = N - 1; j >= 0; j--) {
-
-                    // Record the current value in this cell so that
-                    // we can use it to calculate the value of dp[j - 1]
-                    int old_dpj = dp[j];
-
-                    // If the characters match, we add the
-                    // result of the next recursion call (in this
-                    // case, the value of a cell in the dp table
-                    if (s.charAt(i) == t.charAt(j)) {
-                        dp[j] += prev;
-                    }
-
-                    // Update the prev variable
-                    prev = old_dpj;    
+    // the first column is 0 by default in every other rows but the first, which we need.
+    
+    for(int i=0; i<T.length(); i++) {
+        for(int j=0; j<S.length(); j++) {
+            if(T.charAt(i) == S.charAt(j)) {
+                mem[i+1][j+1] = mem[i][j] + mem[i+1][j];
+            } else {
+                mem[i+1][j+1] = mem[i+1][j];
             }
         }
-        
-        return dp[0];
     }
+    
+    return mem[T.length()][S.length()];
+}
 }
