eab53eb50a6dba30c3802b345438e49245d81015
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-18 14:37:04 -0400

    Time: 6 ms (95.93%), Space: 42.5 MB (89.75%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,25 +1,44 @@
 class Solution {
-    public int numDistinct(String S, String T) {
-    // array creation
-    int[][] mem = new int[T.length()+1][S.length()+1];
-
-    // filling the first row: with 1s
-    for(int j=0; j<=S.length(); j++) {
-        mem[0][j] = 1;
-    }
     
-    // the first column is 0 by default in every other rows but the first, which we need.
+    // Dictionary that we will use for memoization
+    private HashMap<Pair<Integer, Integer>, Integer> memo;
     
-    for(int i=0; i<T.length(); i++) {
-        for(int j=0; j<S.length(); j++) {
-            if(T.charAt(i) == S.charAt(j)) {
-                mem[i+1][j+1] = mem[i][j] + mem[i+1][j];
-            } else {
-                mem[i+1][j+1] = mem[i+1][j];
-            }
+    private int recurse(String s, String t, int i, int j) {
+        
+        int M = s.length();
+        int N = t.length();
+        
+        // Base case
+        if (i == M || j == N || M - i < N - j) {
+            return j == t.length() ? 1 : 0;
         }
+        
+        Pair<Integer, Integer> key = new Pair<Integer, Integer>(i, j);
+        
+        // Check to see if the result for this recursive
+        // call is already cached
+        if (this.memo.containsKey(key)) {
+            return this.memo.get(key);
+        }
+        
+        // Always calculate this result since it's
+        // required for both the cases
+        int ans = this.recurse(s, t, i + 1, j);
+        
+        // If the characters match, then we make another
+        // recursion call and add the result to "ans"
+        if (s.charAt(i) == t.charAt(j)) {
+            ans += this.recurse(s, t, i + 1, j + 1);
+        }
+        
+        // Cache the result
+        this.memo.put(key, ans);
+        return ans;
     }
     
-    return mem[T.length()][S.length()];
-}
+    public int numDistinct(String s, String t) {
+        
+        this.memo = new HashMap<Pair<Integer, Integer>, Integer>();        
+        return this.recurse(s, t, 0, 0);
+    }
 }
