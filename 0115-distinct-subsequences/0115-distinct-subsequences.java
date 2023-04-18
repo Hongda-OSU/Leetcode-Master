7b691617d052c7dc944b8db4071e58e60f646e6c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-18 14:42:02 -0400

    Time: 6 ms (95.93%), Space: 42.9 MB (87.22%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,44 +1,22 @@
 class Solution {
-    
-    // Dictionary that we will use for memoization
     private HashMap<Pair<Integer, Integer>, Integer> memo;
     
-    private int recurse(String s, String t, int i, int j) {
-        
-        int M = s.length();
-        int N = t.length();
-        
-        // Base case
-        if (i == M || j == N || M - i < N - j) {
-            return j == t.length() ? 1 : 0;
-        }
-        
-        Pair<Integer, Integer> key = new Pair<Integer, Integer>(i, j);
-        
-        // Check to see if the result for this recursive
-        // call is already cached
-        if (this.memo.containsKey(key)) {
-            return this.memo.get(key);
-        }
-        
-        // Always calculate this result since it's
-        // required for both the cases
-        int ans = this.recurse(s, t, i + 1, j);
-        
-        // If the characters match, then we make another
-        // recursion call and add the result to "ans"
-        if (s.charAt(i) == t.charAt(j)) {
-            ans += this.recurse(s, t, i + 1, j + 1);
-        }
-        
-        // Cache the result
-        this.memo.put(key, ans);
-        return ans;
+    public int numDistinct(String s, String t) {
+        memo = new HashMap<>();
+        return distinct(s, t, 0, 0);
     }
     
-    public int numDistinct(String s, String t) {
-        
-        this.memo = new HashMap<Pair<Integer, Integer>, Integer>();        
-        return this.recurse(s, t, 0, 0);
+    private int distinct(String s, String t, int i, int j) {
+        int m = s.length(), n = t.length();
+        if (i == m || j == n || m - i < n - j)
+            return j == t.length() ? 1 : 0;
+        Pair<Integer, Integer> key = new Pair<>(i, j);
+        if (memo.containsKey(key))
+            return memo.get(key);
+        int result = distinct(s, t, i + 1, j);
+        if (s.charAt(i) == t.charAt(j))
+            result += distinct(s, t, i + 1, j + 1);
+        memo.put(key, result);
+        return result;
     }
 }
