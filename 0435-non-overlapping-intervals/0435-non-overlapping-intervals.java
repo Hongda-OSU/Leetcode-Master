9f846c4286a605822f02faa28c8d331ab621eea1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-18 15:21:34 -0400

    Time: 7 ms (98.95%), Space: 86.1 MB (98.15%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,33 +1,31 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-         int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int[] arr: intervals) {
-            min = Math.min(min, arr[0]);
-            max = Math.max(max, arr[1]);
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int[] interval : intervals) {
+            min = Math.min(min, interval[0]);
+            max = Math.max(max, interval[1]);
         }
         int[] map = new int[max + 1 - min];
         Arrays.fill(map, -1);
-        for (int[] arr: intervals) {
-            int index = arr[1] - min;
-            if (map[index] == -1) {
-                map[index] = arr[0] - min;
-            } else if (map[index] < arr[0] - min) {
-                map[index] = arr[0] - min;
-            }
+        for (int[] interval : intervals) {
+            int idx = interval[1] - min;
+            if (map[idx] == -1)
+                map[idx] = interval[0] - min;
+            else if (map[idx] < interval[0] - min)
+                map[idx] = interval[0] - min;
         }
-        int nonOverlapCount = 0;
-        int preEnd = -1;
+        int nonOverlapCount = 0, preEnd = -1;
         for (int i = 0; i < map.length; i++) {
-            if (map[i] == -1) continue;
+            if (map[i] == -1)
+                continue;
             if (preEnd == -1) {
                 nonOverlapCount++;
                 preEnd = i;
             } else {
                 if (preEnd <= map[i]) {
-                    nonOverlapCount++;  
-                    preEnd = i;                  
-                } // otherwise, use previous node              
+                    nonOverlapCount++;
+                    preEnd = i;
+                }
             }
         }
         return intervals.length - nonOverlapCount;
