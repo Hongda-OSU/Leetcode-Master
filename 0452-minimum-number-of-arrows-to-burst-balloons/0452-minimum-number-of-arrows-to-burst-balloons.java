06152e3d67ce50c232e0753d97e703553d4c102f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-19 18:36:12 -0400

    Time: 82 ms (23.29%), Space: 77.4 MB (85.50%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,33 +1,17 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        // result will store the minimum number of arrows needed to burst all the balloons
-        int result = 0;
+        Arrays.sort(points, Comparator.comparingInt(o -> o[1])); //Sorting according to end
 
-        // PriorityQueue to sort the balloon ranges in ascending order of their end points
-        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a,b) -> a[1]-b[1]);
+        int ans = 1;
+        int bound = points[0][1];
 
-        // add all the balloon ranges to the PriorityQueue
-        for (int []point : points) priorityQueue.add(point);
-
-        // if there are no balloons, return 0
-        if (priorityQueue.isEmpty()) return 0;
-
-        // while there are balloons in the PriorityQueue, process each balloon range
-        while (!priorityQueue.isEmpty()) {
-            // get the current balloon range
-            int [] prev = priorityQueue.poll();
-
-            // check if any subsequent balloon ranges start before or at the same time as the end of the current range
-            // if a subsequent range does, remove it from the PriorityQueue because it will also be burst when the current range is burst
-            while (!priorityQueue.isEmpty() && prev[1]>=priorityQueue.peek()[0] && prev[1]<=priorityQueue.peek()[1]) {
-                priorityQueue.poll();
+        for(int i = 1; i<points.length; i++){
+            //checking for non-overlapping range
+            if(points[i][0] > bound){
+                bound = points[i][1];
+                ans++;
             }
-
-            // increment the result counter by 1 for each balloon range processed, since each range requires one arrow to burst it
-            result++;
         }
-
-        // return the minimum number of arrows needed to burst all the balloons
-        return result;
+        return ans;
     }
 }
