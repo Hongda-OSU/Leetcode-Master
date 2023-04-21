e5b2af07a16d348df4e253acc541484594c72620
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-21 14:19:15 -0400

    Time: 1 ms (99.23%), Space: 43.7 MB (25.25%) - LeetHub

--- 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
@@ -18,21 +18,19 @@ class Node {
 */
 
 class Solution {
-
     private List<List<Integer>> result = new ArrayList<>();
-
+    
     public List<List<Integer>> levelOrder(Node root) {
-        if (root != null) traverseNode(root, 0);
+        if (root != null)
+            traverseNode(root, 0);
         return result;
     }
-
+    
     private void traverseNode(Node node, int level) {
-        if (result.size() <= level) {
+        if (result.size() <= level)
             result.add(new ArrayList<>());
-        }
         result.get(level).add(node.val);
-        for (Node child : node.children) {
+        for (Node child : node.children)
             traverseNode(child, level + 1);
-        }
     }
 }
