acad5d3bd489898a7ab6499859db13e500ceab54
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-21 13:21:18 -0400

    Time: 4 ms (99.64%), Space: 43 MB (90.07%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,25 +1,32 @@
 class Solution {
-     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> res = new ArrayList<>();
-        for (int i=0; i<l.length; i++) {
-            int start = l[i];
-            int end = r[i];
-            List<Integer> temp = new ArrayList<>();
-            for (int j=start; j<end+1; j++) {
-                temp.add(nums[j]);
-            }
-            res.add(checkIsArithmeticSubarrays(temp));
+     boolean[] checked;
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {       
+        List<Boolean> result = new ArrayList<>();
+        int n = l.length;
+        for (int i = 0; i < n; i++) {
+            result.add(check(nums, l[i], r[i]));
         }
-        return res;
+        return result;
     }
-    
-    public boolean checkIsArithmeticSubarrays(List<Integer> nums) {
-        if (nums.size() < 2) return false;
-        Collections.sort(nums);
-        int delta = nums.get(1) - nums.get(0);
-        for (int i=0; i<nums.size() - 1; i++) {
-            if (nums.get(i + 1) - nums.get(i) != delta)
-                return false;
+    // check unsorted array if it is arithmetic
+    private boolean check(int[] nums, int l , int r) {
+        int n = r - l;
+        if (n == 0) return true;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i = l; i <= r; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+        }
+        if ((max - min) % n != 0) return false;
+        int diff = (max - min) / n;
+        if (diff == 0) return true; // all numbers are equal 
+        checked = new boolean[max - min + 1];
+        for(int i = l; i <= r; i++) {
+            int currentDiff = nums[i] - min; 
+			// the current diff with min should happen only once and diff should be its divider
+            if (checked[currentDiff] || currentDiff % diff != 0) return false;
+            checked[currentDiff] = true;           
         }
         return true;
     }
