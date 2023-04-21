824b5262a5e6cd7e2bb444b8907af3e84cda9a3e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-21 14:35:42 -0400

    Time: 2 ms (91.17%), Space: 42.3 MB (38.87%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,20 +1,35 @@
-class Solution {
-    public boolean isMatch(String text, String pattern) {
-        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
-        dp[text.length()][pattern.length()] = true;
+enum Result {
+    TRUE, FALSE
+}
 
-        for (int i = text.length(); i >= 0; i--){
-            for (int j = pattern.length() - 1; j >= 0; j--){
-                boolean first_match = (i < text.length() &&
-                                       (pattern.charAt(j) == text.charAt(i) ||
-                                        pattern.charAt(j) == '.'));
-                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
-                } else {
-                    dp[i][j] = first_match && dp[i+1][j+1];
-                }
+class Solution {
+    Result[][] memo;
+
+    public boolean isMatch(String text, String pattern) {
+        memo = new Result[text.length() + 1][pattern.length() + 1];
+        return dp(0, 0, text, pattern);
+    }
+
+    public boolean dp(int i, int j, String text, String pattern) {
+        if (memo[i][j] != null) {
+            return memo[i][j] == Result.TRUE;
+        }
+        boolean ans;
+        if (j == pattern.length()){
+            ans = i == text.length();
+        } else{
+            boolean first_match = (i < text.length() &&
+                                   (pattern.charAt(j) == text.charAt(i) ||
+                                    pattern.charAt(j) == '.'));
+
+            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                ans = (dp(i, j+2, text, pattern) ||
+                       first_match && dp(i+1, j, text, pattern));
+            } else {
+                ans = first_match && dp(i+1, j+1, text, pattern);
             }
         }
-        return dp[0][0];
+        memo[i][j] = ans ? Result.TRUE : Result.FALSE;
+        return ans;
     }
 }
