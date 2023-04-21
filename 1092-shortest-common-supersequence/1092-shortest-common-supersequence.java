74e1df0ccf2b159f5c8091cb2c65df3baa9762e8
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-21 14:22:32 -0400

    Time: 17 ms (67.69%), Space: 46.3 MB (43.95%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,50 +1,34 @@
 class Solution {
-    public String shortestCommonSupersequence(String s1, String s2) {
-        int n = s1.length(),m = s2.length();
-        int[][] t = new int[n+1][m+1];
-        for(int i= 0; i< n+1;i++){   //initialized
-            for(int j = 0;j < m+1;j++){
-                if(i == 0||j == 0) t[i][j] = 0;
-            }
-        }
-        for (int i = 1; i < n+1; i++) {
-            for (int j = 1; j < m+1; j++) {
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    t[i][j] = t[i-1][j-1]+1;
-                }
-                else{
-                    t[i][j] = Math.max(t[i-1][j],t[i][j-1]);
+   public String shortestCommonSupersequence(String s1, String s2) {
+
+        // find LCS.
+        int m = s1.length(), n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i + 1][j + 1] = 1 + dp[i][j];    
+                }else {
+                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
                 }
             }
         }
-        StringBuilder sb =new StringBuilder();
-        while(m > 0 && n >0){
-            if(s1.charAt(n-1) == s2.charAt(m-1)){
-                sb.append(s1.charAt(n-1));
-                m--;
-                n--;
+		
+        // Build result.
+        StringBuilder sb = new StringBuilder();
+        int i = m - 1, j = n - 1;
+        while (i >= 0 || j >= 0) { 
+            if (i < 0 ^ j < 0) { // only one string reaches left end.
+                char c = i < 0 ? s2.charAt(j--) : s1.charAt(i--); // remaining chars in the other string.
+                sb.append(c);
+            }else if (s1.charAt(i) == s2.charAt(j)) { // common char in LCS.
+                sb.append(s1.charAt(i)); // append the char of either s1 or s2.
+                --i; --j;  
+            }else { // the char is not in LCS.
+                char c = dp[i][j + 1] > dp[i + 1][j] ? s1.charAt(i--) : s2.charAt(j--); // the char corresponding to larger dp value.
+                sb.append(c);
             }
-            else {
-                if (t[n-1][m] >t[n][m-1]){
-                    sb.append(s1.charAt(n-1));
-                    n--;
-                }
-                else{
-                    sb.append(s2.charAt(m-1));
-                    m--;
-                }
-            }
-        }
-        while(n>0)
-        {
-            sb.append(s1.charAt(n-1));
-            n--;
-        }
-        while(m>0)
-        {
-            sb.append(s2.charAt(m-1));
-            m--;
-        }
+        } 
         return sb.reverse().toString();
     }
 }
