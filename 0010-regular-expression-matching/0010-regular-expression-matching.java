163ee82b8e5d88da3d0b9aba3c76748ad74f692d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-21 14:35:31 -0400

    Time: 4 ms (42.47%), Space: 41.1 MB (70.86%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,27 +1,20 @@
 class Solution {
-    public boolean isMatch(String s, String p) {
-        if (s == null || p == null)
-            return false;
-        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
-        dp[0][0] = true;
-        for (int i = 0; i < p.length(); i++) {
-            if (p.charAt(i) == '*' && dp[0][i - 1])
-                dp[0][i + 1] = true;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < p.length(); j++) {
-                if (p.charAt(j) == '.')
-                    dp[i + 1][j + 1] = dp[i][j];
-                if (p.charAt(j) == s.charAt(i))
-                    dp[i + 1][j + 1] = dp[i][j];
-                if (p.charAt(j) == '*') {
-                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.')
-                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
-                    else
-                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+    public boolean isMatch(String text, String pattern) {
+        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        dp[text.length()][pattern.length()] = true;
+
+        for (int i = text.length(); i >= 0; i--){
+            for (int j = pattern.length() - 1; j >= 0; j--){
+                boolean first_match = (i < text.length() &&
+                                       (pattern.charAt(j) == text.charAt(i) ||
+                                        pattern.charAt(j) == '.'));
+                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+                } else {
+                    dp[i][j] = first_match && dp[i+1][j+1];
                 }
             }
         }
-        return dp[s.length()][p.length()];
+        return dp[0][0];
     }
 }
