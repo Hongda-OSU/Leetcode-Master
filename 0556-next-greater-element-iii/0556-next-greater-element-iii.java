ea9e9ef5b0960477a419d1c4136a39267ada4e65
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-22 15:52:01 -0400

    Time: 0 ms (100.00%), Space: 39.5 MB (61.90%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.java
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -1,38 +1,26 @@
 class Solution {
-    public int nextGreaterElement(int n) {
-        char arr[] = (Integer.toString(n)).toCharArray();
-        
-        int i=arr.length-2;
-        StringBuilder ans = new StringBuilder();
-        while(i>=0 && arr[i] >= arr[i+1])
-            i--;
-        
-        if(i == -1)
+    public int nextGreaterElement(int i) {
+        char[] arr = String.valueOf(i).toCharArray();
+        int m = arr.length - 2;
+        while (m >= 0 && arr[m] >= arr[m + 1])
+            m--;
+        if (m == -1)
             return -1;
-        
-        int k = arr.length-1;
-        
-        while(arr[k] <= arr[i])
-            k--;
-        
-        swap(arr,i,k);
-        
-        for(int j=0;j<=i;j++)
-            ans.append(arr[j]);
-        
-        for(int j=arr.length-1;j>i;j--)
-            ans.append(arr[j]);
-        
-        long ans_ = Long.parseLong(ans.toString());
-        
-        return (ans_ > Integer.MAX_VALUE) ? -1 : (int)ans_;
-        
-        
+        int n = arr.length - 1;
+        while (arr[n] <= arr[m])
+            n--;
+        swap(arr, m, n);
+        Arrays.sort(arr, m + 1, arr.length);
+        long result = Long.parseLong(new String(arr));
+        if (result > Integer.MAX_VALUE)
+            return -1;
+        else
+            return (int)result;
     }
-    void swap(char[] arr,int i,int j)
-    {
-        char temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
+    
+    private void swap(char[] arr, int i, int j) {
+        char tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
