51d7ecab820975dc3e953806a4da94b6ad0cd904
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-23 14:57:14 -0400

    Time: 39 ms (34.71%), Space: 43.6 MB (30.37%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,45 +1,35 @@
 class Solution {
+
     public int cherryPickup(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][][] dpCache = new int[m][n][n];
-        // initial all elements to -1 to mark unseen
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    dpCache[i][j][k] = -1;
+        int dp[][][] = new int[m][n][n];
+
+        for (int row = m - 1; row >= 0; row--) {
+            for (int col1 = 0; col1 < n; col1++) {
+                for (int col2 = 0; col2 < n; col2++) {
+                    int result = 0;
+                    // current cell
+                    result += grid[row][col1];
+                    if (col1 != col2) {
+                        result += grid[row][col2];
+                    }
+                    // transition
+                    if (row != m - 1) {
+                        int max = 0;
+                        for (int newCol1 = col1 - 1; newCol1 <= col1 + 1; newCol1++) {
+                            for (int newCol2 = col2 - 1; newCol2 <= col2 + 1; newCol2++) {
+                                if (newCol1 >= 0 && newCol1 < n && newCol2 >= 0 && newCol2 < n) {
+                                    max = Math.max(max, dp[row + 1][newCol1][newCol2]);
+                                }
+                            }
+                        }
+                        result += max;
+                    }
+                    dp[row][col1][col2] = result;
                 }
             }
         }
-        return dp(0, 0, n - 1, grid, dpCache);
-    }
-
-    private int dp(int row, int col1, int col2, int[][] grid, int[][][] dpCache) {
-        if (col1 < 0 || col1 >= grid[0].length || col2 < 0 || col2 >= grid[0].length) {
-            return 0;
-        }
-        // check cache
-        if (dpCache[row][col1][col2] != -1) {
-            return dpCache[row][col1][col2];
-        }
-        // current cell
-        int result = 0;
-        result += grid[row][col1];
-        if (col1 != col2) {
-            result += grid[row][col2];
-        }
-        // transition
-        if (row != grid.length - 1) {
-            int max = 0;
-            for (int newCol1 = col1 - 1; newCol1 <= col1 + 1; newCol1++) {
-                for (int newCol2 = col2 - 1; newCol2 <= col2 + 1; newCol2++) {
-                    max = Math.max(max, dp(row + 1, newCol1, newCol2, grid, dpCache));
-                }
-            }
-            result += max;
-        }
-
-        dpCache[row][col1][col2] = result;
-        return result;
+        return dp[0][0][n - 1];
     }
 }
