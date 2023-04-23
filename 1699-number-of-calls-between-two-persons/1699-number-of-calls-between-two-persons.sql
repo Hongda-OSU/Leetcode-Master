8bfb5a872f435ca6ddc70bf2ecf8764b7adeccf1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-23 15:11:22 -0400

    Time: 1206 ms (27.84%), Space: 0B (100.00%) - LeetHub

---
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    CASE
+        WHEN from_id > to_id THEN to_id
+        ELSE from_id
+    END AS person1,
+    CASE
+        WHEN from_id > to_id THEN from_id
+        ELSE to_id
+    END AS person2,
+    COUNT(duration) AS call_count,
+    SUM(duration) AS total_duration
+FROM Calls
+GROUP BY person2, person1;
+    
