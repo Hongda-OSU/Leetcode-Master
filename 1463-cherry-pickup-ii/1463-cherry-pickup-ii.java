80e941b01c926b650ba55e12c06789044aa08e09
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-23 14:58:19 -0400

    Time: 27 ms (85.90%), Space: 42.5 MB (95.23%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,48 +1,22 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int dir[] = new int[]{-1, 0, 1};
-
-        int row = grid.length;
-        int col = grid[0].length;
-        int dp[][][] = new int[row][col][col]; 
-        
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                for(int k = 0; k < col; k++){
-                    dp[i][j][k] = -1;
-                }
-            }
-        }
-        int col1 = 0; 
-        int col2 = col - 1; 
-        
-        dp[0][col1][col2] = grid[0][col1] + grid[0][col2]; 
-        int max = dp[0][col1][col2]; 
-        
-        for(int i = 1; i < row; i++){ 
-            for(int c1 = 0; c1 < col; c1++){ 
-                for(int c2 = 0; c2 < col; c2++){
-                    int prev = dp[i - 1][c1][c2]; 
-                    if(prev >= 0){ 
-                        for(int d1: dir){ 
-                            col1 = d1 + c1;
-                            for(int d2: dir){ 
-                                col2 = d2 + c2;
-                                if(inRange(col1, col) && inRange(col2, col)){
-                                    dp[i][col1][col2] = Math.max(dp[i][col1][col2], prev+(col1 == col2 ? grid[i][col1] : (grid[i][col1] + grid[i][col2]))); 
-                                    max = Math.max(max, dp[i][col1][col2]); 
-                                }
-                            }
-                        }
-                    }
-                    
-                }
-            }
-        }
-        return max;
+        int m = grid.length, n = grid[0].length;
+        Integer[][][] dp = new Integer[m][n][n];
+        return dfs(grid, m, n, 0, 0, n - 1, dp);
     }
-
-    public boolean inRange(int val, int limit){
-        return 0 <= val && val < limit;
+    int dfs(int[][] grid, int m, int n, int r, int c1, int c2, Integer[][][] dp) {
+        if (r == m) return 0; // Reach to bottom row
+        if (dp[r][c1][c2] != null) return dp[r][c1][c2];
+        int ans = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int nc1 = c1 + i, nc2 = c2 + j;
+                if (nc1 >= 0 && nc1 < n && nc2 >= 0 && nc2 < n) {
+                    ans = Math.max(ans, dfs(grid, m, n, r + 1, nc1, nc2, dp));
+                }
+            }
+        }
+        int cherries = c1 == c2 ? grid[r][c1] : grid[r][c1] + grid[r][c2];
+        return dp[r][c1][c2] = ans + cherries;
     }
 }
