c800f5a265b4747f94cdd0156e685af765ee1745
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-23 14:38:37 -0400

    Time: 10 ms (94.51%), Space: 51.6 MB (89.71%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,19 +1,15 @@
 class Solution {
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, dfs(i, manager, informTime, map));
-        }
-        return max;
+        int res = 0;
+        for (int i = 0; i < n; ++i)
+            res = Math.max(res, dfs(i, manager, informTime));
+        return res;
     }
-    
-    public int dfs(int id, int[] manager, int[] informTime, HashMap<Integer, Integer> map) {
-        if (manager[id] == -1)
-            return 0;
-        if (map.containsKey(id))
-            return map.get(id);
-        map.put(id, informTime[manager[id]] + dfs(manager[id], manager, informTime, map));
-        return map.get(id);
+    public int dfs(int i, int[] manager, int[] informTime) {
+        if (manager[i] != -1) {
+            informTime[i] += dfs(manager[i], manager, informTime);
+            manager[i] = -1;
+        }
+        return informTime[i];
     }
 }
