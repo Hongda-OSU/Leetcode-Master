fb202165b0f0f0cb9ba910630a26be5b047c9179
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-23 14:54:55 -0400

    Time: 14 ms (94.77%), Space: 42.7 MB (87.38%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,28 +1,35 @@
 class Solution {
+    private int[][][] memo;
+    private int[][] grid;
+    private int n;
+    
     public int cherryPickup(int[][] grid) {
-    int N = grid.length, M = (N << 1) - 1;
-    int[][] dp = new int[N][N];
-    dp[0][0] = grid[0][0];
-	    
-    for (int n = 1; n < M; n++) {
-		for (int i = N - 1; i >= 0; i--) {
-			for (int p = N - 1; p >= 0; p--) {
-				int j = n - i, q = n - p;
-                
-				if (j < 0 || j >= N || q < 0 || q >= N || grid[i][j] < 0 || grid[p][q] < 0) {
-                    dp[i][p] = -1;
-                    continue;
-                 }
-		 
-				 if (i > 0) dp[i][p] = Math.max(dp[i][p], dp[i - 1][p]);
-				 if (p > 0) dp[i][p] = Math.max(dp[i][p], dp[i][p - 1]);
-				 if (i > 0 && p > 0) dp[i][p] = Math.max(dp[i][p], dp[i - 1][p - 1]);
-		 
-				 if (dp[i][p] >= 0) dp[i][p] += grid[i][j] + (i != p ? grid[p][q] : 0);
-             }
-		 }
+        this.grid = grid;
+        n = grid.length;
+        memo = new int[n][n][n];
+        for (int[][] layer : memo) {
+            for (int[] row : layer)
+                Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return Math.max(0, dp(0, 0, 0));
     }
     
-    return Math.max(dp[N - 1][N - 1], 0);
-}
+    private int dp(int r1, int c1, int c2) {
+        int r2 = r1 + c1 - c2;
+        if (n == r1 || n == r2 || n == c1 || n == c2 || grid[r1][c1] == -1 || grid[r2][c2] == -1) 
+            return -999999;
+        else if (r1 == n - 1 && c1 == n - 1) 
+            return grid[r1][c1];
+        else if (memo[r1][c1][c2] != Integer.MIN_VALUE)
+            return memo[r1][c1][c2];
+        else {
+            int result = grid[r1][c1];
+            if (c1 != c2)
+                result += grid[r2][c2];
+            result += Math.max(Math.max(dp(r1, c1 + 1, c2 + 1), dp(r1 + 1, c1, c2 + 1))
+                              , Math.max(dp(r1, c1 + 1, c2), dp(r1 + 1, c1, c2)));
+            memo[r1][c1][c2] = result;
+            return result;
+        }
+    }
 }
