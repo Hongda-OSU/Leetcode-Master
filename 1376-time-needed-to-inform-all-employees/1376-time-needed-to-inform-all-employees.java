27a01a10bbe18acc213cb85473a70a644710b6fa
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-23 14:40:16 -0400

    Time: 10 ms (94.51%), Space: 51.2 MB (95.89%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,11 +1,12 @@
 class Solution {
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        int res = 0;
-        for (int i = 0; i < n; ++i)
-            res = Math.max(res, dfs(i, manager, informTime));
-        return res;
+        int result = 0;
+        for (int i = 0; i < n; i++) 
+            result = Math.max(result, dfs(i, manager, informTime));
+        return result;
     }
-    public int dfs(int i, int[] manager, int[] informTime) {
+    
+    private int dfs(int i, int[] manager, int[] informTime) {
         if (manager[i] != -1) {
             informTime[i] += dfs(manager[i], manager, informTime);
             manager[i] = -1;
