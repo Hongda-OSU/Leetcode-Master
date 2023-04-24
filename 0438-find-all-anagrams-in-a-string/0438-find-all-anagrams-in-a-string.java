36d4685b0d132ca0e4fff0aaa6746f65990aea12
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-24 15:16:42 -0400

    Time: 7 ms (95.06%), Space: 43.9 MB (36.76%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,21 +1,19 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int[] cnt = new int[128];
-        for (char c : p.toCharArray()) cnt[c]++;
-        
-        List<Integer> ans = new ArrayList<>();
-        for (int r = 0, l = 0; r < s.length(); ++r) {
-            char c = s.charAt(r);
-            cnt[c]--;
-            while (cnt[c] < 0) { // If number of characters `c` is more than our expectation
-                cnt[s.charAt(l)]++;  // Slide left until cnt[c] == 0
-                l++;
+        int[] count = new int[128];
+        for (char ch : p.toCharArray())
+            count[ch]++;
+        List<Integer> result = new ArrayList<>();
+        for (int left = 0, right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            count[ch]--;
+            while (count[ch] < 0) {
+                count[s.charAt(left)]++;
+                left++;
             }
-
-            if (r - l + 1 == p.length()) { // If we already filled enough `p.length()` chars
-                ans.add(l); // Add left index `l` to our result
-            }
+            if (right - left + 1 == p.length())
+                result.add(left);
         }
-        return ans;
+        return result;
     }
 }
