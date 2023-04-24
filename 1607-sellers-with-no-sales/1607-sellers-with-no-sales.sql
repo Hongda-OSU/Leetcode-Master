8f2c5a0323dcdb6686a5184d52abfb8fa0810816
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-24 15:51:40 -0400

    Time: 1514 ms (92.98%), Space: 0B (100.00%) - LeetHub

---
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT seller_name
+FROM Seller
+WHERE seller_id 
+    NOT IN (
+        SELECT DISTINCT seller_id
+        FROM Orders
+        WHERE LEFT(sale_date, 4) = '2020'
+    )
+ORDER BY 1;
