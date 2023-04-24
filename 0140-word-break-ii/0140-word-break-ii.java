4f14e73a9dafee7170ebb585664127afe981b826
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-24 15:24:32 -0400

    Time: 0 ms (100.00%), Space: 41.1 MB (47.34%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,46 +1,82 @@
 class Solution {
-    protected Set<String> wordSet;
 
-    private void updateCharSet(String s, HashSet<Character> charSet) {
-        for (int i = 0; i < s.length(); ++i)
-            charSet.add(s.charAt(i));
+    int[] exists;
+    Trie root;
+    
+    public void search(List<String> ans, Trie curr, StringBuilder temp, char[] given, int index){
+        if(index==given.length){
+            return;
+        } 
+        if(curr.children[given[index]-'a'] == null)return;
+        
+        Trie next = curr.children[given[index] - 'a'];
+        temp.append(given[index]);
+        
+        //If the word is present in the dictionary, we can either add a space or we continue going down in the trie
+        if(next.isWord){
+            //If we have reached the end of the string, add the value to Answer and terminate
+            if(index+1 == given.length){
+                ans.add(temp.toString());
+                temp.deleteCharAt(temp.length()-1); 
+                return;
+                }
+            else{
+                //We are adding space
+                temp.append(" ");
+                //Since after adding space, the next letter should be the beginning of a new word from the dictionary, we pass in the ROOT trie node
+                search(ans, root, temp, given, index+1);
+                //remove the space we added in the above line.
+                temp.deleteCharAt(temp.length()-1);      
+            }
+        }
+        
+        //since we have not added a space, we are technically going down in the same trie block. So we are moving the NEXT node in the CURR trie node.
+        search(ans, next, temp, given, index+1);
+            
+        //Deleting the added character
+        temp.deleteCharAt(temp.length()-1);            
+
+    }
+    
+    
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        root = new Trie();
+        exists = new int[26];
+        List<String> ans = new ArrayList();
+        for(String word : wordDict){
+            root.addWord(word);
+        }
+        
+        for(char c : s.toCharArray()){
+            if(exists[c-'a']!=1)
+                return new ArrayList();
+        }
+        search(ans, root, new StringBuilder(), s.toCharArray(), 0);
+        return ans;
     }
 
-    public List<String> wordBreak(String s, List<String> wordDict) {
-        HashSet<Character> stringCharSet = new HashSet<Character>();
-        updateCharSet(s, stringCharSet);
-
-        HashSet<Character> wordCharSet = new HashSet<Character>();
-        wordSet = new HashSet<>();
-        for (String word : wordDict) {
-            wordSet.add(word);
-            updateCharSet(word, wordCharSet);
+    
+    class Trie{
+        boolean isWord;
+        Trie[] children;
+        Trie(){
+            isWord=false;
+            children = new Trie[26];
         }
-
-        // quick check on the sets of characters
-        if (!wordCharSet.containsAll(stringCharSet))
-            return new ArrayList();
-
-        ArrayList<ArrayList<String>> dp = new ArrayList<ArrayList<String>>(s.length() + 1);
-        for (int i = 0; i < s.length() + 1; ++i) {
-            ArrayList<String> emptyList = new ArrayList<String>();
-            dp.add(emptyList);
-        }
-        dp.get(0).add("");
-
-        for (int endIndex = 1; endIndex < s.length() + 1; ++endIndex) {
-            ArrayList<String> sublist = new ArrayList<String>();
-
-            // fill up the values in the dp array.
-            for (int startIndex = 0; startIndex < endIndex; ++startIndex) {
-                String word = s.substring(startIndex, endIndex);
-                if (wordSet.contains(word))
-                    for (String subsentence : dp.get(startIndex))
-                        sublist.add((subsentence + " " + word).strip());
+        public void addWord(String w){
+            Trie curr = this;
+            int index = 0;
+            char[] word = w.toCharArray();
+            while(index<word.length){   
+                exists[word[index]-'a']=1;
+                if(curr.children[word[index]-'a'] == null){
+                    curr.children[word[index]-'a'] = new Trie();
+                }  
+                curr = curr.children[word[index]-'a'];
+                index++;
             }
-            dp.set(endIndex, sublist);
+            curr.isWord = true;
         }
-
-        return dp.get(s.length());
+        
     }
 }
