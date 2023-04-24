8eba641cde0343668977613b25c4bdc90f313c94
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-24 15:46:20 -0400

    Time: 9 ms (94.77%), Space: 39.8 MB (40.12%) - LeetHub

--- 0351-android-unlock-patterns/0351-android-unlock-patterns.java
+++ 0351-android-unlock-patterns/0351-android-unlock-patterns.java
@@ -1,34 +1,37 @@
-public class Solution {
-boolean[] visit = new boolean[9];
-int ret = 0;
-public int numberOfPatterns(int m, int n) {
-    for(int i = 0; i < 9; i++){
-        visit[i] = true;
-        helper(i, 1, m, n);
-        visit[i] = false;
+class Solution {
+    private int[][] jumps;
+    private boolean[] visited;
+    
+    public int numberOfPatterns(int m, int n) {
+        jumps = new int[10][10];
+        jumps[1][3] = jumps[3][1] = 2;
+        jumps[4][6] = jumps[6][4] = 5;
+        jumps[7][9] = jumps[9][7] = 8;
+        jumps[1][7] = jumps[7][1] = 4;
+        jumps[2][8] = jumps[8][2] = 5;
+        jumps[3][9] = jumps[9][3] = 6;
+	    jumps[1][9] = jumps[9][1] = jumps[3][7] = jumps[7][3] = 5;
+        visited = new boolean[10];
+        int result = 0;
+        result += dfs(1, 1, 0, m, n) * 4;
+        result += dfs(2, 1, 0, m, n) * 4;
+        result += dfs(5, 1, 0, m, n);
+        return result;
     }
-    return ret;
-}
-private void helper(int node, int k, int m, int n){
-    if(k >= m){
-        ret++;
-        if(k == n) return;
-    }
-    for(int i = 0; i < 9; i++){
-        if(check(node, i)){
-            visit[i] = true;
-            helper(i, k + 1, m, n);
-            visit[i] = false;
+    
+    private int dfs(int num, int len, int count, int m, int n) {
+        if (len >= m)
+            count++;
+        len++;
+        if (len > n)
+            return count;
+        visited[num] = true;
+        for (int next = 1; next <= 9; next++) {
+            int jump = jumps[num][next];
+            if (!visited[next] && (jump == 0 || visited[jump]))
+                count = dfs(next, len, count, m, n);
         }
-    }
-}
-private boolean check(int a, int b){
-    if(visit[b]) return false;
-    int rowDis = Math.abs(a / 3 - b / 3);
-    int colDis = Math.abs(a % 3 - b % 3);
-    if(rowDis == 2 && colDis == 2) return visit[4];
-    if(rowDis == 2 && colDis == 0) return visit[1 * 3 + a % 3];
-    if(rowDis == 0 && colDis == 2) return visit[a / 3 * 3 + 1];
-    return true;
-}
+        visited[num] = false;
+        return count;
+    } 
 }
