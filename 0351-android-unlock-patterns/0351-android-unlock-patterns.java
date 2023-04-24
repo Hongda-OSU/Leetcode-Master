d924dd3637a3d85df80effe91b651d9c42115c5b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-24 15:38:53 -0400

    Time: 9 ms (94.77%), Space: 39.8 MB (34.30%) - LeetHub

--- 0351-android-unlock-patterns/0351-android-unlock-patterns.java
+++ 0351-android-unlock-patterns/0351-android-unlock-patterns.java
@@ -1,37 +1,36 @@
-public class Solution {
-    // cur: the current position
-    // remain: the steps remaining
-    int DFS(boolean vis[], int[][] skip, int cur, int remain) {
-        if(remain < 0) return 0;
-        if(remain == 0) return 1;
-        vis[cur] = true;
-        int rst = 0;
-        for(int i = 1; i <= 9; ++i) {
-            // If vis[i] is not visited and (two numbers are adjacent or skip number is already visited)
-            if(!vis[i] && (skip[cur][i] == 0 || (vis[skip[cur][i]]))) {
-                rst += DFS(vis, skip, i, remain - 1);
-            }
+class Solution {
+    private int[][] jumps;
+private boolean[] visited;
+
+public int numberOfPatterns(int m, int n) {
+    jumps = new int[10][10];
+    jumps[1][3] = jumps[3][1] = 2;
+    jumps[4][6] = jumps[6][4] = 5;
+    jumps[7][9] = jumps[9][7] = 8;
+    jumps[1][7] = jumps[7][1] = 4;
+    jumps[2][8] = jumps[8][2] = 5;
+    jumps[3][9] = jumps[9][3] = 6;
+	jumps[1][9] = jumps[9][1] = jumps[3][7] = jumps[7][3] = 5;
+    visited = new boolean[10];
+    int count = 0;
+	count += DFS(1, 1, 0, m, n) * 4; // 1, 3, 7, 9 are symmetrical
+	count += DFS(2, 1, 0, m, n) * 4; // 2, 4, 6, 8 are symmetrical
+	count += DFS(5, 1, 0, m, n);
+	return count;
+}
+
+private int DFS(int num, int len, int count, int m, int n) {
+	if (len >= m) count++; // only count if moves are larger than m
+	len++;
+	if (len > n) return count;
+    visited[num] = true;
+    for (int next = 1; next <= 9; next++) {
+        int jump = jumps[num][next];
+        if (!visited[next] && (jump == 0 || visited[jump])) {
+            count = DFS(next, len, count, m, n);
         }
-        vis[cur] = false;
-        return rst;
     }
-    
-    public int numberOfPatterns(int m, int n) {
-        // Skip array represents number to skip between two pairs
-        int skip[][] = new int[10][10];
-        skip[1][3] = skip[3][1] = 2;
-        skip[1][7] = skip[7][1] = 4;
-        skip[3][9] = skip[9][3] = 6;
-        skip[7][9] = skip[9][7] = 8;
-        skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5;
-        boolean vis[] = new boolean[10];
-        int rst = 0;
-        // DFS search each length from m to n
-        for(int i = m; i <= n; ++i) {
-            rst += DFS(vis, skip, 1, i - 1) * 4;    // 1, 3, 7, 9 are symmetric
-            rst += DFS(vis, skip, 2, i - 1) * 4;    // 2, 4, 6, 8 are symmetric
-            rst += DFS(vis, skip, 5, i - 1);        // 5
-        }
-        return rst;
-    }
+	visited[num] = false; // backtracking
+    return count;
+}
 }
