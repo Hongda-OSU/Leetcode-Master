bb6e3928e794df200265165738ed92201f6289cb
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-24 15:10:39 -0400

    Time: 9 ms (87.98%), Space: 43.6 MB (68.53%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,23 +1,32 @@
 class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-        int sLen = s.length(), pLen = p.length();
-        List<Integer> result = new ArrayList<Integer>();
-        if (pLen > sLen) return result;
-        int[] sMapping = new int[26];
-        int[] pMapping = new int[26];
-        for (int i = 0; i < pLen; i++) {
-            pMapping[p.charAt(i) - 'a']++;
-        }
-        for (int j = 0; j < sLen; j++) {
-            // add one more letter on the right side of the window
-            sMapping[s.charAt(j) - 'a']++;
-            // remove one letter from the left side of the window
-            if (j >= pLen) 
-                sMapping[s.charAt(j - pLen) - 'a']--;
-            // compare array in the sliding window with the reference array
-            if (Arrays.equals(sMapping, pMapping))
-                result.add(j - pLen + 1);
-        }
-        return result;
+  public List<Integer> findAnagrams(String s, String p) {
+    int ns = s.length(), np = p.length();
+    if (ns < np) return new ArrayList();
+
+    int [] pCount = new int[26];
+    int [] sCount = new int[26];
+    // build reference array using string p
+    for (char ch : p.toCharArray()) {
+      pCount[(int)(ch - 'a')]++;
     }
+
+    List<Integer> output = new ArrayList();
+    // sliding window on the string s
+    for (int i = 0; i < ns; ++i) {
+      // add one more letter 
+      // on the right side of the window
+      sCount[(int)(s.charAt(i) - 'a')]++;
+      // remove one letter 
+      // from the left side of the window
+      if (i >= np) {
+        sCount[(int)(s.charAt(i - np) - 'a')]--;
+      }
+      // compare array in the sliding window
+      // with the reference array
+      if (Arrays.equals(pCount, sCount)) {
+        output.add(i - np + 1);
+      }
+    }
+    return output;
+  }
 }
