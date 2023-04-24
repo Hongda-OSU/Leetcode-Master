124272bab9504b22e5288581318d25b85efa79a4
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-24 15:11:11 -0400

    Time: 7 ms (95.06%), Space: 43.3 MB (87.89%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,49 +1,21 @@
 class Solution {
-  public List<Integer> findAnagrams(String s, String p) {
-    int ns = s.length(), np = p.length();
-    if (ns < np) return new ArrayList();
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] cnt = new int[128];
+        for (char c : p.toCharArray()) cnt[c]++;
+        
+        List<Integer> ans = new ArrayList<>();
+        for (int r = 0, l = 0; r < s.length(); ++r) {
+            char c = s.charAt(r);
+            cnt[c]--;
+            while (cnt[c] < 0) { // If number of characters `c` is more than our expectation
+                cnt[s.charAt(l)]++;  // Slide left until cnt[c] == 0
+                l++;
+            }
 
-    Map<Character, Integer> pCount = new HashMap();
-    Map<Character, Integer> sCount = new HashMap();
-    // build reference hashmap using string p
-    for (char ch : p.toCharArray()) {
-      if (pCount.containsKey(ch)) {
-        pCount.put(ch, pCount.get(ch) + 1);
-      }
-      else {
-        pCount.put(ch, 1);
-      }
-    }
-
-    List<Integer> output = new ArrayList();
-    // sliding window on the string s
-    for (int i = 0; i < ns; ++i) {
-      // add one more letter 
-      // on the right side of the window
-      char ch = s.charAt(i);
-      if (sCount.containsKey(ch)) {
-        sCount.put(ch, sCount.get(ch) + 1);
-      }
-      else {
-        sCount.put(ch, 1);
-      }
-      // remove one letter 
-      // from the left side of the window
-      if (i >= np) {
-        ch = s.charAt(i - np);
-        if (sCount.get(ch) == 1) {
-          sCount.remove(ch);
+            if (r - l + 1 == p.length()) { // If we already filled enough `p.length()` chars
+                ans.add(l); // Add left index `l` to our result
+            }
         }
-        else {
-          sCount.put(ch, sCount.get(ch) - 1);
-        }
-      }
-      // compare hashmap in the sliding window
-      // with the reference hashmap
-      if (pCount.equals(sCount)) {
-        output.add(i - np + 1);
-      }
+        return ans;
     }
-    return output;
-  }
 }
