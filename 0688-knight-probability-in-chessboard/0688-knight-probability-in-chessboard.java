aaf0154c7fe589699e3b1118ef4ff9654ff21715
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-25 14:16:26 -0400

    Time: 8 ms (53.31%), Space: 41.4 MB (98.19%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,17 +1,30 @@
 class Solution {
-    private int[][]dir = new int[][]{{-2,-1},{-1,-2},{1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1}};
-    private double[][][] dp;
+    int [][] direction =new int[][]{{2,1},{-2,1},{2,-1},{-2,-1},{1,2},{1,-2},{-1,2},{-1,-2}};
+   
     public double knightProbability(int N, int K, int r, int c) {
-        dp = new double[N][N][K + 1];
-        return find(N,K,r,c);
-    }
-    public double find(int N,int K,int r,int c){
-        if(r < 0 || r > N - 1 || c < 0 || c > N - 1) return 0;
-        if(K == 0)  return 1;
-        if(dp[r][c][K] != 0) return dp[r][c][K];
-        double rate = 0;
-        for(int i = 0;i < dir.length;i++)   rate += 0.125 * find(N,K - 1,r + dir[i][0],c + dir[i][1]);
-        dp[r][c][K] = rate;
-        return rate;
+        double [][] prevWays = new double[N][N];
+        prevWays[r][c]=1;
+        double res = 0;
+        for(int k=1; k<=K;++k){
+            double [][] ways = new double[N][N];
+            for(int i=0; i<N;++i){
+                for(int j=0; j<N;++j){
+                    for(int [] dir: direction){
+                        int oldR = i-dir[0];
+                        int oldC = j-dir[1];
+                        if(oldR>=0 && oldC>=0 && oldR<N && oldC<N){
+                            ways[i][j]+=(prevWays[oldR][oldC]/8.0);
+                        }
+                    }
+                }
+            }
+            prevWays=ways;
+        }
+        for(int i=0; i<N;++i){
+            for(int j=0; j<N;++j){
+                res+=prevWays[i][j];
+            }
+        }
+        return res;
     }
 }
