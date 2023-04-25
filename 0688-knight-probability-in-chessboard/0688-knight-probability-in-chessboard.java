0badc8d6190004bb8cf11ba087102a9310b92943
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-25 14:15:46 -0400

    Time: 6 ms (77.71%), Space: 42 MB (73.49%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,29 +1,17 @@
 class Solution {
-    public double knightProbability(int N, int K, int sr, int sc) {
-        double[][] dp = new double[N][N];
-        int[] dr = new int[]{2, 2, 1, 1, -1, -1, -2, -2};
-        int[] dc = new int[]{1, -1, 2, -2, 2, -2, 1, -1};
-
-        dp[sr][sc] = 1;
-        for (; K > 0; K--) {
-            double[][] dp2 = new double[N][N];
-            for (int r = 0; r < N; r++) {
-                for (int c = 0; c < N; c++) {
-                    for (int k = 0; k < 8; k++) {
-                        int cr = r + dr[k];
-                        int cc = c + dc[k];
-                        if (0 <= cr && cr < N && 0 <= cc && cc < N) {
-                            dp2[cr][cc] += dp[r][c] / 8.0;
-                        }
-                    }
-                }
-            }
-            dp = dp2;
-        }
-        double ans = 0.0;
-        for (double[] row: dp) {
-            for (double x: row) ans += x;
-        }
-        return ans;
+    private int[][]dir = new int[][]{{-2,-1},{-1,-2},{1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1}};
+    private double[][][] dp;
+    public double knightProbability(int N, int K, int r, int c) {
+        dp = new double[N][N][K + 1];
+        return find(N,K,r,c);
+    }
+    public double find(int N,int K,int r,int c){
+        if(r < 0 || r > N - 1 || c < 0 || c > N - 1) return 0;
+        if(K == 0)  return 1;
+        if(dp[r][c][K] != 0) return dp[r][c][K];
+        double rate = 0;
+        for(int i = 0;i < dir.length;i++)   rate += 0.125 * find(N,K - 1,r + dir[i][0],c + dir[i][1]);
+        dp[r][c][K] = rate;
+        return rate;
     }
 }
