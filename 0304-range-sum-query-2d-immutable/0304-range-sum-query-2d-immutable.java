3f609f2468d5a878e017fa7c7ca538482133432f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-25 14:06:04 -0400

    Time: 134 ms (33.08%), Space: 65 MB (86.28%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,37 +1,22 @@
 class NumMatrix {
-    int preSum[][];
-public NumMatrix(int[][] matrix) {
+    private int[][] dp;
 
-	// important to get minimum time. . .
-	//without matrix[0] condtns. . faster than 78%. with it, faster than 99.97%
-	
-    if(matrix ==null || matrix.length==0 || matrix[0] == null || matrix[0].length==0)
-        preSum = new int[1][1];
-    else
-    {
-        int n = matrix.length, m=matrix[0].length;
-        preSum = new int[n+1][m+1];
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-            {
-                preSum[i+1][j+1] = matrix[i][j] + preSum[i+1][j] + preSum[i][j+1] - preSum[i][j];
-											//= left + (top- diagnal)
-			}
-			// at each index you save the sum of rectange with i, j as the bottom right and 0,0 as the top right.
-			
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) 
+            return;
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                dp[row + 1][col + 1] = dp[row + 1][col] + dp[row][col + 1] + matrix[row][col] - dp[row][col];
+            }
+        }
     }
-	
-}
-
-public int sumRegion(int row1, int col1, int row2, int col2) {
-    if(preSum.length==1 || preSum[0].length==1)
-        return 0;
     
-    return preSum[row2+1][col2+1] - preSum[row2+1][col1] + preSum[row1][col1] - preSum[row1][col2+1];
-			 
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        // Sum(ABCD)=Sum(OD)−Sum(OB)−Sum(OC)+Sum(OA)
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
 }
-}
-
 /**
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix obj = new NumMatrix(matrix);
