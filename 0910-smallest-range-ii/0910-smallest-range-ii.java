ffb9d93ef8113811912f8629029ab23db8111302
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-25 14:09:27 -0400

    Time: 9 ms (96.45%), Space: 43.5 MB (26.24%) - LeetHub

--- 0910-smallest-range-ii/0910-smallest-range-ii.java
+++ 0910-smallest-range-ii/0910-smallest-range-ii.java
@@ -1,12 +1,24 @@
 class Solution {
-   public int smallestRangeII(int[] A, int K) {
-        Arrays.sort(A);
-        int n = A.length, mx = A[n - 1], mn = A[0], res = mx - mn;
-        for (int i = 0; i < n - 1; ++i) {
-            mx = Math.max(mx, A[i] + 2 * K);
-            mn = Math.min(A[i + 1], A[0] + 2 * K);
-            res = Math.min(res, mx - mn);
+    public int smallestRangeII(int[] nums, int k) {
+        /*
+        1 2 3 4 5 6 7 8 9
+        + + + - - - - - -
+        The maximum = Math.max(lmax + k, rmax - k)
+        The minimum = Math.min(lmin + k, rmin - k)
+        */
+        Arrays.sort(nums);
+        int score = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            score = Math.min(score, getScore(nums, k, i));
         }
-        return res;
+        return score;
+    }
+    private int getScore(int[] nums, int k, int i) {
+        if (i == 0) {
+            return nums[nums.length - 1] - nums[0];
+        }
+        int max = Math.max(nums[i - 1] + k, nums[nums.length - 1] - k);
+        int min = Math.min(nums[0] + k, nums[i] - k);
+        return max - min;
     }
 }
