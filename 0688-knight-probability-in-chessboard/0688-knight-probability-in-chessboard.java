25e5874f6045e15421b87d5d19658d33c2a48495
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-25 14:33:20 -0400

    Time: 4 ms (98.80%), Space: 42.1 MB (52.71%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,39 +1,29 @@
 class Solution {
     public double knightProbability(int n, int k, int row, int column) {
-        double[][][] dp = new double[n][n][k+1];
-        for(double[][] dprow: dp) {
-            for (double[] newRow : dprow) {
-                Arrays.fill(newRow, -1);
-            }
+        double[][][] dp = new double[n][n][k + 1];
+        for (double[][] i : dp) {
+            for (double[] j : i) 
+                Arrays.fill(j, -1);
         }
-        return util (n,row,column,k,dp);
+        return helper(n, k, row, column, dp);
     }
-
-    public double util(int n, int row, int col, int k,double[][][] dp) {
-        //basecases 
-        if(row < 0 || col < 0 || row >=n || col >=n ) {
+    
+    private double helper(int n, int k, int row, int column, double[][][] dp) {
+        if (row < 0 || column < 0 || row >= n || column >= n)
             return 0;
-        }
-
-        if(k == 0) {
+        if (k == 0)
             return 1;
-        }
-
-        if(dp[row][col][k] != -1) {
-            return dp[row][col][k];
-        }
-
+        if (dp[row][column][k] != -1)
+            return dp[row][column][k];
         double prob = 0;
-
-        prob += util(n,row-2,col+1,k-1,dp)/8.0;
-        prob += util(n,row-2,col-1,k-1,dp)/8.0;
-        prob += util(n,row+2,col-1,k-1,dp)/8.0;
-        prob += util(n,row+2,col+1,k-1,dp)/8.0;
-        prob += util(n,row+1,col+2,k-1,dp)/8.0;
-        prob += util(n,row-1,col+2,k-1,dp)/8.0;
-        prob += util(n,row-1,col-2,k-1,dp)/8.0;
-        prob += util(n,row+1,col-2,k-1,dp)/8.0;
-
-        return dp[row][col][k] = prob;
+        prob += helper(n, k - 1, row - 2, column + 1, dp) / 8.0;
+        prob += helper(n, k - 1, row - 2, column - 1, dp) / 8.0;
+        prob += helper(n, k - 1, row + 2, column - 1, dp) / 8.0;
+        prob += helper(n, k - 1, row + 2, column + 1, dp) / 8.0;
+        prob += helper(n, k - 1, row - 1, column + 2, dp) / 8.0;
+        prob += helper(n, k - 1, row - 1, column - 2,  dp) / 8.0;
+        prob += helper(n, k - 1, row + 1, column - 2,  dp) / 8.0;
+        prob += helper(n, k - 1, row + 1, column + 2,  dp) / 8.0;
+        return dp[row][column][k] = prob;
     }
 }
