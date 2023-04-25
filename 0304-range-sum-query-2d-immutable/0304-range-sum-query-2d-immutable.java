30838c7ddca152079ddb0c5ba828ce87da0b61b9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-25 14:03:52 -0400

    Time: 132 ms (54.38%), Space: 70 MB (20.01%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,20 +1,17 @@
 class NumMatrix {
-    private int[][] dp;
-
+    int[][] sum;
     public NumMatrix(int[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) 
-            return;
-        dp = new int[matrix.length + 1][matrix[0].length + 1];
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[0].length; col++) {
-                dp[row + 1][col + 1] = dp[row + 1][col] + dp[row][col + 1] + matrix[row][col] - dp[row][col];
+        int m = matrix.length, n = matrix[0].length;
+        sum = new int[m + 1][n + 1]; // sum[i][j] is sum of all elements inside the rectangle [0,0,i,j]
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
     }
-    
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        // Sum(ABCD)=Sum(OD)−Sum(OB)−Sum(OC)+Sum(OA)
-        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    public int sumRegion(int r1, int c1, int r2, int c2) {
+        r1++; c1++; r2++; c2++; // Since our `sum` starts by 1 so we need to increase r1, c1, r2, c2 by 1
+        return sum[r2][c2] - sum[r2][c1 - 1] - sum[r1 - 1][c2] + sum[r1 - 1][c1 - 1];
     }
 }
 
