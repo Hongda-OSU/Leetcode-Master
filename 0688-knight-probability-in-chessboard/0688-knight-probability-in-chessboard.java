5b7e771af57f163fbbe04d042d10480e5b59c176
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-25 14:16:37 -0400

    Time: 9 ms (43.98%), Space: 41.4 MB (97.29%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -2,27 +2,25 @@ class Solution {
     int [][] direction =new int[][]{{2,1},{-2,1},{2,-1},{-2,-1},{1,2},{1,-2},{-1,2},{-1,-2}};
    
     public double knightProbability(int N, int K, int r, int c) {
-        double [][] prevWays = new double[N][N];
-        prevWays[r][c]=1;
-        double res = 0;
+        double [][][] ways = new double[K+1][N][N];
+        ways[0][r][c]=1;
         for(int k=1; k<=K;++k){
-            double [][] ways = new double[N][N];
             for(int i=0; i<N;++i){
                 for(int j=0; j<N;++j){
                     for(int [] dir: direction){
                         int oldR = i-dir[0];
                         int oldC = j-dir[1];
                         if(oldR>=0 && oldC>=0 && oldR<N && oldC<N){
-                            ways[i][j]+=(prevWays[oldR][oldC]/8.0);
+                            ways[k][i][j]+=(ways[k-1][oldR][oldC]/8.0);
                         }
                     }
                 }
             }
-            prevWays=ways;
         }
+        double res = 0;
         for(int i=0; i<N;++i){
             for(int j=0; j<N;++j){
-                res+=prevWays[i][j];
+                res+=ways[K][i][j];
             }
         }
         return res;
