7608d8feb4e39f693c9f4ca29ea2e862a90d6486
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-25 14:38:48 -0400

    Time: 12 ms (98.51%), Space: 50.3 MB (70.15%) - LeetHub

--- 0750-number-of-corner-rectangles/0750-number-of-corner-rectangles.java
+++ 0750-number-of-corner-rectangles/0750-number-of-corner-rectangles.java
@@ -1,15 +1,21 @@
 class Solution {
     public int countCornerRectangles(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int[][] dp = new int[n][n];
-        int res = 0;
-        for(int i = 0;i < m;i++) {
-            for(int j = 0;j < n;j++) {
-                if(grid[i][j] != 1) continue;
-                for(int k = j+1;k < n;k++) {
-                    if(grid[i][k] == 1) {
-                        res += dp[j][k];
-                        dp[j][k]++;
+        //init array of column pairs, as per problem statement cols less than or equals to 200
+        int[][] colPairs = new int[200][200];
+        int cols = grid[0].length, res = 0;
+        //for every row
+        for (int[] row : grid) {
+            //start loop on columns need to check every pair of columns in the row
+            for (int c1= 0; c1 < cols - 1; c1++) {
+                if (row[c1] == 1) {
+                    for (int c2 = c1+1; c2 < cols; c2++) {
+                        //if both row[c1] and row[c2] are 1 this is potentially a rectangle
+                        if (row[c2] == 1) {
+                            //get the previous value before incrementing number of rectangles will be minus 1 number of
+                            //rows cause we need two rows to form first rectangle
+                            res+=colPairs[c1][c2];
+                            ++colPairs[c1][c2];
+                        }
                     }
                 }
             }
