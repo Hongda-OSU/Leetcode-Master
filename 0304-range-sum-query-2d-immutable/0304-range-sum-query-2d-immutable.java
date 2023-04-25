edb4e57d0ec0cbd96bca28a3dc4c62fe96f262ac
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-25 14:04:31 -0400

    Time: 130 ms (81.87%), Space: 70 MB (24.85%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,18 +1,35 @@
 class NumMatrix {
-    int[][] sum;
-    public NumMatrix(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        sum = new int[m + 1][n + 1]; // sum[i][j] is sum of all elements inside the rectangle [0,0,i,j]
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
-            }
-        }
+    int preSum[][];
+public NumMatrix(int[][] matrix) {
+
+	// important to get minimum time. . .
+	//without matrix[0] condtns. . faster than 78%. with it, faster than 99.97%
+	
+    if(matrix ==null || matrix.length==0 || matrix[0] == null || matrix[0].length==0)
+        preSum = new int[1][1];
+    else
+    {
+        int n = matrix.length, m=matrix[0].length;
+        preSum = new int[n+1][m+1];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+            {
+                preSum[i+1][j+1] = matrix[i][j] + preSum[i+1][j] + preSum[i][j+1] - preSum[i][j];
+											//= left + (top- diagnal)
+			}
+			// at each index you save the sum of rectange with i, j as the bottom right and 0,0 as the top right.
+			
     }
-    public int sumRegion(int r1, int c1, int r2, int c2) {
-        r1++; c1++; r2++; c2++; // Since our `sum` starts by 1 so we need to increase r1, c1, r2, c2 by 1
-        return sum[r2][c2] - sum[r2][c1 - 1] - sum[r1 - 1][c2] + sum[r1 - 1][c1 - 1];
-    }
+	
+}
+
+public int sumRegion(int row1, int col1, int row2, int col2) {
+    if(preSum.length==1 || preSum[0].length==1)
+        return 0;
+    
+    return preSum[row2+1][col2+1] - preSum[row2+1][col1] + preSum[row1][col1] - preSum[row1][col2+1];
+			 
+}
 }
 
 /**
