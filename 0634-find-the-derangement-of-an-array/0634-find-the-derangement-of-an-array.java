113427ffc85a86a1032ef414ad30ecae1d51d857
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-26 16:13:20 -0400

    Time: 22 ms (84.09%), Space: 39.7 MB (70.45%) - LeetHub

---
+++ 0634-find-the-derangement-of-an-array/0634-find-the-derangement-of-an-array.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int findDerangement(int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return 0;
+        int first = 1, second = 0;
+        for (int i = 2; i <= n; i++) {
+            int temp = second;
+            second = (int)(((i - 1L) * (first + second)) % 1000000007);
+            first = temp;
+        }
+        return second;
+    }
+}
