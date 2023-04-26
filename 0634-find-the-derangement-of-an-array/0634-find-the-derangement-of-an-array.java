2e01c67c38844dc97dc9c6a6e295442f892155ee
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-26 16:15:25 -0400

    Time: 22 ms (84.09%), Space: 39.5 MB (86.36%) - LeetHub

--- 0634-find-the-derangement-of-an-array/0634-find-the-derangement-of-an-array.java
+++ 0634-find-the-derangement-of-an-array/0634-find-the-derangement-of-an-array.java
@@ -1,13 +1,14 @@
 class Solution {
-     final static int mod = (int)1e9 + 7;
+    private static int mod = (int)1e9 + 7;
+    
     public int findDerangement(int n) {
-        if (n <= 2) return n - 1;
-        long preDp = 0;
-        long dp = 1;
+        if (n <= 2)
+            return n - 1;
+        long prev = 0, dp = 1;
         for (int i = 3; i <= n; i++) {
-            long temp = dp;
-            dp = (i - 1) * (dp + preDp) % mod;
-            preDp = temp;
+            long tmp = dp;
+            dp = (i - 1) * (dp + prev) % mod;
+            prev = tmp;
         }
         return (int)dp;
     }
