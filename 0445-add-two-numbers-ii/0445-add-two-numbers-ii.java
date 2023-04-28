cda4d674841a07e7e92c8e160c83079fe3e92715
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-27 21:41:51 -0400

    Time: 6 ms (30.59%), Space: 42.6 MB (80.04%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -1,70 +1,29 @@
-class Solution {
+public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // find the length of both lists
-        int n1 = 0, n2 = 0;
-        ListNode curr1 = l1, curr2 = l2;
-        while (curr1 != null) {
-            curr1 = curr1.next; 
-            ++n1;    
-        }
-        while (curr2 != null) {
-            curr2 = curr2.next; 
-            ++n2;    
-        }
-            
-        // parse both lists
-        // and sum the corresponding positions 
-        // without taking carry into account
-        // 3->3->3 + 7->7 --> 3->10->10--> 10->10->3
-        curr1 = l1; 
-        curr2 = l2;
-        ListNode head = null;
-        while (n1 > 0 && n2 > 0) {
-            int val = 0;
-            if (n1 >= n2) {
-                val += curr1.val; 
-                curr1 = curr1.next; 
-                --n1;    
-            }
-            if (n1 < n2) {
-                val += curr2.val; 
-                curr2 = curr2.next;
-                --n2;    
-            }
-                
-            // update the result: add to front
-            ListNode curr = new ListNode(val);
-            curr.next = head;
-            head = curr;    
-        }
-
-        // take the carry into account
-        // to have all elements to be less than 10
-        // 10->10->3 --> 0->1->4 --> 4->1->0
-        curr1 = head;
-        head = null;
-        int carry = 0;
-        while (curr1 != null) {
-            // current sum and carry
-            int val = (curr1.val + carry) % 10;
-            carry = (curr1.val + carry) / 10;
-            
-            // update the result: add to front
-            ListNode curr = new ListNode(val);
-            curr.next = head;
-            head = curr;
-
-            // move to the next elements in the list
-            curr1 = curr1.next;    
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        };
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
         }
         
-        // add the last carry
-        if (carry != 0) {
-            ListNode curr = new ListNode(carry);
-            curr.next = head;
-            head = curr;    
+        int sum = 0;
+        ListNode list = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            list.val = sum % 10;
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
         }
-
-        return head;
+        
+        return list.val == 0 ? list.next : list;
     }
 }
