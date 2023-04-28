ef72ac7ec3ef7300a7b2fb9d48fbd9c3e2f43c26
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-27 22:54:32 -0400

    Time: 2 ms (100.00%), Space: 40.2 MB (36.67%) - LeetHub

--- 0418-sentence-screen-fitting/0418-sentence-screen-fitting.java
+++ 0418-sentence-screen-fitting/0418-sentence-screen-fitting.java
@@ -1,8 +1,48 @@
 class Solution {
-     public int wordsTyping(String[] sentence, int rows, int cols) {
-        String s = String.join(" ", sentence) + " ";
-        int[] offset = new int[s.length()];
-        IntStream.range(1, s.length()).forEach(i -> offset[i] = s.charAt(i) == ' ' ? 1 : offset[i-1]-1);
-        return IntStream.range(0, rows).reduce(0, (a, b) -> a + cols + offset[(a+cols) % s.length()]) / s.length();
+    public int wordsTyping(String[] sentence, int rows, int cols) {
+        int n = sentence.length, total = 0;
+        int[] arr = new int[n];
+        for(int i=0; i<n; ++i) {
+            arr[i] = sentence[i].length(); // Collect word lengths
+            if(arr[i] > cols) // Early terminate if any word larger than cols
+                return 0;
+            total += arr[i]; // Note sum of lengths
+        }
+        
+        total += n; // Size of a loop including spaces
+        int loopsPerRow = cols/total; // This much is guaranteed per row
+        int slotsAfterLoops = cols%total;
+        
+        /* store[i]: Pair {count, end} corresponding to a row starting with i'th word
+            count: Number of times we complete the sentence in this row
+            end: Starting word for the NEXT row */
+        int[][] store = new int[n][];
+        for(int i=0; i<n; ++i) {
+            int count = loopsPerRow, slots = slotsAfterLoops, end = i;
+            while(slots >= nextWordCost(arr, end, end != i)) {
+                slots -= nextWordCost(arr, end, end != i);
+                if(end == n-1)
+                    ++count;
+                end = inc(end, n);
+            }
+            store[i] = new int[] {count, end};
+        }
+        
+        // Compute the answer - evaluating row by row
+        int ans = 0, index = 0;
+        while(rows > 0) {
+            ans += store[index][0];
+            index = store[index][1];
+            --rows;
+        }
+        return ans;
+    }
+    
+    private int nextWordCost(int[] arr, int index, boolean spaceNeeded) {
+        return (spaceNeeded ? 1 : 0) + arr[index];
+    }
+    
+    private int inc(int i, int n) {
+        return (++i >= n) ? (i - n) : i;
     }
 }
