270ef51ae91c8b9f1ece1edb48ee15f84e7ee463
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-28 13:27:35 -0400

    Time: 37 ms (97.55%), Space: 42.5 MB (43.23%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,28 +1,25 @@
 class Solution {
-   public int maxCoins(int[] nums) {
-	// add dummy head and tail
-    int helpNum[] = new int[nums.length + 2]; 
-    for(int i = 1; i <= nums.length; i++)
-        helpNum[i] = nums[i - 1];
-    helpNum[0] = 1;
-    helpNum[nums.length + 1] = 1;
-	
-    int sum[][] = new int[helpNum.length][helpNum.length];
-    for(int gap = 0; gap < nums.length; gap++) {
-        for(int begin = 1; begin < nums.length - gap + 1; begin++) { // we don't consider the dummy head and tail
-            int end = begin + gap;
-            sum[begin][end] = Integer.MIN_VALUE;
-            int left = helpNum[begin - 1];
-            int right = helpNum[end + 1];
-			
-			// head and tail for current subrange need to be special processed
-            sum[begin][end] = Math.max(sum[begin][end], sum[begin + 1][end] + helpNum[begin] * left * right);
-            sum[begin][end] = Math.max(sum[begin][end], sum[begin][end - 1] + helpNum[end] * left * right);
-			
-            for(int j = begin + 1; j < end; j++)
-                sum[begin][end] = Math.max(sum[begin][end], sum[begin][j - 1] + sum[j + 1][end] + helpNum[j] * left * right);
+    public int maxCoins(int[] nums) {
+        int nlen = nums.length;
+        int[] arr = new int[nlen + 2];
+        int alen = arr.length;
+        for (int i = 1; i <= nlen; i++)
+            arr[i] = nums[i - 1];
+        arr[0] = 1;
+        arr[nlen + 1] = 1;
+        int sum[][] = new int[alen][alen];
+        for (int gap = 0; gap < nlen; gap++) {
+            for (int begin = 1; begin < nlen - gap + 1; begin++) {
+                int end = begin + gap;
+                sum[begin][end] = Integer.MIN_VALUE;
+                int left = arr[begin - 1];
+                int right = arr[end + 1];
+                sum[begin][end] = Math.max(sum[begin][end], sum[begin + 1][end] + arr[begin] * left * right);
+                sum[begin][end] = Math.max(sum[begin][end], sum[begin][end - 1] + arr[end] * left * right);
+                for (int j = begin + 1; j < end; j++)
+                    sum[begin][end] = Math.max(sum[begin][end], sum[begin][j - 1] + sum[j + 1][end] + arr[j] * left * right);
+            }
         }
+        return sum[1][nums.length];
     }
-    return sum[1][nums.length];
-}
 }
