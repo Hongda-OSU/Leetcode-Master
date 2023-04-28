2d27dc561703b4113656f4c0ec1e17024e4cff26
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-28 13:13:45 -0400

    Time: 37 ms (97.55%), Space: 42.7 MB (24.62%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,26 +1,28 @@
 class Solution {
    public int maxCoins(int[] nums) {
-	// Corner Case
-	if(nums == null || nums.length == 0) return 0;
+	// add dummy head and tail
+    int helpNum[] = new int[nums.length + 2]; 
+    for(int i = 1; i <= nums.length; i++)
+        helpNum[i] = nums[i - 1];
+    helpNum[0] = 1;
+    helpNum[nums.length + 1] = 1;
 	
-	int len = nums.length;
-	int[][] dp = new int[len][len];
-	
-
-	for(int i = nums.length - 1; i >= 0; i--) {	// i represent the left closed idx of str
-		for(int j = i; j <= nums.length - 1; j++) { // j represent the right closed idx 
-			for(int k = i; k <= j; k++) {
-				int val = 0;
-				val += (k == i) ? 0 : dp[i][k-1];
-				val += (k == j) ? 0 : dp[k+1][j];
-	
-				// Here is nums[i-1/j+1] instead of nums[k-1/k+1]
-				// Since ?*nums[k]*? executed at last.
-				val += ((i==0) ? 1 : nums[i-1]) * nums[k] * ((j==len-1) ? 1 : nums[j+1]);
-				dp[i][j] = Math.max(dp[i][j], val);
-			}
-		}
-	}
-	return dp[0][len-1];
+    int sum[][] = new int[helpNum.length][helpNum.length];
+    for(int gap = 0; gap < nums.length; gap++) {
+        for(int begin = 1; begin < nums.length - gap + 1; begin++) { // we don't consider the dummy head and tail
+            int end = begin + gap;
+            sum[begin][end] = Integer.MIN_VALUE;
+            int left = helpNum[begin - 1];
+            int right = helpNum[end + 1];
+			
+			// head and tail for current subrange need to be special processed
+            sum[begin][end] = Math.max(sum[begin][end], sum[begin + 1][end] + helpNum[begin] * left * right);
+            sum[begin][end] = Math.max(sum[begin][end], sum[begin][end - 1] + helpNum[end] * left * right);
+			
+            for(int j = begin + 1; j < end; j++)
+                sum[begin][end] = Math.max(sum[begin][end], sum[begin][j - 1] + sum[j + 1][end] + helpNum[j] * left * right);
+        }
+    }
+    return sum[1][nums.length];
 }
 }
