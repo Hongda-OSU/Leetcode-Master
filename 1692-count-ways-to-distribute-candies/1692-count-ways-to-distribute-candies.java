29c9812c855c9a208fb0fe5420fb96b0745a658d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-27 22:50:35 -0400

    Time: 153 ms (100.00%), Space: 40 MB (94.12%) - LeetHub

--- 1692-count-ways-to-distribute-candies/1692-count-ways-to-distribute-candies.java
+++ 1692-count-ways-to-distribute-candies/1692-count-ways-to-distribute-candies.java
@@ -1,17 +1,16 @@
 class Solution {
-     static final long MOD=1_000_000_007;
     public int waysToDistribute(int n, int k) {
-        long mod=1_000_000_007;
-        long[] memo=new long[n];
-        Arrays.fill(memo,1);
-        for(int bags=2;bags<=k;bags++) {
-            long diag=memo[bags-1], last=1;
-            for(int candies=bags;candies<n;candies++) {
-                long tmp=memo[candies];
-                last=memo[candies]=(diag+bags*last)%mod;
-                diag=tmp;
+        long mod = 1_000_000_007;
+        long[] memo = new long[n];
+        Arrays.fill(memo, 1);
+        for (int bags = 2; bags <= k; bags++) {
+            long diag = memo[bags - 1], last = 1;
+            for (int candies = bags; candies < n; candies++) {
+                long tmp = memo[candies];
+                last = memo[candies] = (diag + bags * last) % mod;
+                diag = tmp;
             }
         }
-        return (int)memo[n-1];
+        return (int)memo[n - 1];
     }
 }
