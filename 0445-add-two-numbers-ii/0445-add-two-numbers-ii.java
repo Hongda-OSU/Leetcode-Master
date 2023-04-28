bfd756b66e9df49a688009192c4e6bcdf9bf304b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-27 21:49:06 -0400

    Time: 2 ms (100.00%), Space: 42.8 MB (58.79%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -1,29 +1,60 @@
-public class Solution {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> s1 = new Stack<Integer>();
-        Stack<Integer> s2 = new Stack<Integer>();
-        
-        while(l1 != null) {
-            s1.push(l1.val);
-            l1 = l1.next;
-        };
-        while(l2 != null) {
-            s2.push(l2.val);
-            l2 = l2.next;
+        int n1 = 0, n2 = 0;
+        ListNode curr1 = l1, curr2 = l2;
+        while (curr1 != null) {
+            curr1 = curr1.next;
+            n1++;
         }
-        
-        int sum = 0;
-        ListNode list = new ListNode(0);
-        while (!s1.empty() || !s2.empty()) {
-            if (!s1.empty()) sum += s1.pop();
-            if (!s2.empty()) sum += s2.pop();
-            list.val = sum % 10;
-            ListNode head = new ListNode(sum / 10);
-            head.next = list;
-            list = head;
-            sum /= 10;
+        while (curr2 != null) {
+            curr2 = curr2.next;
+            n2++;
         }
-        
-        return list.val == 0 ? list.next : list;
+        curr1 = l1;
+        curr2 = l2;
+        ListNode head = null;
+        while (n1 > 0 && n2 > 0) {
+            int val = 0;
+            if (n1 >= n2) {
+                val += curr1.val;
+                curr1 = curr1.next;
+                n1--;
+            }
+            if (n1 < n2) {
+                val += curr2.val;
+                curr2 = curr2.next;
+                n2--;
+            }
+            ListNode curr = new ListNode(val);
+            curr.next = head;
+            head = curr;
+        }
+        curr1 = head;
+        head = null;
+        int carry = 0;
+        while (curr1 != null) {
+            int val = (curr1.val + carry) % 10;
+            carry = (curr1.val + carry) / 10;
+            ListNode curr = new ListNode(val);
+            curr.next = head;
+            head = curr;
+            curr1 = curr1.next;
+        }
+        if (carry != 0) {
+            ListNode curr = new ListNode(carry);
+            curr.next = head;
+            head = curr;
+        }
+        return head;
     }
 }
