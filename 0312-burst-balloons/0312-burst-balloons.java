ac18ed6ab881e74746473bf30155461a0c68a6dd
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-28 13:12:36 -0400

    Time: 128 ms (35.00%), Space: 42.5 MB (43.23%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -6,26 +6,37 @@ class Solution {
         System.arraycopy(nums, 0, newNums, 1, n - 2);
         newNums[0] = 1;
         newNums[n - 1] = 1;
-        // dp[i][j] represents
-        // maximum if we burst all nums[left]...nums[right], inclusive
-        int[][] dp = new int[n][n];
-        // do not include the first one and the last one
-        // since they are both fake balloons added by ourselves and we can not burst
-        // them
-        for (int left = n - 2; left >= 1; left--) {
-            for (int right = left; right <= n - 2; right++) {
-                // find the last burst one in newNums[left]...newNums[right]
-                for (int i = left; i <= right; i++) {
-                    // newNums[i] is the last burst one
-                    int gain = newNums[left - 1] * newNums[i] * newNums[right + 1];
-                    // recursively call left side and right side
-                    int remaining = dp[left][i - 1] + dp[i + 1][right];
-                    // update
-                    dp[left][right] = Math.max(remaining + gain, dp[left][right]);
-                }
-            }
+
+        // cache the results of dp
+        int[][] memo = new int[n][n];
+
+        // we can not burst the first one and the last one
+        // since they are both fake balloons added by ourselves
+        return dp(memo, newNums, 1, n - 2);
+    }
+
+    public int dp(int[][] memo, int[] nums, int left, int right) {
+        // return maximum if we burst all nums[left]...nums[right], inclusive
+        if (right - left < 0) {
+            return 0;
         }
-        // burst newNums[1]...newNums[n-2], excluding the first one and the last one
-        return dp[1][n - 2];
+
+        // we've already seen this, return from cache
+        if (memo[left][right] > 0) {
+            return memo[left][right];
+        }
+
+        // find the last burst one in nums[left]...nums[right]
+        int result = 0;
+        for (int i = left; i <= right; i++) {
+            // nums[i] is the last burst one
+            int gain = nums[left - 1] * nums[i] * nums[right + 1];
+            // nums[i] is fixed, recursively call left side and right side
+            int remaining = dp(memo, nums, left, i - 1) + dp(memo, nums, i + 1, right);
+            result = Math.max(result, remaining + gain);
+        }
+        // add to the cache
+        memo[left][right] = result;
+        return result;
     }
 }
