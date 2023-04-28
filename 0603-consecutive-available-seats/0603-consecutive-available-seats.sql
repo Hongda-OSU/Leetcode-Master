a37880ca5a4e2c37c905aa2825f4de35c5b0e4dd
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-28 13:47:06 -0400

    Time: 831 ms (31.12%), Space: 0B (100.00%) - LeetHub

---
+++ 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT DISTINCT a.seat_id
+FROM Cinema a
+JOIN Cinema b
+ON ABS(a.seat_id - b.seat_id) = 1
+AND a.free = true
+AND b.free = true
+ORDER BY a.seat_id;
