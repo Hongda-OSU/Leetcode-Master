f11b7e40aabebda53fbba5096bd1d778bf34388b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-28 13:13:15 -0400

    Time: 92 ms (55.03%), Space: 42.8 MB (16.07%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,42 +1,26 @@
 class Solution {
-    public int maxCoins(int[] nums) {
-        // add 1 before and after nums
-        int n = nums.length + 2;
-        int[] newNums = new int[n];
-        System.arraycopy(nums, 0, newNums, 1, n - 2);
-        newNums[0] = 1;
-        newNums[n - 1] = 1;
+   public int maxCoins(int[] nums) {
+	// Corner Case
+	if(nums == null || nums.length == 0) return 0;
+	
+	int len = nums.length;
+	int[][] dp = new int[len][len];
+	
 
-        // cache the results of dp
-        int[][] memo = new int[n][n];
-
-        // we can not burst the first one and the last one
-        // since they are both fake balloons added by ourselves
-        return dp(memo, newNums, 1, n - 2);
-    }
-
-    public int dp(int[][] memo, int[] nums, int left, int right) {
-        // return maximum if we burst all nums[left]...nums[right], inclusive
-        if (right - left < 0) {
-            return 0;
-        }
-
-        // we've already seen this, return from cache
-        if (memo[left][right] > 0) {
-            return memo[left][right];
-        }
-
-        // find the last burst one in nums[left]...nums[right]
-        int result = 0;
-        for (int i = left; i <= right; i++) {
-            // nums[i] is the last burst one
-            int gain = nums[left - 1] * nums[i] * nums[right + 1];
-            // nums[i] is fixed, recursively call left side and right side
-            int remaining = dp(memo, nums, left, i - 1) + dp(memo, nums, i + 1, right);
-            result = Math.max(result, remaining + gain);
-        }
-        // add to the cache
-        memo[left][right] = result;
-        return result;
-    }
+	for(int i = nums.length - 1; i >= 0; i--) {	// i represent the left closed idx of str
+		for(int j = i; j <= nums.length - 1; j++) { // j represent the right closed idx 
+			for(int k = i; k <= j; k++) {
+				int val = 0;
+				val += (k == i) ? 0 : dp[i][k-1];
+				val += (k == j) ? 0 : dp[k+1][j];
+	
+				// Here is nums[i-1/j+1] instead of nums[k-1/k+1]
+				// Since ?*nums[k]*? executed at last.
+				val += ((i==0) ? 1 : nums[i-1]) * nums[k] * ((j==len-1) ? 1 : nums[j+1]);
+				dp[i][j] = Math.max(dp[i][j], val);
+			}
+		}
+	}
+	return dp[0][len-1];
+}
 }
