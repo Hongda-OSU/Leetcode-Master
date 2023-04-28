4a9f43f72a49b822b028817ba3a6440a69093485
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-28 13:09:36 -0400

    Time: 18 ms (91.67%), Space: 47 MB (7.64%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -13,55 +13,30 @@
  *     }
  * }
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode(int x) { val = x; }
- * }
- */
 class BSTIterator {
-
-    ArrayList<Integer> nodesSorted;
-    int index;
+    private ArrayList<Integer> nodesSorted;
+    private int index;
 
     public BSTIterator(TreeNode root) {
-
-        // Array containing all the nodes in the sorted order
-        this.nodesSorted = new ArrayList<Integer>();
-
-        // Pointer to the next smallest element in the BST
-        this.index = -1;
-
-        // Call to flatten the input binary search tree
-        this._inorder(root);
+        nodesSorted = new ArrayList<>();
+        index = -1;
+        inorder(root);
     }
-
-    private void _inorder(TreeNode root) {
-
-        if (root == null) {
+    
+    private void inorder(TreeNode root) {
+        if (root == null)
             return;
-        }
-
-        this._inorder(root.left);
-        this.nodesSorted.add(root.val);
-        this._inorder(root.right);
+        inorder(root.left);
+        nodesSorted.add(root.val);
+        inorder(root.right);
     }
-
-    /**
-     * @return the next smallest number
-     */
+    
     public int next() {
-        return this.nodesSorted.get(++this.index);
+        return nodesSorted.get(++index);
     }
-
-    /**
-     * @return whether we have a next smallest number
-     */
+    
     public boolean hasNext() {
-        return this.index + 1 < this.nodesSorted.size();
+        return index + 1 < nodesSorted.size();
     }
 }
 
