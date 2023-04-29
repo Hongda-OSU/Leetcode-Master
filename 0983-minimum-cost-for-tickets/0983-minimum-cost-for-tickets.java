1460467a93a36401e916c04024bec3c340072c8b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-29 17:25:48 -0400

    Time: 0 ms (100.00%), Space: 41 MB (41.41%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,31 +1,27 @@
 class Solution {
-    int[] days, costs;
-    Integer[] memo;
-    int[] durations = new int[]{1, 7, 30};
-
+    private static int[] days, costs;
+    private static Integer[] memo;
+    private static final int[] durations = new int[]{1, 7, 30};
+    
     public int mincostTickets(int[] days, int[] costs) {
         this.days = days;
         this.costs = costs;
         memo = new Integer[days.length];
-
         return dp(0);
     }
-
-    public int dp(int i) {
+    
+    private int dp(int i) {
         if (i >= days.length)
             return 0;
         if (memo[i] != null)
             return memo[i];
-
-        int ans = Integer.MAX_VALUE;
-        int j = i;
-        for (int k = 0; k < 3; ++k) {
+        int result = Integer.MAX_VALUE, j = i;
+        for (int k = 0; k < 3; k++) {
             while (j < days.length && days[j] < days[i] + durations[k])
                 j++;
-            ans = Math.min(ans, dp(j) + costs[k]);
+            result = Math.min(result, dp(j) + costs[k]);
         }
-
-        memo[i] = ans;
-        return ans;
+        memo[i] = result;
+        return result;
     }
 }
