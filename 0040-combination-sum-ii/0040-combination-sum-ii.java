422ab5d926985e742dc82362929f843dc9c5f6d7
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-29 17:13:38 -0400

    Time: 3 ms (67.08%), Space: 42.8 MB (60.08%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,35 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<>();
-        LinkedList<Integer> comb = new LinkedList<>();
-
+        List<List<Integer>> result = new ArrayList<>();
+        LinkedList<Integer> combine = new LinkedList<>();
         Arrays.sort(candidates);
-
-        backtrack(candidates, comb, target, 0, results);
-        return results;
+        backtrack(candidates, combine, target, 0, result);
+        return result;
     }
-
-    private void backtrack(int[] candidates, LinkedList<Integer> comb,
-                           Integer remain, Integer curr,
-                           List<List<Integer>> results) {
-        if (remain == 0) {
-            // copy the current combination to the final list.
-            results.add(new ArrayList<Integer>(comb));
+    
+    private void backtrack(int[] candidates, LinkedList<Integer> combine, int target, int curr, List<List<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<>(combine));
             return;
         }
-
-        for (int nextCurr = curr; nextCurr < candidates.length; ++nextCurr) {
-            if (nextCurr > curr && candidates[nextCurr] == candidates[nextCurr - 1])
+        for (int next = curr; next < candidates.length; next++) {
+            if (next > curr && candidates[next] == candidates[next - 1])
                 continue;
-
-            Integer pick = candidates[nextCurr];
-            // optimization: early stopping
-            if (remain - pick < 0)
+            int pick = candidates[next];
+            if (target - pick < 0) 
                 break;
-
-            comb.addLast(pick);
-            backtrack(candidates, comb, remain - pick, nextCurr + 1, results);
-            comb.removeLast();
+            combine.addLast(pick);
+            backtrack(candidates, combine, target - pick, next + 1, result);
+            combine.removeLast();
         }
     }
 }
