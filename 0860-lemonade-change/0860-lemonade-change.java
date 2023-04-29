88a5548791796fb0683ce6873e4dc032e081dcab
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-29 16:57:19 -0400

    Time: 2 ms (72.45%), Space: 54.7 MB (41.73%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,20 +1,28 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-         int five = 0, ten = 0;
-    for (int i : bills) {
-        if (i == 5)  //if customer gives $5 then we simply take it as lemonade costs $5 
-            five++;
-        else if (i == 10) { //if we have $10 then we reduce number of $5 bills by one and increase $10 by one
-            five--; ten++;
+         int count1=0 , count2=0;
+        for(int i=0;i<bills.length;i++){
+            if(bills[i]==5)
+                count1++;
+            else if(bills[i]==10){
+                if(count1<1)
+                    return false;
+                else{
+                    count1--;
+                    count2++;
+                }
+            }
+            else if(bills[i]==20){
+                if(count2>0 && count1>0){
+                    count2--;
+                    count1--;
+                }
+                else if(count2<1 && count1>=3)
+                    count1-=3;
+                else
+                    return false;
+            }
         }
-        else if (ten > 0 && five>=1) { //if we get $20 then we first try to give back $10+$5
-            ten--; five--;
-        }
-        else        // if we cannot give back $10+$5 then unfortunately we have to give back $5+$5+$5
-            five -= 3;
-        if (five < 0)  // if at any time we do not have any $5 left then we return false
-            return false;
-    }
-    return true;
+        return true;
     }
 }
