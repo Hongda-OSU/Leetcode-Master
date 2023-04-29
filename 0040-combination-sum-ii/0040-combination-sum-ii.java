9216f476c50e93efbadbb84cd8849530d95e718b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-29 17:07:51 -0400

    Time: 2 ms (99.67%), Space: 43.1 MB (37.26%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,22 +1,35 @@
 class Solution {
-   public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<>();
+        LinkedList<Integer> comb = new LinkedList<>();
+
         Arrays.sort(candidates);
-        backtrack(result, new ArrayList<>(), candidates, target, 0);
-        return result;
+
+        backtrack(candidates, comb, target, 0, results);
+        return results;
     }
-    
-    public void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start) {
-        if (remain < 0) return;
+
+    private void backtrack(int[] candidates, LinkedList<Integer> comb,
+                           Integer remain, Integer curr,
+                           List<List<Integer>> results) {
         if (remain == 0) {
-            result.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = start; i < candidates.length; i++) {
-                if (i > start && candidates[i] == candidates[i - 1]) continue; // skip duplicates
-                tempList.add(candidates[i]);
-                backtrack(result, tempList, candidates, remain - candidates[i], i + 1);
-                tempList.remove(tempList.size() - 1);
-            }
+            // copy the current combination to the final list.
+            results.add(new ArrayList<Integer>(comb));
+            return;
+        }
+
+        for (int nextCurr = curr; nextCurr < candidates.length; ++nextCurr) {
+            if (nextCurr > curr && candidates[nextCurr] == candidates[nextCurr - 1])
+                continue;
+
+            Integer pick = candidates[nextCurr];
+            // optimization: early stopping
+            if (remain - pick < 0)
+                break;
+
+            comb.addLast(pick);
+            backtrack(candidates, comb, remain - pick, nextCurr + 1, results);
+            comb.removeLast();
         }
     }
 }
