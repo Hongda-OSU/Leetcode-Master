50e6f958b4542c8406b77a239dbc9a724c3b9029
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-29 17:16:47 -0400

    Time: 0 ms (100.00%), Space: 40.6 MB (68.86%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,19 +1,31 @@
 class Solution {
+    int[] days, costs;
+    Integer[] memo;
+    int[] durations = new int[]{1, 7, 30};
+
     public int mincostTickets(int[] days, int[] costs) {
-        int lastDay = days[days.length - 1];
-        int[] dp = new int[lastDay + 1];
-        boolean[] isTravelDays = new boolean[lastDay + 1];
-        for (int day : days)
-            isTravelDays[day] = true;
-        for (int i = 1; i <= lastDay; i++) {
-            if (!isTravelDays[i]) {
-                dp[i] = dp[i - 1];
-                continue;
-            }
-            dp[i] = costs[0] + dp[i - 1];
-            dp[i] = Math.min(costs[1] + dp[Math.max(i - 7, 0)], dp[i]);
-            dp[i] = Math.min(costs[2] + dp[Math.max(i - 30, 0)], dp[i]);
+        this.days = days;
+        this.costs = costs;
+        memo = new Integer[days.length];
+
+        return dp(0);
+    }
+
+    public int dp(int i) {
+        if (i >= days.length)
+            return 0;
+        if (memo[i] != null)
+            return memo[i];
+
+        int ans = Integer.MAX_VALUE;
+        int j = i;
+        for (int k = 0; k < 3; ++k) {
+            while (j < days.length && days[j] < days[i] + durations[k])
+                j++;
+            ans = Math.min(ans, dp(j) + costs[k]);
         }
-        return dp[lastDay];
+
+        memo[i] = ans;
+        return ans;
     }
 }
