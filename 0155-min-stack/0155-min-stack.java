f8676068ce7d0032f154e383c3dd423adae68b7d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-30 15:05:14 -0400

    Time: 6 ms (47.17%), Space: 44.5 MB (38.09%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,35 +1,36 @@
 class MinStack {
-
-    private Stack<Integer> stack = new Stack<>();
-    private Stack<Integer> minStack = new Stack<>();
     
+    private Stack<int[]> stack = new Stack<>();
     
     public MinStack() { }
     
     
     public void push(int x) {
-        stack.push(x);
-        if (minStack.isEmpty() || x <= minStack.peek()) {
-            minStack.push(x);
+        
+        /* If the stack is empty, then the min value
+         * must just be the first value we add. */
+        if (stack.isEmpty()) {
+            stack.push(new int[]{x, x});
+            return;
         }
+        
+        int currentMin = stack.peek()[1];
+        stack.push(new int[]{x, Math.min(x, currentMin)});
     }
     
     
     public void pop() {
-        if (stack.peek().equals(minStack.peek())) {
-            minStack.pop();
-        }
         stack.pop();
     }
     
     
     public int top() {
-        return stack.peek();
+        return stack.peek()[0];
     }
-
+    
     
     public int getMin() {
-        return minStack.peek();
+        return stack.peek()[1];
     }
 }
 
