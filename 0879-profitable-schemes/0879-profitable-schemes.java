b80291569089003aacef77388e712b08757e8f51
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-30 16:11:29 -0400

    Time: 138 ms (20.97%), Space: 53.1 MB (20.52%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -1,27 +1,36 @@
 class Solution {
     int mod = 1000000007;
-    int[][][] dp = new int[101][101][101];
-
-    public int profitableSchemes(int n, int minProfit, int[] group, int[] profits) {
-        // Initializing the base case.
-        for (int count = 0; count <= n; count++) {
-            dp[group.length][count][minProfit] = 1;
+    int[][][] memo = new int[101][101][101];
+    
+    int find(int pos, int count, int profit, int n, int minProfit, int[] group, int[] profits) {
+        if (pos == group.length) {
+            // If profit exceeds the minimum required; it's a profitable scheme.
+            return profit >= minProfit ? 1 : 0;
         }
-
-        for (int index = group.length - 1; index >= 0; index--) {
-            for (int count = 0; count <= n; count++) {
-                for(int profit = 0; profit <= minProfit; profit++) {
-                    // Ways to get a profitable scheme without this crime.
-                    dp[index][count][profit] = dp[index + 1][count][profit];
-                    if (count + group[index] <= n) {
-                        // Adding ways to get profitable schemes, including this crime.
-                        dp[index][count][profit]
-                                = (dp[index][count][profit] + dp[index + 1][count + group[index]][Math.min(minProfit, profit + profits[index])]) % mod;
-                    }
-                }
+        
+        if (memo[pos][count][profit] != -1) {
+            // Repeated subproblem, return the stored answer.
+            return memo[pos][count][profit];
+        }
+        
+        // Ways to get a profitable scheme without this crime.
+        int totalWays = find(pos + 1, count, profit, n, minProfit, group, profits);
+        if (count + group[pos] <= n) {
+            // Adding ways to get profitable schemes, including this crime.
+            totalWays += find(pos + 1, count + group[pos], Math.min(minProfit, profit + profits[pos]), n, minProfit, group, profits);
+        }
+        
+        return memo[pos][count][profit] = totalWays % mod;
+    }
+    
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        // Initializing all states as -1.
+        for (int i = 0; i <= group.length; i++) {
+            for(int j = 0; j <= n; j++) {
+                Arrays.fill(memo[i][j], -1);
             }
         }
-
-        return dp[0][0][0];
+        
+        return find(0, 0, 0, n, minProfit, group, profit);
     }
 }
