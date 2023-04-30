81a02daba297bbe4433ed1b907509108bf914dbf
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-04-30 16:13:09 -0400

    Time: 26 ms (89.81%), Space: 40.9 MB (77.86%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -1,21 +1,20 @@
 class Solution {
     private int mod = (int)1e9 + 7;
-    public int profitableSchemes(int n, int m, int[] group, int[] profit) {
-    
-        int[][] dp = new int[n + 1][m + 1];
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        int[][] dp = new int[n + 1][minProfit + 1];
         dp[0][0] = 1;
         for (int k = 1; k <= group.length; k++) {
             int g = group[k - 1];
             int p = profit[k - 1];
             for (int i = n; i >= g; i--) {
-                for (int j = m; j >= 0; j--) {
+                for (int j = minProfit; j >= 0; j--) {
                     dp[i][j] = (dp[i][j] + dp[i - g][Math.max(0, j - p)])%mod;
                 }
             }
         }
         int sum = 0;                                                       
         for(int i = 0; i <= n; i++){
-            sum = (sum + dp[i][m])%mod;
+            sum = (sum + dp[i][minProfit])%mod;
         }
         return sum;
     }
