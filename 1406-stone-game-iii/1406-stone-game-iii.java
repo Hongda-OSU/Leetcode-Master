138b7c27412405ad9d3cf07afc035b555252951a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-01 15:51:34 -0400

    Time: 11 ms (78.49%), Space: 51.3 MB (69.89%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,47 +1,15 @@
 class Solution {
-   Integer[] cache;
-    
-    public String stoneGameIII(int[] piles) {
-        int n = piles.length;
-        
-        cache = new Integer[n + 1];
-        
-        //AliceScore = Stone Collected by Alice - Stone Collected by Bob
-        int AliceScore = getAliceScore(piles, 0);
-        
-        if(AliceScore > 0) return "Alice";
-        
-        if(AliceScore < 0) return "Bob";
-        
-        return "Tie";
-    }
-    
-    private int getAliceScore(int[] piles, int index){
-        if(index == piles.length) return 0;
-        
-        //reterive from cache
-        if(cache[index] != null) return cache[index];
-        
-        int maxScore = Integer.MIN_VALUE;
-        
-        int stone = 0;
-        
-        for(int x = 0; x < 3; x++){
-            int i = index + x;
-            
-            //ignore the invalid index
-            if(i >= piles.length) continue;
-            
-            stone += piles[i];
-
-            //calculate score
-            int score = stone - getAliceScore(piles, i + 1);
-            
-            //update score
-            maxScore = Math.max(maxScore, score);
+     public String stoneGameIII(int[] A) {
+        int n = A.length, dp[] = new int[n+1];
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = Integer.MIN_VALUE;
+            for (int k = 0, take = 0; k < 3 && i + k < n; ++k) {
+                take += A[i + k];
+                dp[i] = Math.max(dp[i], take - dp[i + k + 1]);
+            }
         }
-        
-        //save in cache
-        return cache[index] = maxScore;
+        if (dp[0] > 0) return "Alice";
+        if (dp[0] < 0) return "Bob";
+        return "Tie";
     }
 }
