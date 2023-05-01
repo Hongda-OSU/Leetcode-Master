7cb42f12e54c75f2dfd5bd8fb7f51eb902ea1fc1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-01 15:50:15 -0400

    Time: 11 ms (78.49%), Space: 50.8 MB (91.40%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,39 +1,15 @@
 class Solution {
-    int notComputed = 1000000000;
-    int[] dp;
-
-    private int f(int[] stoneValue, int n, int i) {
-        if (i == n) {
-            return 0;
+  public String stoneGameIII(int[] A) {
+        int n = A.length, dp[] = new int[n+1];
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = Integer.MIN_VALUE;
+            for (int k = 0, take = 0; k < 3 && i + k < n; ++k) {
+                take += A[i + k];
+                dp[i] = Math.max(dp[i], take - dp[i + k + 1]);
+            }
         }
-        if (dp[i] != notComputed) {
-            return dp[i];
-        }
-        dp[i] = stoneValue[i] - f(stoneValue, n, i + 1);
-        if (i + 2 <= n) {
-            dp[i] = Math.max(dp[i], stoneValue[i]
-                + stoneValue[i + 1] - f(stoneValue, n, i + 2));
-        }
-        if (i + 3 <= n) {
-            dp[i] = Math.max(dp[i], stoneValue[i] + stoneValue[i + 1]
-                + stoneValue[i + 2] - f(stoneValue, n, i + 3));
-        }
-        return dp[i];
-    }
-
-    public String stoneGameIII(int[] stoneValue) {
-        int n = stoneValue.length;
-        dp = new int [n + 1];
-        for (int i = 0; i < n; i++) {
-            dp[i] = notComputed;
-        }
-        int dif = f(stoneValue, stoneValue.length, 0);
-        if (dif > 0) {
-            return "Alice";
-        }
-        if (dif < 0) {
-            return "Bob";
-        }
+        if (dp[0] > 0) return "Alice";
+        if (dp[0] < 0) return "Bob";
         return "Tie";
     }
 }
