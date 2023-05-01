67febaa45c5ce2a8eea059fa448b642150ef670f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-01 15:50:26 -0400

    Time: 184 ms (5.38%), Space: 62.1 MB (5.38%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,15 +1,24 @@
 class Solution {
-  public String stoneGameIII(int[] A) {
-        int n = A.length, dp[] = new int[n+1];
-        for (int i = n - 1; i >= 0; --i) {
-            dp[i] = Integer.MIN_VALUE;
-            for (int k = 0, take = 0; k < 3 && i + k < n; ++k) {
-                take += A[i + k];
-                dp[i] = Math.max(dp[i], take - dp[i + k + 1]);
+  public String stoneGameIII(int[] arr) {
+        int score = minimax(arr, 0, 1, new Integer[arr.length][2]);
+        if (score > 0) return "Alice";
+        if (score < 0) return "Bob";
+        return "Tie";
+    }
+    int minimax(int[] arr, int i, int maxPlayer, Integer[][] dp) {
+        if (i >= arr.length) return 0;
+        if (dp[i][maxPlayer] != null) return dp[i][maxPlayer];
+        int ans = maxPlayer == 1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        int score = 0;
+        for (int j = i; j < Math.min(arr.length, i + 3); j++) {
+            if (maxPlayer == 1) {
+                score += arr[j];
+                ans = Math.max(ans, score + minimax(arr, j + 1, 0, dp));
+            } else {
+                score -= arr[j];
+                ans = Math.min(ans, score + minimax(arr, j + 1, 1, dp));
             }
         }
-        if (dp[0] > 0) return "Alice";
-        if (dp[0] < 0) return "Bob";
-        return "Tie";
+        return dp[i][maxPlayer] = ans;
     }
 }
