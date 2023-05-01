94cc3ad6025bda40154db44661a6a09f49d09120
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-01 15:06:34 -0400

    Time: 57 ms (73.03%), Space: 45.1 MB (21.91%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.java
+++ 1797-design-authentication-manager/1797-design-authentication-manager.java
@@ -1,44 +1,31 @@
 class AuthenticationManager {
-
-     int time=0;
-
-    HashMap<String,Integer>mp=new HashMap<>();
+    private static TreeMap<Integer, String> treeMap;
+    private static Map<String, Integer> expireTime;
+    private static int life;
 
     public AuthenticationManager(int timeToLive) {
-        
-    time=timeToLive;
-
+        this.treeMap = new TreeMap<>();
+        this.expireTime = new HashMap<>();
+        this.life = timeToLive;
     }
     
     public void generate(String tokenId, int currentTime) {
-        
-    mp.put(tokenId,currentTime+time);
-
+        expireTime.put(tokenId, this.life + currentTime);
+        treeMap.put(this.life + currentTime, tokenId);
     }
     
     public void renew(String tokenId, int currentTime) {
-        
-    if(!mp.containsKey(tokenId)||mp.get(tokenId)<=currentTime)
-    {
-    return;
-    }
-
-    mp.put(tokenId,currentTime+time);
-
+        int expire = expireTime.getOrDefault(tokenId, -1);
+        var tail = treeMap.tailMap(currentTime + 1);
+        if (!tail.isEmpty() && expire >= tail.firstKey()) {
+            treeMap.remove(expire);
+            treeMap.put(this.life + currentTime, tokenId);
+            expireTime.put(tokenId, this.life + currentTime);
+        }
     }
     
     public int countUnexpiredTokens(int currentTime) {
-    
-    List<String>lr=new ArrayList<>(mp.keySet());
-    
-    for(int i=0;i<lr.size();i++){
-    
-    if(mp.get(lr.get(i))<=currentTime)mp.remove(lr.get(i));
-
-    }
-
-    return mp.size();
-    
+        return treeMap.tailMap(currentTime + 1).size();
     }
 }
 
