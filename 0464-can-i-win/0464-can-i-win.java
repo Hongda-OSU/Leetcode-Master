972a336b557fa468be5fdad3285f3aca5263b476
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-02 15:53:07 -0400

    Time: 82 ms (93.49%), Space: 46.6 MB (81.40%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.java
+++ 0464-can-i-win/0464-can-i-win.java
@@ -1,25 +1,30 @@
 class Solution {
+    private int numLimit, targetTotal;
+    
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        if (desiredTotal <= 0) return true;
-        // 1 + 2 + ... + maxChoosableInteger < desiredTotal means can't reach to desiredTotal
-        if (maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal) return false;
-        int[] dp = new int[1 << maxChoosableInteger];
-        return dfs(dp, 0, maxChoosableInteger, desiredTotal);
+        numLimit = maxChoosableInteger;
+        targetTotal = desiredTotal;
+        int max = (numLimit * (numLimit + 1)) / 2;
+        if (max < targetTotal)
+            return false;
+        int[] dp = new int[(1 << numLimit)];
+        if (solve(0, 0, 0, dp))
+            return true;
+        return false;
     }
     
-    public boolean dfs(int[] dp, int chs, int max, int target) {
-        // targer <= 0 means the prior one wins
-        if (target <= 0) return false;
-        if (dp[chs] != 0) return dp[chs] == 1;
-        boolean win = false;
-        for (int i = 0; i < max; i++) {
-            // i + 1 not use
-            if ((chs & (1 << i)) == 0) {
-                // thers is a trick: short circuit, when win is true, the next dfs won't be invoke
-                win = win || !dfs(dp, chs ^ (1 << i), max, target - i - 1);
+    private boolean solve(int mask, int lstSum, int player, int[] dp) {
+        if (dp[mask] != 0)
+            return dp[mask] == 1;
+        for (int i = 0; i < numLimit; i++) {
+            if ((mask & (1 << i)) == 0) {
+                if (lstSum + i + 1 >= targetTotal || !solve((mask | (1 << i)), lstSum + i + 1, player + 1, dp)) {
+                    dp[mask] = 1;
+                    return true;
+                }   
             }
         }
-        dp[chs] = win ? 1 : -1;
-        return win;
+            dp[mask] = -1;
+            return false;
     }
-}
+} 
