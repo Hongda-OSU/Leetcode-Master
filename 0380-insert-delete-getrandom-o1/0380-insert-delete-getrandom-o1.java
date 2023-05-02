efdb8bad081920b9a5b3135f11856f7dd23eedf0
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-02 14:51:01 -0400

    Time: 30 ms (77.29%), Space: 93 MB (23.66%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,42 +1,32 @@
 public class RandomizedSet {
+	Random random;
+	Map<Integer, Integer> map;
+	List<Integer> list;
+	public RandomizedSet() {
+		random = new Random();
+		map = new HashMap<>();
+		list = new ArrayList<>();
+	}
 
-    HashMap<Integer, Integer> valToInd;
-    List<Integer> list;
-    int ind = 0;
-    
-    /** Initialize your data structure here. */
-    public RandomizedSet() {
-        valToInd = new HashMap<>();
-        list = new ArrayList<>();
-    }
-    
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    public boolean insert(int val) {
-        if(valToInd.containsKey(val)) return false;
-        list.add(val);
-        valToInd.put(val,list.size()-1);
-        return true;
-    }
-    
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
-    public boolean remove(int val) {
-        int ind = valToInd.getOrDefault(val,-1);
-        if(ind == -1) return false;
-        Collections.swap(list,ind,list.size()-1);
-        int swappedWith = list.get(ind);
-        valToInd.put(swappedWith,ind);
-        list.remove(list.size()-1);
-        valToInd.remove(val);
-        return true;
-    }
-    
-    /** Get a random element from the set. */
-    public int getRandom() {
-        int max = list.size();
-        int min = 0;
-        int ind = (int)(Math.random() * (max - min) + min);
-        return list.get(ind);
-    }
+	public boolean insert(int val) {
+		if (map.containsKey(val)) return false;
+		list.add(val);
+		return map.put(val, list.size()-1) == null;
+	}
+
+	public boolean remove(int val) {
+		if (!map.containsKey(val)) return false;
+		Integer position = map.get(val);
+		Collections.swap(list, position, list.size() - 1);
+		map.put(list.get(position),position);
+		list.remove(list.size()-1);
+		return map.remove(val) != null;
+	}
+
+	public int getRandom() {
+		int i = random.nextInt(list.size());
+		return list.get(i);
+	}
 }
 
 /**
