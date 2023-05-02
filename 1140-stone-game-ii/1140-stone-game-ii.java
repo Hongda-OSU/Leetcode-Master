867a57415a94f451040c6c7d589fb7c672278d1d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-02 15:59:17 -0400

    Time: 3 ms (100.00%), Space: 42.3 MB (46.41%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,23 +1,23 @@
 class Solution {
-   public int stoneGameII(int[] piles) {
-        int[] presum =  Arrays.copyOf(piles, piles.length);
-        for (int i = presum.length - 2; i >= 0; i--) presum[i] += presum[i + 1];
-        return dfs(presum, 1, 0, new int[piles.length][piles.length]);
+    public int stoneGameII(int[] piles) {
+        int len = piles.length;
+        int[] preSum = Arrays.copyOf(piles, len);
+        for (int i = len - 2; i >= 0; i--)
+            preSum[i] += preSum[i + 1];
+        return dfs(preSum, 1, 0, new int[len][len]);
     }
-    private int dfs(int[] presum, int m, int p, int[][] memo) {
-        if (p + 2 * m >= presum.length) { // last player takes all
-            return presum[p];
-        }
-
-        if (memo[p][m] > 0) return memo[p][m];
-        int res = 0, take = 0;
+    
+    private int dfs(int[] preSum, int m, int p, int[][] memo) {
+        if (p + 2 * m >= preSum.length)
+            return preSum[p];
+        if (memo[p][m] > 0)
+            return memo[p][m];
+        int result = 0, take = 0;
         for (int i = 1; i <= 2 * m; i++) {
-            // current take
-            take = presum[p] - presum[p + i];
-            // take max of current + what lefts from other player max take
-            res = Math.max(res, take + presum[p + i] - dfs(presum, Math.max(i, m), p + i, memo));
+            take = preSum[p] - preSum[p + i];
+            result = Math.max(result, take + preSum[p + i] - dfs(preSum, Math.max(i, m), p + i, memo));
         }
-        memo[p][m] = res;
-        return res;
+        memo[p][m] = result;
+        return result;
     }
 }
