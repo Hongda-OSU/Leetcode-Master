32887c4f12db1ec5d6314bb15e2640838983564e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-02 14:51:22 -0400

    Time: 29 ms (95.26%), Space: 92.8 MB (29.01%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,32 +1,48 @@
-public class RandomizedSet {
-	Random random;
-	Map<Integer, Integer> map;
-	List<Integer> list;
-	public RandomizedSet() {
-		random = new Random();
-		map = new HashMap<>();
-		list = new ArrayList<>();
-	}
-
-	public boolean insert(int val) {
-		if (map.containsKey(val)) return false;
-		list.add(val);
-		return map.put(val, list.size()-1) == null;
-	}
-
-	public boolean remove(int val) {
-		if (!map.containsKey(val)) return false;
-		Integer position = map.get(val);
-		Collections.swap(list, position, list.size() - 1);
-		map.put(list.get(position),position);
-		list.remove(list.size()-1);
-		return map.remove(val) != null;
-	}
-
-	public int getRandom() {
-		int i = random.nextInt(list.size());
-		return list.get(i);
-	}
+class RandomizedSet {
+    HashMap<Integer,Integer> list=null;
+    int[] array=null;
+    int index=0;
+    Random random=null;
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        list=new HashMap<Integer,Integer>();
+        array=new int[100001];
+        random=new Random();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(list.containsKey(val)){
+            return false;
+        }
+        else{
+            array[index]=val;
+            list.put(val,index);
+            index++;
+            return true;
+        }
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(!list.containsKey(val)){
+            return false;
+        }
+        else{
+            int pos=list.remove(val);
+            array[pos]=array[index-1];
+            if(list.containsKey(array[index-1])){
+            list.put(array[index-1],pos);
+            }
+            index--;
+            return true;
+        }
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return array[random.nextInt(index)];
+    }
 }
 
 /**
