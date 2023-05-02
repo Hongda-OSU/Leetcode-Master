e803432e79660cdd12797b320d6b779778f2c8e7
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-02 14:51:46 -0400

    Time: 30 ms (77.29%), Space: 92.9 MB (23.66%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,47 +1,44 @@
 class RandomizedSet {
-    HashMap<Integer,Integer> list=null;
-    int[] array=null;
-    int index=0;
-    Random random=null;
-    /** Initialize your data structure here. */
+
+    List<Integer> nums;
+    Map<Integer, Integer> idxMap;
+    Random random;
+
     public RandomizedSet() {
-        list=new HashMap<Integer,Integer>();
-        array=new int[100001];
-        random=new Random();
+        nums = new ArrayList<>();
+        idxMap = new HashMap<>();
+        random = new Random();
     }
-    
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+
     public boolean insert(int val) {
-        if(list.containsKey(val)){
+        if (idxMap.containsKey(val)) {
             return false;
         }
-        else{
-            array[index]=val;
-            list.put(val,index);
-            index++;
-            return true;
-        }
+
+        idxMap.put(val, nums.size());
+        nums.add(val);
+        return true;
     }
-    
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
+
     public boolean remove(int val) {
-        if(!list.containsKey(val)){
+        if (!idxMap.containsKey(val)) {
             return false;
         }
-        else{
-            int pos=list.remove(val);
-            array[pos]=array[index-1];
-            if(list.containsKey(array[index-1])){
-            list.put(array[index-1],pos);
-            }
-            index--;
-            return true;
+
+        int idx = idxMap.get(val);
+        int lastIdx = nums.size() - 1;
+        if (idx != lastIdx) {
+            int lastVal = nums.get(lastIdx);
+            nums.set(idx, lastVal);
+            idxMap.put(lastVal, idx);
         }
+        nums.remove(lastIdx);
+        idxMap.remove(val);
+        return true;
     }
-    
-    /** Get a random element from the set. */
+
     public int getRandom() {
-        return array[random.nextInt(index)];
+        return nums.get(random.nextInt(nums.size()));
     }
 }
 
