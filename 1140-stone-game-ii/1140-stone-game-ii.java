30e0c1a59c6b9ef8291fed7b5505ba89170a14ab
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-02 15:55:32 -0400

    Time: 3 ms (100.00%), Space: 42.2 MB (53.04%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,32 +1,23 @@
 class Solution {
-    private int f(int[] piles, int[][][] dp, int p, int i, int m) {
-        if (i == piles.length) {
-            return 0;
-        }
-        if (dp[p][i][m] != -1) {
-            return dp[p][i][m];
-        }
-        int res = p == 1 ? 1000000 : -1, s = 0;
-        for (int x = 1; x <= Math.min(2 * m, piles.length - i); x++) {
-            s += piles[i + x - 1];
-            if (p == 0) {
-                res = Math.max(res, s + f(piles, dp, 1, i + x, Math.max(m, x)));
-            }
-            else {
-                res = Math.min(res, f(piles, dp, 0, i + x, Math.max(m, x)));
-            }
-        }
-        return dp[p][i][m] = res;
+   public int stoneGameII(int[] piles) {
+        int[] presum =  Arrays.copyOf(piles, piles.length);
+        for (int i = presum.length - 2; i >= 0; i--) presum[i] += presum[i + 1];
+        return dfs(presum, 1, 0, new int[piles.length][piles.length]);
     }
-    public int stoneGameII(int[] piles) {
-        int[][][] dp = new int[2][piles.length + 1][piles.length + 1];
-        for (int p = 0; p < 2; p++) {
-            for (int i = 0; i <= piles.length; i++) {
-                for (int m = 0; m <= piles.length; m++) {
-                    dp[p][i][m] = -1;
-                }
-            }
+    private int dfs(int[] presum, int m, int p, int[][] memo) {
+        if (p + 2 * m >= presum.length) { // last player takes all
+            return presum[p];
         }
-        return f(piles, dp, 0, 0, 1);
+
+        if (memo[p][m] > 0) return memo[p][m];
+        int res = 0, take = 0;
+        for (int i = 1; i <= 2 * m; i++) {
+            // current take
+            take = presum[p] - presum[p + i];
+            // take max of current + what lefts from other player max take
+            res = Math.max(res, take + presum[p + i] - dfs(presum, Math.max(i, m), p + i, memo));
+        }
+        memo[p][m] = res;
+        return res;
     }
 }
