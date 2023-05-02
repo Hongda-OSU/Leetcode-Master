9033e582a05bef218bbb8ddf9f9ba8ad1bdfab5b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-02 15:30:51 -0400

    Time: 5 ms (43.53%), Space: 43.2 MB (47.21%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.java
+++ 0622-design-circular-queue/0622-design-circular-queue.java
@@ -1,74 +1,39 @@
-class Node {
-  public int value;
-  public Node nextNode;
-
-  public Node(int value) {
-    this.value = value;
-    this.nextNode = null;
-  }
-}
-
 class MyCircularQueue {
+    static int arr[]=new int[1000],k,front,end;
 
-  private Node head, tail;
-  private int count;
-  private int capacity;
-
-  /** Initialize your data structure here. Set the size of the queue to be k. */
-  public MyCircularQueue(int k) {
-    this.capacity = k;
-  }
-
-  /** Insert an element into the circular queue. Return true if the operation is successful. */
-  public boolean enQueue(int value) {
-    if (this.count == this.capacity)
-      return false;
-
-    Node newNode = new Node(value);
-    if (this.count == 0) {
-      head = tail = newNode;
-    } else {
-      tail.nextNode = newNode;
-      tail = newNode;
+    public MyCircularQueue(int k) {
+        this.k=k;
+        front=end=0;
     }
-    this.count += 1;
-    return true;
-  }
-
-  /** Delete an element from the circular queue. Return true if the operation is successful. */
-  public boolean deQueue() {
-    if (this.count == 0)
-      return false;
-    this.head = this.head.nextNode;
-    this.count -= 1;
-    return true;
-  }
-
-  /** Get the front item from the queue. */
-  public int Front() {
-    if (this.count == 0)
-      return -1;
-    else
-      return this.head.value;
-  }
-
-  /** Get the last item from the queue. */
-  public int Rear() {
-    if (this.count == 0)
-      return -1;
-    else
-      return this.tail.value;
-  }
-
-  /** Checks whether the circular queue is empty or not. */
-  public boolean isEmpty() {
-    return (this.count == 0);
-  }
-
-  /** Checks whether the circular queue is full or not. */
-  public boolean isFull() {
-    return (this.count == this.capacity);
-  }
+    
+    public boolean enQueue(int value) {
+        if(end-front==k) return false;
+        arr[end%k]=value;
+        end++;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(front==end) return false;
+        front++;
+        return true;
+    }
+    
+    public int Front() {
+        return (isEmpty())?-1:arr[front%k];
+    }
+    
+    public int Rear() {
+        return (isEmpty())?-1:arr[(end-1)%k];
+    }
+    
+    public boolean isEmpty() {
+        return end==front;
+    }
+    
+    public boolean isFull() {
+        return end-front==k;
+    }
 }
 
 /**
