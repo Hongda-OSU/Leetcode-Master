f2de1f1ce4f11a11211493423a5dac76c709e0ef
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-03 19:14:58 -0400

    Time: 37 ms (80.37%), Space: 47.3 MB (8.93%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -1,10 +1,68 @@
 class Solution {
+    private boolean backtrack(int[] arr, int index, int count, int currSum, int k, 
+                              int targetSum, Integer mask, HashMap<Integer, Boolean> memo) {
+                                  
+        int n = arr.length;
+      
+        // We made k - 1 subsets with target sum and last subset will also have target sum.
+        if (count == k - 1) { 
+            return true;
+        }
+        
+        // No need to proceed further.
+        if (currSum > targetSum) { 
+            return false;
+        }
+        
+        // If we have already computed the current combination.
+        if (memo.containsKey(mask)) {
+            return memo.get(mask);
+        }
+      
+        // When curr sum reaches target then one subset is made.
+        // Increment count and reset current sum.
+        if (currSum == targetSum) {
+            boolean ans = backtrack(arr, 0, count + 1, 0, k, targetSum, mask, memo);
+            memo.put(mask, ans);
+            return ans;
+        }
+        
+        // Try not picked elements to make some combinations.
+        for (int j = index; j < n; ++j) {
+            if (((mask >> j) & 1) == 0) {
+                // Include this element in current subset.
+                mask = (mask | (1 << j));
+                
+                // If using current jth element in this subset leads to make all valid subsets.
+                if (backtrack(arr, j + 1, count, currSum + arr[j], k, targetSum, mask, memo)) {
+                    return true;
+                }
+                
+                // Backtrack step.
+                mask = (mask ^ (1 << j));
+            }
+        } 
+      
+        // We were not able to make a valid combination after picking each element from the array,
+        // hence we can't make k subsets.
+        memo.put(mask, false);
+        return false;
+    }
+    
+    void reverse(int[] arr) {
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) { 
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+  
     public boolean canPartitionKSubsets(int[] arr, int k) {
         int totalArraySum = 0;
         int n = arr.length;
         
         for (int i = 0; i < n; ++i) {
-            totalArraySum += arr[i];
+             totalArraySum += arr[i];
         }
       
         // If total sum not divisible by k, we can't make subsets.
@@ -12,35 +70,16 @@ class Solution {
             return false;
         }
       
+        // Sort in decreasing order.
+        Arrays.sort(arr);
+        reverse(arr);
+        
         int targetSum = totalArraySum / k;
+        Integer mask = 0;
         
-        int[] subsetSum = new int[(1 << n)];
-        for (int i = 0; i < (1 << n); ++i) {
-            subsetSum[i] = -1;
-        }
-        // Initially only one state is valid, i.e don't pick anything.
-        subsetSum[0] = 0;
-        
-        for (int mask = 0; mask < (1 << n); mask++) {
-            // If the current state has not been reached earlier.
-            if (subsetSum[mask] == -1) {
-                continue;
-            }  
-            
-            for (int i = 0; i < n; i++) {
-                // If the number arr[i] was not picked earlier, and arr[i] + subsetSum[mask]
-                // is not greater than the targetSum then add arr[i] to the subset
-                // sum at subsetSum[mask] and store the result at subsetSum[mask | (1 << i)].
-                if ((mask & (1 << i)) == 0 && subsetSum[mask] + arr[i] <= targetSum) { 
-                    subsetSum[mask | (1 << i)] = (subsetSum[mask] + arr[i]) % targetSum;
-                }
-            }
-            
-            if (subsetSum[(1 << n) - 1] == 0) {
-                return true;
-            }
-        }
-        
-        return subsetSum[(1 << n) - 1] == 0;
+        // Memoize the ans using taken element's string as key.
+        HashMap<Integer, Boolean> memo = new HashMap<>();
+      
+        return backtrack(arr, 0, 0, 0, k, targetSum, mask, memo);
     }
 }
