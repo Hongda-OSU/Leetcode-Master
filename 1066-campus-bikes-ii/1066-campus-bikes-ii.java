6c1ecafc2f711df370189f2223b7e45933df9da8
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-03 20:18:50 -0400

    Time: 2 ms (96.88%), Space: 39.9 MB (92.50%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.java
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.java
@@ -3,21 +3,19 @@ class Solution {
         int n = bikes.length;
         int[] dp = new int[1 << n];
         return dfs(workers, bikes, 0, 0, dp);
-    }
+    } 
     
     private int dfs(int[][] workers, int[][] bikes, int workerIndex, int state, int[] dp) {
-        if (workerIndex == workers.length) return 0;
-        if (dp[state] != 0) return dp[state];
-        
+        if (workerIndex == workers.length)
+            return 0;
+        if (dp[state] != 0)
+            return dp[state];
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < bikes.length; i++) {
             if ((state & (1 << i)) == 0) {
-                min = Math.min(min, Math.abs(workers[workerIndex][0] - bikes[i][0]) +
-				Math.abs(workers[workerIndex][1] - bikes[i][1]) +
-				dfs(workers, bikes, workerIndex + 1, state | (1 << i), dp));
+                min = Math.min(min, Math.abs(workers[workerIndex][0] - bikes[i][0]) + Math.abs(workers[workerIndex][1] - bikes[i][1]) + dfs(workers, bikes, workerIndex + 1, state | (1 << i), dp));
             }
         }
-        
         dp[state] = min;
         return min;
     }
