29273d08d42afa4416345d886aa3abcba4d2ba06
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-04 14:31:56 -0400

    Time: 41 ms (5.25%), Space: 51.4 MB (76.45%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,26 +1,16 @@
 class Solution {
-    public int findMaxLength(int[] nums) {
-        if (nums == null || nums.length == 0) { // Base Case
-            return 0;
-        }
-        // Converting all 0 to -1
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0) nums[i] = -1;
-        }
-        int sum = 0; // current
-        int max = 0; // final-ans
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1); // put reference in the starting of 0 & -1, as i have tell you in the starting  
-        for(int i = 0; i < nums.length; i++){
-            sum += nums[i]; // cumulative sum
-            if(map.containsKey(sum)){ // if cumulative sum key :- 0, -1, 1 already present
-                int last = map.get(sum); // we get it's value
-                max = Math.max(max, i - last); // and update max
+    public int findMaxLength(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        // right - left = 0 -> left = right. Since left = right, let's name it sum
+        int ans = 0, sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i] == 0 ? -1 : 1;
+            if (map.containsKey(sum)) {
+                ans = Math.max(ans, i - map.get(sum));
             }
-            else{ // if it's not present then create it's key-value pair
-                map.put(sum, i);
-            }
+            map.putIfAbsent(sum, i);
         }
-        return max; // finally return it
+        return ans;
     }
 }
