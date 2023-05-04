f7c5dc189ff9513d33d9c08a41ff24c74454e9f5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-04 00:40:30 -0400

    Time: 822 ms (38.08%), Space: 0B (100.00%) - LeetHub

---
+++ 1285-find-the-start-and-end-number-of-continuous-ranges/1285-find-the-start-and-end-number-of-continuous-ranges.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+SELECT l1.log_id AS start_id,
+    MIN(l2.log_id) AS end_id
+FROM (
+    SELECT log_id 
+    FROM Logs
+    WHERE log_id - 1 NOT IN (
+        SELECT log_id
+        FROM Logs
+    )) l1, (
+    SELECT log_id
+    FROM Logs
+    WHERE log_id + 1 NOT IN (
+        SELECT log_id
+        FROM Logs
+    )) l2
+WHERE l1.log_id <= l2.log_id
+GROUP BY l1.log_id;
