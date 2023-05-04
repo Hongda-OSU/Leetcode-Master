479ee9d1f516e7e59a0114399a85ddc492d27e16
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-03 20:14:12 -0400

    Time: 2 ms (96.88%), Space: 40.2 MB (81.88%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.java
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.java
@@ -1,30 +1,24 @@
 class Solution {
     public int assignBikes(int[][] workers, int[][] bikes) {
-        int minDist = Integer.MAX_VALUE;
-        int N = workers.length;
-        int M = bikes.length;
-        int[] dp = new int[1 << M];
-        
-        for (int s = 1; s < (1 << M); ++s) {
-            int w = Integer.bitCount(s);
-            if (w > N) continue;
-            
-            dp[s] = Integer.MAX_VALUE;
-            for (int i = 0; i < M; ++i) {
-                if ((s & (1 << i)) == 0) continue;
-                
-                int preS = s ^ (1 << i);
-                dp[s] = Math.min(dp[s], dp[preS] + getDistance(workers[w-1], bikes[i]));
-            }
-            
-            if (w == N)
-                minDist = Math.min(minDist, dp[s]);
-        }
-        
-        return minDist;
+        int n = bikes.length;
+        int[] dp = new int[1 << n];
+        return dfs(workers, bikes, 0, 0, dp);
     }
     
-    private int getDistance(int[] a, int[] b) {
-        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    private int dfs(int[][] workers, int[][] bikes, int workerIndex, int state, int[] dp) {
+        if (workerIndex == workers.length) return 0;
+        if (dp[state] != 0) return dp[state];
+        
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < bikes.length; i++) {
+            if ((state & (1 << i)) == 0) {
+                min = Math.min(min, Math.abs(workers[workerIndex][0] - bikes[i][0]) +
+				Math.abs(workers[workerIndex][1] - bikes[i][1]) +
+				dfs(workers, bikes, workerIndex + 1, state | (1 << i), dp));
+            }
+        }
+        
+        dp[state] = min;
+        return min;
     }
 }
