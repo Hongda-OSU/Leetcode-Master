c9e2c840c657fad07c4e22826e1986624111295e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-04 14:54:42 -0400

    Time: 1 ms (100.00%), Space: 43.4 MB (39.21%) - LeetHub

--- 0255-verify-preorder-sequence-in-binary-search-tree/0255-verify-preorder-sequence-in-binary-search-tree.java
+++ 0255-verify-preorder-sequence-in-binary-search-tree/0255-verify-preorder-sequence-in-binary-search-tree.java
@@ -1,13 +1,13 @@
 class Solution {
-   public boolean verifyPreorder(int[] preorder) {
-    int low = Integer.MIN_VALUE, i = -1;
-    for (int p : preorder) {
-        if (p < low)
-            return false;
-        while (i >= 0 && p > preorder[i])
-            low = preorder[i--];
-        preorder[++i] = p;
+    public boolean verifyPreorder(int[] preorder) {
+        int low = Integer.MIN_VALUE, i = -1;
+        for (int p : preorder) {
+            if (p < low) 
+                return false;
+            while (i >= 0 && p > preorder[i])
+                low = preorder[i--];
+            preorder[++i] = p;
+        }
+        return true;
     }
-    return true;
-}
 }
