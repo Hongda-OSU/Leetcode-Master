98c91817e71c28f02843e864dcb665026f0ae63d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-04 00:34:04 -0400

    Time: 1146 ms (57.87%), Space: 0B (100.00%) - LeetHub

---
+++ 1077-project-employees-iii/1077-project-employees-iii.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT p.project_id,
+    p.employee_id
+FROM Project p
+JOIN Employee e
+ON p.employee_id = e.employee_id
+WHERE (p.project_id, e.experience_years) IN (
+    SELECT a.project_id,
+        MAX(b.experience_years)
+    FROM Project a 
+    JOIN Employee b
+    ON a.employee_id = b.employee_id
+    GROUP BY a.project_id
+);
