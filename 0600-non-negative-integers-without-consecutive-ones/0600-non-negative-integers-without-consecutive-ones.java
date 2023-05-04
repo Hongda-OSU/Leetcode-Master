225a24fd1bf034cef4993e3ccbee1df9a15a684c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-04 15:31:31 -0400

    Time: 1 ms (100.00%), Space: 39.9 MB (57.33%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
@@ -1,21 +1,22 @@
-public class Solution {
-    public int findIntegers(int num) {
-        int[] f = new int[32];
-        f[0] = 1;
-        f[1] = 2;
-        for (int i = 2; i < f.length; i++)
-            f[i] = f[i - 1] + f[i - 2];
-        int i = 30, sum = 0, prev_bit = 0;
+class Solution {
+    public int findIntegers(int n) {
+        int[] arr = new int[32];
+        arr[0] = 1;
+        arr[1] = 2;
+        for (int i = 2; i < arr.length; i++) 
+            arr[i] = arr[i - 1] + arr[i - 2];
+        int i = 30, sum = 0, prev = 0;
         while (i >= 0) {
-            if ((num & (1 << i)) != 0) {
-                sum += f[i];
-                if (prev_bit == 1) {
+            if ((n & (1 << i)) != 0) {
+                sum += arr[i];
+                if (prev == 1) {
                     sum--;
                     break;
                 }
-                prev_bit = 1;
-            } else
-                prev_bit = 0;
+                prev = 1;
+            } else {
+                prev = 0;
+            }
             i--;
         }
         return sum + 1;
