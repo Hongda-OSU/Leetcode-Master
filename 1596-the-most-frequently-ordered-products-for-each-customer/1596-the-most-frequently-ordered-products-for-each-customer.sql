899f70db59ef248e7f47cd9f4779be08169d699f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-04 00:48:37 -0400

    Time: 5570 ms (5.10%), Space: 0B (100.00%) - LeetHub

---
+++ 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT t.customer_id,
+    t.product_id,
+    product_name
+FROM (
+    SELECT customer_id,
+        product_id,
+        dense_rank() over (PARTITION BY customer_id ORDER BY COUNT(*) DESC) prank
+    FROM Orders
+    GROUP BY 1, 2
+) t
+JOIN Products p 
+ON p.product_id = t.product_id
+WHERE prank = 1;
