c2e34c5177d0825e37d29e02a56464f4442e9090
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-04 14:48:17 -0400

    Time: 19 ms (53.19%), Space: 50.3 MB (55.32%) - LeetHub

--- 1798-maximum-number-of-consecutive-values-you-can-make/1798-maximum-number-of-consecutive-values-you-can-make.java
+++ 1798-maximum-number-of-consecutive-values-you-can-make/1798-maximum-number-of-consecutive-values-you-can-make.java
@@ -1,19 +1,13 @@
 class Solution {
     public int getMaximumConsecutive(int[] coins) {
-      if(coins.length==0 && coins==null)
-          return 0;
-      TreeMap<Integer,Integer> map=new TreeMap<Integer,Integer>();
-      for(int i:coins)
-          map.put(i,map.getOrDefault(i,0)+1);
-      int range=0;
-      for(int i:map.keySet()){
-       if(range==0 && i==1)
-       range=i*map.get(i);
-       else if(range!=0 && range+1>=i)
-         range+=i*map.get(i);
-       else 
-        break;
-      }
-     return range+1;
+        Arrays.sort(coins);
+        int result=1;
+        
+        int idx=0;
+        while(idx<coins.length && result>=coins[idx]){
+            result+=coins[idx];
+            idx++;
+        }
+        return result;
     }
 }
