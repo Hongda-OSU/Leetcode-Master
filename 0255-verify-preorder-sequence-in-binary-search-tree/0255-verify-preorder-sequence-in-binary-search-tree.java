74fb3c2010845630a8667aa1b31955aa092550bc
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-04 14:52:28 -0400

    Time: 3 ms (92.81%), Space: 42.9 MB (77.34%) - LeetHub

---
+++ 0255-verify-preorder-sequence-in-binary-search-tree/0255-verify-preorder-sequence-in-binary-search-tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    int i = 1;
+    public boolean verifyPreorder(int[] A) {
+        return A.length == 0 || check(A, Integer.MIN_VALUE, A[0]) && check(A, A[0], Integer.MAX_VALUE);
+    }
+
+    public boolean check(int[] A, int left, int right) {
+        if (i == A.length || A[i] > right) return true;
+        int mid = A[i++];
+        return (mid > left) && check(A, left, mid) && check(A, mid, right);
+    }
+}
