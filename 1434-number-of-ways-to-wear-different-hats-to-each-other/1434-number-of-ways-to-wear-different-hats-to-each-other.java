2279cf1ad0194688133ceecacc17ac548eccc3cc
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-03 20:30:28 -0400

    Time: 27 ms (57.69%), Space: 43 MB (28.20%) - LeetHub

--- 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.java
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.java
@@ -1,30 +1,30 @@
 class Solution {
-    int mod = (int) Math.pow(10, 9) + 7, mask = 0;
-    List<List<Integer>> ps;
-    Integer[][] dp;
     public int numberWays(List<List<Integer>> hats) {
-        ps = new ArrayList<>();
-        mask = (1 << hats.size()) - 1;
-        for (int i = 0; i < 41; i++) ps.add(new ArrayList<>());
-        for (int i = 0; i < hats.size(); i++) {
-            for(int h : hats.get(i)) {
-                ps.get(h).add(i);
-            }
+        int n = hats.size();
+        List<Integer>[] h2p = new List[41];
+        for (int i = 1; i <= 40; i++)
+            h2p[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int hat : hats.get(i))
+                h2p[hat].add(i);
         }
-        dp = new Integer[41][1 << hats.size()];
-        return dfs(0, 0);
+        Integer[][] dp = new Integer[41][1024];
+        return dfs(h2p, (1 << n) - 1, 1, 0, dp);
     }
     
-    private int dfs(int h, int pa) {  // pa for people assigned mask;
-        if (pa == mask) return 1;
-        if (h > 40) return 0;
-        if (dp[h][pa] != null) return dp[h][pa];
-        int res = dfs(h + 1, pa);
-        for (int p : ps.get(h)) {
-            if ((pa & (1 << p)) > 0) continue; // p is assigned with a hat;
-            res = (res + dfs(h + 1, pa | (1 << p))) % mod;
+    private int dfs(List<Integer>[] h2p, int allMask, int hat, int assignedPeople, Integer[][] dp) {
+        if (assignedPeople == allMask)
+            return 1;
+        if (hat > 40)
+            return 0;
+        if (dp[hat][assignedPeople] != null)
+            return dp[hat][assignedPeople];
+        int result = dfs(h2p, allMask, hat + 1, assignedPeople, dp);
+        for (int p : h2p[hat]) {
+            if (((assignedPeople >> p) & 1) == 1) continue;
+            result += dfs(h2p, allMask, hat + 1, assignedPeople | (1 << p), dp);
+            result %= 1_000_000_007;
         }
-        dp[h][pa] = res;
-        return res;
+        return dp[hat][assignedPeople] = result;
     }
 }
