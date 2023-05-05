a82437a1fba5ec3a8896e7c47160fd2f40301232
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-05 16:27:11 -0400

    Time: 1 ms (100.00%), Space: 42.5 MB (48.32%) - LeetHub

--- 0549-binary-tree-longest-consecutive-sequence-ii/0549-binary-tree-longest-consecutive-sequence-ii.java
+++ 0549-binary-tree-longest-consecutive-sequence-ii/0549-binary-tree-longest-consecutive-sequence-ii.java
@@ -1,36 +1,45 @@
-public class Solution {
-    int maxval = 0;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int maxVal = 0;
     
     public int longestConsecutive(TreeNode root) {
         longestPath(root);
-        return maxval;
+        return maxVal;
     }
     
-    public int[] longestPath(TreeNode root) {
-        if (root == null) {
-            return new int[] {0,0};
+    private int[] longestPath(TreeNode node) {
+        if (node == null)
+            return new int[]{0, 0};
+        int increase = 1, decrease = 1;
+        if (node.left != null) {
+            int[] left = longestPath(node.left);
+            if (node.val == node.left.val + 1)
+                decrease = left[1] + 1;
+            else if (node.val == node.left.val - 1)
+                increase = left[0] + 1;
         }
-        
-        int inr = 1, dcr = 1;
-        if (root.left != null) {
-            int[] left = longestPath(root.left);
-            if (root.val == root.left.val + 1) {
-                dcr = left[1] + 1;
-            } else if (root.val == root.left.val - 1) {
-                inr = left[0] + 1;
-            }
+        if (node.right != null) {
+            int[] right = longestPath(node.right);
+            if (node.val == node.right.val + 1)
+                decrease = Math.max(decrease, right[1] + 1);
+            else if (node.val == node.right.val - 1)
+                increase = Math.max(increase, right[0] + 1);
         }
-        
-        if (root.right != null) {
-            int[] right = longestPath(root.right);
-            if (root.val == root.right.val + 1) {
-                dcr = Math.max(dcr, right[1] + 1);
-            } else if (root.val == root.right.val - 1) {
-                inr = Math.max(inr, right[0] + 1);
-            }
-        }
-        
-        maxval = Math.max(maxval, dcr + inr - 1);
-        return new int[] {inr, dcr};
+        maxVal = Math.max(maxVal, decrease + increase - 1);
+        return new int[] {increase, decrease};
     }
 }
