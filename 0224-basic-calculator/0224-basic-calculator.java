8bbed77d435eb23daca1901cffc82d144827f74a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-05 15:57:07 -0400

    Time: 35 ms (18.33%), Space: 42.9 MB (47.28%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,68 +1,78 @@
 class Solution {
-    public int calculate(String s) {
 
-        Stack<Integer> stack = new Stack<Integer>();
-        int operand = 0;
-        int result = 0; // For the on-going result
-        int sign = 1;  // 1 means positive, -1 means negative
+    public int evaluateExpr(Stack<Object> stack) {
+        
+        // If stack is empty or the expression starts with
+        // a symbol, then append 0 to the stack.
+        // i.e. [1, '-', 2, '-'] becomes [1, '-', 2, '-', 0]
+        if (stack.empty() || !(stack.peek() instanceof Integer)) {
+            stack.push(0);
+        }
 
-        for (int i = 0; i < s.length(); i++) {
+        int res = (int) stack.pop();
 
-            char ch = s.charAt(i);
-            if (Character.isDigit(ch)) {
+        // Evaluate the expression till we get corresponding ')'
+        while (!stack.empty() && !((char) stack.peek() == ')')) {
 
-                // Forming operand, since it could be more than one digit
-                operand = 10 * operand + (int) (ch - '0');
+            char sign = (char) stack.pop();
 
-            } else if (ch == '+') {
-
-                // Evaluate the expression to the left,
-                // with result, sign, operand
-                result += sign * operand;
-
-                // Save the recently encountered '+' sign
-                sign = 1;
-
-                // Reset operand
-                operand = 0;
-
-            } else if (ch == '-') {
-
-                result += sign * operand;
-                sign = -1;
-                operand = 0;
-
-            } else if (ch == '(') {
-
-                // Push the result and sign on to the stack, for later
-                // We push the result first, then sign
-                stack.push(result);
-                stack.push(sign);
-
-                // Reset operand and result, as if new evaluation begins for the new sub-expression
-                sign = 1;
-                result = 0;
-
-            } else if (ch == ')') {
-
-                // Evaluate the expression to the left
-                // with result, sign and operand
-                result += sign * operand;
-
-                // ')' marks end of expression within a set of parenthesis
-                // Its result is multiplied with sign on top of stack
-                // as stack.pop() is the sign before the parenthesis
-                result *= stack.pop();
-
-                // Then add to the next operand on the top.
-                // as stack.pop() is the result calculated before this parenthesis
-                // (operand on stack) + (sign on stack * (result from parenthesis))
-                result += stack.pop();
-
-                // Reset the operand
-                operand = 0;
+            if (sign == '+') {
+                res += (int) stack.pop();
+            } else {
+                res -= (int) stack.pop();
             }
         }
-        return result + (sign * operand);
+        return res;
+    }
+
+    public int calculate(String s) {
+
+        int operand = 0;
+        int n = 0;
+        Stack<Object> stack = new Stack<Object>();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+
+                // Forming the operand - in reverse order.
+                operand = (int) Math.pow(10, n) * (int) (ch - '0') + operand;
+                n += 1;
+
+            } else if (ch != ' ') {
+                if (n != 0) {
+
+                    // Save the operand on the stack
+                    // As we encounter some non-digit.
+                    stack.push(operand);
+                    n = 0;
+                    operand = 0;
+
+                }
+                if (ch == '(') {
+
+                    int res = evaluateExpr(stack);
+                    stack.pop();
+
+                    // Append the evaluated result to the stack.
+                    // This result could be of a sub-expression within the parenthesis.
+                    stack.push(res);
+
+                } else {
+                    // For other non-digits just push onto the stack.
+                    stack.push(ch);
+                }
+            }
+        }
+
+        //Push the last operand to stack, if any.
+        if (n != 0) {
+            stack.push(operand);
+        }
+
+        // Evaluate any left overs in the stack.
+        return evaluateExpr(stack);
     }
 }
