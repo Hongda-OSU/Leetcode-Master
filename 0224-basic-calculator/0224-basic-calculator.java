e75c40ff40af7adf1beda2c4eb5b901e20adbc6b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-05 16:00:57 -0400

    Time: 2 ms (100.00%), Space: 42.8 MB (55.20%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,23 +1,27 @@
 class Solution {
-    int idx; // this index traverse the string in one pass, between different level of recursion
+    private int index;
+    
     public int calculate(String s) {
-        idx = 0; // Initialization should be here
-        return calc(s);
+        index = 0;
+        return calculator(s);
     }
     
-    private int calc(String s) {
-        int res = 0, num = 0, sign = 1;
-        while (idx < s.length()) {
-            char c = s.charAt(idx++);
-            if (c >= '0' && c <= '9') num = num * 10 + c - '0';
-            else if (c == '(') num = calc(s); // ( is start of a new sub-problem, Let recursion solve the sub-problem
-            else if (c == ')') return res + sign * num;
-            else if (c == '+' || c == '-') { // only when we meet a new sign, we know a while number has been read
-                res += sign * num;
+    private int calculator(String str) {
+        int result = 0, num = 0, sign = 1;
+        while (index < str.length()) {
+            char ch = str.charAt(index++);
+            if (ch >= '0' && ch <= '9')
+                num = num * 10 + ch - '0';
+            else if (ch == '(')
+                num = calculator(str);
+            else if (ch == ')')
+                return result + sign * num;
+            else if (ch == '+' || ch == '-') {
+                result += sign * num;
                 num = 0;
-                sign = c == '-' ? -1 : 1;
+                sign = ch == '-' ? -1 : 1;
             }
         }
-        return res + sign * num; // last number is not processed yet
+        return result + sign * num;
     }
 }
