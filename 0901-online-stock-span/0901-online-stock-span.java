207563cd7e675d7d8ab08c61c4a5683322cb79d9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-05 16:08:40 -0400

    Time: 51 ms (82.13%), Space: 51 MB (32.36%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,26 +1,18 @@
 class StockSpanner {
-//    Initialize a stack with int array.
-//      First Index would be price, and
-//      Second Index Will be span at that price.
-    Stack<int[]> stack;
+    Stack<int[]> st;
     public StockSpanner() {
-        stack = new Stack<>();
+        st = new Stack<>();
     }
-
     public int next(int price) {
-//        At each level initialize the span to 1.
         int span = 1;
-//        If The top element of stack price is below or equal to the current price:
-//          Then, the previous element will be span of current.
-        while(!stack.isEmpty() && stack.peek()[0] <= price){
-            span += stack.pop()[1];
+        while (!st.isEmpty() && st.peek()[0] <= price) {
+            span += st.pop()[1];
         }
-//        At each level add the price and the span. 
-//        So that Next time when visited for consecutive days, you have the value of current that will avoid traversal till the current days span. 
-        stack.add(new int[]{price, span});
+        st.push(new int[] {price, span});
         return span;
     }
 }
+
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
