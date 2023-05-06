4ac80a1c4016c9afa66714ce381e2c6be47560ff
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-06 16:32:30 -0400

    Time: 2 ms (71.50%), Space: 42.8 MB (35.01%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,33 +1,24 @@
 class Solution {
     public int myAtoi(String s) {
-        s = s.trim();
-        boolean isNegative = false;
-        int num = 0;
-        int i = 0;
-
-        if (s.length() == 0) {
+        if (s.equals(""))
             return 0;
-        }
-
-        if (s.charAt(0) == '-') {
-            isNegative = true;
-            i++;
-        } else if (s.charAt(0) == '+') {
-            i++;
-        }
-
-        for (; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!Character.isDigit(c)) {
+        s = s.trim();
+        if (s.length() == 0)
+            return 0;
+        int sign = s.charAt(0) == '-' ? -1 : 1;
+        long result = 0;
+        int max = Integer.MAX_VALUE, min = Integer.MIN_VALUE;
+        int i = (s.charAt(0) == '+' || s.charAt(0) == '-') ? 1 : 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i)))
                 break;
-            }
-            int digit = Character.getNumericValue(c);
-            if (num > (Integer.MAX_VALUE - digit) / 10) {
-                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-            }
-            num = num * 10 + digit;
+            result = result * 10 + s.charAt(i) - '0';
+            if (sign == -1 && -1 * result < min)
+                return min;
+            if (sign == 1 && result > max)
+                return max;
+            i++;
         }
-
-        return isNegative ? -num : num;
+        return (int)(sign * result);
     }
 }
