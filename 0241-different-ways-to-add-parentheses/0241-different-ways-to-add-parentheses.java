7e3f1e36d4bcf842b00400307e514bf8a91950e9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-06 14:33:14 -0400

    Time: 2 ms (85.76%), Space: 42.4 MB (67.17%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,29 +1,29 @@
 class Solution {
-    public List<Integer> diffWaysToCompute(String exp) {
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i=0;i<exp.length();i++)
-        {
-            char ch = exp.charAt(i);
-            if(ch == '-' || ch == '+' || ch == '*')
-            {
-                List<Integer> left = diffWaysToCompute(exp.substring(0,i));
-                List<Integer> right = diffWaysToCompute(exp.substring(i+1));
-                for(int m : left)
-                {
-                    for(int n : right)
-                    {
-                        if(ch == '-')
-                            list.add(m-n);
-                        else if(ch == '+')
-                            list.add(m+n);
-                        else list.add(m*n);
+    public List<Integer> diffWaysToCompute(String str) { 
+        HashMap<String,List<Integer>> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch=='+'||ch=='-'||ch=='*'){
+                List<Integer> left = map.getOrDefault(str,diffWaysToCompute(str.substring(0,i)));
+                List<Integer> right = map.getOrDefault(str,diffWaysToCompute(str.substring(i+1)));
+                for(Integer c1 : left){
+                    for(Integer c2 : right){
+                        if(ch == '+'){
+                            res.add(c1+c2);
+                        }
+                        else if(ch == '-'){
+                            res.add(c1-c2);
+                        }
+                        else if(ch == '*'){
+                            res.add(c1*c2);
+                        }
                     }
                 }
             }
         }
-        if(list.size() == 0)
-            list.add(Integer.valueOf(exp));
-        return list;
+        if(res.size()==0)res.add(Integer.parseInt(str));
+        map.put(str,res);
+        return res;
     }
 }
