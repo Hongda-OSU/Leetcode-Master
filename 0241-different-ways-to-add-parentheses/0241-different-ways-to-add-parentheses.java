1b9789d1a0b3c9da6c5ac388fafea9f72f9a40b7
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-06 14:34:58 -0400

    Time: 1 ms (100.00%), Space: 40.8 MB (92.72%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,29 +1,32 @@
 class Solution {
-    public List<Integer> diffWaysToCompute(String str) { 
-        HashMap<String,List<Integer>> map = new HashMap<>();
-        List<Integer> res = new ArrayList<>();
-        for(int i=0;i<str.length();i++){
-            char ch = str.charAt(i);
-            if(ch=='+'||ch=='-'||ch=='*'){
-                List<Integer> left = map.getOrDefault(str,diffWaysToCompute(str.substring(0,i)));
-                List<Integer> right = map.getOrDefault(str,diffWaysToCompute(str.substring(i+1)));
-                for(Integer c1 : left){
+    HashMap<String, List<Integer>> map = new HashMap<>();
+    public List<Integer> diffWaysToCompute(String expression) {
+        if (map.containsKey(expression)) {
+            return map.get(expression);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char current = expression.charAt(i);
+            if (current == '+' || current == '-' || current == '*') {
+                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> right = diffWaysToCompute(expression.substring(i+1));
+                 for(Integer c1 : left){
                     for(Integer c2 : right){
-                        if(ch == '+'){
-                            res.add(c1+c2);
+                        if(current == '+'){
+                            result.add(c1+c2);
                         }
-                        else if(ch == '-'){
-                            res.add(c1-c2);
+                        else if(current == '-'){
+                            result.add(c1-c2);
                         }
-                        else if(ch == '*'){
-                            res.add(c1*c2);
+                        else if(current == '*'){
+                            result.add(c1*c2);
                         }
                     }
                 }
             }
         }
-        if(res.size()==0)res.add(Integer.parseInt(str));
-        map.put(str,res);
-        return res;
+        if(result.size()==0)result.add(Integer.parseInt(expression));
+        map.put(expression,result);
+        return result;
     }
 }
