1946bd7c039ddc4cccc5fce1a6c1b4ad63a979d8
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-06 16:32:24 -0400

    Time: 2 ms (71.50%), Space: 42.5 MB (60.04%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,42 +1,33 @@
 class Solution {
-    public int myAtoi(String input) {
-        int sign = 1; 
-        int result = 0; 
-        int index = 0;
-        int n = input.length();
-        
-        // Discard all spaces from the beginning of the input string.
-        while (index < n && input.charAt(index) == ' ') { 
-            index++; 
-        }
-        
-        // sign = +1, if it's positive number, otherwise sign = -1. 
-        if (index < n && input.charAt(index) == '+') {
-            sign = 1;
-            index++;
-        } else if (index < n && input.charAt(index) == '-') {
-            sign = -1;
-            index++;
-        }
-        
-        // Traverse next digits of input and stop if it is not a digit
-        while (index < n && Character.isDigit(input.charAt(index))) {
-            int digit = input.charAt(index) - '0';
+    public int myAtoi(String s) {
+        s = s.trim();
+        boolean isNegative = false;
+        int num = 0;
+        int i = 0;
 
-            // Check overflow and underflow conditions. 
-            if ((result > Integer.MAX_VALUE / 10) || 
-                (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {     
-                // If integer overflowed return 2^31-1, otherwise if underflowed return -2^31.    
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-            
-            // Append current digit to the result.
-            result = 10 * result + digit;
-            index++;
+        if (s.length() == 0) {
+            return 0;
         }
-        
-        // We have formed a valid number without any overflow/underflow.
-        // Return it after multiplying it with its sign.
-        return sign * result;
+
+        if (s.charAt(0) == '-') {
+            isNegative = true;
+            i++;
+        } else if (s.charAt(0) == '+') {
+            i++;
+        }
+
+        for (; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!Character.isDigit(c)) {
+                break;
+            }
+            int digit = Character.getNumericValue(c);
+            if (num > (Integer.MAX_VALUE - digit) / 10) {
+                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            num = num * 10 + digit;
+        }
+
+        return isNegative ? -num : num;
     }
 }
