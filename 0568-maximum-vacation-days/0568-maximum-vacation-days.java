475a52ce7d5e24b327cacf3d3f20050f718a0b76
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-06 17:21:34 -0400

    Time: 50 ms (83.13%), Space: 44.7 MB (26.51%) - LeetHub

--- 0568-maximum-vacation-days/0568-maximum-vacation-days.java
+++ 0568-maximum-vacation-days/0568-maximum-vacation-days.java
@@ -1,20 +1,17 @@
 public class Solution {
     public int maxVacationDays(int[][] flights, int[][] days) {
         if (days.length == 0 || flights.length == 0) return 0;
-        int[] dp = new int[days.length];
+        int[][] dp = new int[days.length][days[0].length + 1];
         for (int week = days[0].length - 1; week >= 0; week--) {
-            int[] temp = new int[days.length];
             for (int cur_city = 0; cur_city < days.length; cur_city++) {
-                temp[cur_city] = days[cur_city][week] + dp[cur_city];
+                dp[cur_city][week] = days[cur_city][week] + dp[cur_city][week + 1];
                 for (int dest_city = 0; dest_city < days.length; dest_city++) {
                     if (flights[cur_city][dest_city] == 1) {
-                        temp[cur_city] = Math.max(days[dest_city][week] + dp[dest_city], temp[cur_city]);
+                        dp[cur_city][week] = Math.max(days[dest_city][week] + dp[dest_city][week + 1], dp[cur_city][week]);
                     }
                 }
             }
-            dp = temp;
         }
-
-        return dp[0];
+        return dp[0][0];
     }
 }
