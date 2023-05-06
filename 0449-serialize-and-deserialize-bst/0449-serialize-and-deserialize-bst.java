0a88f87cdecf58dcecd5d63930881d59e888aa19
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-06 16:09:21 -0400

    Time: 4 ms (94.36%), Space: 43.3 MB (87.29%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
@@ -1,21 +1,13 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Codec {
-    // Encodes a tree to a list.
-    public void postorder(TreeNode root, StringBuilder sb) {
-        if (root == null)
-            return;
-        postorder(root.left, sb);
-        postorder(root.right, sb);
-        sb.append(intToString(root.val));
-    }
-
-    // Encodes integer to bytes string
-    public String intToString(int x) {
-        char[] bytes = new char[4];
-        for (int i = 3; i > -1; --i) {
-            bytes[3 - i] = (char) (x >> (i * 8) & 0xff);
-        }
-        return new String(bytes);
-    }
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
@@ -24,39 +16,48 @@ public class Codec {
         return sb.toString();
     }
 
-    // Decodes list to tree.
-    public TreeNode helper(Integer lower, Integer upper, ArrayDeque<Integer> nums) {
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        ArrayDeque<Integer> nums = new ArrayDeque<>();
+        int n = data.length();
+        for (int i = 0; i < (int)(n / 4); i++)
+            nums.add(stringToInt(data.substring(4 * i, 4 * i + 4)));
+        return helper(Integer.MIN_VALUE, Integer.MAX_VALUE, nums);
+    }
+    
+    private void postorder(TreeNode root, StringBuilder sb) {
+        if (root == null)
+            return;
+        postorder(root.left, sb);
+        postorder(root.right, sb);
+        sb.append(intToString(root.val));
+    }
+    
+    private String intToString(int x) {
+        char[] bytes = new char[4];
+        for (int i = 3; i > -1; i--) 
+            bytes[3 - i] = (char)(x >> (i * 8) & 0xff);
+        return new String(bytes);
+    }
+    
+    private int stringToInt(String str) {
+        int result = 0;
+        for (char ch : str.toCharArray())
+            result = (result << 8) + (int)ch;
+        return result;
+    }
+    
+    private TreeNode helper(Integer low, Integer high, ArrayDeque<Integer> nums) {
         if (nums.isEmpty())
             return null;
         int val = nums.getLast();
-        if (val < lower || val > upper)
+        if (val < low || val > high)
             return null;
-
         nums.removeLast();
         TreeNode root = new TreeNode(val);
-        root.right = helper(val, upper, nums);
-        root.left = helper(lower, val, nums);
+        root.right = helper(val, high, nums);
+        root.left = helper(low, val, nums);
         return root;
-    }
-
-    // Decodes bytes string to integer
-    public int stringToInt(String bytesStr) {
-        int result = 0;
-        for (char b : bytesStr.toCharArray()) {
-            result = (result << 8) + (int) b;
-        }
-        return result;
-    }
-
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        ArrayDeque<Integer> nums = new ArrayDeque<Integer>();
-        int n = data.length();
-        for (int i = 0; i < (int) (n / 4); ++i) {
-            nums.add(stringToInt(data.substring(4 * i, 4 * i + 4)));
-        }
-
-        return helper(Integer.MIN_VALUE, Integer.MAX_VALUE, nums);
     }
 }
 
