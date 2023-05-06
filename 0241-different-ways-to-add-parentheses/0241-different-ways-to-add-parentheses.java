5a15234233a94ba5a2825c37a48d04fb356fb158
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-06 14:33:00 -0400

    Time: 2 ms (85.76%), Space: 42.8 MB (33.04%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,49 +1,29 @@
 class Solution {
-    public Map<String, List<Integer>> memo;
-    
-    public List<Integer> diffWaysToCompute(String expression) {
-        memo = new HashMap<>();
-        int len = expression.length();
-        List<Integer> result = memo.get(expression);
-        if (result != null)
-            return result;
-        result = new ArrayList<>();
-        if (isDigit(expression)) {
-            result.add(Integer.parseInt(expression));
-            memo.put(expression, result);
-            return result;
-        }
-        for (int i = 0; i < len; i++) {
-            char ch = expression.charAt(i);
-            if (ch == '+' || ch == '-' || ch == '*') {
-                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
-                List<Integer> right = diffWaysToCompute(expression.substring(i + 1, len));
-                for (Integer num1 : left) {
-                    for (Integer num2 : right) {
-                        switch (ch) {
-                            case '+':
-                                result.add(num1 + num2);
-                                break;
-                            case '-':
-                                result.add(num1 - num2);
-                                break;
-                            case '*':
-                                result.add(num1 * num2);
-                                break;
-                        }
+    public List<Integer> diffWaysToCompute(String exp) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0;i<exp.length();i++)
+        {
+            char ch = exp.charAt(i);
+            if(ch == '-' || ch == '+' || ch == '*')
+            {
+                List<Integer> left = diffWaysToCompute(exp.substring(0,i));
+                List<Integer> right = diffWaysToCompute(exp.substring(i+1));
+                for(int m : left)
+                {
+                    for(int n : right)
+                    {
+                        if(ch == '-')
+                            list.add(m-n);
+                        else if(ch == '+')
+                            list.add(m+n);
+                        else list.add(m*n);
                     }
                 }
             }
         }
-        memo.put(expression, result);
-        return result;
-    }
-    
-    public boolean isDigit(String str) {
-        for (Character ch : str.toCharArray()) {
-            if (!Character.isDigit(ch))
-                return false;
-        }
-        return true;
+        if(list.size() == 0)
+            list.add(Integer.valueOf(exp));
+        return list;
     }
 }
