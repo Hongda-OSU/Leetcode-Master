0ea18ef634b5c26ec2495c2789c49299a0a83d38
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-07 18:17:09 -0400

    Time: 16 ms (30.09%), Space: 50.7 MB (45.81%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -8,40 +8,85 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
+    ListNode tail = new ListNode();
+    ListNode nextSubList = new ListNode();
+
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode mid = getMid(head);
-        ListNode left = sortList(head);
-        ListNode right = sortList(mid);
-        return merge(left, right);
-    }
-    
-    public ListNode merge(ListNode left, ListNode right) {
-        ListNode dummy = new ListNode(0);
-        ListNode head = dummy;
-        while (left != null && right != null) {
-            if (left.val < right.val) {
-                head.next = left;
-                left = left.next;
-            } else {
-                head.next = right;
-                right = right.next;
+        if (head == null || head.next == null)
+            return head;
+        int n = getCount(head);
+        ListNode start = head;
+        ListNode dummyHead = new ListNode();
+        for (int size = 1; size < n; size = size * 2) {
+            tail = dummyHead;
+            while (start != null) {
+                if (start.next == null) {
+                    tail.next = start;
+                    break;
+                }
+                ListNode mid = split(start, size);
+                merge(start, mid);
+                start = nextSubList;
             }
-            head = head.next;
+            start = dummyHead.next;
         }
-        head.next = (left != null) ? left : right;
-        return dummy.next;
+        return dummyHead.next;
     }
-    
-    public ListNode getMid(ListNode head) {
-        ListNode slow = head, fast = head, prev = null;
-        while (fast != null && fast.next != null) {
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
+
+    ListNode split(ListNode start, int size) {
+        ListNode midPrev = start;
+        ListNode end = start.next;
+        //use fast and slow approach to find middle and end of second linked list
+        for (int index = 1; index < size && (midPrev.next != null || end.next != null); index++) {
+            if (end.next != null) {
+                end = (end.next.next != null) ? end.next.next : end.next;
+            }
+            if (midPrev.next != null) {
+                midPrev = midPrev.next;
+            }
         }
-        prev.next = null;
-        return slow;
+        ListNode mid = midPrev.next;
+        midPrev.next = null;
+        nextSubList = end.next;
+        end.next = null;
+        // return the start of second linked list
+        return mid;
+    }
+
+    void merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode newTail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                newTail.next = list1;
+                list1 = list1.next;
+                newTail = newTail.next;
+            } else {
+                newTail.next = list2;
+                list2 = list2.next;
+                newTail = newTail.next;
+            }
+        }
+        newTail.next = (list1 != null) ? list1 : list2;
+        // traverse till the end of merged list to get the newTail
+        while (newTail.next != null) {
+            newTail = newTail.next;
+        }
+        // link the old tail with the head of merged list
+        tail.next = dummyHead.next;
+        // update the old tail to the new tail of merged list
+        tail = newTail;
+    }
+
+    int getCount(ListNode head) {
+        int cnt = 0;
+        ListNode ptr = head;
+        while (ptr != null) {
+            ptr = ptr.next;
+            cnt++;
+        }
+        return cnt;
     }
 }
