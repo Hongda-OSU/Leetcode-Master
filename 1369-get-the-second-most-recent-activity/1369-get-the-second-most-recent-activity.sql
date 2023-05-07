514b94ab24288c5f4aba9aaec5c31bd6b2fa6306
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-06 22:21:10 -0400

    Time: 956 ms (7.75%), Space: 0B (100.00%) - LeetHub

---
+++ 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT username,
+    activity,
+    startDate,
+    endDate
+FROM
+(
+    SELECT *,
+        COUNT(activity) OVER (PARTITION BY username) cnt,
+        ROW_NUMBER() OVER(PARTITION BY username ORDER BY startDate DESC) n
+    FROM UserActivity
+) tbl
+WHERE n = 2 or cnt < 2;
