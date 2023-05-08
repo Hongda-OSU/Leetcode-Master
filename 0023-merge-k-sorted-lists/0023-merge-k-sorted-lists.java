18b7d5e5021d72f26a212c704d859da8caff9f5a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-08 15:11:47 -0400

    Time: 1 ms (100.00%), Space: 44.5 MB (39.98%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,21 +10,43 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) 
+        if (lists == null || lists.length == 0) {
             return null;
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, (a, b) -> a.val - b.val);
+        }
+        return mergeKListsHelper(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode mergeKListsHelper(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        if (start + 1 == end) {
+            return merge(lists[start], lists[end]);
+        }
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeKListsHelper(lists, start, mid);
+        ListNode right = mergeKListsHelper(lists, mid + 1, end);
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        for (ListNode node : lists) {
-            if (node != null)
-                pq.add(node);
+        ListNode curr = dummy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
         }
-        while (!pq.isEmpty()) {
-            tail.next = pq.poll();
-            tail = tail.next;
-            if (tail.next != null)
-                pq.add(tail.next);
-        }
+        
+        curr.next = (l1 != null) ? l1 : l2;
+        
         return dummy.next;
     }
 }
+
