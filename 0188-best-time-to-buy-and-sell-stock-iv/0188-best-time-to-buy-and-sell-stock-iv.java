f7c21ecbfd85a380ede7b3916039d7514aac4126
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-08 17:32:11 -0400

    Time: 8 ms (46.13%), Space: 41.3 MB (78.24%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,16 +1,75 @@
-class Solution {
+public class Solution {
     public int maxProfit(int k, int[] prices) {
-        if (k == 0)
+        int n = prices.length;
+
+        // solve special cases
+        if (n <= 0 || k <= 0) {
             return 0;
-        int[] profit = new int[k + 1], cost = new int[k + 1];
-        profit[0] = 0;
-        Arrays.fill(cost, Integer.MAX_VALUE);
-        for (int price : prices) {
-            for (int i = 0; i < k; i++) {
-                cost[i + 1] = Math.min(cost[i + 1], price - profit[i]);
-                profit[i + 1] = Math.max(profit[i + 1], price - cost[i + 1]);
+        }
+
+        // find all consecutively increasing subsequence
+        ArrayList<int[]> transactions = new ArrayList<>();
+        int start = 0;
+        int end = 0;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] >= prices[i - 1]) {
+                end = i;
+            } else {
+                if (end > start) {
+                    int[] t = { start, end };
+                    transactions.add(t);
+                }
+                start = i;
             }
         }
-        return profit[k];
+        if (end > start) {
+            int[] t = { start, end };
+            transactions.add(t);
+        }
+
+        while (transactions.size() > k) {
+            // check delete loss
+            int delete_index = 0;
+            int min_delete_loss = Integer.MAX_VALUE;
+            for (int i = 0; i < transactions.size(); i++) {
+                int[] t = transactions.get(i);
+                int profit_loss = prices[t[1]] - prices[t[0]];
+                if (profit_loss < min_delete_loss) {
+                    min_delete_loss = profit_loss;
+                    delete_index = i;
+                }
+            }
+
+            // check merge loss
+            int merge_index = 0;
+            int min_merge_loss = Integer.MAX_VALUE;
+            for (int i = 1; i < transactions.size(); i++) {
+                int[] t1 = transactions.get(i - 1);
+                int[] t2 = transactions.get(i);
+                int profit_loss = prices[t1[1]] - prices[t2[0]];
+                if (profit_loss < min_merge_loss) {
+                    min_merge_loss = profit_loss;
+                    merge_index = i;
+                }
+            }
+
+            // delete or merge
+            if (min_delete_loss <= min_merge_loss) {
+                transactions.remove(delete_index);
+            } else {
+                int[] t1 = transactions.get(merge_index - 1);
+                int[] t2 = transactions.get(merge_index);
+                t1[1] = t2[1];
+                transactions.remove(merge_index);
+            }
+
+        }
+
+        int res = 0;
+        for (int[] t : transactions) {
+            res += prices[t[1]] - prices[t[0]];
+        }
+
+        return res;
     }
 }
