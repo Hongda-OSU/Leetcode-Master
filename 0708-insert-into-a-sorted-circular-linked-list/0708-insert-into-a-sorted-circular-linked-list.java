12689ff8008b065997505be07ffc018260e2743e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-08 16:02:09 -0400

    Time: 0 ms (100.00%), Space: 42.7 MB (12.50%) - LeetHub

--- 0708-insert-into-a-sorted-circular-linked-list/0708-insert-into-a-sorted-circular-linked-list.java
+++ 0708-insert-into-a-sorted-circular-linked-list/0708-insert-into-a-sorted-circular-linked-list.java
@@ -18,38 +18,29 @@ class Node {
 */
 
 class Solution {
-  public Node insert(Node head, int insertVal) {
-    if (head == null) {
-      Node newNode = new Node(insertVal, null);
-      newNode.next = newNode;
-      return newNode;
-    }
-
-    Node prev = head;
-    Node curr = head.next;
-    boolean toInsert = false;
-
-    do {
-      if (prev.val <= insertVal && insertVal <= curr.val) {
-        // Case 1).
-        toInsert = true;
-      } else if (prev.val > curr.val) {
-        // Case 2).
-        if (insertVal >= prev.val || insertVal <= curr.val)
-          toInsert = true;
-      }
-
-      if (toInsert) {
-        prev.next = new Node(insertVal, curr);
+    public Node insert(Node head, int insertVal) {
+        if(head == null) {
+            Node newNode = new Node(insertVal);
+            newNode.next = newNode;
+            return newNode;
+        }
+        Node node = head;
+        
+        while(node.next != head) {
+            if(node.val <= node.next.val) {
+                if(insertVal >= node.val && insertVal <= node.next.val) {
+                    break;
+                }
+            } else {
+                if(insertVal >= node.val || insertVal <= node.next.val) {
+                    break;
+                }
+            }
+            node = node.next;
+        }
+        
+        Node next = node.next;
+        node.next = new Node(insertVal, next);
         return head;
-      }
-
-      prev = curr;
-      curr = curr.next;
-    } while (prev != head);
-
-    // Case 3).
-    prev.next = new Node(insertVal, curr);
-    return head;
-  }
+    }
 }
