2d04b02ddcc1e7513d566c044dbff7be58800467
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-08 16:06:25 -0400

    Time: 0 ms (100.00%), Space: 42.5 MB (20.22%) - LeetHub

--- 0708-insert-into-a-sorted-circular-linked-list/0708-insert-into-a-sorted-circular-linked-list.java
+++ 0708-insert-into-a-sorted-circular-linked-list/0708-insert-into-a-sorted-circular-linked-list.java
@@ -19,26 +19,22 @@ class Node {
 
 class Solution {
     public Node insert(Node head, int insertVal) {
-        if(head == null) {
-            Node newNode = new Node(insertVal);
-            newNode.next = newNode;
-            return newNode;
+        if (head == null) {
+            Node node = new Node(insertVal);
+            node.next = node;
+            return node;
         }
         Node node = head;
-        
-        while(node.next != head) {
-            if(node.val <= node.next.val) {
-                if(insertVal >= node.val && insertVal <= node.next.val) {
+        while (node.next != head) {
+            if (node.val <= node.next.val) {
+                if (insertVal >= node.val && insertVal <= node.next.val)
                     break;
-                }
             } else {
-                if(insertVal >= node.val || insertVal <= node.next.val) {
+                if (insertVal >= node.val || insertVal <= node.next.val)
                     break;
-                }
             }
             node = node.next;
         }
-        
         Node next = node.next;
         node.next = new Node(insertVal, next);
         return head;
