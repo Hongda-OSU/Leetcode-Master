006aba59ef6cac59633da9d9528ddd777182d7ca
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-08 15:57:13 -0400

    Time: 1 ms (100.00%), Space: 44.2 MB (77.42%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,43 +10,36 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
+        if (lists == null || lists.length == 0)
             return null;
-        }
         return mergeKListsHelper(lists, 0, lists.length - 1);
     }
     
     private ListNode mergeKListsHelper(ListNode[] lists, int start, int end) {
-        if (start == end) {
+        if (start == end)
             return lists[start];
-        }
-        if (start + 1 == end) {
+        if (start + 1 == end)
             return merge(lists[start], lists[end]);
-        }
         int mid = start + (end - start) / 2;
         ListNode left = mergeKListsHelper(lists, start, mid);
         ListNode right = mergeKListsHelper(lists, mid + 1, end);
         return merge(left, right);
     }
     
-    private ListNode merge(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode();
         ListNode curr = dummy;
-        
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                curr.next = l1;
-                l1 = l1.next;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                curr.next = left;
+                left = left.next;
             } else {
-                curr.next = l2;
-                l2 = l2.next;
+                curr.next = right;
+                right = right.next;
             }
             curr = curr.next;
         }
-        
-        curr.next = (l1 != null) ? l1 : l2;
-        
+        curr.next = (left != null) ? left : right;
         return dummy.next;
     }
 }
-
