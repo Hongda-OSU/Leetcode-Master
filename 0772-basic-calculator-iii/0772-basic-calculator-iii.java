f0f5580357b8a2eb9028a8a60074b5fbd317d854
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-09 16:01:19 -0400

    Time: 3 ms (92.45%), Space: 42.3 MB (49.48%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.java
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.java
@@ -1,24 +1,22 @@
 class Solution {
-    int index = 0;
+    private int index = 0;
+    
     public int calculate(String s) {
-        int number = 0; /// Current number
+        int number = 0, len = s.length();
         char sign = '+';
-        /// Stack to track current state of equation calculations
         Stack<Integer> stack = new Stack<>();
-        while(index <= s.length()) {
-            char curr = (index == s.length()) ? '#': s.charAt(index);
-            if(curr == '(') {
+        while (index <= len) {
+            char curr = (index == len) ? '#' : s.charAt(index);
+            if (curr == '(') {
                 index++;
-                /// Start Of Braces - Ask current function to get result until end of Braces i.e. ")"
                 number = calculate(s);
-            } else if(curr == ')') {
-                /// End of Braces - Update Stack and return the current result
+            } else if (curr == ')') {
                 updateStack(stack, number, sign);
                 return getResult(stack);
             } else {
-                /// if current character is a number append it to existing one
-                if(Character.isDigit(curr)) number = (number * 10) + curr - '0';
-                else { /// evalute here 
+                if (Character.isDigit(curr))
+                    number = (number * 10) + curr - '0';
+                else {
                     updateStack(stack, number, sign);
                     sign = curr;
                     number = 0;
@@ -26,25 +24,22 @@ class Solution {
             }
             index++;
         }
-        //// Finally Calculate based on numbers present on Stack
         return getResult(stack);
     }
     
-    
-    //// Helper to update current Stack with provided number and Sign
-    private void updateStack(Stack<Integer> stackInst, int number, char sign) {
+    private void updateStack(Stack<Integer> stack, int number, char sign) {
         switch(sign) {
-            case '+': stackInst.push(number); break;
-            case '-': stackInst.push(-number); break;
-            case '*': stackInst.push(stackInst.pop() * number); break;
-            case '/': stackInst.push(stackInst.pop() / number); break;   
+            case '+': stack.push(number); break;
+            case '-': stack.push(-number); break;
+            case '*': stack.push(stack.pop() * number); break;
+            case '/': stack.push(stack.pop() / number); break;   
         }
     }
     
-    //// Add up all number from existing Stack to get result
-    private int getResult(Stack<Integer> stackInst) {
-        int res = 0;
-        while(!stackInst.isEmpty()) res += stackInst.pop();
-        return res;
+    private int getResult(Stack<Integer> stack) {
+        int result = 0;
+        while (!stack.isEmpty())
+            result += stack.pop();
+        return result;
     }
 }
