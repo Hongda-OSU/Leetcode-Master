8337617d2806fadd7b38bf01e1d816981a2b1142
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-10 13:56:07 -0400

    Time: 5 ms (97.48%), Space: 42.6 MB (96.56%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.java
+++ 0641-design-circular-deque/0641-design-circular-deque.java
@@ -1,100 +1,90 @@
 class MyCircularDeque {
-DoublyLinkedList head;
-DoublyLinkedList tail;
-int k;
-int size;
+    private DoublyLinkedList head, tail;
+    private int k, size;
 
-/** Initialize your data structure here. Set the size of the deque to be k. */
-public MyCircularDeque(int k) {
-    head = new DoublyLinkedList(-1);
-    tail = new DoublyLinkedList(-1);
-    head.next = tail;
-    tail.prev = head;
-    this.k = k;
-    this.size = size;
-}
-
-/** Adds an item at the front of Deque. Return true if the operation is successful. */
-public boolean insertFront(int value) {
-    if(size == k) return false;
-    DoublyLinkedList temp = new DoublyLinkedList(value);
-    DoublyLinkedList curr = head.next;
-    head.next = temp;
-    temp.prev = head;
-    temp.next = curr;
-    curr.prev = temp;
-    size += 1;
-    return true;
-}
-
-/** Adds an item at the rear of Deque. Return true if the operation is successful. */
-public boolean insertLast(int value) {
-    if(size == k) return false;
-    DoublyLinkedList temp = new DoublyLinkedList(value);
-    DoublyLinkedList curr = tail.prev;
-    temp.next = tail;
-    tail.prev = temp;
-    curr.next = temp;
-    temp.prev = curr;
-    size += 1;
-    return true;
+    public MyCircularDeque(int k) {
+        head = new DoublyLinkedList(-1);
+        tail = new DoublyLinkedList(-1);
+        head.next = tail;
+        tail.prev = head;
+        this.k = k;
+        this.size = size;
+    }
     
-}
-
-/** Deletes an item from the front of Deque. Return true if the operation is successful. */
-public boolean deleteFront() {
-    if(size == 0) return false;
-
-    DoublyLinkedList curr = head.next;
-    head.next = curr.next;
-    head.next.prev = head;
-    size -= 1;
-    return true;
-}
-
-/** Deletes an item from the rear of Deque. Return true if the operation is successful. */
-public boolean deleteLast() {
-    if(size == 0) return false;
-
-    DoublyLinkedList curr = tail.prev;
-    tail.prev = curr.prev;
-    tail.prev.next= tail;
-    size -= 1;
-    return true;
-}
-
-/** Get the front item from the deque. */
-public int getFront() {
-    if(size == 0) return -1;
+    public boolean insertFront(int value) {
+        if (size == k)
+            return false;
+        DoublyLinkedList temp = new DoublyLinkedList(value);
+        DoublyLinkedList curr = head.next;
+        head.next = temp;
+        temp.prev = head;
+        temp.next = curr;
+        curr.prev = temp;
+        size++;
+        return true;
+    }
     
-    return head.next.val;
+    public boolean insertLast(int value) {
+        if (size == k)
+            return false;
+        DoublyLinkedList temp = new DoublyLinkedList(value);
+        DoublyLinkedList curr = tail.prev;
+        temp.next = tail;
+        tail.prev = temp;
+        curr.next = temp;
+        temp.prev = curr;
+        size++;
+        return true;
+    }
     
-}
-
-/** Get the last item from the deque. */
-public int getRear() {
-    if(size == 0) return -1;
-    return tail.prev.val;
-}
-
-/** Checks whether the circular deque is empty or not. */
-public boolean isEmpty() {
-    return size == 0;
-}
-
-/** Checks whether the circular deque is full or not. */
-public boolean isFull() {
-    return size == k;
-}
-
-class DoublyLinkedList{
-    DoublyLinkedList prev;
-    DoublyLinkedList next;
-    int val;
-    DoublyLinkedList(int val){
-        this.val = val;
+    public boolean deleteFront() {
+        if (size == 0)
+            return false;
+        DoublyLinkedList curr = head.next;
+        head.next = curr.next;
+        head.next.prev = head;
+        size--;
+        return true;
+    }
+    
+    public boolean deleteLast() {
+        if (size == 0)
+            return false;
+        DoublyLinkedList curr = tail.prev;
+        tail.prev = curr.prev;
+        tail.prev.next = tail;
+        size--;
+        return true;
+    }
+    
+    public int getFront() {
+        if (size == 0)
+            return -1;
+        return head.next.val;
+    }
+    
+    public int getRear() {
+        if (size == 0)
+            return -1;
+        return tail.prev.val;
+    }
+    
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    public boolean isFull() {
+        return size == k;
     }
 }
+
+class DoublyLinkedList {
+    public DoublyLinkedList next, prev;
+    public int val;
+    
+    public DoublyLinkedList(int val) {
+        this.val = val;
+    }
 }
 
 /**
