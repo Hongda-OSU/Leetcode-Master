74e9a964bdf18fd4adb29b4c2fbda6461a95bb30
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-11 21:15:24 -0400

    Time: 275 ms (17.31%), Space: 61.5 MB (8.65%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -1,17 +1,30 @@
 class Solution {
-    int dp[][];
+    
+    HashMap<String,Long> map = new HashMap();
+    
     public int numWays(int steps, int arrLen) {
-        dp=new int[501][501];
-        for(int row[]:dp) Arrays.fill(row,-1);
-        return rec(steps,arrLen,0);
+        
+        return (int) (ways(steps,arrLen,0) % ((Math.pow(10,9)) +7));
     }
-    public int rec(int steps,int len,int index){
-        if(index<0 || index>=len || steps<0) return 0;
-        if(steps==0 && index==0) return 1;
-        if(dp[steps][index]!=-1) return dp[steps][index];
-
-        int ans = ((rec(steps-1,len,index+1) + rec(steps-1,len,index-1))%1000000007 + rec(steps-1,len,index))%1000000007;
-
-        return dp[steps][index]=ans;
+    
+    public long ways(int steps,int arrLen,int index){
+        String curr = index + "->" + steps;
+        
+        if(index == 0 && steps == 0){
+            return 1;
+        }else if(index < 0 || (index >= arrLen) || steps == 0){
+            return 0;
+        }
+        
+        if(map.containsKey(curr)){
+            return map.get(curr);
+        }
+        long stay = ways(steps-1,arrLen,index);
+        long right = ways(steps-1,arrLen,index+1);
+        long left = ways(steps-1,arrLen,index-1);
+        
+        map.put(curr , (stay+right+left) % 1000000007);
+        
+        return (right + left + stay) % 1000000007;
     }
 }
