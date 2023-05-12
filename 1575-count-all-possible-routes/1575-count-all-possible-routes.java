00b47b957978405271acc9ca4b8b8e742f7f8dc1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-12 15:39:09 -0400

    Time: 83 ms (63.08%), Space: 42.2 MB (84.62%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.java
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -1,26 +1,31 @@
 class Solution {
-    public int countRoutes(int[] locations, int start, int finish, int fuel) {
-        int n = locations.length;
-        long[][] dp = new long[n][fuel + 1];
-        for (int i = 0; i < n; ++i) {
-            Arrays.fill(dp[i], -1);
+    public int solve(int[] locations, int currCity, int finish, int remainingFuel, int memo[][]) {
+        if (remainingFuel < 0) {
+            return 0;
         }
-        return (int) solve(locations, start, finish, dp, fuel);
-    }
-	// dp[curCity][fuel] = number of ways to reach finish, when we are at city `curCity` with fuel `fuel`
-    private long solve(int[] locations, int curCity, int e, long[][] dp, int fuel) {
-        // 4. There is no further way left.
-        if (fuel < 0) return 0;
-        if (dp[curCity][fuel] != -1) return dp[curCity][fuel];
-        // 3. Now, if we have atleast 1 way of reaching `end`, add 1 to the answer. But don't stop right here, keep going, there might be more ways :)
-        long ans = (curCity == e) ? 1 : 0;
-        for (int nextCity = 0; nextCity < locations.length; ++nextCity) {
-            // 1. Visit all cities except `curCity`.
-            if (nextCity != curCity) {
-                // 2. Continue this process recursively.
-                ans = (ans + solve(locations, nextCity, e, dp, fuel - Math.abs(locations[curCity] - locations[nextCity]))) % 1000000007;
+        if (memo[currCity][remainingFuel] != -1) {
+            return memo[currCity][remainingFuel];
+        }
+
+        int ans = currCity == finish ? 1 : 0;
+        for (int nextCity = 0; nextCity < locations.length; nextCity++) {
+            if (nextCity != currCity) {
+                ans = (ans + solve(locations, nextCity, finish,
+                remainingFuel - Math.abs(locations[currCity] - locations[nextCity]),
+                                   memo)) % 1000000007;
             }
         }
-        return dp[curCity][fuel] = ans;
+
+        return memo[currCity][remainingFuel] = ans;
+    }
+
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        int n = locations.length;
+        int memo[][] = new int[n][fuel + 1];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(memo[i], -1);
+        }
+
+        return solve(locations, start, finish, fuel, memo);
     }
 }
