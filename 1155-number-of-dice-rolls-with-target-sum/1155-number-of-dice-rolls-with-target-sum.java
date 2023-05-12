05dc08403d3c678cc5a07e3e8968cab609f6f53c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-11 21:04:15 -0400

    Time: 11 ms (90.83%), Space: 42 MB (39.41%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,31 +1,25 @@
 class Solution {
     private int[][] dp;
-
-    public int numRollsToTarget(int dices, int faces, int target) {
-        dp = new int[dices + 1][target + 1];
-        for (int[] arr : dp) {
+    
+    public int numRollsToTarget(int n, int k, int target) {
+        dp = new int[n + 1][target + 1];
+        for (int[] arr : dp)
             Arrays.fill(arr, -1);
-        }
-        return recursion(dices, faces, target);
+        return helper(n, k, target);
     }
-
-    private int recursion(int dices, int faces, int target) {
+    
+    private int helper(int dices, int faces, int target) {
         int result = 0;
-        if (dices == 0 && target == 0) {
+        if (dices == 0 && target == 0)
             return 1;
-        }
-        if (target < 0 || dices<=0) {
+        if (target < 0 || dices <= 0)
             return 0;
-        }
-        if (dp[dices][target] != -1) {
+        if (dp[dices][target] != -1)
             return dp[dices][target];
-        }
         for (int i = 1; i <= faces; i++) {
-            result += recursion(dices - 1, faces, target - i);
-
-            if (result >= 1000000007) {
-                result %= 1000000007;
-            }
+            result += helper(dices - 1, faces, target - i);
+            if (result >= 1_000_000_007)
+                result %= 1_000_000_007;
         }
         dp[dices][target] = result;
         return result;
