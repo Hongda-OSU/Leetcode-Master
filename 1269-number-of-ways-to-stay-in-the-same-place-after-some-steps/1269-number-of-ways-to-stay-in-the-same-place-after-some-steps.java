791dc563d877b8a81950d9f5aca869a940ce0b5b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-11 21:21:04 -0400

    Time: 13 ms (75.96%), Space: 43.5 MB (72.12%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -1,40 +1,23 @@
 class Solution {
-    
     public int numWays(int steps, int arrLen) {
         int[][] memo = new int[steps + 1][steps + 1];
-        return helper(steps , arrLen , 0 , memo );
+        return helper(steps, arrLen, 0, memo);
     }
     
-    private int helper(int moves , int N , int i , int[][]memo){
-        
-        if(i  > moves){
-            return 0 ;
-         }
-        
-        if(moves == 0 && i == 0){
+    private int helper(int moves, int n, int i, int[][] memo) {
+        if (i > moves) 
+            return 0;
+        if (moves == 0 && i == 0)
             return 1;
-        }
-        
-        if(memo[moves][i] != 0){
+        if (memo[moves][i] != 0)
             return memo[moves][i];
-        }
-        
-        int res = 0 , MOD = 1_000_000_007;
-        
-        res = helper(moves - 1  , N , i , memo) % MOD; 
-        
-        if(i > 0){
-            res = ( res % MOD + helper(moves - 1 , N , i - 1 , memo) % MOD ) % MOD;
-        }
-        if( i < N - 1){
-            res = ( res % MOD +  helper(moves - 1 , N ,  i + 1 , memo) % MOD ) % MOD;
-        }
-        
-        memo[moves][i] = res;
-        
-      return res;  
-        
+        int result = 0, mod = 1_000_000_007;
+        result = helper(moves - 1, n, i, memo) % mod;
+        if (i > 0) 
+            result = (result % mod + helper(moves - 1, n, i - 1, memo) % mod) % mod;
+        if (i < n - 1)
+            result = (result % mod + helper(moves - 1, n, i + 1, memo) % mod) % mod;
+        memo[moves][i] = result;
+        return result;
     }
-    
-    
 }
