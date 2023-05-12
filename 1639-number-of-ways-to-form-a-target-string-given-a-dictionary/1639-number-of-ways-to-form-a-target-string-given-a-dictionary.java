dca7d5cb995595ceafb49af2a75da21a1dc310ad
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-12 16:19:13 -0400

    Time: 159 ms (11.56%), Space: 58.5 MB (33.30%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -1,36 +1,26 @@
 class Solution {
-    int alphabet = 26;
-    int mod = 1000000007;
-    int cnt[][];
-    long dp[][];
-
-    long f(String target, int i, int j) {
-        if (j == 0) {
-            return i == 0 ? 1 : 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        dp[i][j] = f(target, i, j - 1);
-        if (i > 0) {
-            dp[i][j] += cnt[target.charAt(i - 1) - 'a'][j - 1] * f(target, i - 1, j - 1);
-        }
-        dp[i][j] %= mod;
-        return dp[i][j];
-    }
-    
     public int numWays(String[] words, String target) {
+        int alphabet = 26;
+        int mod = 1000000007;
         int m = target.length(), k = words[0].length();
-        cnt = new int[alphabet][k];
+        int cnt[][] = new int[alphabet][k];
         for (int j = 0; j < k; j++) {
             for (String word : words) {
                 cnt[word.charAt(j) - 'a'][j]++;
             }
         }
-        dp = new long[m + 1][k + 1];
+        long dp[][] = new long[m + 1][k + 1];
+        dp[0][0] = 1;
         for (int i = 0; i <= m; i++) {
-            Arrays.fill(dp[i], -1);
+            for (int j = 0; j < k; j++) {
+                if (i < m) {
+                    dp[i + 1][j + 1] += cnt[target.charAt(i) - 'a'][j] * dp[i][j];
+                    dp[i + 1][j + 1] %= mod;
+                }
+                dp[i][j + 1] += dp[i][j];
+                dp[i][j + 1] %= mod;
+            }
         }
-        return (int)f(target, m, k);
+        return (int)dp[m][k];
     }
 }
