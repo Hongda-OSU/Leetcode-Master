08722e25007b2f84cf78821d04fc685a288584da
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-12 15:38:30 -0400

    Time: 311 ms (18.46%), Space: 45.3 MB (7.69%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.java
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -1,33 +1,28 @@
 class Solution {
-    long MOD = 1000_000_007;
-    int[] locations;
-    public int countRoutes(int[] locations, int start, int finish, int fuel) {
-        this.locations = locations;
-        return (int) countRoutesDp(start, finish, fuel, new HashMap<>());
-    }
-    
-    /// Use Return Type as Long - Since internal result may not fit Int limit before MOD 
-    private long countRoutesDp(int start, int finish, int fuel, Map<String, Long> memo) {
-        if(fuel < 0) return 0;
-        long totalWays = 0;
-        //// Start is Same as Finish - Add 1 and then Continue further
-        if(start == finish) totalWays += 1; 
-        /// Unique Key - Start And Remaining Fuel
-        String key = start + "#" + fuel;
-        if(memo.containsKey(key)) return memo.get(key);
-        
-        for(int nextLoc=0;nextLoc<locations.length;nextLoc++) {
-            /// Continue Since previous start was same has next location
-            if(nextLoc == start) continue;
-            /// required fuel for this Path
-            int reqFuel = Math.abs(locations[start] - locations[nextLoc]);
-            if(reqFuel <= fuel) { /// Continue to this (nextLoc) location only if sufficient fuel is available
-                totalWays += countRoutesDp(nextLoc, finish, fuel - reqFuel, memo) % MOD;
-            }
+    long[][][] dp = new long[101][201][2];
+    long mod = (long)1e9+7;
+    public long helper(int[] locations, int start, int finish, int fuel,int flag,long[][][] dp){
+        if(fuel<0) return 0;
+        if(start==finish && flag==0){
+            if(fuel==0) return 1;
+            if(fuel>0) return 1 + helper(locations,start,finish,fuel,1,dp);
         }
-        /// MOD the result before Returning
-        totalWays %= MOD;
-        memo.put(key, totalWays);
-        return totalWays;
+        if(dp[start][fuel][flag]!=-1) return dp[start][fuel][flag];
+        long ans = 0;
+        for(int i=0;i<locations.length;i++){
+            int x = Math.abs(locations[start]-locations[i]);
+            if(i!=start){
+                if(flag==0) ans += (helper(locations,i,finish,fuel-x,flag,dp)%mod);
+                else ans += (helper(locations,i,finish,fuel-x,0,dp)%mod);
+            }
+            ans%=mod;
+        }
+        ans%=mod;
+        return dp[start][fuel][flag]=ans;
+    }
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        int n = locations.length;
+        for(long[][] d1:dp) for(long[] d2:d1) Arrays.fill(d2,-1);
+        return (int)helper(locations,start,finish,fuel,0,dp);
     }
 }
