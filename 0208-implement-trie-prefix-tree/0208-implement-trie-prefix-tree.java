460f61bee9385de650588192aa80e60b2470ded3
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-12 14:38:41 -0400

    Time: 37 ms (86.39%), Space: 53.4 MB (38.57%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,27 +1,16 @@
-class TrieNode {
-    boolean isWord;
-    TrieNode[] children;
-    
-    public TrieNode() {
-        isWord = false;
-        children = new TrieNode[26]; // 26 English lowercase letters
-    }
-}
-
 class Trie {
-    TrieNode root;
-    
+    private TrieNode root;
+
     public Trie() {
         root = new TrieNode();
     }
     
     public void insert(String word) {
         TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            int index = c - 'a';
-            if (node.children[index] == null) {
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (node.children[index] == null)
                 node.children[index] = new TrieNode();
-            }
             node = node.children[index];
         }
         node.isWord = true;
@@ -29,11 +18,10 @@ class Trie {
     
     public boolean search(String word) {
         TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            int index = c - 'a';
-            if (node.children[index] == null) {
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (node.children[index] == null)
                 return false;
-            }
             node = node.children[index];
         }
         return node.isWord;
@@ -41,18 +29,25 @@ class Trie {
     
     public boolean startsWith(String prefix) {
         TrieNode node = root;
-        for (char c : prefix.toCharArray()) {
-            int index = c - 'a';
-            if (node.children[index] == null) {
+        for (char ch : prefix.toCharArray()) {
+            int index = ch - 'a';
+            if (node.children[index] == null)
                 return false;
-            }
             node = node.children[index];
         }
         return true;
     }
 }
 
-
+class TrieNode {
+    public boolean isWord;
+    public TrieNode[] children;
+    
+    public TrieNode() {
+        this.isWord = false;
+        this.children = new TrieNode[26];
+    }
+}
 
 /**
  * Your Trie object will be instantiated and called as such:
