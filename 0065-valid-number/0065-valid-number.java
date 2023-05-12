669774f69a1cc7afb24955d1166a9227124eda2c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-12 14:22:31 -0400

    Time: 2 ms (96.93%), Space: 42.6 MB (61.12%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,44 +1,33 @@
 class Solution {
-    // This is the DFA we have designed above
-    private static final List<Map<String, Integer>> dfa = List.of(
-        Map.of("digit", 1, "sign", 2, "dot", 3),
-        Map.of("digit", 1, "dot", 4, "exponent", 5), 
-        Map.of("digit", 1, "dot", 3), 
-        Map.of("digit", 4), 
-        Map.of("digit", 4, "exponent", 5),
-        Map.of("sign", 6, "digit", 7),
-        Map.of("digit", 7),
-        Map.of("digit", 7)
-    );
-
-    // These are all of the valid finishing states for our DFA.
-    private static final Set<Integer> validFinalStates = Set.of(1, 4, 7);
-
     public boolean isNumber(String s) {
-        int currentState = 0;
-        String group = "";
+        boolean seenDigit = false;
+        boolean seenExponent = false;
+        boolean seenDot = false;
         
         for (int i = 0; i < s.length(); i++) {
             char curr = s.charAt(i);
             if (Character.isDigit(curr)) {
-                group = "digit";
+                seenDigit = true;
             } else if (curr == '+' || curr == '-') {
-                group = "sign";
+                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
             } else if (curr == 'e' || curr == 'E') {
-                group = "exponent";
+                if (seenExponent || !seenDigit) {
+                    return false;
+                }
+                seenExponent = true;
+                seenDigit = false;
             } else if (curr == '.') {
-                group = "dot";
+                if (seenDot || seenExponent) {
+                    return false;
+                }
+                seenDot = true;
             } else {
                 return false;
             }
-            
-            if (!dfa.get(currentState).containsKey(group)) {
-                return false;
-            }
-            
-            currentState = dfa.get(currentState).get(group);
         }
         
-        return validFinalStates.contains(currentState);
+        return seenDigit;
     }
 }
