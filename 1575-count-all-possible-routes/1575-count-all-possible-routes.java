fc1650d9125d7d269fd87467dac8e592ee6fff32
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-12 15:38:15 -0400

    Time: 735 ms (6.15%), Space: 49.5 MB (7.69%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.java
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -1,19 +1,33 @@
 class Solution {
+    long MOD = 1000_000_007;
+    int[] locations;
     public int countRoutes(int[] locations, int start, int finish, int fuel) {
-        long[][] dp = new long[locations.length][fuel+1];
-        long MOD = (long)(1e9 + 7);
-        Arrays.fill(dp[finish],1);
-        long ans = 0;
-        for(int j = 0; j <= fuel; j++) {
-            for(int i = 0; i < locations.length; i++) {
-                for(int k = 0; k < locations.length; k++) {
-                    if(k == i) continue;
-                    if(Math.abs(locations[i] - locations[k]) <= j) {
-                        dp[i][j] = (dp[i][j] + dp[k][j - Math.abs(locations[i] - locations[k])]) % MOD; 
-                    }
-                }
+        this.locations = locations;
+        return (int) countRoutesDp(start, finish, fuel, new HashMap<>());
+    }
+    
+    /// Use Return Type as Long - Since internal result may not fit Int limit before MOD 
+    private long countRoutesDp(int start, int finish, int fuel, Map<String, Long> memo) {
+        if(fuel < 0) return 0;
+        long totalWays = 0;
+        //// Start is Same as Finish - Add 1 and then Continue further
+        if(start == finish) totalWays += 1; 
+        /// Unique Key - Start And Remaining Fuel
+        String key = start + "#" + fuel;
+        if(memo.containsKey(key)) return memo.get(key);
+        
+        for(int nextLoc=0;nextLoc<locations.length;nextLoc++) {
+            /// Continue Since previous start was same has next location
+            if(nextLoc == start) continue;
+            /// required fuel for this Path
+            int reqFuel = Math.abs(locations[start] - locations[nextLoc]);
+            if(reqFuel <= fuel) { /// Continue to this (nextLoc) location only if sufficient fuel is available
+                totalWays += countRoutesDp(nextLoc, finish, fuel - reqFuel, memo) % MOD;
             }
         }
-        return (int)dp[start][fuel];
+        /// MOD the result before Returning
+        totalWays %= MOD;
+        memo.put(key, totalWays);
+        return totalWays;
     }
 }
