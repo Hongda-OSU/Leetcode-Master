c5cff9c16d6f7b113241ebeea4e66ceb64987db9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-12 16:20:22 -0400

    Time: 93 ms (79.03%), Space: 51.8 MB (58.47%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -1,16 +1,33 @@
 class Solution {
+     String[] words;
+    String target;
+    Integer[][] memo;
+    int m, n;
+    int[][] charAtIndexCnt;
     public int numWays(String[] words, String target) {
-        int n = target.length();
-        long mod = (long)1e9 + 7, res[] = new long[n + 1];
-        res[0] = 1;
-        for (int i = 0; i < words[0].length(); ++i) {
-            int[] count = new int[26];
-            for (String w : words)
-                count[w.charAt(i) - 'a']++;
-            for (int j = n - 1; j >= 0; --j) {
-                res[j + 1] += res[j] * count[target.charAt(j) - 'a'] % mod;
-            }
+        this.words = words; this.target = target;
+        m = words[0].length(); n = target.length();
+        memo = new Integer[m][n];
+        charAtIndexCnt = new int[128][m];
+        
+        for (String word : words)
+            for (int i = 0; i < word.length(); i++)
+                charAtIndexCnt[word.charAt(i)][i] += 1; // Count the number of character `c` at index `i` of all words
+        return dp(0, 0);
+    }
+    
+    int dp(int k, int i) {
+        if (i == n)  // Formed a valid target
+            return 1;
+        if (k == m)  // Reached to length of words[x] but don't found any result
+            return 0;
+        if (memo[k][i] != null) return memo[k][i];
+        char c = target.charAt(i);
+        long ans = dp(k + 1, i);  // Skip k_th index of words
+        if (charAtIndexCnt[c][k] > 0) { // Take k_th index of words if found character `c` at index k_th
+            ans += (long) dp(k + 1, i + 1) * charAtIndexCnt[c][k];
+            ans %= 1_000_000_007;
         }
-        return (int)(res[n] % mod);
+        return memo[k][i] = (int) ans;
     }
 }
