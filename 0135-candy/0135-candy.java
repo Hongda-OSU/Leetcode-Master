d947aff17639030205c2e9ff530c84efcef3415f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-13 17:48:44 -0400

    Time: 3 ms (67.86%), Space: 43.5 MB (42.46%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,36 +1,19 @@
 public class Solution {
-    public int count(int n) {
-        return (n * (n + 1)) / 2;
-    }
     public int candy(int[] ratings) {
-        if (ratings.length <= 1) {
-            return ratings.length;
-        }
-        int candies = 0;
-        int up = 0;
-        int down = 0;
-        int oldSlope = 0;
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
         for (int i = 1; i < ratings.length; i++) {
-            int newSlope = (ratings[i] > ratings[i - 1]) ? 1 
-                : (ratings[i] < ratings[i - 1] ? -1 
-                : 0);
-
-            if ((oldSlope > 0 && newSlope == 0) || (oldSlope < 0 && newSlope >= 0)) {
-                candies += count(up) + count(down) + Math.max(up, down);
-                up = 0;
-                down = 0;
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
-            if (newSlope > 0) {
-                up++;
-            } else if (newSlope < 0) {
-                down++;
-            } else {
-                candies++;
-            }
-
-            oldSlope = newSlope;
         }
-        candies += count(up) + count(down) + Math.max(up, down) + 1;
-        return candies;
+        int sum = candies[ratings.length - 1];
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+            sum += candies[i];
+        }
+        return sum;
     }
 }
