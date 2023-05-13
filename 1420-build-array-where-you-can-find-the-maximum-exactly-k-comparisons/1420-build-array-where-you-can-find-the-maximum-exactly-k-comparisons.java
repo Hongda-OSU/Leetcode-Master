d8c56d6c50c7a17f20a16ee72337df51f1f0cba9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-13 17:34:37 -0400

    Time: 185 ms (25.71%), Space: 43.3 MB (20.00%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -1,28 +1,39 @@
 class Solution {
-    public int numOfArrays(int n, int m, int k) {
-        Integer[][][] dp = new Integer[n + 1][m + 1][k + 1];
-        return dfs(n, m, k, 0, 0, 0, dp);
+    
+    long[][][] memo;
+    long mod = (long)1e9 + 7;
+    int N, M;
+    long rec(int i, int msf, int kleft) {
+        if (i == N) {
+            return kleft == 0 ? 1 : 0;
+        }        
+        if (kleft < 0) return 0;
+        if (memo[i][msf][kleft] != -1)  return memo[i][msf][kleft];
+        
+        long ans = 0;
+        for (int j = 1; j <= M; j++) {
+            ans = (ans + rec(i+1, Math.max(msf, j), j > msf ? kleft-1 : kleft)) % mod;
+        }
+                   
+        return memo[i][msf][kleft] = ans;
     }
-    // dfs(... i, currMax, currCost) the number of ways to build the valid array `arr[i:]`
-    //     keeping in mind that current maximum element is `currMax` and current search cost is `currCost`
-    int dfs(int n, int m, int k, int i, int currMax, int currCost, Integer[][][] dp) {
-        if (i == n) {
-            if (k == currCost) return 1; // valid if the value search cost is equal to k
-            return 0;
-        }
-        if (dp[i][currMax][currCost] != null) return dp[i][currMax][currCost];
-        int ans = 0;
-        for (int num = 1; num <= m; num++) {
-            int newCost = currCost;
-            int newMax = currMax;
-            if (num > currMax) {
-                newCost++;
-                newMax = num;
+    
+    
+    public int numOfArrays(int n, int m, int K) {
+        memo = new long[n+1][m+1][K+1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= K; k++) {
+                    memo[i][j][k] = -1;
+                }
             }
-            if (newCost > k) break;
-            ans += dfs(n, m, k, i + 1, newMax, newCost, dp);
-            ans %= 1_000_000_007;
         }
-        return dp[i][currMax][currCost] = ans;
+        this.N = n;
+        M = m;
+        long ans = 0;
+        for (int i = 1; i <= m; i++) {
+            ans = (ans + rec(1, i, K-1)) % mod;
+        }
+        return (int)ans;
     }
 }
