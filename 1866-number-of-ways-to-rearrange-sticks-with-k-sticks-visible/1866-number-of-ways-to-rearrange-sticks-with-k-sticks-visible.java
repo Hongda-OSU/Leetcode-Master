8ff89605524c134ddfac4885054906558549f402
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-13 17:25:48 -0400

    Time: 110 ms (78.38%), Space: 56.4 MB (14.86%) - LeetHub

--- 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
+++ 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
@@ -1,10 +1,23 @@
 class Solution {
-      int dp[][] = new int[1001][1001], mod = 1000000007;
     public int rearrangeSticks(int n, int k) {
-        if (n == k) return 1;
-        if (k == 0) return 0;
-        if (dp[n][k] == 0)
-            dp[n][k] = (int)((1L * rearrangeSticks(n - 1, k - 1) + 1L * rearrangeSticks(n - 1, k) * (n - 1)) % mod);
-        return dp[n][k];
+        //Let's use DP to solve, dp[i][j]: we have i sticks and j left visible
+        //imagine for i+1, we add a smallest stick into the group
+        //so for dp[i+1][j],when we put this smallest stick not in the first place, 
+        //this stick will have no impact on left visible
+        //and there are i places to put, so i * dp[i][j]
+        //when this smallest stick is put in the first place, then we can only have j-1 visible for the remaining
+        //so dp[i][j-1]
+        //dp[i+1][j] = dp[i][j] * i + dp[i][j-1]
+        //dp[0][0] = 1
+        long[][] dp = new long[n+1][k+1];
+        dp[0][0] = 1L;
+        int mod = (int)(1e9+7);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= Math.min(i, k); j++) {
+                dp[i][j] = dp[i-1][j] * (long)(i-1)+ dp[i-1][j-1];
+                dp[i][j] %= mod;
+            }
+        }
+        return (int)(dp[n][k] % mod);
     }
 }
