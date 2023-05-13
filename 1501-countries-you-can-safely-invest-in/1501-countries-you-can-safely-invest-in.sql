e252bd42918d0822da6ed52cb7d115ab55fa0a94
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-12 20:52:22 -0400

    Time: 2049 ms (77.98%), Space: 0B (100.00%) - LeetHub

--- 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -6,7 +6,8 @@ ON SUBSTRING(phone_number, 1, 3) = country_code
 JOIN Calls ca
 ON p.id IN (ca.caller_id, ca.callee_id)
 GROUP BY co.name
-HAVING AVG(duration) > (
+HAVING AVG(duration) > 
+(
     SELECT AVG(duration)
     FROM Calls
 );
