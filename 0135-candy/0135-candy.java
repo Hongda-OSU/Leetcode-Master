992becc8489ea8bebadce3db623162faa666568d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-13 17:49:00 -0400

    Time: 2156 ms (5.04%), Space: 43.8 MB (25.57%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,22 +1,24 @@
 public class Solution {
     public int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        boolean hasChanged = true;
+        while (hasChanged) {
+            hasChanged = false;
+            for (int i = 0; i < ratings.length; i++) {
+                if (i != ratings.length - 1 && ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                    candies[i] = candies[i + 1] + 1;
+                    hasChanged = true;
+                }
+                if (i > 0 && ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
+                    candies[i] = candies[i - 1] + 1;
+                    hasChanged = true;
+                }
+            }
+        }
         int sum = 0;
-        int[] left2right = new int[ratings.length];
-        int[] right2left = new int[ratings.length];
-        Arrays.fill(left2right, 1);
-        Arrays.fill(right2left, 1);
-        for (int i = 1; i < ratings.length; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                left2right[i] = left2right[i - 1] + 1;
-            }
-        }
-        for (int i = ratings.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                right2left[i] = right2left[i + 1] + 1;
-            }
-        }
-        for (int i = 0; i < ratings.length; i++) {
-            sum += Math.max(left2right[i], right2left[i]);
+        for (int candy : candies) {
+            sum += candy;
         }
         return sum;
     }
