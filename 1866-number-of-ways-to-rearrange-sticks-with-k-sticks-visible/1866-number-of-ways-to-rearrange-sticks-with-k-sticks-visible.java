c5a4e1e138c249afad60d409bfc6740c4e4d4581
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-13 17:27:57 -0400

    Time: 102 ms (95.95%), Space: 55.9 MB (29.73%) - LeetHub

--- 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
+++ 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
@@ -1,12 +1,11 @@
 class Solution {
     public int rearrangeSticks(int n, int k) {
-        long [][] dp = new long [n + 1][k + 1];
+        long[][] dp = new long[n + 1][k + 1];
         int mod = (int) 1e9 + 7;
         dp[0][0] = 1L;
-        for (int i=1; i<=n; i++) {
-            for (int j=1; j<=Math.min(i, k); j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= Math.min(i, k); j++)
                 dp[i][j] = (dp[i - 1][j] * (long)(i - 1) + dp[i - 1][j - 1]) % mod;
-            }
         }
         return (int)(dp[n][k] % mod);
     }
