901c03cbd43581f3364ae309b4bcf9e5793e428d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-13 16:29:35 -0400

    Time: 97 ms (34.13%), Space: 59.3 MB (75.96%) - LeetHub

--- 1804-implement-trie-ii-prefix-tree/1804-implement-trie-ii-prefix-tree.java
+++ 1804-implement-trie-ii-prefix-tree/1804-implement-trie-ii-prefix-tree.java
@@ -1,68 +1,89 @@
 class Trie {
-
-	private static class TrieNode {
-		char val;
-		int wordCount;
-		int prefixCount;
-		Map<Character, TrieNode> children = new HashMap<>(26);
-
-		TrieNode(char val) {
-			this.val = val;
-		}
-	}
-
-	private final TrieNode root;
-
-	public Trie() {
-		root = new TrieNode('#');
-	}
-
-	public void insert(String word) {
-		var node = root;
-
-		for (var i = 0; i < word.length(); i++) {
-			var key = word.charAt(i);
-			node = node.children.computeIfAbsent(word.charAt(i), k -> new TrieNode(key));
-			node.prefixCount++;
-		}
-
-		node.wordCount++;
-	}
-
-	public int countWordsEqualTo(String word) {
-		return countWords(word, false);
-	}
-
-	public int countWordsStartingWith(String prefix) {
-		return countWords(prefix, true);
-	}
-
-	private int countWords(String word, boolean isPrefix) {
-		var node = root;
-		for (var i = 0; i < word.length(); i++) {
-			var key = word.charAt(i);
-			if (!node.children.containsKey(key))
-				return 0;
-			node = node.children.get(key);
-		}
-		return isPrefix ? node.prefixCount : node.wordCount;
-	}
-
-	public void erase(String word) {
-		var parent = root;
-
-		for (var i = 0; i < word.length(); i++) {
-			var key = word.charAt(i);
-			var node = parent.children.get(key);
-
-			if (--node.prefixCount == 0) {
-				parent.children.remove(key);
-				return;
-			}
-			parent = node;
-		}
-		parent.wordCount--;
-	}
+    TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        int n = word.length();
+        TrieNode cur = root;
+        for (int i = 0; i < n; i++) {
+            char ch = word.charAt(i);
+            Map<Character, TrieNode> children = cur.children;
+            TrieNode child = children.get(ch);
+            if (child == null) {
+                child = new TrieNode();
+                children.put(ch, child);
+            }
+            child.prefixCnt++;
+            cur = child;
+        }
+        cur.wordCnt++;
+    }
+    
+    public int countWordsEqualTo(String word) {
+        TrieNode cur = root;
+        int n = word.length();
+        for (int i = 0; i < n; i++) {
+            char ch = word.charAt(i);
+            Map<Character, TrieNode> children = cur.children;
+            TrieNode child = children.get(ch);
+            if (child == null) {
+                return 0;
+            }
+            cur = child;
+        }
+        return cur.wordCnt;
+    }
+    
+    public int countWordsStartingWith(String prefix) {
+        TrieNode cur = root;
+        int n = prefix.length();
+        for (int i = 0; i < n; i++) {
+            char ch = prefix.charAt(i);
+            Map<Character, TrieNode> children = cur.children;
+            TrieNode child = children.get(ch);
+            if (child == null) {
+                return 0;
+            }
+            cur = child;
+        }
+        return cur.prefixCnt;
+    }
+    
+    public void erase(String word) {
+        Deque<TrieNode> stack = new ArrayDeque<>();
+        TrieNode cur = root;
+        int n = word.length();
+        for (int i = 0; i < n; i++) {
+            stack.offerFirst(cur);
+            char ch = word.charAt(i);
+            Map<Character, TrieNode> children = cur.children;
+            TrieNode child = children.get(ch);
+            cur = child;
+        }
+        cur.wordCnt--;
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = word.charAt(i);
+            //System.out.println(ch);
+            TrieNode parent = stack.pollFirst();
+            Map<Character, TrieNode> children = parent.children;
+            TrieNode child = children.get(ch);
+            child.prefixCnt--;
+            if (child.prefixCnt == 0) {
+                children.remove(ch);
+            }
+        }
+    }
+    
+    static class TrieNode {
+        Map<Character, TrieNode> children;
+        int prefixCnt;
+        int wordCnt;
+        TrieNode() {
+            children = new HashMap<>();
+        }
+    }
 }
 
 /**
