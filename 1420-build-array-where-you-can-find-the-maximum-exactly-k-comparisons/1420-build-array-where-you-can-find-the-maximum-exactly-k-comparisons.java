c7602b98da37884318a3dea919c8ec92d3d810fe
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-13 17:34:59 -0400

    Time: 11 ms (100.00%), Space: 42.1 MB (71.43%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -1,39 +1,29 @@
 class Solution {
-    
-    long[][][] memo;
-    long mod = (long)1e9 + 7;
-    int N, M;
-    long rec(int i, int msf, int kleft) {
-        if (i == N) {
-            return kleft == 0 ? 1 : 0;
-        }        
-        if (kleft < 0) return 0;
-        if (memo[i][msf][kleft] != -1)  return memo[i][msf][kleft];
-        
-        long ans = 0;
-        for (int j = 1; j <= M; j++) {
-            ans = (ans + rec(i+1, Math.max(msf, j), j > msf ? kleft-1 : kleft)) % mod;
-        }
-                   
-        return memo[i][msf][kleft] = ans;
-    }
-    
-    
-    public int numOfArrays(int n, int m, int K) {
-        memo = new long[n+1][m+1][K+1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                for (int k = 0; k <= K; k++) {
-                    memo[i][j][k] = -1;
+public int numOfArrays(int n, int m, int k) {
+        long[][][] dp =new long[n][k][m];
+        long mod = 1000000007;
+        Arrays.fill(dp[0][0], 1);
+        for (int i = 1;i<n;i++) {
+            for (int cost = 0;cost <Math.min(i+1, k);cost++) {
+                for (int max = 0;max < m;max++){
+                    long sum = 0;
+                    sum += dp[i-1][cost][max] * (max+1);
+                    
+                    if (cost != 0) {
+                        long[] arr = dp[i-1][cost-1];
+                        for (int prevMax = 0;prevMax < max; prevMax++) {
+                            sum += arr[prevMax];
+                        }
+                    }
+                    dp[i][cost][max] = sum %mod;
                 }
             }
         }
-        this.N = n;
-        M = m;
         long ans = 0;
-        for (int i = 1; i <= m; i++) {
-            ans = (ans + rec(1, i, K-1)) % mod;
+        for (int max = 0;max < m;max++) {
+            ans += dp[n-1][k-1][max];
+            ans %= mod;
         }
-        return (int)ans;
+        return (int) ans;
     }
 }
