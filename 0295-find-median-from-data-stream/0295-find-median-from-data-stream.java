557b02eb9d435ca0150a39c6f8c0343feea88a1a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-14 14:34:22 -0400

    Time: 170 ms (25.23%), Space: 61.8 MB (43.69%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,68 +1,23 @@
-import java.io.*; 
-import java.util.*; 
-import java.lang.*;
-
-class MyComparator implements Comparator<int[]>{
-	public int compare(int[] a, int[] b) {
-		if (a[0]!=b[0]) {
-			return a[0] - b[0];
-		}
-		return a[1] - b[1];
-	}
-}
-
-
 class MedianFinder {
-	int time;
-	MyComparator MyCom;
+    private PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+    private PriorityQueue<Integer> min = new PriorityQueue<>();
 
-    /** initialize your data structure here. */
-	TreeSet<int[]> set ;
-	int[] point1;
-	int[] point2;
-	public MedianFinder() {
-        time = 0;
-        MyCom = new MyComparator();
-        set = new TreeSet<>(MyCom);
-    }
-	
-    public void addNum(int num) {
-    	int[] item = new int[] {num, time++};
-        set.add(item);
-        if (time == 1) {
-        	point1 = set.first();
-        	point2 = set.first();
-        	return;
-        }
-                
-        if (point1 == point2) {
-        	if (MyCom.compare(item, point1)<0) {
-        		point2 = point1;
-        		point1 = set.lower(point1);
-        	}
-        	else {
-        		point1 = point2;
-        		point2 = set.higher(point2);
-        	}
-        	return;
-        }
+    public MedianFinder() {
         
-        if (point1 != point2) {
-        	if (MyCom.compare(item, point1) < 0) {
-        		point2 = point1;
-        	}
-        	else if (MyCom.compare(item, point2) > 0) {
-        		point1 = point2;
-        	}
-        	else {
-        		point1 = item;
-        		point2 = item;
-        	}
-        }
+    }
+    
+    public void addNum(int num) {
+        min.offer(num);
+        max.offer(min.poll());
+        if (min.size() < max.size())
+            min.offer(max.poll());
     }
     
     public double findMedian() {
-        return ((double)point1[0] + (double)point2[0])/2.0;
+        if (min.size() == max.size())
+            return (min.peek() + max.peek()) / 2.0;
+        else
+            return min.peek();
     }
 }
 
