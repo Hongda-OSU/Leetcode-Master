c5d3b605afdc31a0f3a9581e31c915fc063d7844
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-14 14:34:15 -0400

    Time: 136 ms (52.14%), Space: 64 MB (14.00%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,25 +1,68 @@
+import java.io.*; 
+import java.util.*; 
+import java.lang.*;
+
+class MyComparator implements Comparator<int[]>{
+	public int compare(int[] a, int[] b) {
+		if (a[0]!=b[0]) {
+			return a[0] - b[0];
+		}
+		return a[1] - b[1];
+	}
+}
+
+
 class MedianFinder {
-     List<Integer> list; 
-    public MedianFinder() {
-        this.list = new ArrayList<Integer>();
+	int time;
+	MyComparator MyCom;
+
+    /** initialize your data structure here. */
+	TreeSet<int[]> set ;
+	int[] point1;
+	int[] point2;
+	public MedianFinder() {
+        time = 0;
+        MyCom = new MyComparator();
+        set = new TreeSet<>(MyCom);
     }
-    
+	
     public void addNum(int num) {
-        if(list.size() == 0) list.add(0, num);  
-        else{
-            int i=0;
-            while(list.get(i) <= num && i<list.size()-1) i++;
-            //*****The reason why I didn't traversed till the last index was to escape from IndexOutOfBounds Error*****
-            if(list.get(i) <= num) list.add(i+1, num);
-            else list.add(i, num);
+    	int[] item = new int[] {num, time++};
+        set.add(item);
+        if (time == 1) {
+        	point1 = set.first();
+        	point2 = set.first();
+        	return;
+        }
+                
+        if (point1 == point2) {
+        	if (MyCom.compare(item, point1)<0) {
+        		point2 = point1;
+        		point1 = set.lower(point1);
+        	}
+        	else {
+        		point1 = point2;
+        		point2 = set.higher(point2);
+        	}
+        	return;
+        }
+        
+        if (point1 != point2) {
+        	if (MyCom.compare(item, point1) < 0) {
+        		point2 = point1;
+        	}
+        	else if (MyCom.compare(item, point2) > 0) {
+        		point1 = point2;
+        	}
+        	else {
+        		point1 = item;
+        		point2 = item;
+        	}
         }
     }
     
     public double findMedian() {
-        int n = list.size();
-        if(n == 0) return 0;
-        if(n%2 == 1) return list.get(n/2);
-        return (double)((list.get(n/2) + list.get(n/2 - 1)))/2;
+        return ((double)point1[0] + (double)point2[0])/2.0;
     }
 }
 
