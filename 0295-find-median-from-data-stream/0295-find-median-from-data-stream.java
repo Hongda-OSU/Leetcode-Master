82bcbfecaff0c78bc12f56c37c2e2b4a9ceb63a8
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-14 14:40:55 -0400

    Time: 121 ms (72.89%), Space: 61.6 MB (65.11%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,50 +1,35 @@
 class MedianFinder {
- 
-    PriorityQueue<Integer> min ;
-    PriorityQueue<Integer> max ;
-    
+    private PriorityQueue<Integer> min, max;
+
     public MedianFinder() {
-        min  = new PriorityQueue<>();
-        max  = new PriorityQueue<>( Comparator.reverseOrder());
+        min = new PriorityQueue<>();
+        max = new PriorityQueue<>(Comparator.reverseOrder());
     }
     
     public void addNum(int num) {
-        
-        if( min.isEmpty() && max.isEmpty() )
+        if (min.isEmpty() && max.isEmpty())
             max.add(num);
-        
-        else if( num > max.peek() ) {
-            min.add(num);            
-            if( min.size() - max.size() >= 2) {
-                max.add(min.poll());    
-            }
-                
+        else if (num > max.peek()) {
+            min.add(num);
+            if (min.size() - max.size() >= 2)
+                max.add(min.poll());
+        } else {
+            max.add(num);
+            if (max.size() - min.size() >= 2)
+                min.add(max.poll());
         }
-        else {
-            max.add(num);            
-            if( max.size() - min.size() >= 2 ) {
-                min.add(max.poll());    
-            }
-            
-        }
-        
     }
     
     public double findMedian() {
-        
-        if( max.size() == min.size() ) {
-            double ans = (double)max.peek() + (double)min.peek();
-            ans = ans / (double)2;
-            return ans ;
+        if (max.size() == min.size()) {
+            double result = (double)max.peek() + (double)min.peek();
+            result = result / (double)2;
+            return result;
         }
-    
-        if( min.size() > max.size() ) 
+        if (min.size() > max.size())
             return (double)min.peek();
-    
         return (double)max.peek();
     }
-
-    
 }
 
 /**
