b4e30d97f2844917b3d792daf5ac96e8ea97f6ef
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-14 19:56:22 -0400

    Time: 0 ms (100.00%), Space: 41.1 MB (31.33%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -1,108 +1,69 @@
 class Solution {
-    public List<String> fullJustify(String[] words, int maxWidth) {
+    private int maxWidth;
+    private String[] words;
+    
+    public List<String> fullJustify(String[] ws, int maxW) {        
         
-        // the final justified list of lines to return.
-        List<String> justified = new ArrayList();
-       
-        // a queue to store all the words of a current line.
-        Queue<String> currentLine = new LinkedList();
+        List<String> text = new LinkedList<>();        
+        maxWidth = maxW;
+        words = ws; 
+        // Use start/end indexes into words array to avoid making a list of words in current line (for efficiency).
+        int start = 0; 
+        int end = 1;
+        int lineLen = words[start].length();
         
-        // efficiently build a new line with a string builder.
-        StringBuilder sb = new StringBuilder();
-        
-        // keep track of all the lengths of each word on the current line (to compare to maxWidth).
-        int currentLineLen = 0;
-        
-        for(String word : words){
+        for (; end < words.length; end++) {
+            String word = words[end];
             
-            // track both the current words length and the count of all the words in the current line 
-            // (this count represents the single space between each word so far)
-            int n = word.length();
-            int m = currentLine.size();
-            
-            // if all the words so far in the line, plus a single space between each of them, plus the current word length is 
-            // less than or equal to the max width of a line, then add the current word to the current line.
-            if(currentLineLen + n + m <= maxWidth){
-                currentLine.add(word);
-                currentLineLen += n;
-            
-            // otherwise, render the current line with the correct spacing between each word and start a new line with the current word.
-            }else{
-                int extraSpace = maxWidth - currentLineLen;
-                justified.add(getLine(extraSpace, currentLine, sb));
-                
-                currentLine.add(word);
-                sb.setLength(0);
-                currentLineLen = n;
+            if (lineLen + word.length() + 1 <= maxWidth) { // Word fits in this line. Plus one is for a space.
+                lineLen += word.length() + 1; 
+            } else { // Word does not fit in this line, space out current line and start a new one.
+                text.add(constructLine(start, end, lineLen));
+                start = end;
+                lineLen = word.length();
+            }   
+        }
+        
+        if (end - start > 0) {
+            text.add(lineWithoutInterWordPadding(start, end, lineLen));
+        }
+        
+        return text;
+    }
+    
+    private String lineWithoutInterWordPadding(int start, int end, int lineLength) {
+        // Don't use stream Collectors.joining(" ") here, even though it is more readable. The below is faster.
+        StringBuilder line = new StringBuilder();
+        line.append(words[start]);
+        for (int i = start + 1; i < end; i++) {
+            line.append(" ");
+            line.append(words[i]);
+        }
+        return line.toString() + " ".repeat(maxWidth - lineLength);
+    }
+    
+    private String constructLine(int start, int end, int lineLength) {
+        int numSpaces = end - (start + 1);
+        
+        if (numSpaces == 0) { // Only one word in the line.
+            return lineWithoutInterWordPadding(start, end, lineLength);
+        }
+        
+        int padding = maxWidth - lineLength;       // Extra padding needed.
+        int spaceNeeded = padding / numSpaces + 1; // Padding between each word (extra + minimum one).
+        int leftOverSpaces = padding % numSpaces;  // Extra spaces that don't divide evenly between words.
+        
+        StringBuilder line = new StringBuilder();
+        line.append(words[start]);
+        
+        for (int i = start + 1; i < end; i++) {
+            line.append(" ".repeat(spaceNeeded));
+            if (leftOverSpaces-- > 0) {
+                line.append(' ');
             }
+            line.append(words[i]);
         }
         
-        // if after looping through each word, there are still words left in the current line
-        // then add them to the justified list of lines through a special final string.
-        if(!currentLine.isEmpty()){
-            int remainingSpace = maxWidth - (currentLineLen + currentLine.size() - 1);
-            justified.add(generateLastLine(remainingSpace, currentLine, sb));
-        }
-        
-        return justified;
-    }
-    
-    /**
-     * This method renders a standard line (not the final line) by taking the total amount of space
-     * left in the line an distributing the space as equally as possible between the words of the, but 
-     * giving words on the left more space between them than words on the right if the remaining free space
-     * cant be split equally between all the words.
-    **/
-    private String getLine(int extraSpace, Queue<String> currentLine, StringBuilder sb){
-        
-        int wordCount = currentLine.size() - 1;
-        
-        boolean singleWordLine = wordCount == 0;
-        
-        while(!currentLine.isEmpty()){
-            sb.append(currentLine.poll());
-
-            if(!currentLine.isEmpty() || singleWordLine){
-                
-                int currentSpace = !singleWordLine ? extraSpace / wordCount : extraSpace;
-                
-                if(!singleWordLine && extraSpace % wordCount != 0)
-                    currentSpace++;
-                
-                addEmptySpace(currentSpace, sb);
-                
-                extraSpace -= currentSpace;
-                wordCount--;
-            }
-        }
-        
-        return sb.toString();
-    }
-    
-    /**
-     * This method renders the final line by placing a single space in between each word and then adding
-     * any remaing empty space to the end of the line.
-    **/ 
-    private String generateLastLine(int remainingSpace, Queue<String> currentLine, StringBuilder sb){
-         
-        while(!currentLine.isEmpty()){
-            sb.append(currentLine.poll());
-            
-            if(!currentLine.isEmpty())
-                sb.append(" ");
-        }
-        
-        if(remainingSpace > 0)
-            addEmptySpace(remainingSpace, sb);
-        
-        return sb.toString();
-    }
-    
-    /**
-     * This method will add the number of empty spaces to the current string builder to match a given length.
-    **/
-    private void addEmptySpace(int len, StringBuilder sb){
-        for(int i = 0; i < len; i++)
-            sb.append(" ");
+        return line.toString();
     }
 }
