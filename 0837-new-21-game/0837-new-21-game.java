3621ef7052d553affa4b289957f4aab079232841
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-14 16:59:14 -0400

    Time: 3 ms (97.80%), Space: 42.1 MB (53.85%) - LeetHub

--- 0837-new-21-game/0837-new-21-game.java
+++ 0837-new-21-game/0837-new-21-game.java
@@ -1,21 +1,18 @@
 class Solution {
     public double new21Game(int n, int k, int maxPts) {
-        double dp[] = new double[n + 1];
+        double[] dp = new double[n + 1];
         dp[0] = 1;
         double s = k > 0 ? 1 : 0;
         for (int i = 1; i <= n; i++) {
             dp[i] = s / maxPts;
-            if (i < k) {
+            if (i < k)
                 s += dp[i];
-            }
-            if (i - maxPts >= 0 && i - maxPts < k) {
+            if (i - maxPts >= 0 && i - maxPts < k)
                 s -= dp[i - maxPts];
-            }
         }
-        double ans = 0;
-        for (int i = k; i <= n; i++) {
-            ans += dp[i];
-        }
-        return ans;
+        double result = 0;
+        for (int i = k; i <= n; i++)
+            result += dp[i];
+        return result;
     }
 }
