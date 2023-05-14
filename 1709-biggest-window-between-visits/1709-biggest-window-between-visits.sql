c47fe2848760e2be14f09db16b83bb355fd7dbad
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-14 00:18:23 -0400

    Time: 961 ms (80.48%), Space: 0B (100.00%) - LeetHub

--- 1709-biggest-window-between-visits/1709-biggest-window-between-visits.sql
+++ 1709-biggest-window-between-visits/1709-biggest-window-between-visits.sql
@@ -4,7 +4,7 @@ SELECT user_id,
 FROM 
 (
     SELECT user_id,
-         DATEDIFF(LEAD(visit_date, 1, '2021-01-01') OVER (PARTITION BY user_id ORDER BY visit_date), visit_date) AS diff
+        DATEDIFF(LEAD(visit_date, 1, '2021-01-01') OVER (PARTITION BY user_id ORDER BY visit_date), visit_date) AS diff
     FROM UserVisits
 ) a
 GROUP BY user_id
