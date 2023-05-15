4b0d398ebdd3c9329501333cdbe9e81f9fe7a739
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-15 15:50:11 -0400

    Time: 47 ms (18.16%), Space: 44 MB (33.76%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,23 +1,21 @@
 class Solution {
     private int lcs(String s1, String s2, int m, int n) {
-        int[] dp = new int[n + 1];
-        int[] dpPrev = new int[n + 1];
+        int[][] dp = new int[m + 1][n + 1];
 
         for (int i = 0; i <= m; i++) {
             for (int j = 0; j <= n; j++) {
                 if (i == 0 || j == 0) {
                     // One of the two strings is empty.
-                    dp[j] = 0;
+                    dp[i][j] = 0;
                 } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[j] = 1 + dpPrev[j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    dp[j] = Math.max(dpPrev[j], dp[j - 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
-            dpPrev = dp.clone();
         }
 
-        return dp[n];
+        return dp[m][n];
     }
 
     public int minInsertions(String s) {
