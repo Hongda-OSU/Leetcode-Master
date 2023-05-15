e24618aba2563c3a85a38f1cbd803336930aaada
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-14 20:05:05 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (59.06%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -2,68 +2,51 @@ class Solution {
     private int maxWidth;
     private String[] words;
     
-    public List<String> fullJustify(String[] ws, int maxW) {        
-        
-        List<String> text = new LinkedList<>();        
-        maxWidth = maxW;
-        words = ws; 
-        // Use start/end indexes into words array to avoid making a list of words in current line (for efficiency).
-        int start = 0; 
-        int end = 1;
-        int lineLen = words[start].length();
-        
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> text = new LinkedList<>();
+        this.maxWidth = maxWidth;
+        this.words = words;
+        int start = 0, end = 1, lineLength = words[start].length();
         for (; end < words.length; end++) {
             String word = words[end];
-            
-            if (lineLen + word.length() + 1 <= maxWidth) { // Word fits in this line. Plus one is for a space.
-                lineLen += word.length() + 1; 
-            } else { // Word does not fit in this line, space out current line and start a new one.
-                text.add(constructLine(start, end, lineLen));
+            if (lineLength + word.length() + 1 <= maxWidth)
+                lineLength += word.length() + 1;
+            else {
+                text.add(constructLine(start, end, lineLength));
                 start = end;
-                lineLen = word.length();
-            }   
+                lineLength = word.length();
+            }
         }
-        
-        if (end - start > 0) {
-            text.add(lineWithoutInterWordPadding(start, end, lineLen));
-        }
-        
+        if (end - start > 0)
+            text.add(lineWithoutInterWordPadding(start, end, lineLength));
         return text;
-    }
-    
-    private String lineWithoutInterWordPadding(int start, int end, int lineLength) {
-        // Don't use stream Collectors.joining(" ") here, even though it is more readable. The below is faster.
-        StringBuilder line = new StringBuilder();
-        line.append(words[start]);
-        for (int i = start + 1; i < end; i++) {
-            line.append(" ");
-            line.append(words[i]);
-        }
-        return line.toString() + " ".repeat(maxWidth - lineLength);
     }
     
     private String constructLine(int start, int end, int lineLength) {
         int numSpaces = end - (start + 1);
-        
-        if (numSpaces == 0) { // Only one word in the line.
+        if (numSpaces == 0)
             return lineWithoutInterWordPadding(start, end, lineLength);
-        }
-        
-        int padding = maxWidth - lineLength;       // Extra padding needed.
-        int spaceNeeded = padding / numSpaces + 1; // Padding between each word (extra + minimum one).
-        int leftOverSpaces = padding % numSpaces;  // Extra spaces that don't divide evenly between words.
-        
-        StringBuilder line = new StringBuilder();
-        line.append(words[start]);
-        
+        int padding = maxWidth - lineLength;
+        int spaceNeeded = padding / numSpaces + 1;
+        int leftOverSpaces = padding % numSpaces;
+        StringBuilder sb = new StringBuilder();
+        sb.append(words[start]);
         for (int i = start + 1; i < end; i++) {
-            line.append(" ".repeat(spaceNeeded));
-            if (leftOverSpaces-- > 0) {
-                line.append(' ');
-            }
-            line.append(words[i]);
+            sb.append(" ".repeat(spaceNeeded));
+            if (leftOverSpaces-- > 0)
+                sb.append(" ");
+            sb.append(words[i]);
         }
-        
-        return line.toString();
+        return sb.toString();
+    }
+    
+    private String lineWithoutInterWordPadding(int start, int end, int lineLength) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(words[start]);
+        for (int i = start + 1; i < end; i++) {
+            sb.append(" ");
+            sb.append(words[i]);
+        } 
+        return sb.toString() + " ".repeat(maxWidth - lineLength);
     }
 }
