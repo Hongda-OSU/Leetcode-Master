a9b6911ea649d7700cd07da2ff6c667e7c031336
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-15 15:50:19 -0400

    Time: 58 ms (7.66%), Space: 44.1 MB (24.80%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,27 +1,28 @@
 class Solution {
-    private int lcs(String s1, String s2, int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
-
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0 || j == 0) {
-                    // One of the two strings is empty.
-                    dp[i][j] = 0;
-                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+    private int lcs(String s1, String s2, int m, int n, int[][] memo) {
+        if (m == 0 || n == 0) {
+            return 0;
         }
-
-        return dp[m][n];
+        if (memo[m][n] != -1) {
+            return memo[m][n];
+        }
+        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+            return memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
+        }
+        return memo[m][n] = Math.max(lcs(s1, s2, m - 1, n, memo), lcs(s1, s2, m, n - 1, memo));
     }
 
     public int minInsertions(String s) {
         int n = s.length();
         String sReverse = new StringBuilder(s).reverse().toString();
+        int[][] memo = new int[n + 1][n + 1];
 
-        return n - lcs(s, sReverse, n, n);
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
+        return n - lcs(s, sReverse, n, n, memo);
     }
 }
