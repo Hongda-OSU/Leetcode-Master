eb724ebe316bf5bb543aca1e5f32fb6d489f7986
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-15 14:56:32 -0400

    Time: 24 ms (67.87%), Space: 42.4 MB (35.59%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,24 +1,33 @@
 class Solution {
+    int[][] memo;
+    int newCuts[];
+    private int cost(int left, int right) {
+        if (memo[left][right] != -1) {
+            return memo[left][right];
+        }
+        if (right - left == 1) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int mid = left + 1; mid < right; mid++) {
+            int cost = cost(left, mid) + cost(mid, right) + newCuts[right] - newCuts[left];
+            ans = Math.min(ans, cost);
+        }
+        memo[left][right] = ans;
+        return ans;
+    }
     public int minCost(int n, int[] cuts) {
         int m = cuts.length;
-        int[] newCuts = new int[m + 2];
+        newCuts = new int[m + 2];
         System.arraycopy(cuts, 0, newCuts, 1, m);
         newCuts[m + 1] = n;
         Arrays.sort(newCuts);
         
-        int[][] dp = new int[m + 2][m + 2];
+        memo = new int[m + 2][m + 2];
+        for (int r = 0; r < m + 2; ++r) {
+            Arrays.fill(memo[r], -1);
+        }
         
-        for (int diff = 2; diff < m + 2; ++diff) {
-            for (int left = 0; left < m + 2 - diff; ++left) {
-                int right = left + diff;
-                int ans = Integer.MAX_VALUE;
-                for (int mid = left + 1; mid < right; ++mid) {
-                    ans = Math.min(ans, dp[left][mid] + dp[mid][right] + newCuts[right] - newCuts[left]);
-                }
-                dp[left][right] = ans;
-            }
-        } 
-        
-        return dp[0][m + 1];
+        return cost(0, newCuts.length - 1);
     }    
 }
