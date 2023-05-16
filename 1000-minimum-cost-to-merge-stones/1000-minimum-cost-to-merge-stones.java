26ad40f8a2a7cbd76cafcfd5a1d5bbe3ad321a14
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-16 15:44:12 -0400

    Time: 2 ms (93.66%), Space: 41.8 MB (45.77%) - LeetHub

--- 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.java
+++ 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.java
@@ -1,22 +1,54 @@
-class Solution {
-  public int mergeStones(int[] stones, int K) {
+public class Solution {
+    Integer[][][] memo;
+    int[] preSum;
+
+    public int mergeStones(int[] stones, int K) {
+        if (stones == null || stones.length == 0) return -1;
+
         int n = stones.length;
-        if ((n - 1) % (K - 1) > 0) return -1;
+        if (n == 1) return 0;
+        preSum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            preSum[i + 1] = preSum[i] + stones[i];
+        }
 
-        int[] prefix = new int[n+1];
-        for (int i = 0; i <  n; i++)
-            prefix[i + 1] = prefix[i] + stones[i];
+        if (n == K) {
+            return preSum[n];
+        }
 
-        int[][] dp = new int[n][n];
-        for (int m = K; m <= n; ++m)
-            for (int i = 0; i + m <= n; ++i) {
-                int j = i + m - 1;
-                dp[i][j] = Integer.MAX_VALUE;
-                for (int mid = i; mid < j; mid += K - 1)
-                    dp[i][j] = Math.min(dp[i][j], dp[i][mid] + dp[mid + 1][j]);
-                if ((j - i) % (K - 1) == 0)
-                    dp[i][j] += prefix[j + 1] - prefix[i];
+        if ((n - 1) % (K - 1) != 0) return -1;
+
+        memo = new Integer[n][n][K + 1];
+        for (int i = 0; i < n; i++) {
+            memo[i][i][1] = 0;
+        }
+
+        return calculate(stones, 0, n - 1, 1, K);
+    }
+
+    private int calculate(int[] stones, int i, int j, int piles, int K) {
+        if ((j - i + 1 - piles) % (K - 1) != 0) {
+            return Integer.MAX_VALUE;
+        }
+        if (memo[i][j][piles] != null) {
+            return memo[i][j][piles];
+        }
+        int cost = Integer.MAX_VALUE;
+        if (piles == 1) {
+            int prev = calculate(stones, i, j, K, K);
+            if (prev != Integer.MAX_VALUE) {
+                cost = prev + preSum[j + 1] - preSum[i];
             }
-        return dp[0][n - 1];
+        } else {
+            for (int mid = i; mid < j; mid++) {
+                int left = calculate(stones, i, mid, 1, K);
+                if (left >= cost) continue;
+                int right = calculate(stones, mid + 1, j, piles - 1, K);
+                if (right >= cost) continue;
+                cost = Math.min(cost, left + right);
+            }
+        }
+        memo[i][j][piles] = cost;
+        return cost;
     }
 }
