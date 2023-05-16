451001501b62ab886fbe378cdac9ae8dddabc27f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-16 14:30:01 -0400

    Time: 2 ms (51.34%), Space: 43.7 MB (5.97%) - LeetHub

--- 0281-zigzag-iterator/0281-zigzag-iterator.java
+++ 0281-zigzag-iterator/0281-zigzag-iterator.java
@@ -1,41 +1,48 @@
 public class ZigzagIterator {
     private List<List<Integer>> vectors = new ArrayList<>();
-    private LinkedList<Pair<Integer, Integer>> queue = new LinkedList<>();
+    // pointer to vector, and pointer to element
+    private Integer pVec = 0, pElem = 0;
+    private Integer totalNum = 0, outputCount = 0;
 
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
         this.vectors.add(v1);
         this.vectors.add(v2);
-        int index = 0;
         for (List<Integer> vec : this.vectors) {
-            if (vec.size() > 0)
-                // <index_to_vec, index_to_element_within_vec>
-                this.queue.add(new Pair<Integer, Integer>(index, 0));
-            index++;
+            this.totalNum += vec.size();
         }
     }
 
     public int next() {
-        // if (this.queue.size() == 0)
-        // throw new Exception("Out of elements!");
+        Integer iterNum = 0, ret = null;
+        while (iterNum < this.vectors.size()) {
+            List<Integer> currVec = this.vectors.get(this.pVec);
+            if (this.pElem < currVec.size()) {
+                ret = currVec.get(this.pElem);
+                this.outputCount += 1;
+            }
 
-        // <index_to_vec, index_to_element_within_vec>
-        Pair<Integer, Integer> pointer = this.queue.removeFirst();
-        Integer vecIndex = pointer.getKey();
-        Integer elemIndex = pointer.getValue();
-        Integer nextElemIndex = elemIndex + 1;
-        // append the pointer for the next round
-        // if there are some elements left.
-        if (nextElemIndex < this.vectors.get(vecIndex).size())
-            this.queue.addLast(new Pair<>(vecIndex, nextElemIndex));
+            iterNum += 1;
+            this.pVec = (this.pVec + 1) % this.vectors.size();
+            // increment the element pointer once iterating all vectors
+            if (this.pVec == 0)
+                this.pElem += 1;
 
-        return this.vectors.get(vecIndex).get(elemIndex);
+            if (ret != null)
+                return ret;
+        }
+        // one should raise an exception here.
+        return 0;
     }
 
     public boolean hasNext() {
-        return this.queue.size() > 0;
+        return this.outputCount < this.totalNum;
     }
 }
 
+/**
+ * Your ZigzagIterator object will be instantiated and called as such: ZigzagIterator i = new
+ * ZigzagIterator(v1, v2); while (i.hasNext()) v[f()] = i.next();
+ */
 /**
  * Your ZigzagIterator object will be instantiated and called as such:
  * ZigzagIterator i = new ZigzagIterator(v1, v2);
