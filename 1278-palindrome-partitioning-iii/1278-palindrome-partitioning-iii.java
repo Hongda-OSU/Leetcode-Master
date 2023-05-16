4c672dcd22ae7c6200410d27849844841c40b618
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-16 15:35:40 -0400

    Time: 2 ms (98.06%), Space: 41 MB (38.24%) - LeetHub

--- 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.java
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.java
@@ -1,29 +1,37 @@
 class Solution {
-    public int helper(int[][] change,String s,int end,int k,Integer[][] dp){
-        if(k == end) return 0;
-        if(k == 1) return change[0][end-1];
-        if(dp[end][k] != null) return dp[end][k];
-        int ans = Integer.MAX_VALUE;
-        for(int i = k-1;i < end;i++){
-            ans = Math.min(ans,helper(change,s,i,k-1,dp)+change[i][end-1]);
-        }
-        return dp[end][k] = ans;
-    }
     public int palindromePartition(String s, int k) {
         int n = s.length();
         int[][] dp = new int[n][n];
-        for(int g = 0;g<n;g++){
-            for(int i = 0,j = g;j < n;i++,j++){
-                if(g == 0) dp[i][j] = 0;
-                else if(g == 1){
-                    if(s.charAt(i) == s.charAt(j)) dp[i][j] = 0;
-                    else dp[i][j] = 1;
-                }else{
-                    if(s.charAt(i) == s.charAt(j)) dp[i][j] = dp[i+1][j-1];
-                    else dp[i][j] = 1+dp[i+1][j-1];
+        for (int p = 0; p < n; p++) {
+            for (int i = 0, j = p; j < n; i++, j++) {
+                if (p == 0)
+                    dp[i][j] = 0;
+                else if (p == 1) {
+                    if (s.charAt(i) == s.charAt(j))
+                        dp[i][j] = 0;
+                    else 
+                        dp[i][j] = 1;
+                } else {
+                    if (s.charAt(i) == s.charAt(j))
+                        dp[i][j] = dp[i + 1][j - 1];
+                    else 
+                        dp[i][j] = 1 + dp[i + 1][j - 1];
                 }
             }
         }
-        return helper(dp,s,n,k,new Integer[n+1][k+1]);
+        return helper(dp, s, n, k, new Integer[n + 1][k + 1]);
+    }
+    
+    private int helper(int[][] dp, String str, int end, int k, Integer[][] memo) {
+        if (k == end)
+            return 0;
+        if (k == 1)
+            return dp[0][end - 1];
+        if (memo[end][k] != null)
+            return memo[end][k];
+        int result = Integer.MAX_VALUE;
+        for (int i = k - 1; i < end; i++)
+            result = Math.min(result, helper(dp, str, i, k - 1, memo) + dp[i][end - 1]);
+        return memo[end][k] = result;
     }
 }
