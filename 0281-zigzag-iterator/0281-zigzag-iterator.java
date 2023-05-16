9884f5507333bbe55f25823dc3520495883bd6aa
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-16 14:29:52 -0400

    Time: 2 ms (51.34%), Space: 43.3 MB (59.40%) - LeetHub

--- 0281-zigzag-iterator/0281-zigzag-iterator.java
+++ 0281-zigzag-iterator/0281-zigzag-iterator.java
@@ -1,24 +1,38 @@
 public class ZigzagIterator {
-    public Queue<Iterator> queue;
+    private List<List<Integer>> vectors = new ArrayList<>();
+    private LinkedList<Pair<Integer, Integer>> queue = new LinkedList<>();
 
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        queue = new LinkedList<>();
-        if (!v1.isEmpty())
-            queue.offer(v1.iterator());
-        if (!v2.isEmpty())
-            queue.offer(v2.iterator());
+        this.vectors.add(v1);
+        this.vectors.add(v2);
+        int index = 0;
+        for (List<Integer> vec : this.vectors) {
+            if (vec.size() > 0)
+                // <index_to_vec, index_to_element_within_vec>
+                this.queue.add(new Pair<Integer, Integer>(index, 0));
+            index++;
+        }
     }
 
     public int next() {
-        Iterator iter = queue.poll();
-        int result = (int)iter.next();
-        if (iter.hasNext())
-            queue.offer(iter);
-        return result;
+        // if (this.queue.size() == 0)
+        // throw new Exception("Out of elements!");
+
+        // <index_to_vec, index_to_element_within_vec>
+        Pair<Integer, Integer> pointer = this.queue.removeFirst();
+        Integer vecIndex = pointer.getKey();
+        Integer elemIndex = pointer.getValue();
+        Integer nextElemIndex = elemIndex + 1;
+        // append the pointer for the next round
+        // if there are some elements left.
+        if (nextElemIndex < this.vectors.get(vecIndex).size())
+            this.queue.addLast(new Pair<>(vecIndex, nextElemIndex));
+
+        return this.vectors.get(vecIndex).get(elemIndex);
     }
 
     public boolean hasNext() {
-        return queue.peek() != null;
+        return this.queue.size() > 0;
     }
 }
 
