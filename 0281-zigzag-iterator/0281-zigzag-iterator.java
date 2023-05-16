c257c568e40aebdbadd191605442cc608f021136
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-16 14:30:37 -0400

    Time: 1 ms (100.00%), Space: 43.5 MB (13.13%) - LeetHub

--- 0281-zigzag-iterator/0281-zigzag-iterator.java
+++ 0281-zigzag-iterator/0281-zigzag-iterator.java
@@ -1,48 +1,34 @@
 public class ZigzagIterator {
-    private List<List<Integer>> vectors = new ArrayList<>();
-    // pointer to vector, and pointer to element
-    private Integer pVec = 0, pElem = 0;
-    private Integer totalNum = 0, outputCount = 0;
-
+    int i1 = 0;
+    int i2 = 0;
+    
+    boolean flag = false;
+    
+    List<Integer> l1;
+    List<Integer> l2;
+    
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        this.vectors.add(v1);
-        this.vectors.add(v2);
-        for (List<Integer> vec : this.vectors) {
-            this.totalNum += vec.size();
-        }
+        l1 = v1;
+        l2 = v2;
     }
 
     public int next() {
-        Integer iterNum = 0, ret = null;
-        while (iterNum < this.vectors.size()) {
-            List<Integer> currVec = this.vectors.get(this.pVec);
-            if (this.pElem < currVec.size()) {
-                ret = currVec.get(this.pElem);
-                this.outputCount += 1;
-            }
-
-            iterNum += 1;
-            this.pVec = (this.pVec + 1) % this.vectors.size();
-            // increment the element pointer once iterating all vectors
-            if (this.pVec == 0)
-                this.pElem += 1;
-
-            if (ret != null)
-                return ret;
-        }
-        // one should raise an exception here.
-        return 0;
+        flag = !flag;
+        
+        if (i1 < l1.size() && (flag  || i2 >= l2.size())) 
+            return l1.get(i1++);
+            
+        if (i2 < l2.size() && (!flag || i1 >= l1.size())) 
+            return l2.get(i2++);
+        
+        return -1;
     }
 
     public boolean hasNext() {
-        return this.outputCount < this.totalNum;
+        return i1 < l1.size() || i2 < l2.size();
     }
 }
 
-/**
- * Your ZigzagIterator object will be instantiated and called as such: ZigzagIterator i = new
- * ZigzagIterator(v1, v2); while (i.hasNext()) v[f()] = i.next();
- */
 /**
  * Your ZigzagIterator object will be instantiated and called as such:
  * ZigzagIterator i = new ZigzagIterator(v1, v2);
