25dfe13d5dfcb8d2a86c3c6fc995ef21b50f209c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-16 15:25:17 -0400

    Time: 6 ms (50.98%), Space: 41 MB (38.24%) - LeetHub

--- 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.java
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.java
@@ -1,38 +1,44 @@
 class Solution {
-    public int palindromePartition(String s, int k) {
-        int[][] toPal = new int[s.length()][s.length()];
-        int[][] dp = new int[k+1][s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            toPal[i][i] = 0;
-        }
-        for (int i = s.length()-1; i >= 0; i--) {
-            for (int j = i + 1; j < s.length(); j++) {
-                toPal[i][j] = getChanges(s, i, j);
+   public int palindromePartition(String s, int k) {
+        int len = s.length();
+        if (len <= k) return 0;
+
+        int[][] dp = new int[k][len];
+        int[][] cache = new int[len][len]; // caching the cost of s.substring(left, right)
+        char[] chars = s.toCharArray();  // optimization: avoid calling substrings
+
+        // initialize cache
+        for (int i = 0; i < len; ++i) {
+            for (int j = i; j < len; ++j) { // optimization: j is always >= i
+                cache[i][j] = -1;
             }
         }
-        for (int i = 0; i < s.length(); i++) {
-            dp[1][i] = toPal[0][i];
-        }
-        for (int i = 2; i <= k; i++) {
-            for (int end = i-1; end < s.length(); end++) {
-                int min = s.length();
-                for (int start = end-1; start >= i-2; start--) {
-                    min = Math.min(min, dp[i-1][start] + toPal[start+1][end]);
+
+        for (int i = 0; i < k; ++i) {
+            for (int j = i; j < len; ++j) {
+                if (i == 0) {
+                    dp[i][j] = cost(chars, i, j, cache); // k = 1
+                } else {
+                    int min = Integer.MAX_VALUE;
+                    for (int p = i; p <= j; ++p) {
+                        min = Math.min(min, dp[i - 1][p - 1] + cost(chars, p, j, cache));
+                    }
+                    dp[i][j] = min;
                 }
-                dp[i][end] = min;
             }
         }
-        return dp[k][s.length()-1];
+
+        return dp[k - 1][len - 1];
     }
-    
-    
-    private int getChanges(String s, int start, int end) {
-        int changes = 0;
-        while(start < end) {
-            if (s.charAt(start++) != s.charAt(end--)) {
-                changes++;
-            }
+
+    protected int cost(char[] s, int left, int right, int[][] cache) {
+        if (cache[left][right] != -1) return cache[left][right];
+
+        int diff = 0, ll = left, rr = right;
+        while (ll < rr) {
+            if (s[ll++] != s[rr--]) diff++;
         }
-        return changes;
+
+        return cache[left][right] = diff;
     }
 }
