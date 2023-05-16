3e42b23fa0ce70c668787a0feb0cc61faa72ad07
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-16 15:26:16 -0400

    Time: 2 ms (98.04%), Space: 40.8 MB (55.88%) - LeetHub

--- 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.java
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.java
@@ -1,40 +1,29 @@
 class Solution {
+    public int helper(int[][] change,String s,int end,int k,Integer[][] dp){
+        if(k == end) return 0;
+        if(k == 1) return change[0][end-1];
+        if(dp[end][k] != null) return dp[end][k];
+        int ans = Integer.MAX_VALUE;
+        for(int i = k-1;i < end;i++){
+            ans = Math.min(ans,helper(change,s,i,k-1,dp)+change[i][end-1]);
+        }
+        return dp[end][k] = ans;
+    }
     public int palindromePartition(String s, int k) {
-        if(s.length() == k) return 0;
         int n = s.length();
-        int dp[][] = new int[n][n];
-        
-        //Counting no.of changes to make a substring palindrome for each substring
-        for(int i = n-1; i>=0; i--){
-            for(int j = i; j<n; j++){
-                if(i-j == 0) dp[i][j] = 0;
-                else{
-                    dp[i][j] = dp[i+1][j-1];
-                    if(s.charAt(i) != s.charAt(j)){
-                        dp[i][j] += 1;
-                    }
+        int[][] dp = new int[n][n];
+        for(int g = 0;g<n;g++){
+            for(int i = 0,j = g;j < n;i++,j++){
+                if(g == 0) dp[i][j] = 0;
+                else if(g == 1){
+                    if(s.charAt(i) == s.charAt(j)) dp[i][j] = 0;
+                    else dp[i][j] = 1;
+                }else{
+                    if(s.charAt(i) == s.charAt(j)) dp[i][j] = dp[i+1][j-1];
+                    else dp[i][j] = 1+dp[i+1][j-1];
                 }
             }
         }
-       
-        int countdp[][] = new int [k+1][n];
-        
-        // Count min no. of changes for each substring for k partitions
-        for(int i = 1; i<=k; i++){
-            for(int j = 0; j<n; j++){
-                if(i == 1) countdp[i][j] = dp[i-1][j];
-                else{
-                    if(i - j < 1){
-                        int min = 100000;
-                        for(int l = j; l>=i-1 ; l--){
-                            min = Math.min(countdp[i-1][l-1] + dp[l][j], min);
-                            if(min == 0) break;
-                        }
-                        countdp[i][j] = min;
-                    }
-                }
-            }
-        }
-        return countdp[k][n-1];
+        return helper(dp,s,n,k,new Integer[n+1][k+1]);
     }
 }
