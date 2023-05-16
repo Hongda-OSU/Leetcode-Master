c4667cdb38577cbf010cf8becb2691e649dadc2a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-16 14:09:29 -0400

    Time: 581 ms (97.15%), Space: 0B (100.00%) - LeetHub

--- 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
+++ 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
@@ -7,7 +7,7 @@ FROM
 (
     SELECT *,
         COUNT(activity) OVER (PARTITION BY username) cnt,
-        ROW_NUMBER() OVER(PARTITION BY username ORDER BY startDate DESC) n
+        ROW_NUMBER() OVER (PARTITION BY username ORDER BY startDate DESC) n
     FROM UserActivity
 ) tbl
 WHERE n = 2 or cnt < 2;
