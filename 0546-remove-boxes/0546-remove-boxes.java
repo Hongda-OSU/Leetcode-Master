6d42d1057e82e3cf2db1cde60d7bb7aabcc4deb4
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:22:57 -0400

    Time: 161 ms (12.50%), Space: 52.7 MB (26.79%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.java
+++ 0546-remove-boxes/0546-remove-boxes.java
@@ -2,24 +2,31 @@ class Solution {
    public int removeBoxes(int[] boxes) {
     int n = boxes.length;
     int[][][] dp = new int[n][n][n];
-    return removeBoxesSub(boxes, 0, n - 1, 0, dp);
-}
-    
-private int removeBoxesSub(int[] boxes, int i, int j, int k, int[][][] dp) {
-    if (i > j) return 0;
-    if (dp[i][j][k] > 0) return dp[i][j][k];
-    
-	int i0 = i, k0 = k; // Need to record the intial values of i and k in order to apply the following optimization
-    for (; i + 1 <= j && boxes[i + 1] == boxes[i]; i++, k++); // optimization: all boxes of the same color counted continuously from the first box should be grouped together
-    int res = (k + 1) * (k + 1) + removeBoxesSub(boxes, i + 1, j, 0, dp);
-    
-    for (int m = i + 1; m <= j; m++) {
-        if (boxes[i] == boxes[m]) {
-            res = Math.max(res, removeBoxesSub(boxes, i + 1, m - 1, 0, dp) + removeBoxesSub(boxes, m, j, k + 1, dp));
-        }
+    	
+    for (int j = 0; j < n; j++) {
+    	for (int k = 0; k <= j; k++) {
+    	    dp[j][j][k] = (k + 1) * (k + 1);
+    	}
     }
-        
-    dp[i0][j][k0] = res; // When updating the dp matrix, we should use the initial values of i, j and k
-    return res;
+    	
+    for (int l = 1; l < n; l++) {
+    	for (int j = l; j < n; j++) {
+    	    int i = j - l;
+    	        
+    	    for (int k = 0; k <= i; k++) {
+    	        int res = (k + 1) * (k + 1) + dp[i + 1][j][0];
+    	            
+    	        for (int m = i + 1; m <= j; m++) {
+    	            if (boxes[m] == boxes[i]) {
+    	                res = Math.max(res, dp[i + 1][m - 1][0] + dp[m][j][k + 1]);
+    	            }
+    	        }
+    	            
+    	        dp[i][j][k] = res;
+    	    }
+    	}
+    }
+    
+    return (n == 0 ? 0 : dp[0][n - 1][0]);
 }
 }
