1918fcee42d0977c7f53bdcab3659054e78d641c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:08:31 -0400

    Time: 20 ms (67.38%), Space: 42.7 MB (41.84%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.java
+++ 0664-strange-printer/0664-strange-printer.java
@@ -1,25 +1,35 @@
 class Solution {
-    public int strangePrinter(String s) {
+      public int strangePrinter(String s) {
+
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
         int n = s.length();
-        int dp[][] = new int[n][n];
-        for (int k = 1; k <= n; k++) {
-            for (int l = 0; l <= n - k; l++) {
-                int r = l + k - 1;
-                int j = -1;
-                dp[l][r] = n;
-                for (int i = l; i < r; i++) {
-                    if (s.charAt(i) != s.charAt(r) && j == -1) {
-                        j = i;
-                    }
-                    if (j != -1) {
-                        dp[l][r] = Math.min(dp[l][r], 1 + dp[j][i] + dp[i + 1][r]);
-                    }
+        int[][] state = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            state[i][i] = 1;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int dist = 1; dist + i < n; dist++) {
+                int j = dist + i;
+                if (dist == 1) {
+                    state[i][j] = (s.charAt(i) == s.charAt(j)) ? 1 : 2;
+                    continue;
                 }
-                if (j == -1) {
-                    dp[l][r] = 0;
+                state[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k + 1 <= j; k++) {
+                    int tmp = state[i][k] + state[k + 1][j];
+                    state[i][j] = Math.min(state[i][j], tmp);
+                }
+                if (s.charAt(i) == s.charAt(j)) {
+                    state[i][j]--;
                 }
             }
         }
-        return dp[0][n - 1] + 1;
+
+        return state[0][n - 1];
     }
 }
