a28893fe1b7ef363322fc604f410d545520c11de
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 14:59:44 -0400

    Time: 89 ms (85.90%), Space: 51.1 MB (59.84%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.java
+++ 1166-design-file-system/1166-design-file-system.java
@@ -1,33 +1,23 @@
 class FileSystem {
+    private HashMap<String, Integer> paths;
 
-    HashMap<String, Integer> paths;
-    
     public FileSystem() {
-        this.paths = new HashMap<String, Integer>();
+        paths = new HashMap<>();
     }
     
     public boolean createPath(String path, int value) {
-        
-        // Step-1: basic path validations
-        if (path.isEmpty() || (path.length() == 1 && path.equals("/")) || this.paths.containsKey(path)) {
+        if (path.isEmpty() || (path.length() == 1 && path.equals("/")) || paths.containsKey(path))
             return false;
-        }
-        
-        int delimIndex = path.lastIndexOf("/");
-        String parent = path.substring(0, delimIndex);
-        
-        // Step-2: if the parent doesn't exist. Note that "/" is a valid parent.
-        if (parent.length() > 1 && !this.paths.containsKey(parent)) {
+        int index = path.lastIndexOf("/");
+        String parent = path.substring(0, index);
+        if (parent.length() > 1 && !paths.containsKey(parent))
             return false;
-        }
-        
-        // Step-3: add this new path and return true.
-        this.paths.put(path, value);
+        paths.put(path, value);
         return true;
     }
     
     public int get(String path) {
-        return this.paths.getOrDefault(path, -1);
+        return paths.getOrDefault(path, -1);
     }
 }
 
