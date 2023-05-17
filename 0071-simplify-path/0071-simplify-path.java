ecce7636b3c0ee8d795f1b0e7cdbc3ad18f2c366
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 16:13:28 -0400

    Time: 1 ms (100.00%), Space: 42.6 MB (66.66%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,39 +1,39 @@
 class Solution {
     public String simplifyPath(String path) {
-        int len=path.length();
-        char []stack=new char[len];
-        int i=0;
-		    int j=-1;
-        while(i<len){
-            if(j>=0 && (stack[j] == '/' || i==len-1) && path.charAt(i)=='/') {
+        int len = path.length();
+        char[] stack = new char[len];
+        int i = 0, j = -1;
+        while (i < len) {
+            if (j >= 0 && (stack[j] == '/' || i == len - 1) && path.charAt(i) == '/') {
                 i++;
                 continue;
             }
-            if(path.charAt(i)=='.' && stack[j] == '/' ){
-                int dot=0;
-                while(i<len && path.charAt(i)!='/'){
+            if (path.charAt(i) == '.' && stack[j] == '/') {
+                int dot = 0;
+                while (i < len && path.charAt(i) != '/') {
                     dot++;
-                    if(path.charAt(i)!='.'){
-                        dot+=5;
+                    if (path.charAt(i) != '.') {
+                        dot += 5;
                         break;
                     }
-                    stack[++j]=path.charAt(i);
+                    stack[++j] = path.charAt(i);
                     i++;
                 }
-                if(dot<3){
-                     while(j>=0 && dot>0){
-                    if(stack[j--]=='/')dot--;
+                if (dot < 3) {
+                    while (j >= 0 && dot > 0) {
+                        if (stack[j--] == '/')
+                            dot--;
+                        if (j == -1)
+                            j = 0;
                     }
-                    if(j==-1) j=0;
-                }
-            }
-            else{
-                    stack[++j]=path.charAt(i);
-                    i++;
                 } 
+            } else {
+                stack[++j] = path.charAt(i);
+                i++;
+            }
         }
-        if(stack[j]=='/'&& j>1) j--;
-
-        return String.valueOf(stack,0,j+1);
-  }
+        if (stack[j] == '/' && j > 1)
+            j--;
+        return String.valueOf(stack, 0, j + 1);
+    }
 }
