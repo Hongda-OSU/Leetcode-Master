a53b1d10d52b219fbb71e3bbf38969c77870a4d2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:07:54 -0400

    Time: 57 ms (14.89%), Space: 43.1 MB (9.22%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.java
+++ 0664-strange-printer/0664-strange-printer.java
@@ -1,34 +1,25 @@
 class Solution {
-    int dp[][];
-
-    private int f(String s, int n, int l, int r) {
-        if (dp[l][r] != -1) {
-            return dp[l][r];
-        }
-        dp[l][r] = n;
-        int j = -1;
-        for (int i = l; i < r; i++) {
-            if (s.charAt(i) != s.charAt(r) && j == -1) {
-                j = i;
-            }
-            if (j != -1) {
-                dp[l][r] = Math.min(dp[l][r], 1 + f(s, n, j, i) + f(s, n, i + 1, r));
-            }
-        }
-        if (j == -1) {
-            dp[l][r] = 0;
-        }
-        return dp[l][r];
-    }
-
     public int strangePrinter(String s) {
         int n = s.length();
-        dp = new int[n][n];
-        for (int l = 0; l < n; l++) {
-            for (int r = 0; r < n; r++) {
-                dp[l][r] = -1;
+        int dp[][] = new int[n][n];
+        for (int k = 1; k <= n; k++) {
+            for (int l = 0; l <= n - k; l++) {
+                int r = l + k - 1;
+                int j = -1;
+                dp[l][r] = n;
+                for (int i = l; i < r; i++) {
+                    if (s.charAt(i) != s.charAt(r) && j == -1) {
+                        j = i;
+                    }
+                    if (j != -1) {
+                        dp[l][r] = Math.min(dp[l][r], 1 + dp[j][i] + dp[i + 1][r]);
+                    }
+                }
+                if (j == -1) {
+                    dp[l][r] = 0;
+                }
             }
         }
-        return f(s, n, 0, n - 1) + 1;
+        return dp[0][n - 1] + 1;
     }
 }
