33d0222aef32ae2422bed6219d34604b2a7ef70a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:29:47 -0400

    Time: 143 ms (17.86%), Space: 52.4 MB (55.36%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.java
+++ 0546-remove-boxes/0546-remove-boxes.java
@@ -1,45 +1,20 @@
-public class Solution {
+class Solution {
+  int[][][] memo; 
     public int removeBoxes(int[] boxes) {
-        List<Integer> colors = new ArrayList<>();
-        List<Integer> lens = new ArrayList<>();
-        // preprocessing
-        // [1,1,1,3,3,2,3,3,3,1,1] would become
-        // colors : [1,3,2,3,1]
-        // lens :   [3,2,1,3,2]
-        for (int box : boxes) {
-            if (!colors.isEmpty() && colors.get(colors.size() - 1) == box) {
-                // continuous, increase length count by 1
-                lens.set(lens.size() - 1, lens.get(lens.size() - 1) + 1);
-            } else {
-                // new color
-                colors.add(box);
-                lens.add(1);
-            }
-        }
-        int N = boxes.length;
-        int M = colors.size();
-        // dp[i][j][k] means the maximal score for colors[i:j] with k boxes of same color merged after j
-        // i and j are inclusive, so dp[0][M - 1][0] will be the final answer
-        int[][][] dp = new int[M][M][N];
-        return dfs(colors, lens, 0, M - 1, 0, dp);
+        int size = boxes.length;
+        memo = new int[size][size][size];
+        return dfs(boxes, 0, size - 1, 0); 
     }
     
-    // top-down dfs search with memoization
-    private int dfs(List<Integer> colors, List<Integer> lens, int l, int r, int k, int[][][] dp) {
-        if (l > r) return 0;
-        if (dp[l][r][k] > 0) return dp[l][r][k];
-        // merging boxes with colors[r]
-        int score = dfs(colors, lens, l, r - 1, 0, dp) + (lens.get(r) + k) * (lens.get(r) + k);
-        // merge boxes with colors[l:i] and colors[l + 1:r - 1] where i from l to r - 1
-        for (int i = l; i < r; i++) {
-            if (colors.get(i) == colors.get(r)) {
-                // notice here : since we use top-down approach, colors[i + 1:r - 1] has already been merged, so k = 0;
-                // so color[i] is adjacent to color[r] now
-                score = Math.max(score, 
-                    dfs(colors, lens, l, i, lens.get(r) + k, dp) + dfs(colors, lens, i + 1, r - 1, 0, dp));
+    int dfs(int[] boxes, int l, int r, int k) {
+        if (l > r) return 0; 
+        if (memo[l][r][k] != 0) return memo[l][r][k]; 
+        memo[l][r][k] = dfs(boxes, l + 1 , r, 0) + (k + 1) * (k + 1); 
+        for (int i = l + 1; i <= r; i++) {
+            if (boxes[l] == boxes[i]) {
+                memo[l][r][k] = Math.max(memo[l][r][k], dfs(boxes, l + 1, i - 1, 0) + dfs(boxes, i, r, k + 1)); 
             }
         }
-        dp[l][r][k] = score;
-        return score;
+        return memo[l][r][k]; 
     }
 }
