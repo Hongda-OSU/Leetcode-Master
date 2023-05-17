5b5902c922139467c6106c225888819e7f6ccd6c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 16:07:24 -0400

    Time: 4 ms (86.14%), Space: 42.6 MB (55.56%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,39 +1,21 @@
 class Solution {
     public String simplifyPath(String path) {
-
-        // Initialize a stack
-        Stack<String> stack = new Stack<String>();
-        String[] components = path.split("/");
-
-        // Split the input string on "/" as the delimiter
-        // and process each portion one by one
-        for (String directory : components) {
-
-            // A no-op for a "." or an empty string
-            if (directory.equals(".") || directory.isEmpty()) {
-                continue;
-            } else if (directory.equals("..")) {
-
-                // If the current component is a "..", then
-                // we pop an entry from the stack if it's non-empty
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
-            } else {
-
-                // Finally, a legitimate directory name, so we add it
-                // to our stack
-                stack.add(directory);
-            }
+        Stack<String> s = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        String[] p =path.split("/");
+        
+        for(int i=0;i<p.length;i++){
+            if(!s.isEmpty()  && p[i].equals("..")) s.pop();
+            else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals(".."))
+                s.push(p[i]);
         }
-
-        // Stich together all the directory names together
-        StringBuilder result = new StringBuilder();
-        for (String dir : stack) {
-            result.append("/");
-            result.append(dir);
+        
+        
+        if(s.isEmpty()) return "/";
+        while(!s.isEmpty()){
+            res.insert(0,s.pop()).insert(0,"/");
         }
-
-        return result.length() > 0 ? result.toString() : "/" ;
+        
+        return res.toString();
     }
 }
