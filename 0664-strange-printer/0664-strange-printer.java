b8fcb8c2ef3fdc50b1ac925e91366c39567134f1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:09:21 -0400

    Time: 12 ms (95.04%), Space: 42.9 MB (27.66%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.java
+++ 0664-strange-printer/0664-strange-printer.java
@@ -1,22 +1,45 @@
 class Solution {
-
-public int strangePrinter(String s) {
-    if (s.equals("")) return 0;
-    int len = s.length();
-    int[][] dp = new int[len][len];
-    for (int i = 0; i < len; i++)
-        dp[i][i] = 1;
-    for (int l = 2; l <= len; l++) {
-        for (int i = 0; i < len && l + i - 1 < len; i++) {
-            int j = l + i - 1;
-            dp[i][j] = dp[i][j - 1] + (s.charAt(i) == s.charAt(j) ? 0 : 1);
-            for (int k = i + 1; k < j; k++) {
-                if (s.charAt(k) == s.charAt(j)) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i][k - 1] + dp[k][j - 1]);
-                }
+   public int strangePrinter(String s) {
+      // Memoizatiion ----- Approach - 1 
+      // Integer[][] dp = new Integer[s.length()][s.length()];
+      // return solveMemo(s, 0, s.length() - 1, dp);
+      
+      // Tabulation ------ Approach - 2
+      int[][] dp = new int[s.length()][s.length()]; // dp[i][j] defines minimum turns for substring i to j
+      for(int i = dp.length - 1; i >= 0; i--){
+        for(int j = 0; j < dp.length; j++){
+          if(i > j){ // invalid substring
+            continue;
+          }else if(i == j){// single character || base condition
+            dp[i][j] = 1;
+          }else{
+            int min = Integer.MAX_VALUE;
+            for(int k = i; k < j; k++){ // For finding split which results in minimum turns;
+              min = Math.min(min, dp[i][k] + dp[k + 1][j]); 
             }
+            if(s.charAt(i) == s.charAt(j)){
+              min--;
+            }
+            dp[i][j] = min;
+          }
         }
+      }
+      return dp[0][s.length() - 1];
+      // TC -> O(n ^ 3)
     }
-    return dp[0][len - 1];
-}
+	  // Memoization
+    public int solveMemo(String s, int i, int j, Integer[][] dp){
+      if(i == j){
+        return 1;
+      }
+      if(dp[i][j] != null){
+        return dp[i][j];
+      }
+      int ans = Integer.MAX_VALUE;
+      for(int k = i; k < j; k++){
+        ans = Math.min(ans, solveMemo(s, i, k, dp) + solveMemo(s, k + 1, j, dp));
+      }
+      
+      return dp[i][j] = s.charAt(i) == s.charAt(j) ? ans - 1 : ans;
+    }
 }
