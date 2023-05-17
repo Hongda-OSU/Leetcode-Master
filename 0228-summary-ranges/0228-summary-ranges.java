fcb72c15f49f41d5c0da8dce5a7758234b8d509d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:38:38 -0400

    Time: 5 ms (78.46%), Space: 41 MB (40.22%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,21 +1,27 @@
 class Solution {
-    public List<String> summaryRanges(int[] nums) {
-        List<String> ranges = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int start = nums[i];
-            // Keep iterating until the next element is one more than the current element.
-            while (i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
-                i++;
-            }
-
-            if (start != nums[i]) {
-                ranges.add(start + "->" + nums[i]);
-            } else {
-                ranges.add(String.valueOf(start));
-            }
+     public List<String> summaryRanges(int[] nums) {
+        // Create a list of string to store the output result...
+        List<String> output = new ArrayList<String>();
+        // Start traversing the array from idx = 0 till idx < sizeofarray in a while loop.
+        int idx = 0;
+        while(idx < nums.length) {
+            // Initialize beg and last index for identifying the continuous element in the array...
+            int beg, last;
+            // Mark the number at current index as beginning element of the range...
+            beg = nums[idx];
+            // Traverse the array beggining from current index & find the last element whose difference from previous element is exactly 1, i.e. nums[idx + 1] == nums[idx] + 1...
+            while(idx+1 < nums.length && nums[idx+1] == nums[idx] + 1) 
+                idx++;
+            // Set this element as last element of the range...
+            last = nums[idx];
+            // If continuous element isn't present...
+            if(beg == last)
+                output.add(beg + "");
+            // If present...
+            else
+                output.add( beg + "->" + last );
+            idx++;          
         }
-
-        return ranges;
+        return output;      // Return the output result list...
     }
 }
