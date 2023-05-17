73cc7fbc8613d6b0ec84b5a9fb2d2edc3931a56e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 16:08:45 -0400

    Time: 2 ms (99.66%), Space: 42.7 MB (55.56%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,14 +1,39 @@
 class Solution {
     public String simplifyPath(String path) {
-           Stack<String> stack = new Stack<>();
-    String[] p = path.split("/");
-    for (int i = 0; i < p.length; i++) {
-        if (!stack.empty() && p[i].equals(".."))
-            stack.pop();
-        else if (!p[i].equals(".") && !p[i].equals("") && !p[i].equals(".."))
-            stack.push(p[i]);
-    }
-    List<String> list = new ArrayList(stack);
-    return "/"+String.join("/", list);
-    }
+        int len=path.length();
+        char []stack=new char[len];
+        int i=0;
+		    int j=-1;
+        while(i<len){
+            if(j>=0 && (stack[j] == '/' || i==len-1) && path.charAt(i)=='/') {
+                i++;
+                continue;
+            }
+            if(path.charAt(i)=='.' && stack[j] == '/' ){
+                int dot=0;
+                while(i<len && path.charAt(i)!='/'){
+                    dot++;
+                    if(path.charAt(i)!='.'){
+                        dot+=5;
+                        break;
+                    }
+                    stack[++j]=path.charAt(i);
+                    i++;
+                }
+                if(dot<3){
+                     while(j>=0 && dot>0){
+                    if(stack[j--]=='/')dot--;
+                    }
+                    if(j==-1) j=0;
+                }
+            }
+            else{
+                    stack[++j]=path.charAt(i);
+                    i++;
+                } 
+        }
+        if(stack[j]=='/'&& j>1) j--;
+
+        return String.valueOf(stack,0,j+1);
+  }
 }
