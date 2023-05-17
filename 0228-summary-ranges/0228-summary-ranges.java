19e66bad739f19c4104500db4145530a449d09aa
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:38:59 -0400

    Time: 5 ms (78.46%), Space: 41.2 MB (14.05%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,27 +1,37 @@
 class Solution {
      public List<String> summaryRanges(int[] nums) {
-        // Create a list of string to store the output result...
-        List<String> output = new ArrayList<String>();
-        // Start traversing the array from idx = 0 till idx < sizeofarray in a while loop.
-        int idx = 0;
-        while(idx < nums.length) {
-            // Initialize beg and last index for identifying the continuous element in the array...
-            int beg, last;
-            // Mark the number at current index as beginning element of the range...
-            beg = nums[idx];
-            // Traverse the array beggining from current index & find the last element whose difference from previous element is exactly 1, i.e. nums[idx + 1] == nums[idx] + 1...
-            while(idx+1 < nums.length && nums[idx+1] == nums[idx] + 1) 
-                idx++;
-            // Set this element as last element of the range...
-            last = nums[idx];
-            // If continuous element isn't present...
-            if(beg == last)
-                output.add(beg + "");
-            // If present...
-            else
-                output.add( beg + "->" + last );
-            idx++;          
-        }
-        return output;      // Return the output result list...
+        
+        ArrayList<String> ans = new ArrayList<>();
+
+        if(nums == null || nums.length == 0)
+          return ans;
+
+
+        int start = 0;
+        int end = 0;
+        int n = nums.length;
+        int i = 0;        
+        while(i < n)
+        {
+            if(i + 1 < n && nums[i + 1] - nums[i] == 1)
+            {
+                end++;
+            }
+            else {
+              
+              if(start == end)
+              {
+                  ans.add(String.valueOf(nums[end]));
+              }
+              else{
+                  ans.add(nums[start] + "->" + nums[end]);
+              }
+
+              end++;
+              start = end;
+            }
+          i++;      
+        } 
+       return ans;
     }
 }
