275d82efb17e0bdef8f3975e39815897ee05c8b8
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:23:59 -0400

    Time: 118 ms (25.89%), Space: 53 MB (16.96%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.java
+++ 0546-remove-boxes/0546-remove-boxes.java
@@ -1,29 +1,21 @@
 class Solution {
-    public int removeBoxes(int[] boxes) {
+  public int removeBoxes(int[] boxes) {
         int n = boxes.length;
-        int[][][] memo = new int[n][n][n];
-        return removeBoxesSub(boxes, 0, n - 1, 0, memo);
-    }
-
-    private int removeBoxesSub(int[] boxes, int i, int j, int k, int[][][] memo) {
-        if (i > j) return 0;
-        if (memo[i][j][k] > 0) return memo[i][j][k];
-    
-        int tmpi = i, tmpk = k;
-        while (i + 1 <= j && boxes[i + 1] == boxes[i]) {
-            i++;
-            k++;
-        }
-        
-        int res = (k + 1) * (k + 1) + removeBoxesSub(boxes, i + 1, j, 0, memo);
-
-        for (int m = i + 1; m <= j; m++) {
-            if (boxes[i] == boxes[m]) {
-                res = Math.max(res, removeBoxesSub(boxes, i + 1, m - 1, 0, memo) + removeBoxesSub(boxes, m, j, k + 1, memo));
-            }
-        }
-
-        memo[tmpi][j][tmpk] = res;
-        return res;
+        int[][][] dp = new int[n][n][n];
+        for(int i=0;i<n;i++)
+            for(int streak = 0; streak<n; streak++)
+                dp[i][i][streak] = (streak+1)*(streak+1);
+        for(int len=1; len<n;len++)
+            for(int l=0;l+len<n;l++) {
+                int r = l+len;
+                for(int streak=0;streak<=l;streak++) {
+                    int max = (streak+1)*(streak+1) +dp[l+1][r][0];
+                    for(int i=l+1;i<=r;i++)
+                        if(boxes[i] == boxes[l])
+                            max = Math.max(max, dp[l+1][i-1][0]+dp[i][r][streak+1]);
+                    dp[l][r][streak]=max;
+                }
+            }               
+        return dp[0][n-1][0];      
     }
 }
