dfb6802773624a3ce5a4c03c9bf731c4b6a79fb5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:42:42 -0400

    Time: 0 ms (100.00%), Space: 41 MB (40.22%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,23 +1,20 @@
 class Solution {
-   public List<String> summaryRanges(int[] nums) {
+    public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
-        if (nums.length == 0) {
+        if (nums.length == 0)
             return result;
-        }
         StringBuilder sb = new StringBuilder();
-        for (int i=0; i< nums.length; i++) {
-            int currentNumber = nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            int curr = nums[i];
             sb.setLength(0);
-            while (i + 1 < nums.length && nums[i+1] - nums[i] == 1) {
+            while (i + 1 < nums.length && nums[i + 1] - nums[i] == 1)
                 i++;
-            }
-            
-            if(currentNumber != nums[i]) {
-                sb.append(currentNumber);
+            if (curr != nums[i]) {
+                sb.append(curr);
                 sb.append("->");
                 sb.append(nums[i]);
             } else {
-                sb.append(currentNumber);
+                sb.append(curr);
             }
             result.add(sb.toString());
         }
