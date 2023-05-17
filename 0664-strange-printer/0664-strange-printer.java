ca8f7711d89a480a36072d54a7d38a7541c543a4
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-17 15:08:46 -0400

    Time: 53 ms (17.73%), Space: 43.1 MB (9.22%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.java
+++ 0664-strange-printer/0664-strange-printer.java
@@ -1,35 +1,21 @@
 class Solution {
-      public int strangePrinter(String s) {
-
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-
+    public int strangePrinter(String s) {
         int n = s.length();
-        int[][] state = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            state[i][i] = 1;
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int dist = 1; dist + i < n; dist++) {
-                int j = dist + i;
-                if (dist == 1) {
-                    state[i][j] = (s.charAt(i) == s.charAt(j)) ? 1 : 2;
-                    continue;
-                }
-                state[i][j] = Integer.MAX_VALUE;
-                for (int k = i; k + 1 <= j; k++) {
-                    int tmp = state[i][k] + state[k + 1][j];
-                    state[i][j] = Math.min(state[i][j], tmp);
-                }
-                if (s.charAt(i) == s.charAt(j)) {
-                    state[i][j]--;
+        if (n == 0) return 0;
+        
+        int[][] dp = new int[101][101];
+        for (int i = 0; i < n; i++) dp[i][i] = 1;
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                dp[j][j + i] = i + 1;
+                for (int k = j + 1; k <= j + i; k++) {
+                    int temp = dp[j][k - 1] + dp[k][j + i];
+                    if (s.charAt(k - 1) == s.charAt(j + i)) temp--;
+                    dp[j][j + i] = Math.min(dp[j][j + i], temp);
                 }
             }
         }
-
-        return state[0][n - 1];
+        return dp[0][n - 1];
     }
 }
