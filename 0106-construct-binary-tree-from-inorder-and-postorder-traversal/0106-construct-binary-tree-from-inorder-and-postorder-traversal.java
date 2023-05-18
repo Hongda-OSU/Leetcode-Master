abc3e367456211904741cf6a43993d54a573a8b9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-18 16:37:30 -0400

    Time: 2 ms (96.86%), Space: 43.5 MB (6.40%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,43 +14,29 @@
  * }
  */
 class Solution {
-  int post_idx;
-  int[] postorder;
-  int[] inorder;
-  HashMap<Integer, Integer> idx_map = new HashMap<Integer, Integer>();
-
-  public TreeNode helper(int in_left, int in_right) {
-    // if there is no elements to construct subtrees
-    if (in_left > in_right)
-      return null;
-
-    // pick up post_idx element as a root
-    int root_val = postorder[post_idx];
-    TreeNode root = new TreeNode(root_val);
-
-    // root splits inorder list
-    // into left and right subtrees
-    int index = idx_map.get(root_val);
-
-    // recursion 
-    post_idx--;
-    // build right subtree
-    root.right = helper(index + 1, in_right);
-    // build left subtree
-    root.left = helper(in_left, index - 1);
-    return root;
-  }
-
-  public TreeNode buildTree(int[] inorder, int[] postorder) {
-    this.postorder = postorder;
-    this.inorder = inorder;
-    // start from the last postorder element
-    post_idx = postorder.length - 1;
-
-    // build a hashmap value -> its index
-    int idx = 0;
-    for (Integer val : inorder)
-      idx_map.put(val, idx++);
-    return helper(0, inorder.length - 1);
-  }
-}
+    private int index;
+    private int[] postorder, inorder;
+    private HashMap<Integer, Integer> indexMap = new HashMap<>();
+    
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        this.postorder = postorder;
+        this.inorder = inorder;
+        index = postorder.length - 1;
+        int idx = 0;
+        for (Integer i : inorder) 
+            indexMap.put(i, idx++);
+        return helper(0, inorder.length - 1);
+    }
+    
+    private TreeNode helper(int left, int right) {
+        if (left > right)
+            return null;
+        int rootVal = postorder[index];
+        TreeNode root = new TreeNode(rootVal);
+        int idx = indexMap.get(rootVal);
+        index--;
+        root.right = helper(idx + 1, right);
+        root.left = helper(left, idx - 1);
+        return root;
+    }
+} 
