81cd9f9cdebf37889d6d7b0ede19f8e4cd68fa98
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-18 15:29:28 -0400

    Time: 0 ms (100.00%), Space: 40.7 MB (82.86%) - LeetHub

--- 0294-flip-game-ii/0294-flip-game-ii.java
+++ 0294-flip-game-ii/0294-flip-game-ii.java
@@ -3,13 +3,11 @@ class Solution {
         int curlen = 0, maxlen = 0;
         List<Integer> boardInitState = new ArrayList<>();
         for (int i = 0; i < currentState.length(); ++i) {
-            if (currentState.charAt(i) == '+') {
+            if (currentState.charAt(i) == '+') 
                 curlen++;
-            }
             if (i + 1 == currentState.length() || currentState.charAt(i) == '-') {
-                if (curlen >= 2) {
+                if (curlen >= 2) 
                     boardInitState.add(curlen);
-                }
                 maxlen = Math.max(maxlen, curlen);
                 curlen = 0;
             }
@@ -17,7 +15,7 @@ class Solution {
         int[] g = new int[maxlen + 1];
         for (int len = 2; len <= maxlen; ++len) {
             Set<Integer> gsub = new HashSet<>();
-            for (int lenFirstGame = 0; lenFirstGame < len / 2; ++lenFirstGame) {
+            for (int lenFirstGame = 0; lenFirstGame < len / 2; lenFirstGame++) {
                 int lenSecondGame = len - lenFirstGame - 2;
                 gsub.add(g[lenFirstGame] ^ g[lenSecondGame]);
             }
@@ -25,18 +23,16 @@ class Solution {
         }
 
         int gFinal = 0;
-        for (int s : boardInitState) {
+        for (int s : boardInitState) 
             gFinal ^= g[s];
-        }
         return gFinal != 0;
     }
 
     private int firstMissingNumber(Set<Integer> lut) {
         int m = lut.size();
         for (int i = 0; i < m; ++i) {
-            if (!lut.contains(i)) {
+            if (!lut.contains(i)) 
                 return i;
-            }
         }
         return m;
     }
