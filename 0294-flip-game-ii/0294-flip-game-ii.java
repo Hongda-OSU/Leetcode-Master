da700052d3f647cbb45da56d848fee98b72e14f2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-18 15:27:16 -0400

    Time: 8 ms (82.86%), Space: 44.4 MB (5.71%) - LeetHub

--- 0294-flip-game-ii/0294-flip-game-ii.java
+++ 0294-flip-game-ii/0294-flip-game-ii.java
@@ -1,13 +1,56 @@
 class Solution {
-    public boolean canWin(String s) {
-        
-        for(int i=1;i<s.length();i++){
-            if(s.charAt(i)=='+'&&s.charAt(i-1)=='+' &&!canWin(s.substring(0,i-1)+"--"+s.substring(i+1,s.length())) ){
-               /*自己能翻牌+敌人会输=我们赢*/
-                return true;    
-            }
-        }    
-        
+    public boolean canWin(String currentState) {
+        return canWin(currentState, new HashMap<String,Boolean>());
+    }
+  
+    private boolean canWin(String currentState, Map<String,Boolean> state){
+      currentState = trimDashes(currentState);
+      if(state.containsKey(currentState))
+        return state.get(currentState);
+      else if(currentState.indexOf("++") == -1){
+        state.put(currentState,false);
         return false;
+      }
+      
+      char[] candidates = currentState.toCharArray();
+      for (int i = 1; i < candidates.length; i++)
+        if(candidates[i] == '+' && candidates[i-1] == '+' && testCandidate(candidates,state,i))
+          return true;
+      
+      state.put(currentState,false);
+      return false;
+    }
+  
+    private boolean testCandidate(char[] candidates, Map<String,Boolean> state, int i){
+          candidates[i] = '-';
+          candidates[i-1] = '-';
+          String currentState = String.valueOf(candidates);
+          candidates[i] = '+';
+          candidates[i-1] = '+';
+          
+          boolean win = canWin(currentState,state);
+          state.put(currentState, win);
+      
+          // This is notted, because the above calculation is after the
+          // current player moves, so if the other player wins, the current player loses
+          return !win;
+    }
+    
+	// This method is not needed, but cuts down performance from 12ms to 5ms. The presence
+	// of a '-' on either terminal does not affect outcome, so it is more efficient to cut them
+	// out so as to minimize the input size for more complicated operations later on
+    private String trimDashes(String currentState){
+      int begin = 0;
+      while(begin < currentState.length() && currentState.charAt(begin) == '-')
+        begin++;
+      
+      int end = currentState.length()-1;
+      while(end >= 0 && currentState.charAt(end) == '-')
+        end--;
+      
+      if(begin < end)
+        return currentState.substring(begin,end+1);
+      else
+        return "";
     }
 }
