234187ecba0545226683aca74efc42cfa8ccb371
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-18 15:06:31 -0400

    Time: 2 ms (100.00%), Space: 56.6 MB (34.66%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -1,27 +1,24 @@
 class SparseVector {
-  Map<Integer, Integer> indexMap = new HashMap<>();
-  int n = 0;
-  SparseVector(int[] nums) {
-    for (int i = 0; i < nums.length; i++)
-      if (nums[i] != 0)
-        indexMap.put(i, nums[i]);
-    n = nums.length;
-  }
-  
-	// Return the dotProduct of two sparse vectors
-  public int dotProduct(SparseVector vec) {
-    if (indexMap.size() == 0 || vec.indexMap.size() == 0) return 0;
-    if (indexMap.size() > vec.indexMap.size())
-      return vec.dotProduct(this);
-    int productSum = 0;
-    for (Map.Entry<Integer, Integer> entry : indexMap.entrySet()) {
-      int index = entry.getKey();
-      Integer vecValue = vec.indexMap.get(index);
-      if (vecValue == null) continue; 
-      productSum += (entry.getValue() * vecValue);
+    public int[] arr;
+    SparseVector(int[] nums) {
+        this.arr = nums;
     }
-    return productSum;
-  }
+    
+    public int[] getArray(){
+        return this.arr;
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int[] vectorArr = vec.getArray();
+        int[] arr = this.getArray();
+        int sum = 0;
+        
+        for(int i = 0; i < vectorArr.length; i++){
+            sum += vectorArr[i]*arr[i];
+        }
+        return sum;
+    }
 }
 
 // Your SparseVector object will be instantiated and called as such:
