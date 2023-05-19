8dfe4d238764b16cd06f48a810ae77b93c2f769f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-19 15:29:25 -0400

    Time: 83 ms (37.37%), Space: 39.5 MB (70.43%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.java
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -1,27 +1,24 @@
-
 public class Solution {
     int count = 0;
+    
     public int countArrangement(int N) {
-        int[] nums = new int[N];
-        for (int i = 1; i <= N; i++)
-            nums[i - 1] = i;
-        permute(nums, 0);
+        if (N == 0) return 0;
+        helper(N, 1, new int[N + 1]);
         return count;
     }
-    public void permute(int[] nums, int l) {
-        if (l == nums.length) {
+    
+    private void helper(int N, int pos, int[] used) {
+        if (pos > N) {
             count++;
+            return;
         }
-        for (int i = l; i < nums.length; i++) {
-            swap(nums, i, l);
-            if (nums[l] % (l + 1) == 0 || (l + 1) % nums[l] == 0)
-                permute(nums, l + 1);
-            swap(nums, i, l);
+        
+        for (int i = 1; i <= N; i++) {
+            if (used[i] == 0 && (i % pos == 0 || pos % i == 0)) {
+                used[i] = 1;
+                helper(N, pos + 1, used);
+                used[i] = 0;
+            }
         }
-    }
-    public void swap(int[] nums, int x, int y) {
-        int temp = nums[x];
-        nums[x] = nums[y];
-        nums[y] = temp;
     }
 }
