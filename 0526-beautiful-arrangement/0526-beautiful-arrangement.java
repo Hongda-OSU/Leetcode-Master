8f4f914ba781e9fe4009bcb14e093ee155e23994
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-19 15:34:23 -0400

    Time: 3 ms (97.74%), Space: 39.5 MB (70.43%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.java
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -1,10 +1,16 @@
 class Solution {
-   private int count = 0;
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
-    }
+    private int count = 0;
+    
+    public int countArrangement(int n) {
+        if (n == 0)
+            return 0;
+        int[] nums = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+            nums[i] = i;
+        helper(nums, n);
+        return count;
+    } 
+    
     private void helper(int[] nums, int start) {
         if (start == 0) {
             count++;
@@ -12,15 +18,15 @@ class Solution {
         }
         for (int i = start; i > 0; i--) {
             swap(nums, start, i);
-            if (nums[start] % start == 0 || start % nums[start] == 0) helper(nums, start-1);
-            swap(nums,i, start);
+            if (nums[start] % start == 0 || start % nums[start] == 0)
+                helper(nums, start - 1);
+            swap(nums, i, start);
         }
     }
-    public int countArrangement(int N) {
-        if (N == 0) return 0;
-        int[] nums = new int[N+1];
-        for (int i = 0; i <= N; i++) nums[i] = i;
-        helper(nums, N);
-        return count;
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-}
+} 
