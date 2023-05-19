3163b59803cfbc60db101a5ed19415f8d0ddbbe4
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-19 14:50:36 -0400

    Time: 2 ms (89.31%), Space: 40.3 MB (62.60%) - LeetHub

--- 1628-design-an-expression-tree-with-evaluate-function/1628-design-an-expression-tree-with-evaluate-function.java
+++ 1628-design-an-expression-tree-with-evaluate-function/1628-design-an-expression-tree-with-evaluate-function.java
@@ -5,67 +5,57 @@
 
 abstract class Node {
     public abstract int evaluate();
-    // define your fields here  
+    // define your fields here
 };
 
-class TreeNode extends Node{
-    // Constructor 
-    String val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(String val) {
+class NumNode extends Node {
+    private int val;
+    public NumNode(int val) {
         this.val = val;
     }
-    
-    // Abstract method cannot have body; so fill in body here.
     public int evaluate() {
-        return dfs(this); // use this keyword for current TreeNode object.
+        return this.val;
     }
-    
-    private int dfs(TreeNode root) {
-        if (root.left == null && root.right == null) {
-            return Integer.valueOf(root.val);
-        }
-        int left = dfs(root.left);
-        int right = dfs(root.right);
-        String operator = root.val;
-        int res = 0;
-        if (operator.equals("+")) {
-            res = left + right;
-        } else if (operator.equals("-")) {
-            res = left - right;
-        } else if (operator.equals("*")) {
-            res = left * right;
-        } else {
-            res = left / right;
-        }
-        return res;
-    }
-    
 };
 
-
-/**
- * This is the TreeBuilder class.
- * You can treat it as the driver code that takes the postinfix input 
- * and returns the expression tree represnting it as a Node.
- */
+class OpNode extends Node {
+    private char op;
+    private Node left;
+    private Node right;
+    public OpNode(char op, Node left, Node right) {
+        this.op = op;
+        this.left = left;
+        this.right = right;
+    }
+    public int evaluate() {
+        switch(this.op) {
+            case '+':
+                return this.left.evaluate() + this.right.evaluate();
+            case '-':
+                return this.left.evaluate() - this.right.evaluate();
+            case '*':
+                return this.left.evaluate() * this.right.evaluate();
+            case '/':
+                return this.left.evaluate() / this.right.evaluate();
+            default:
+                return 0;
+        }
+    }
+};
 
 class TreeBuilder {
-    String operators = "+-*/";
-    Stack<TreeNode> stack = new Stack<>();
     Node buildTree(String[] postfix) {
-        for (String str : postfix) {
-            if (operators.contains(str)) {
-                TreeNode cur = new TreeNode(str);
-                cur.right = stack.pop();
-                cur.left = stack.pop();
-                stack.push(cur);
-            } else {
-                stack.push(new TreeNode(str));
+        Stack<Node> st = new Stack<Node>();
+        for (String s : postfix){
+            if(Character.isDigit(s.charAt(0))){
+                st.add(new NumNode(Integer.parseInt(s)));
+            }else{
+                Node right = st.pop();
+                Node left = st.pop();
+                st.add(new OpNode(s.charAt(0), left, right));
             }
         }
-        return stack.pop();
+        return st.peek();
     }
 };
 
