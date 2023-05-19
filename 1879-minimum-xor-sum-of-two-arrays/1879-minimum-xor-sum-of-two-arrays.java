f532f2990220a3fb0ee457ffadd8c78eac6f5cae
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-19 15:42:53 -0400

    Time: 11 ms (90.24%), Space: 40.7 MB (65.85%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.java
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.java
@@ -1,16 +1,20 @@
 class Solution {
-   int dfs(int[] dp, int[] a, int[] b, int i, int mask) {
-    if (i >= a.length)
-        return 0;
-    if (dp[mask] == Integer.MAX_VALUE)
-        for (int j = 0; j < b.length; ++j)
-            if ((mask & (1 << j)) == 0)
-                dp[mask] = Math.min(dp[mask], (a[i] ^ b[j]) + dfs(dp, a, b, i + 1, mask + (1 << j)));
-    return dp[mask];
-}
-public int minimumXORSum(int[] nums1, int[] nums2) {
-    int dp[] = new int[1 << nums2.length];
-    Arrays.fill(dp, Integer.MAX_VALUE);
-    return dfs(dp, nums1, nums2, 0, 0);
-}
-}
+    public int minimumXORSum(int[] nums1, int[] nums2) {
+        int[] dp = new int[1 << nums2.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        return dfs(dp, nums1, nums2, 0, 0);
+    }
+    
+    private int dfs(int[] dp, int[] nums1, int[] nums2, int i, int mask) {
+        if (i >= nums1.length)
+            return 0;
+        if (dp[mask] == Integer.MAX_VALUE) {
+            for (int j = 0; j < nums2.length; j++) {
+                if ((mask & (1 << j)) == 0)
+                    dp[mask] = Math.min(dp[mask], (nums1[i] ^ nums2[j]) + 
+                                        dfs(dp, nums1, nums2, i + 1, mask + (1 << j)));
+            }
+        }
+        return dp[mask];
+    }
+} 
