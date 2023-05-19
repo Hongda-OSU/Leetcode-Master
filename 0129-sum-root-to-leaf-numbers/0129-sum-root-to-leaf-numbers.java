f8ff4b75eb84bccff53e691f6c17bf55a1644dc5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-19 17:16:18 -0400

    Time: 0 ms (100.00%), Space: 39.8 MB (94.48%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,22 +14,20 @@
  * }
  */
 class Solution {
-  int rootToLeaf = 0;
-
-  public void preorder(TreeNode r, int currNumber) {
-    if (r != null) {
-      currNumber = currNumber * 10 + r.val;
-      // if it's a leaf, update root-to-leaf sum
-      if (r.left == null && r.right == null) {
-        rootToLeaf += currNumber;
-      }
-      preorder(r.left, currNumber);
-      preorder(r.right, currNumber) ;
+    private int rootToLeaf = 0;
+    
+    public int sumNumbers(TreeNode root) {
+        preorder(root, 0);
+        return rootToLeaf;
     }
-  }
-
-  public int sumNumbers(TreeNode root) {
-    preorder(root, 0);
-    return rootToLeaf;
-  }
+    
+    private void preorder(TreeNode node, int curr) {
+        if (node != null) {
+            curr = curr * 10 + node.val;
+            if (node.left == null && node.right == null) 
+                rootToLeaf += curr;
+            preorder(node.left, curr);
+            preorder(node.right, curr);
+        }
+    }
 }
