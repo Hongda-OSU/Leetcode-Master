1a032041d6d6e875089e6594c14c640db9b62642
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-19 17:14:32 -0400

    Time: 0 ms (100.00%), Space: 40.2 MB (62.13%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,27 +14,22 @@
  * }
  */
 class Solution {
-  public int sumNumbers(TreeNode root) {
-    int rootToLeaf = 0, currNumber = 0;
-    Deque<Pair<TreeNode, Integer>> stack = new ArrayDeque();
-    stack.push(new Pair(root, 0));
+  int rootToLeaf = 0;
 
-    while (!stack.isEmpty()) {
-      Pair<TreeNode, Integer> p = stack.pop();
-      root = p.getKey();
-      currNumber = p.getValue();
-
-      if (root != null) {
-        currNumber = currNumber * 10 + root.val;
-        // if it's a leaf, update root-to-leaf sum
-        if (root.left == null && root.right == null) {
-          rootToLeaf += currNumber;
-        } else {
-          stack.push(new Pair(root.right, currNumber));
-          stack.push(new Pair(root.left, currNumber));
-        }
+  public void preorder(TreeNode r, int currNumber) {
+    if (r != null) {
+      currNumber = currNumber * 10 + r.val;
+      // if it's a leaf, update root-to-leaf sum
+      if (r.left == null && r.right == null) {
+        rootToLeaf += currNumber;
       }
+      preorder(r.left, currNumber);
+      preorder(r.right, currNumber) ;
     }
+  }
+
+  public int sumNumbers(TreeNode root) {
+    preorder(root, 0);
     return rootToLeaf;
   }
 }
