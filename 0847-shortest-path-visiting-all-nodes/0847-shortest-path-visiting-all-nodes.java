312b1b4a9be87c184b14fc60712f16dd00f74a6e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-20 15:52:03 -0400

    Time: 10 ms (81.85%), Space: 44.2 MB (23.69%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.java
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.java
@@ -1,104 +1,39 @@
 class Solution {
+    /**
+    Very similar to 815 especially the bit part.
+    Our state defined as {currentNode, visitedNodeBit}
+    When node 1,2,4 has been visited, we use 10110 to represent. (node 0 and 3 are not visited)
+    Add all nodes to bfsQueue initially and run BFS.
+    When visitedNodeBit = 11111..111 (however many nodes/bit). We find the ans.
+    Time/Space complexity: O(n*2^n)
+    */
     public int shortestPathLength(int[][] graph) {
-        
-        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
-        
-        int n = graph.length;
-        
-        // Adjency list of graph
-        
-        for(int i=0;i<n;i++){
-            if(!hm.containsKey(i)){
-                hm.put(i, new ArrayList<Integer>());
+        if(graph.length==1)
+            return 0;
+        int ansBit = (1 << graph.length) - 1;
+        int [][] visitedMap = new int [graph.length][ansBit+1]; //[How many nodes][each node may have 2^n visited bit]
+        Queue<int []> bfsQueue = new LinkedList<>();
+        for(int i=0; i<graph.length; i++)
+            bfsQueue.add(new int [] {i, 1 << i}); //Adding all nodes initially because we can start anywhere.
+        int shortestPath = 0;
+        while(!bfsQueue.isEmpty()){
+            int size = bfsQueue.size();
+            shortestPath++;
+            for(int i=0; i<size; i++){
+                int [] state = bfsQueue.poll();
+                int nodeNum = state[0];
+                int visitedNodeBit = state[1];
+                for(int neighbors : graph[nodeNum]){
+                    int newVisitedNodeBit = visitedNodeBit | (1 << neighbors);//Adding neighbor node to visitedNodeBit. Add 2^nodeNum using bit OR.  001 | 010 --> 011. Visited node 0 and 1. 
+                    if(visitedMap[neighbors][newVisitedNodeBit] == 1) //If the same node was visited again with same visitedNodeBit, it means this node doesn't
+                        continue;//need to be visited again.  For example: 1->0->1->0.  First 1 we have {1, 10}, then we have {0, 11}, then we will have {1, 11}.
+                    visitedMap[neighbors][newVisitedNodeBit] = 1;//Lastly, we have {0, 11} which is a state we already had before. So we don't visit this again.
+                    if(newVisitedNodeBit==ansBit)  
+                        return shortestPath;
+                    bfsQueue.add(new int [] {neighbors, newVisitedNodeBit});
+                }   
             }
-            
-            int m = graph[i].length;
-            for(int j=0;j<m;j++){
-                hm.get(i).add(graph[i][j]);
-            }
-            
         }
-        
-        //dist 2d array
-        //row: bitmask -> visited node set bits are 1
-        //column: leading node
-        
-        int row = (int)Math.pow(2,n);
-        int col = n;
-        int dist[][] = new int[row][col];
-        
-        for(int i=0;i<row;i++){
-            Arrays.fill(dist[i], -1);
-        }
-        
-        
-        //Queue: [{leading node 1, mask},{leading node 2, mask} ... ]
-        LinkedList<int[]> q = new LinkedList<>();
-        
-        for(int i=0;i<n;i++){
-            
-            int lead = i;
-            int mask = setbit(0,i);
-            
-            q.add(new int[]{lead, mask});
-            dist[mask][lead] = 0;
-        }
-        
-        
-        // Applying simulatneous BFS
-        while(q.size()>0){
-            
-            
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                
-                int[] path = q.remove();
-                
-                int lead = path[0];
-                int mask = path[1];
-                
-                if(mask == row-1){   //all nodes visited
-                    return dist[mask][lead];
-                }
-                
-                // iterate over neighbours of lead
-                if(hm.containsKey(lead)){
-                    
-                    for(int child: hm.get(lead)){
-                    
-                        int newlead = child;
-                        int newmask = setbit(mask, newlead);
-
-                        // avoid cycle: intelligent bfs : checking if this set is already visited 
-                        // set : lead, mask(visited nodes)
-                        if(dist[newmask][newlead]!=-1){
-                            continue;
-                        }
-
-                        dist[newmask][newlead] = dist[mask][lead]+1;
-                        q.add(new int[]{newlead, newmask});
-
-                    }
-
-                }
-                
-            }
-            
-        }
-        
-        
-        return 1221; //magic - LOL
-        
-        
-        
+        return -1;
     }
-    
-    
-    private int setbit(int mask, int i){
-        return mask|(1<<i);
-    }
-    
-    
-    
-    
 }
