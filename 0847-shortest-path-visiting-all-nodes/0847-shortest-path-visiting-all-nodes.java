dc889bd801a4ce14b0518b7e7cbb2d4e4296da25
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-20 15:52:15 -0400

    Time: 9 ms (97.54%), Space: 43.2 MB (36.00%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.java
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.java
@@ -1,37 +1,42 @@
 class Solution {
-    /**
-    Very similar to 815 especially the bit part.
-    Our state defined as {currentNode, visitedNodeBit}
-    When node 1,2,4 has been visited, we use 10110 to represent. (node 0 and 3 are not visited)
-    Add all nodes to bfsQueue initially and run BFS.
-    When visitedNodeBit = 11111..111 (however many nodes/bit). We find the ans.
-    Time/Space complexity: O(n*2^n)
-    */
     public int shortestPathLength(int[][] graph) {
-        if(graph.length==1)
+        int len = graph.length;
+        
+        if(len == 1)
             return 0;
-        int ansBit = (1 << graph.length) - 1;
-        int [][] visitedMap = new int [graph.length][ansBit+1]; //[How many nodes][each node may have 2^n visited bit]
-        Queue<int []> bfsQueue = new LinkedList<>();
-        for(int i=0; i<graph.length; i++)
-            bfsQueue.add(new int [] {i, 1 << i}); //Adding all nodes initially because we can start anywhere.
-        int shortestPath = 0;
-        while(!bfsQueue.isEmpty()){
-            int size = bfsQueue.size();
-            shortestPath++;
-            for(int i=0; i<size; i++){
-                int [] state = bfsQueue.poll();
-                int nodeNum = state[0];
-                int visitedNodeBit = state[1];
-                for(int neighbors : graph[nodeNum]){
-                    int newVisitedNodeBit = visitedNodeBit | (1 << neighbors);//Adding neighbor node to visitedNodeBit. Add 2^nodeNum using bit OR.  001 | 010 --> 011. Visited node 0 and 1. 
-                    if(visitedMap[neighbors][newVisitedNodeBit] == 1) //If the same node was visited again with same visitedNodeBit, it means this node doesn't
-                        continue;//need to be visited again.  For example: 1->0->1->0.  First 1 we have {1, 10}, then we have {0, 11}, then we will have {1, 11}.
-                    visitedMap[neighbors][newVisitedNodeBit] = 1;//Lastly, we have {0, 11} which is a state we already had before. So we don't visit this again.
-                    if(newVisitedNodeBit==ansBit)  
-                        return shortestPath;
-                    bfsQueue.add(new int [] {neighbors, newVisitedNodeBit});
-                }   
+        
+        int target = (1 << len) - 1;
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] dp = new boolean[len][target + 1];
+        int covered = 0;
+        int minDegree = Integer.MAX_VALUE;
+        int firstIndex = -1;
+        
+        for(int[] arr : graph){
+            minDegree = Math.min(minDegree, arr.length); // get the min degree a node has
+        }
+        
+        for(int i = 0; i < graph.length; i++)
+            if(graph[i].length == minDegree)
+                q.add(new int[]{i, 1 << i}); // add all nodes with the minimum degree, no need to consider all nodes
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            covered++;
+            while(size-- > 0){
+                int[] temp = q.remove();
+                int index = temp[0];
+                int val = temp[1];
+                
+                for(int nextIndex : graph[index]){
+                    int newVal = val | (1 << nextIndex);
+                    if(newVal == target)
+                        return covered;
+                    if(!dp[nextIndex][newVal]){
+                        q.add(new int[]{nextIndex, newVal});
+                        dp[nextIndex][newVal] = true;
+                    }
+                }
             }
         }
         return -1;
