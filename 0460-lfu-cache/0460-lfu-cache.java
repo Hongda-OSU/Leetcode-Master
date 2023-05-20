a55115935eda58952a034b334f57323ca7b2fa92
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-20 15:15:30 -0400

    Time: 47 ms (99.53%), Space: 119.5 MB (83.01%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.java
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -1,107 +1,97 @@
 class LFUCache {
-    public static class Node {
-        public int key;
-        public int value;
-        public int freq;
-        public Node before, after;
-
-        public Node() {
-        }
-
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-            this.freq = 1;
-        }
-
-        public void remove() {
-            this.before.after = after;
-            this.after.before = before;
-        }
-
-        public void add(Node n) {
-            this.before = n;
-            this.after = n.after;
-            n.after = this;
-            this.after.before = this;
-        }
-    }
-
-    public int cap;
-    public int size;
-    public int minFreq;
-    Node[] map;
-    Node[] freqmap;
+    public int cap, size, minFreq;
+    public Node[] map, freqMap;
 
     public LFUCache(int capacity) {
         this.cap = capacity;
         this.size = 0;
         this.minFreq = 1;
         this.map = new Node[100001];
-        this.freqmap = new Node[20001];
+        this.freqMap = new Node[20001];
     }
-
+    
     public int get(int key) {
-        Node n = map[key];
-        if (n == null) {
+        Node node = map[key];
+        if (node == null)
             return -1;
-        }
         update(key);
-        return n.value;
+        return node.value;
     }
-
+    
     public void put(int key, int value) {
-        Node n = map[key];
-        if (n == null) {
-            if (cap == 0) {
+        Node node = map[key];
+        if (node == null) {
+            if (cap == 0)
                 return;
-            }
-            ++size;
+            size += 1;
             if (size > cap) {
-                Node head = freqmap[minFreq];
-                Node lfu = head.before;
+                Node head = freqMap[minFreq];
+                Node lfu = head.prev;
                 lfu.remove();
                 map[lfu.key] = null;
-                if (head == head.after) {
-                    freqmap[minFreq] = null;
-                }
-                --size;
+                if (head == head.next)
+                    freqMap[minFreq] = null;
+                size -= 1;
             }
-            n = new Node(key, value);
-            map[key] = n;
-            if (freqmap[1] == null) {
+            node = new Node(key, value);
+            map[key] = node;
+            if (freqMap[1] == null) {
                 Node head = new Node();
-                head.before = head;
-                head.after = head;
-                freqmap[1] = head;
+                head.prev = head;
+                head.next = head;
+                freqMap[1] = head;
             }
-            n.add(freqmap[1]);
+            node.add(freqMap[1]);
             minFreq = 1;
         } else {
-            n.value = value;
+            node.value = value;
             update(key);
         }
     }
-
-    public void update(int key) {
-        Node n = map[key];
-        n.remove();
-        int freq = n.freq;
-        if (freqmap[freq] == freqmap[freq].after) {
-            freqmap[freq] = null;
-            if (freq == minFreq) {
-                ++minFreq;
-                ;
-            }
+    
+    private void update(int key) {
+        Node node = map[key];
+        node.remove();
+        int freq = node.freq;
+        if (freqMap[freq] == freqMap[freq].next) {
+            freqMap[freq] = null;
+            if (freq == minFreq)
+                minFreq += 1;
         }
-        freq = ++n.freq;
-        if (freqmap[freq] == null) {
+        freq = ++node.freq;
+        if (freqMap[freq] == null) {
             Node head = new Node();
-            head.before = head;
-            head.after = head;
-            freqmap[freq] = head;
+            head.prev = head;
+            head.next = head;
+            freqMap[freq] = head;
         }
-        n.add(freqmap[freq]);
+        node.add(freqMap[freq]);
+    }
+}
+
+class Node {
+    public int key, value, freq;
+    public Node prev, next;
+    
+    public Node() {   
+    }
+    
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        this.freq = 1;
+    }
+    
+    public void remove() {
+        this.prev.next = next;
+        this.next.prev = prev;
+    }
+    
+    public void add(Node node) {
+        this.prev = node;
+        this.next = node.next;
+        node.next = this;
+        this.next.prev = this;
     }
 }
 
