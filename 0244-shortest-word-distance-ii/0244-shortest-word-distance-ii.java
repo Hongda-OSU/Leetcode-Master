c7f2e00357207d744ec7f4b52e4f90b0062b3819
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-20 15:24:40 -0400

    Time: 35 ms (17.03%), Space: 50.9 MB (5.02%) - LeetHub

--- 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.java
+++ 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.java
@@ -1,38 +1,27 @@
 class WordDistance {
 
-    HashMap<String, ArrayList<Integer>> locations;
-
+  HashMap<String,TreeSet<Integer>> map = new HashMap<>();	 
     public WordDistance(String[] words) {
-        this.locations = new HashMap<String, ArrayList<Integer>>();
-
-        // Prepare a mapping from a word to all it's locations (indices).
-        for (int i = 0; i < words.length; i++) {
-            ArrayList<Integer> loc = this.locations.getOrDefault(words[i], new ArrayList<Integer>());
-            loc.add(i);
-            this.locations.put(words[i], loc);
-        }
-    }
-
-    public int shortest(String word1, String word2) {
-        ArrayList<Integer> loc1, loc2;
-
-        // Location lists for both the words
-        // the indices will be in SORTED order by default
-        loc1 = this.locations.get(word1);
-        loc2 = this.locations.get(word2);
-
-        int l1 = 0, l2 = 0, minDiff = Integer.MAX_VALUE;
-        while (l1 < loc1.size() && l2 < loc2.size()) {
-            minDiff = Math.min(minDiff, Math.abs(loc1.get(l1) - loc2.get(l2)));
-            if (loc1.get(l1) < loc2.get(l2)) {
-                l1++;
-            } else {
-                l2++;
-            }
-        }
-
-        return minDiff;
-    }
+        	int len = words.length;
+	        for(int i=0; i<len; i++){
+	        	TreeSet<Integer> ls = map.get(words[i]);
+	        	if(ls==null) ls=new TreeSet<>();
+	        	ls.add(i);
+	        	map.put(words[i], ls);
+	        }
+    } 
+	    public int shortest(String word1, String word2) {
+	        TreeSet<Integer> tree_1 = map.get(word1);
+	        TreeSet<Integer> tree_2 = map.get(word2);
+	        int result=Integer.MAX_VALUE;
+	        for(int i:tree_1){
+	        	Integer floor = tree_2.floor(i);
+	        	Integer ceiling = tree_2.ceiling(i);
+	        	if(floor!=null) result=Math.min(result, Math.abs(i-floor));
+	        	if(ceiling!=null) result=Math.min(result, Math.abs(i-ceiling));
+	        }
+	        return result;
+	    }
 }
 
 /**
