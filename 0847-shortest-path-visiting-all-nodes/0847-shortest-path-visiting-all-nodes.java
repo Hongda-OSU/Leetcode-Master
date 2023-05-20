00aaa3e8d07f2e1165384124a75941497abd5065
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-20 16:14:52 -0400

    Time: 9 ms (97.54%), Space: 43.3 MB (29.85%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.java
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.java
@@ -1,40 +1,32 @@
 class Solution {
     public int shortestPathLength(int[][] graph) {
         int len = graph.length;
-        
-        if(len == 1)
+        if (len == 1)
             return 0;
-        
         int target = (1 << len) - 1;
-        Queue<int[]> q = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
         boolean[][] dp = new boolean[len][target + 1];
-        int covered = 0;
-        int minDegree = Integer.MAX_VALUE;
-        int firstIndex = -1;
-        
-        for(int[] arr : graph){
-            minDegree = Math.min(minDegree, arr.length); // get the min degree a node has
+        int covered = 0, minDegree= Integer.MAX_VALUE, firstIndex = -1;
+        for (int[] arr : graph)
+            minDegree = Math.min(minDegree, arr.length);
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[i].length == minDegree)
+                queue.add(new int[]{i, 1 << i});
         }
-        
-        for(int i = 0; i < graph.length; i++)
-            if(graph[i].length == minDegree)
-                q.add(new int[]{i, 1 << i}); // add all nodes with the minimum degree, no need to consider all nodes
-        
-        while(!q.isEmpty()){
-            int size = q.size();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
             covered++;
-            while(size-- > 0){
-                int[] temp = q.remove();
+            while (size-- > 0) {
+                int[] temp = queue.remove();
                 int index = temp[0];
-                int val = temp[1];
-                
-                for(int nextIndex : graph[index]){
-                    int newVal = val | (1 << nextIndex);
-                    if(newVal == target)
+                int value = temp[1];
+                for (int next : graph[index]) {
+                    int newVal = value | (1 << next);
+                    if (newVal == target)
                         return covered;
-                    if(!dp[nextIndex][newVal]){
-                        q.add(new int[]{nextIndex, newVal});
-                        dp[nextIndex][newVal] = true;
+                    if (!dp[next][newVal]) {
+                        queue.add(new int[]{next, newVal});
+                        dp[next][newVal] = true;
                     }
                 }
             }
