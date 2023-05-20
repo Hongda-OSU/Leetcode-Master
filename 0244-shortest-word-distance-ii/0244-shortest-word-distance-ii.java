5d9e1e28d958b3a105e0def6de34faea88d87a91
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-20 15:25:05 -0400

    Time: 31 ms (90.83%), Space: 50.4 MB (5.02%) - LeetHub

--- 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.java
+++ 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.java
@@ -1,31 +1,38 @@
-class WordDistance {
+public class WordDistance {
 
-   HashMap<String,List<Integer>> map = new HashMap<>();	 	
-	 	public WordDistance(String[] words) {
-	 		int len = words.length;
-	        for(int i=0; i<len; i++){
-	        	List<Integer> ls = map.get(words[i]);
-	        	if(ls==null) ls=new ArrayList<>();
-	        	ls.add(i);
-	        	map.put(words[i], ls);
-	        }
-	    }
-	    
-	    public int shortest(String word1, String word2) {
-	    	List<Integer> list_1 = map.get(word1);
-	    	List<Integer> list_2 = map.get(word2); 
-	    	int size = list_2.size();
-	        int result=Integer.MAX_VALUE;
-	        for(int i:list_1){
-	        	int index = Collections.binarySearch(list_2, i);
-	        	if(index<0) index=-(index+1);
-	        	if(index>0) result=Math.min(Math.abs(i-list_2.get(index-1)), result);
-	        	if(index<size) result=Math.min(Math.abs(i-list_2.get(index)), result); 
-	        }
-	        return result;
-	    }
+private Map<String, List<Integer>> map;
+
+public WordDistance(String[] words) {
+    map = new HashMap<String, List<Integer>>();
+    for(int i = 0; i < words.length; i++) {
+        String w = words[i];
+        if(map.containsKey(w)) {
+            map.get(w).add(i);
+        } else {
+            List<Integer> list = new ArrayList<Integer>();
+            list.add(i);
+            map.put(w, list);
+        }
+    }
 }
 
+public int shortest(String word1, String word2) {
+    List<Integer> list1 = map.get(word1);
+    List<Integer> list2 = map.get(word2);
+    int ret = Integer.MAX_VALUE;
+    for(int i = 0, j = 0; i < list1.size() && j < list2.size(); ) {
+        int index1 = list1.get(i), index2 = list2.get(j);
+        if(index1 < index2) {
+            ret = Math.min(ret, index2 - index1);
+            i++;
+        } else {
+            ret = Math.min(ret, index1 - index2);
+            j++;
+        }
+    }
+    return ret;
+}
+}
 /**
  * Your WordDistance object will be instantiated and called as such:
  * WordDistance obj = new WordDistance(wordsDict);
