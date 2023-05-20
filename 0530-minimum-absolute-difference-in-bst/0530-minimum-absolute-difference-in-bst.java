20ea35a7b49fc600b7f993a2b23bd69671c72c55
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-20 16:31:25 -0400

    Time: 0 ms (100.00%), Space: 43.3 MB (15.07%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -1,24 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int minDifference = Integer.MAX_VALUE;
-    // Initially, it will be null.
-    TreeNode prevNode;
-
-    void inorderTraversal(TreeNode node) {
-        if (node == null) {
+    private int minDiff = Integer.MAX_VALUE;
+    private TreeNode prev;
+    
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return minDiff;
+    }
+    
+    private void inorder(TreeNode node) {
+        if (node == null)
             return;
-        }
-
-        inorderTraversal(node.left);
-        // Find the difference with the previous value if it is there.
-        if (prevNode != null) {
-            minDifference = Math.min(minDifference, node.val - prevNode.val);
-        }
-        prevNode = node;
-        inorderTraversal(node.right);
+        inorder(node.left);
+        if (prev != null)
+            minDiff = Math.min(minDiff, node.val - prev.val);
+        prev = node;
+        inorder(node.right);
     }
-
-    int getMinimumDifference(TreeNode root) {
-        inorderTraversal(root);
-        return minDifference;
-    }
-};
+}
