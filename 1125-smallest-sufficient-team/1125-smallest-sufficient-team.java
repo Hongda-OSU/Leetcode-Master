d4466a1fc7b4df36c68cbbdaf849c0f332af971e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-21 16:50:39 -0400

    Time: 78 ms (31.95%), Space: 42.8 MB (77.78%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
@@ -1,42 +1,31 @@
 class Solution {
-    int n;
-    int skillsMaskOfPerson[];
-    long dp[];
-    private Long f(int skillsMask) {
-        if (skillsMask == 0) {
-            return 0L;
-        }
-        if (dp[skillsMask] != -1) {
-            return dp[skillsMask];
-        }
-        for (int i = 0; i < n; i++) {
-            int smallerSkillsMask = skillsMask & ~skillsMaskOfPerson[i];
-            if (smallerSkillsMask != skillsMask) {
-                long peopleMask = f(smallerSkillsMask) | (1L << i);
-                if (dp[skillsMask] == -1 || Long.bitCount(peopleMask) <
-                                                Long.bitCount(dp[skillsMask])) {
-                    dp[skillsMask] = peopleMask;
-                }
-            }
-        }
-        return dp[skillsMask];
-    }
     public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
-        n = people.size();
-        int m = req_skills.length;
+        int n = people.size(), m = req_skills.length;
         HashMap<String, Integer> skillId = new HashMap<String, Integer>();
         for (int i = 0; i < m; i++) {
             skillId.put(req_skills[i], i);
         }
-        skillsMaskOfPerson = new int[n];
+        int skillsMaskOfPerson[] = new int[n];
         for (int i = 0; i < n; i++) {
             for (String skill : people.get(i)) {
                 skillsMaskOfPerson[i] |= 1 << skillId.get(skill);
             }
         }
-        dp = new long [1 << m];
-        Arrays.fill(dp, -1);
-        long answerMask = f((1 << m) - 1);
+        long dp[] = new long [1 << m];
+        Arrays.fill(dp, (1L << n) - 1);
+        dp[0] = 0;
+        for (int skillsMask = 1; skillsMask < (1 << m); skillsMask++) {
+            for (int i = 0; i < n; i++) {
+                int smallerSkillsMask = skillsMask & ~skillsMaskOfPerson[i];
+                if (smallerSkillsMask != skillsMask) {
+                    long peopleMask = dp[smallerSkillsMask] | (1L << i);
+                    if (Long.bitCount(peopleMask) < Long.bitCount(dp[skillsMask])) {
+                        dp[skillsMask] = peopleMask;
+                    }
+                }
+            }
+        }
+        long answerMask = dp[(1 << m) - 1];
         int ans[] = new int [Long.bitCount(answerMask)];
         int ptr = 0;
         for (int i = 0; i < n; i++) {
