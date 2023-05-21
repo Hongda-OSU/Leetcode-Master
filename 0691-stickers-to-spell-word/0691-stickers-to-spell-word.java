8c3ad6eb56b41928b3a15470efe7a21e80019c4d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-21 17:10:46 -0400

    Time: 9 ms (93.87%), Space: 42.6 MB (90.57%) - LeetHub

--- 0691-stickers-to-spell-word/0691-stickers-to-spell-word.java
+++ 0691-stickers-to-spell-word/0691-stickers-to-spell-word.java
@@ -1,34 +1,73 @@
 class Solution {
-    public int minStickers(String[] stickers, String target) {
-        int m = stickers.length;
-        int[][] mp = new int[m][26];
-        Map<String, Integer> dp = new HashMap<>();
-        for (int i = 0; i < m; i++) 
-            for (char c:stickers[i].toCharArray()) mp[i][c-'a']++;
-        dp.put("", 0);
-        return helper(dp, mp, target);
+    private boolean empty(int[] freq) {
+        for(int f: freq) if(f > 0) return false;
+        return true;
     }
-    private int helper(Map<String, Integer> dp, int[][] mp, String target) {
-        if (dp.containsKey(target)) return dp.get(target);
-        int ans = Integer.MAX_VALUE, n = mp.length;
-        int[] tar = new int[26];
-        for (char c:target.toCharArray()) tar[c-'a']++;
-        // try every sticker
-        for (int i = 0; i < n; i++) {
-            // optimization
-            if (mp[i][target.charAt(0)-'a'] == 0) continue;
-            StringBuilder sb = new StringBuilder();
-            // apply a sticker on every character a-z
-            for (int j = 0; j < 26; j++) {
-                if (tar[j] > 0 ) 
-                    for (int k = 0; k < Math.max(0, tar[j]-mp[i][j]); k++)
-                        sb.append((char)('a'+j));
-            }
-            String s = sb.toString();
-            int tmp = helper(dp, mp, s);
-            if (tmp != -1) ans = Math.min(ans, 1+tmp);
+    private String toString(int[] freq) {
+        StringBuilder sb = new StringBuilder();
+        char c = 'a';
+        for(int f: freq) {
+            while(f-- > 0) sb.append(c);
+            c++;
         }
-        dp.put(target, ans == Integer.MAX_VALUE? -1:ans);
-        return dp.get(target);
+        return sb.toString();
+    }
+    public int minStickers(String[] stickers, String target) {
+        // Optimization 1: Maintain frequency only for characters present in target
+        int[] targetNaiveCount = new int[26];
+        for(char c: target.toCharArray()) targetNaiveCount[c - 'a']++;
+        int[] index = new int[26];
+        int N = 0;  // no of distinct characters in target
+        for(int i = 0; i < 26; i++) index[i] = targetNaiveCount[i] > 0 ? N++ : -1;
+        int[] targetCount = new int[N];
+        int t = 0;
+        for(int c: targetNaiveCount) if(c > 0) {
+            targetCount[t++] = c;
+        }
+        int[][] stickersCount = new int[stickers.length][N];
+        for(int i = 0; i < stickers.length; i++) {
+            for(char c: stickers[i].toCharArray()) {
+                int j = index[c - 'a'];
+                if(j >= 0) stickersCount[i][j]++;
+            }
+        }
+        // Optimization 2: Remove stickers dominated by some other sticker
+        int start = 0;
+        for(int i = 0; i < stickers.length; i++) {
+            for(int j = start; j < stickers.length; j++) if(j != i) {
+                int k = 0;
+                while(k < N && stickersCount[i][k] <= stickersCount[j][k]) k++;
+                if(k == N) {
+                    int[] tmp = stickersCount[i];
+                    stickersCount[i] = stickersCount[start];
+                    stickersCount[start++] = tmp;
+                    break;
+                }
+            }
+        }
+        // Perform BFS with target as source and an empty string as destination
+        Queue<int[]> Q = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        Q.add(targetCount);
+        int steps = 0;
+        while(!Q.isEmpty()) {
+            steps++;
+            int size = Q.size();
+            while(size-- > 0) {
+                int[] freq = Q.poll();
+                String cur = toString(freq);
+                if(visited.add(cur)) {
+                    // Optimization 3: Only use stickers that are capable of removing first character from current string
+                    int first = cur.charAt(0) - 'a';
+                    for(int i = start; i < stickers.length; i++) if(stickersCount[i][first] != 0) {
+                        int[] next = freq.clone();
+                        for(int j = 0; j < N; j++) next[j] = Math.max(next[j] - stickersCount[i][j], 0);
+                        if(empty(next)) return steps;
+                        Q.add(next);
+                    }
+                }
+            }
+        }
+        return -1;
     }
 }
