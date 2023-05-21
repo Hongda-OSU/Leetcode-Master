624681bdae9a5be661c04bbbcababa646f2b8143
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-21 16:50:53 -0400

    Time: 29 ms (59.03%), Space: 46.5 MB (36.11%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
@@ -1,38 +1,24 @@
 class Solution {
-    public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
-        int n = people.size(), m = req_skills.length;
-        HashMap<String, Integer> skillId = new HashMap<String, Integer>();
-        for (int i = 0; i < m; i++) {
-            skillId.put(req_skills[i], i);
-        }
-        int skillsMaskOfPerson[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            for (String skill : people.get(i)) {
-                skillsMaskOfPerson[i] |= 1 << skillId.get(skill);
-            }
-        }
-        long dp[] = new long [1 << m];
-        Arrays.fill(dp, (1L << n) - 1);
-        dp[0] = 0;
-        for (int skillsMask = 1; skillsMask < (1 << m); skillsMask++) {
-            for (int i = 0; i < n; i++) {
-                int smallerSkillsMask = skillsMask & ~skillsMaskOfPerson[i];
-                if (smallerSkillsMask != skillsMask) {
-                    long peopleMask = dp[smallerSkillsMask] | (1L << i);
-                    if (Long.bitCount(peopleMask) < Long.bitCount(dp[skillsMask])) {
-                        dp[skillsMask] = peopleMask;
-                    }
+   public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
+        int n = req_skills.length, m = people.size();
+        HashMap<String, Integer> skill_index = new HashMap<>();
+        for (int i = 0; i < n; ++i)
+            skill_index.put(req_skills[i], i);
+        List<Integer>[] dp = new List[1 << n];
+        dp[0] = new ArrayList<>();
+        for (int i = 0; i < m; ++i) {
+            int cur_skill = 0;
+            for (String s : people.get(i))
+                cur_skill |= 1 << skill_index.get(s);
+            for (int prev = 0; prev < dp.length; ++prev) {
+                if (dp[prev] == null) continue;
+                int comb = prev | cur_skill;
+                if (dp[comb] == null || dp[prev].size() + 1 < dp[comb].size()) {
+                    dp[comb] = new ArrayList<>(dp[prev]);
+                    dp[comb].add(i);
                 }
             }
         }
-        long answerMask = dp[(1 << m) - 1];
-        int ans[] = new int [Long.bitCount(answerMask)];
-        int ptr = 0;
-        for (int i = 0; i < n; i++) {
-            if (((answerMask >> i) & 1) == 1) {
-                ans[ptr++] = i;
-            }
-        }
-        return ans;
+        return dp[(1 << n) - 1].stream().mapToInt(i -> i).toArray();
     }
 }
