0eac653636d704d07a3cc26b16adea40295d6378
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-21 16:05:06 -0400

    Time: 1049 ms (32.00%), Space: 0B (100.00%) - LeetHub

--- 2010-the-number-of-seniors-and-juniors-to-join-the-company-ii/2010-the-number-of-seniors-and-juniors-to-join-the-company-ii.sql
+++ 2010-the-number-of-seniors-and-juniors-to-join-the-company-ii/2010-the-number-of-seniors-and-juniors-to-join-the-company-ii.sql
@@ -1,6 +1,25 @@
 # Write your MySQL query statement below
-WITH CTE AS (SELECT employee_id, experience, SUM(salary) OVER(PARTITION BY experience ORDER BY salary ASC) AS RN FROM Candidates)
-      
-SELECT employee_id FROM CTE WHERE experience = 'Senior' AND RN < 70000
+WITH a AS 
+(
+    SELECT employee_id,
+        experience,
+        salary,
+        SUM(salary) OVER(PARTITION BY experience ORDER BY salary) AS budget
+    FROM Candidates
+), b AS (
+    SELECT employee_id,
+        budget 
+    FROM a 
+    WHERE experience = "senior" 
+    AND budget <= 70000
+), c AS (
+    SELECT employee_id
+    FROM a
+    WHERE budget <= 70000 - (SELECT IFNULL(MAX(budget), 0) FROM b)
+)
+
+SELECT employee_id 
+FROM b
 UNION
-SELECT employee_id FROM CTE WHERE experience = 'Junior' AND RN < (SELECT 70000 - IFNULL(MAX(RN),0) FROM CTE WHERE experience = 'Senior' AND RN < 70000)
+SELECT employee_id
+FROM c;
