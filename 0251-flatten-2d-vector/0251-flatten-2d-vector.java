b06b03dd50305aa78d63abbe8a257ed8257f80db
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-21 16:46:42 -0400

    Time: 11 ms (71.32%), Space: 47.2 MB (59.69%) - LeetHub

--- 0251-flatten-2d-vector/0251-flatten-2d-vector.java
+++ 0251-flatten-2d-vector/0251-flatten-2d-vector.java
@@ -1,37 +1,31 @@
-import java.util.NoSuchElementException;
-
 class Vector2D {
-
-    // Constructor will put all the nums into this list.
-    private List<Integer> nums = new ArrayList<>();
-    // Keep track of where the Iterator is up to.
-    private int position = 0;
-
+    int indexList, indexEle;
+    int[][] vec; 
     public Vector2D(int[][] v) {
-        // We need to iterate over the 2D vector, getting all the integers
-        // out of it and putting them into nums (a field).
-        for (int[] innerVector : v) {
-            for (int num : innerVector) {
-                nums.add(num);
+        indexList = 0; //index of list in the vec
+        indexEle = 0; // index of element in current list of vec
+        vec = v;
+    }
+    
+    public int next() {
+        if(hasNext()) {
+            int res = vec[indexList][indexEle];
+            indexEle++;
+            return res;
+        }
+        return -1;
+    }
+    
+    public boolean hasNext() {
+        while(indexList < vec.length){
+            if(indexEle < vec[indexList].length)
+                return true;
+            else{
+                indexList++;
+                indexEle = 0;
             }
         }
-    }
-
-    public int next() {
-        // In Java, we throw a NoSuchElementException when next() is called
-        // on an exhausted Iterator.
-        if (!hasNext()) throw new NoSuchElementException();
-        // Store the number we need to return, as we still need to move position forward.
-        int result = nums.get(position);
-        // Move the position pointer forward by 1, so that it's ready for
-        // the next call to next, and gives a correct hasNext result.
-        position++;
-        return result;
-    }
-
-    public boolean hasNext() {
-        // There's nums left as long as position is a valid index of the list.
-        return position < nums.size();
+        return false;
     }
 }
 
