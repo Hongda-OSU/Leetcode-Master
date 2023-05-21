4059c5e6893132aea13cefe303697d2e091c0df9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-21 16:25:47 -0400

    Time: 51 ms (80.94%), Space: 48.8 MB (56.87%) - LeetHub

--- 0353-design-snake-game/0353-design-snake-game.java
+++ 0353-design-snake-game/0353-design-snake-game.java
@@ -1,99 +1,69 @@
-class SnakeGame {
+public class SnakeGame {
 
-HashMap<Pair<Integer, Integer>, Boolean> snakeMap;
-Deque<Pair<Integer, Integer>> snake;
-int[][] food;
-int foodIndex;
-int width;
-int height;
-
-/**
- * Initialize your data structure here.
- *
- * @param width - screen width
- * @param height - screen height
- * @param food - A list of food positions E.g food = [[1,1], [1,0]] means the first food is
- *     positioned at [1,1], the second is at [1,0].
- */
-public SnakeGame(int width, int height, int[][] food) {
-    this.width = width;
-    this.height = height;
-    this.food = food;
-    this.snakeMap = new HashMap<Pair<Integer, Integer>, Boolean>();
-    this.snakeMap.put(new Pair<Integer, Integer>(0,0), true); // intially at [0][0]
-    this.snake = new LinkedList<Pair<Integer, Integer>>();
-    this.snake.offerLast(new Pair<Integer, Integer>(0,0));
-}
-
-/**
- * Moves the snake.
- *
- * @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down
- * @return The game's score after the move. Return -1 if game over. Game over when snake crosses
- *     the screen boundary or bites its body.
- */
-public int move(String direction) {
-
-    Pair<Integer, Integer> snakeCell = this.snake.peekFirst();
-    int newHeadRow = snakeCell.getKey();
-    int newHeadColumn = snakeCell.getValue();
-
-    switch (direction) {
-    case "U":
-        newHeadRow--;
-        break;
-    case "D":
-        newHeadRow++;
-        break;
-    case "L":
-        newHeadColumn--;
-        break;
-    case "R":
-        newHeadColumn++;
-        break;
-    }
-
-    Pair<Integer, Integer> newHead = new Pair<Integer, Integer>(newHeadRow, newHeadColumn);
-    Pair<Integer, Integer> currentTail = this.snake.peekLast();
-
-    // Boundary conditions.
-    boolean crossesBoundary1 = newHeadRow < 0 || newHeadRow >= this.height;
-    boolean crossesBoundary2 = newHeadColumn < 0 || newHeadColumn >= this.width;
-
-    // Checking if the snake bites itself.
-    boolean bitesItself = this.snakeMap.containsKey(newHead) && !(newHead.getKey() == currentTail.getKey() && newHead.getValue() == currentTail.getValue());
+    //2D position info is encoded to 1D and stored as two copies 
+    Set<Integer> set; // this copy is good for fast loop-up for eating body case
+    Deque<Integer> body; // this copy is good for updating tail
+    int score;
+    int[][] food;
+    int foodIndex;
+    int width;
+    int height;
     
-    // If any of the terminal conditions are satisfied, then we exit with rcode -1.
-    if (crossesBoundary1 || crossesBoundary2 || bitesItself) {
-        return -1;
+    public SnakeGame(int width, int height, int[][] food) {
+        this.width = width;
+        this.height = height;
+        this.food = food;
+        set = new HashSet<>();
+        set.add(0); //intially at [0][0]
+        body = new LinkedList<>();
+        body.offerLast(0);
     }
-
-    // If there's an available food item and it is on the cell occupied by the snake after the move,
-    // eat it.
-    if ((this.foodIndex < this.food.length)
-        && (this.food[this.foodIndex][0] == newHeadRow)
-        && (this.food[this.foodIndex][1] == newHeadColumn)) {
-        this.foodIndex++;
-    } else {
-        this.snake.pollLast();
-        this.snakeMap.remove(currentTail);
-    }
-
-    // A new head always gets added
-    this.snake.addFirst(newHead);
-
-    // Also add the head to the set
-    this.snakeMap.put(newHead, true);
-
-    return this.snake.size() - 1;
-}
     
+  
+    public int move(String direction) {
+        //case 0: game already over: do nothing
+        if (score == -1) {
+            return -1;
+        }
+        
+        // compute new head
+        int rowHead = body.peekFirst() / width;
+        int colHead = body.peekFirst() % width;
+        switch (direction) {
+            case "U" : rowHead--;
+                       break;
+            case "D" : rowHead++;
+                       break;
+            case "L" : colHead--;
+                       break;
+            default :  colHead++;
+        }
+        int head = rowHead * width + colHead;
+        
+        //case 1: out of boundary or eating body
+        set.remove(body.peekLast()); // new head is legal to be in old tail's position, remove from set temporarily 
+        if (rowHead < 0 || rowHead == height || colHead < 0 || colHead == width || set.contains(head)) {
+            return score = -1;
+        }
+        
+        // add head for case2 and case3
+        set.add(head); 
+        body.offerFirst(head);
+        
+        //case2: eating food, keep tail, add head
+        if (foodIndex < food.length && rowHead == food[foodIndex][0] && colHead == food[foodIndex][1]) {
+            set.add(body.peekLast()); // old tail does not change, so add it back to set
+            foodIndex++;
+            return ++score;
+        }
+        
+        //case3: normal move, remove tail, add head
+        body.pollLast();
+        return score;
+        
+    }
 }
 
-/**
- * Your SnakeGame object will be instantiated and called as such: SnakeGame obj = new
- * SnakeGame(width, height, food); int param_1 = obj.move(direction);
- */
 /**
  * Your SnakeGame object will be instantiated and called as such:
  * SnakeGame obj = new SnakeGame(width, height, food);
