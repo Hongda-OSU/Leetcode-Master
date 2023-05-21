be164a483925c7d937dad01a43b60c58b2d0b6d5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-21 16:34:44 -0400

    Time: 46 ms (99.03%), Space: 49.2 MB (31.66%) - LeetHub

--- 0353-design-snake-game/0353-design-snake-game.java
+++ 0353-design-snake-game/0353-design-snake-game.java
@@ -1,74 +1,54 @@
 class SnakeGame {
+    private int len, rows, cols;
+    private int[][] food;
+    private LinkedList<Position> snake;
 
-      class Position{
-        int x;
-        int y;
-        public Position(int x,int y){
-            this.x = x;
-            this.y = y;
-        }
-        public boolean isEqual(Position p){
-            return this.x==p.x && this.y == p.y ;
-        }
-    }
-    int len;
-    int rows ,cols;
-    
-    int[][] food;
-    LinkedList<Position> snake;
-   
-    /** Initialize your data structure here.
-        @param width - screen width
-        @param height - screen height 
-        @param food - A list of food positions
-        E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
     public SnakeGame(int width, int height, int[][] food) {
+        this.len = 0;
         this.rows = height;
         this.cols = width;
         this.food = food;
-   
-        snake = new LinkedList<Position>();
-        snake.add(new Position(0,0));
-        len = 0;
+        this.snake = new LinkedList<>();
+        snake.add(new Position(0, 0));
     }
     
-    /** Moves the snake.
-        @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down 
-        @return The game's score after the move. Return -1 if game over. 
-        Game over when snake crosses the screen boundary or bites its body. */
     public int move(String direction) {
-    	//if(len>=food.length) return len;
-    
-        Position cur = new Position(snake.get(0).x,snake.get(0).y);
-        
-        switch(direction){
-        case "U": 
-            cur.x--;  break;
-        case "L": 
-            cur.y--; break;
-        case "R": 
-            cur.y++;   break;
-        case "D": 
-            cur.x++;   break;
+        Position curr = new Position(snake.get(0).x, snake.get(0).y);
+        switch (direction) {
+            case "U" -> curr.x--;
+            case "L" -> curr.y--;
+            case "R" -> curr.y++;
+            case "D" -> curr.x++;
         }
-        
-        if(cur.x<0 || cur.x>= rows || cur.y<0 || cur.y>=cols) return -1;
-        
-
-        for(int i=1;i<snake.size()-1;i++){
+        if (curr.x < 0 || curr.x >= rows || curr.y < 0 || curr.y >= cols)
+            return -1;
+        for (int i = 1; i < snake.size() - 1; i++) {
             Position next = snake.get(i);
-            if(next.isEqual(cur)) return -1;	       
+            if (next.equal(curr))
+                return -1;
         }
-        snake.addFirst(cur);     
-        if(len<food.length){
-            Position p = new Position(food[len][0],food[len][1]);	        
-            if(cur.isEqual(p)){	            
+        snake.addFirst(curr);
+        if (len < food.length) {
+            Position p = new Position(food[len][0], food[len][1]);
+            if (curr.equal(p))
                 len++;
-            }
         }
-        while(snake.size()>len+1) snake.removeLast();
-       
+        while (snake.size() > len + 1)
+            snake.removeLast();
         return len;
+    }
+}
+
+class Position {
+    public int x, y;
+    
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public boolean equal(Position p) {
+        return this.x == p.x && this.y == p.y;
     }
 }
 
