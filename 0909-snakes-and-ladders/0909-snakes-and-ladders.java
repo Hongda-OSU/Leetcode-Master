29f7a1b9eb2023e6bb2a22ab26bb805a6e1a4f59
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-22 14:15:39 -0400

    Time: 4 ms (97.01%), Space: 43.3 MB (8.41%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,34 +1,43 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        Pair<Integer, Integer>[] cells = new Pair[n * n + 1];
-        int label = 1;
-        Integer[] columns = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            columns[i] = i;
-        }
-        for (int row = n - 1; row >= 0; row--) {
-            for (int column : columns) {
-                cells[label++] = new Pair<>(row, column);
+        int[] arr = new int[n * n];
+        int i = n - 1, j = 0, index = 0, inc = 1;
+        while (index < n * n) {
+            arr[index++] = board[i][j];
+            if (inc == 1 && j == n - 1) {
+                inc = -1;
+                i--;
+            } else if (inc == -1 && j == 0) {
+                inc = 1;
+                i--;
+            } else {
+                j += inc;
             }
-            Collections.reverse(Arrays.asList(columns));
         }
-        int[] dist = new int[n * n + 1];
-        Arrays.fill(dist, -1);
-        Queue<Integer> q = new LinkedList<Integer>();
-        dist[1] = 0;
-        q.add(1);
+        boolean[] visited = new boolean[n * n];
+        Queue<Integer> q = new LinkedList<>();
+        int start = arr[0] > -1 ? arr[0] - 1 : 0;
+        q.offer(start);
+        visited[start] = true;
+        int step = 0;
         while (!q.isEmpty()) {
-            int curr = q.remove();
-            for (int next = curr + 1; next <= Math.min(curr + 6, n * n); next++) {
-                int row = cells[next].getKey(), column = cells[next].getValue();
-                int destination = board[row][column] != -1 ? board[row][column] : next;
-                if (dist[destination] == -1) {
-                    dist[destination] = dist[curr] + 1;
-                    q.add(destination);
+            int size = q.size();
+            while (size-- > 0) {
+                int cur = q.poll();
+                if (cur == n * n - 1) {
+                    return step;
+                }
+                for (int next = cur + 1; next <= Math.min(cur + 6, n * n - 1); next++) {
+                    int dest = arr[next] > -1 ? arr[next] - 1 : next;
+                    if (!visited[dest]) {
+                        visited[dest] = true;
+                        q.offer(dest);
+                    }
                 }
             }
+            step++;
         }
-        return dist[n * n];
+        return -1;
     }
 }
