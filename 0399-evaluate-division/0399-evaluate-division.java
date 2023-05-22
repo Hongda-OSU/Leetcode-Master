180f9648efde017b22a75d06869ab61a9eb7c0c1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-22 14:05:51 -0400

    Time: 1 ms (92.16%), Space: 41.3 MB (53.46%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,47 +1,50 @@
 class Solution {
-    HashMap<String, ArrayList<Pair>> graph = new HashMap<>();
-    public class Pair{
-        String v;
-        double w;
-        Pair(String v, double w){
-            this.v = v;
-            this.w = w;
-        }
-        public String toString(){
-            return v + " " + w;
-        }
-    }
-    
-    public double dfs(String src, String dest, HashSet<String> vis){
-        vis.add(src);
-        if(src.equals(dest)) return 1;
-        double ans = 1;
-        for(Pair e : graph.get(src)){
-            if(!vis.contains(e.v)){
-                double ret = dfs(e.v, dest, vis);
-                if(ret > 0) return e.w * ret;
-            }
-        }
-        return -1.0;
-    }
+    private HashMap<String, ArrayList<Pair>> graph = new HashMap<>();
     
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        double[] qAnswer = new double[queries.size()];
-        for(int i = 0 ; i < equations.size() ; i++){
+        double[] result = new double[queries.size()];
+        for (int i = 0; i < equations.size(); i++) {
             String u = equations.get(i).get(0), v = equations.get(i).get(1);
             double w = values[i];
             graph.putIfAbsent(u, new ArrayList<>());
             graph.putIfAbsent(v, new ArrayList<>());
             graph.get(u).add(new Pair(v, w));
-            graph.get(v).add(new Pair(u, 1/w));
+            graph.get(v).add(new Pair(u, 1 / w));
         }
-        
-        for(int i = 0 ; i < queries.size() ; i++){
+        for (int i = 0; i < queries.size(); i++) {
             String u = queries.get(i).get(0), v = queries.get(i).get(1);
             double q = graph.containsKey(u) ? dfs(u, v, new HashSet<>()) : -1.0;
-            qAnswer[i] = q;
+            result[i] = q;
         }
-        
-        return qAnswer;
+        return result;
+    }
+    
+    private double dfs(String u, String v, HashSet<String> visited) {
+        visited.add(u);
+        if (u.equals(v))
+            return 1;
+        double result = 1;
+        for (Pair p : graph.get(u)) {
+            if (!visited.contains(p.str)) {
+                double d = dfs(p.str, v, visited);
+                if (d > 0)
+                    return p.w * d;
+            }
+        }
+        return -1.0;
+    }
+}
+
+class Pair {
+    public String str;
+    public double w;
+    
+    public Pair(String str, double w) {
+        this.str = str;
+        this.w = w;
+    }
+    
+    public String toString() {
+        return str + " " + w;
     }
 }
