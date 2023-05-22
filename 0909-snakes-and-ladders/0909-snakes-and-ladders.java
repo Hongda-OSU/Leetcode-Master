f00cc463754f8baf75ce07c5b08abb19f727b3d3
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-22 14:14:48 -0400

    Time: 14 ms (15.70%), Space: 43.9 MB (5.42%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -15,33 +15,17 @@ class Solution {
         }
         int[] dist = new int[n * n + 1];
         Arrays.fill(dist, -1);
-        class Vertex implements Comparable<Vertex> {
-            int distance, label;
-
-            public Vertex(int distance, int label) {
-                this.distance = distance;
-                this.label = label;
-            }
-
-            public int compareTo(Vertex v) {
-                return this.distance - v.distance;
-            }
-        }
-        PriorityQueue<Vertex> q = new PriorityQueue<Vertex>();
+        Queue<Integer> q = new LinkedList<Integer>();
         dist[1] = 0;
-        q.add(new Vertex(0, 1));
+        q.add(1);
         while (!q.isEmpty()) {
-            Vertex vertex = q.remove();
-            int d = vertex.distance, curr = vertex.label;
-            if (d != dist[curr]) {
-                continue;
-            }
+            int curr = q.remove();
             for (int next = curr + 1; next <= Math.min(curr + 6, n * n); next++) {
                 int row = cells[next].getKey(), column = cells[next].getValue();
                 int destination = board[row][column] != -1 ? board[row][column] : next;
-                if (dist[destination] == -1 || dist[curr] + 1 < dist[destination]) {
+                if (dist[destination] == -1) {
                     dist[destination] = dist[curr] + 1;
-                    q.add(new Vertex(dist[destination], destination));
+                    q.add(destination);
                 }
             }
         }
