1f8694cd2a6e9f1568b2a10cd00da37481e54fef
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-22 13:43:29 -0400

    Time: 0 ms (100.00%), Space: 40.5 MB (55.96%) - LeetHub

--- 1349-maximum-students-taking-exam/1349-maximum-students-taking-exam.java
+++ 1349-maximum-students-taking-exam/1349-maximum-students-taking-exam.java
@@ -1,41 +1,36 @@
-/**
-the same as 
-[2123. Minimum Operations to Remove Adjacent Ones in Matrix](https://leetcode.com/problems/minimum-operations-to-remove-adjacent-ones-in-matrix/)  (Bipartile, boy girl and girl boy matches, Hungarian, unweighted) 
- */
 class Solution {
-    int m, n;
-    Integer[][] match;
-    int[][] seen;
+    private int m, n;
+    private Integer[][] match;
+    private int[][] seen;
     private static final int[][] dirs = {{0, 1}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    
     public int maxStudents(char[][] seats) {
         m = seats.length;
         n = seats[0].length;
-
         match = new Integer[m][n];
         seen = new int[m][n];
-        int ans = 0;
+        int result = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (seats[i][j] == '.') {
-                    ans++;
-                    if (match[i][j] == null) 
-                        ans -= dfs(seats, i, j, seen[i][j] = i * n + j);
+                    result++;
+                    if (match[i][j] == null)
+                        result -= dfs(seats, i, j, seen[i][j] = i * n + j);
                 }
             }
         }
-        return ans;
+        return result;
     }
-
+    
     private int dfs(char[][] seats, int x, int y, int v) {
         for (int[] dir : dirs) {
-            int tx = x + dir[0];
-            int ty = y + dir[1];
-            // only invisted girls with whom the boy has connection and has not invited before
-            if (tx >= 0 && tx < m && ty >= 0 && ty < n && seats[tx][ty] == '.' && seen[tx][ty] != v) {
-                seen[tx][ty] = v;
-                if (match[tx][ty] == null || dfs(seats, match[tx][ty] / n, match[tx][ty] % n, v) == 1) {
-                    match[x][y] = tx * n + ty;
-                    match[tx][ty] = x * n + y;
+            int dx = x + dir[0];
+            int dy = y + dir[1];
+            if (dx >= 0 && dx < m && dy >= 0 && dy < n && seats[dx][dy] == '.' && seen[dx][dy] != v) {
+                seen[dx][dy] = v;
+                if (match[dx][dy] == null || dfs(seats, match[dx][dy] / n, match[dx][dy] % n, v) == 1) {
+                    match[x][y] = dx * n + dy;
+                    match[dx][dy] = x * n + y;
                     return 1;
                 }
             }
