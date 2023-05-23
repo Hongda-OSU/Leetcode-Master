ed07a02e25d9a4845df17a90cf2802ad16f553f0
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 16:30:42 -0400

    Time: 181 ms (73.48%), Space: 41.1 MB (99.43%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.java
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.java
@@ -1,54 +1,48 @@
 class Solution {
-    public int backtrack(int[] nums, int mask, int pairsPicked, int[] memo) {
-        // If we have picked all the numbers from 'nums' array, we can't get more score.
-        if (2 * pairsPicked == nums.length) {
-            return 0;
-        }
+    public int maxScore(int[] nums) {
+        int maxStates = 1 << nums.length; // 2^(nums array size)
+        int finalMask = maxStates - 1;
 
-        // If we already solved this sub-problem then return the stored result.
-        if (memo[mask] != -1) {
-            return memo[mask];
-        }
+        // 'dp[i]' stores max score we can get after picking remaining numbers represented by 'i'.
+        int[] dp = new int[maxStates];
 
-        int maxScore = 0;
+        // Iterate on all possible states one-by-one.
+        for (int state = finalMask; state >= 0; state--) {
+            // If we have picked all numbers, we know we can't get more score as no number is remaining.
+            if (state == finalMask) {
+                dp[state] = 0;
+                continue;
+            }
 
-        // Iterate on 'nums' array to pick the first and second number of the pair.
-        for (int firstIndex = 0; firstIndex < nums.length; ++firstIndex) {
-            for (int secondIndex = firstIndex + 1; secondIndex < nums.length; ++secondIndex) {
-    
-                // If the numbers are same, or already picked, then we move to next number.
-                if (((mask >> firstIndex) & 1) == 1 || ((mask >> secondIndex) & 1) == 1) {
-                    continue;
+            int numbersTaken = Integer.bitCount(state);
+            int pairsFormed = numbersTaken / 2;
+            // States representing even numbers are taken are only valid.
+            if (numbersTaken % 2 != 0) {
+                continue;
+            }
+
+            // We have picked 'pairsFormed' pairs, we try all combinations of one more pair now.
+            // We iterate on two numbers using two nested for loops.
+            for (int firstIndex = 0; firstIndex < nums.length; firstIndex++) {
+                for (int secondIndex = firstIndex + 1; secondIndex < nums.length; secondIndex++) {
+                    // We only choose those numbers which were not already picked.
+                    if (((state >> firstIndex) & 1) == 1 || ((state >> secondIndex) & 1) == 1) {
+                        continue;
+                    }
+                    int currentScore = (pairsFormed + 1) * gcd(nums[firstIndex], nums[secondIndex]);
+                    int stateAfterPickingCurrPair = state | (1 << firstIndex) | (1 << secondIndex);
+                    int remainingScore = dp[stateAfterPickingCurrPair];
+                    dp[state] = Math.max(dp[state], currentScore + remainingScore);
                 }
-
-                // Both numbers are marked as picked in this new mask.
-                int newMask = mask | (1 << firstIndex) | (1 << secondIndex);
-
-                // Calculate score of current pair of numbers, and the remaining array.
-                int currScore = (pairsPicked + 1) * gcd(nums[firstIndex], nums[secondIndex]);
-                int remainingScore = backtrack(nums, newMask, pairsPicked + 1, memo);
-
-                // Store the maximum score.
-                maxScore = Math.max(maxScore, currScore + remainingScore);
-                // We will use old mask in loop's next interation, 
-                // means we discarded the picked number and backtracked.
             }
         }
 
-        // Store the result of the current sub-problem.
-        memo[mask] = maxScore;
-        return maxScore;
+        // Returning score we get from 'n' remaining numbers of array.
+        return dp[0];
     }
 
-    public int maxScore(int[] nums) {
-        int memoSize = 1 << nums.length; // 2^(nums array size)
-        int[] memo = new int[memoSize];
-        Arrays.fill(memo, -1);
-        return backtrack(nums, 0, 0, memo);
-    }
-
-    // Utility function to calculate the gcd of two numbers.
-    public int gcd(int a, int b) {
+    // Helper method to calculate GCD using Euclidean algorithm.
+    private int gcd(int a, int b) {
         if (b == 0) {
             return a;
         }
