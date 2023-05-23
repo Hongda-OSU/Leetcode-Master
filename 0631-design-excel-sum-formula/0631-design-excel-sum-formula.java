74c3ea064425723e19741bfcbc9ebe7b860cb0cf
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 16:10:33 -0400

    Time: 7 ms (90.32%), Space: 41.2 MB (100.00%) - LeetHub

--- 0631-design-excel-sum-formula/0631-design-excel-sum-formula.java
+++ 0631-design-excel-sum-formula/0631-design-excel-sum-formula.java
@@ -1,132 +1,93 @@
-public class Excel {
-    private final static int BASE = 'A';
-    private final Expression[][] spreadsheet;
+class Excel {
+    private Cell[][] table;
 
-    public Excel(int H, char W) {
-        this.spreadsheet = new Expression[H+1][W - BASE+1];
+    public Excel(int height, char width) {
+        table = new Cell[height + 1][width - 'A' + 1];
     }
-
-    public void set(int r, char c, int v) {
-        spreadsheet[r][c - BASE] = new Value(v);
+    
+    public void set(int row, char column, int val) {
+        if (table[row][column - 'A'] == null)
+            table[row][column - 'A'] = new Cell(val);
+        else
+            table[row][column - 'A'].setValue(val);
     }
-
-    public int get(int r, char c) {
-        Expression cell = spreadsheet[r][c - BASE];
-        if (cell != null) {
-            return cell.evaluate(this);
-        } else {
+    
+    public int get(int row, char column) {
+        if (table[row][column - 'A'] == null)
             return 0;
-        }
+        else
+            return table[row][column - 'A'].getValue();
     }
-
-    public int sum(int r, char c, String[] strs) {
-        Expression[] expressions = new Expression[strs.length];
-
-        for (int i = 0; i < strs.length; i++) {
-            String[] split = strs[i].split(":");
-            if (split.length == 1) {
-                expressions[i] = new Reference(split[0]);
+    
+    public int sum(int row, char column, String[] numbers) {
+        if (table[row][column - 'A'] == null)
+            table[row][column - 'A'] = new Cell(numbers);
+        else 
+            table[row][column - 'A'].setFormula(numbers);
+        return table[row][column - 'A'].getValue();
+    }
+    
+    class Cell {
+    public int val = 0;
+    public HashMap<Cell, Integer> formula = new HashMap<>();
+    
+    public Cell(int val) {
+        setValue(val);
+    }
+    
+    public Cell(String[] formulaStr) {
+        setFormula(formulaStr);
+    }
+    
+    public void setValue(int val) {
+        formula.clear();
+        this.val = val;
+    }
+    
+    public void setFormula(String[] formulaStr) {
+        formula.clear();
+        for (String str : formulaStr) {
+            if (str.indexOf(":") < 0) {
+                int[] pos = getPos(str);
+                addFormulaCell(pos[0], pos[1]);
             } else {
-                String topLeft = split[0];
-                String bottomRight = split[1];
-                expressions[i] = new Rectangle(topLeft, bottomRight);
-            }
-        }
-
-        spreadsheet[r][c - BASE] = new Formula(expressions);
-        return spreadsheet[r][c - BASE].evaluate(this);
-    }
-
-    private int getRow(String cell) {
-        return Integer.valueOf(cell.substring(1));
-    }
-
-    private int getColumn(String cell) {
-        return cell.charAt(0) - BASE;
-    }
-
-    private interface Expression {
-        int evaluate(Excel excel);
-    }
-
-    private class Value implements Expression {
-        private final int value;
-
-        Value(int value) {
-            this.value = value;
-        }
-
-        public int evaluate(Excel excel) {
-            return value;
-        }
-    }
-
-    private class Formula implements Expression {
-        private final Expression[] expressions;
-
-        Formula(Expression[] expressions) {
-            this.expressions = expressions;
-        }
-
-        public int evaluate(Excel excel) {
-            int value = 0;
-
-            for (Expression expression : expressions) {
-                value += expression.evaluate(excel);
-            }
-
-            return value;
-        }
-    }
-
-    private class Reference implements Expression {
-        private final int row;
-        private final int column;
-
-        Reference(String cell) {
-            this.row = getRow(cell);
-            this.column = getColumn(cell);
-        }
-
-        public int evaluate(Excel excel) {
-            Expression cell = excel.spreadsheet[row][column];
-            if (cell != null) {
-                return cell.evaluate(excel);
-            } else {
-                return 0;
-            }
-        }
-    }
-
-    private class Rectangle implements Expression {
-        private final int rowStart;
-        private final int rowEnd;
-        private final int columnStart;
-        private final int columnEnd;
-
-        Rectangle(String topLeft, String bottomRight) {
-            this.rowStart = getRow(topLeft);
-            this.rowEnd = getRow(bottomRight);
-            this.columnStart = getColumn(topLeft);
-            this.columnEnd = getColumn(bottomRight);
-        }
-
-        public int evaluate(Excel excel) {
-            int value = 0;
-
-            for (int i = rowStart; i <= rowEnd; i++) {
-                for (int j = columnStart; j <= columnEnd; j++) {
-                    Expression cell = excel.spreadsheet[i][j];
-                    if (cell != null) {
-                        value += cell.evaluate(excel);
-                    }
+                String[] pos = str.split(":");
+                int[] start = getPos(pos[0]);
+                int[] end = getPos(pos[1]);
+                for (int r = start[0]; r <= end[0]; r++) {
+                    for (int c = start[1]; c <= end[1]; c++) 
+                        addFormulaCell(r, c);
                 }
             }
-
-            return value;
         }
     }
+    
+    public int getValue() {
+        if (formula.isEmpty())
+            return val;
+        int sum = 0;
+        for (Cell cell : formula.keySet())
+            sum += cell.getValue() * formula.get(cell);
+        return sum;
+    }
+    
+    private int[] getPos(String str) {
+        int[] pos = new int[2];
+        pos[1] = str.charAt(0) - 'A';
+        pos[0] = Integer.parseInt(str.substring(1));
+        return pos;
+    }
+    
+    private void addFormulaCell(int r, int c) {
+        if (table[r][c] == null)
+            table[r][c] = new Cell(0);
+        Cell cell = table[r][c];
+        formula.put(cell, (formula.containsKey(cell) ? formula.get(cell) : 0) + 1);
+    }
 }
+}
+
+
 
 /**
  * Your Excel object will be instantiated and called as such:
