0ace02b3d54ee49d9d076c6f93f6664bcfe32c88
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 17:37:10 -0400

    Time: 0 ms (100.00%), Space: 43.6 MB (8.05%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.java
+++ 0427-construct-quad-tree/0427-construct-quad-tree.java
@@ -39,31 +39,30 @@ class Node {
 */
 
 class Solution {
-    private Node solve(int[][] grid, int x1, int y1, int length) {
-        // Return a leaf node if the matrix size is one.
-        if (length == 1) {
-            return new Node(grid[x1][y1] == 1, true);
-        }
-
-        // Recursive calls to the four sub-matrices.
-        Node topLeft = solve(grid, x1, y1, length / 2);
-        Node topRight = solve(grid, x1, y1 + length / 2, length / 2);
-        Node bottomLeft = solve(grid, x1 + length / 2, y1, length / 2);
-        Node bottomRight = solve(grid, x1 + length / 2, y1 + length / 2, length / 2);
-
-        // If the four returned nodes are leaf and have the same values
-        // Return a leaf node with the same value.
-        if (topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf
-                && topLeft.val == topRight.val && topRight.val == bottomLeft.val
-                && bottomLeft.val == bottomRight.val) {
-            return new Node(topLeft.val, true);
-        }
-
-        // If the four nodes aren't identical, return a non-leaf node with corresponding child pointers.
-        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
-    }
-
     public Node construct(int[][] grid) {
-        return solve(grid, 0, 0, grid.length);
+        return helper(grid, 0, 0, grid.length);
+    }
+    
+    private Node helper(int[][] grid, int x1, int y1, int length) {
+        if (same(grid, x1, y1, length))
+            return new Node(grid[x1][y1] == 1,  true);
+        else {
+            Node root = new Node(false, false);
+            root.topLeft = helper(grid, x1, y1, length / 2);
+            root.topRight = helper(grid, x1, y1 + length / 2, length / 2);
+            root.bottomLeft = helper(grid, x1 + length / 2, y1, length / 2);
+            root.bottomRight = helper(grid, x1 + length / 2, y1 + length / 2, length / 2);
+            return root;
+        }
+    }
+    
+    private boolean same(int[][] grid, int x1, int y1, int length) {
+        for (int i = x1; i < x1 + length; i++) {
+            for (int j = y1; j < y1 + length; j++) {
+                if (grid[i][j] != grid[x1][y1])
+                    return false;
+            }
+        }
+        return true;
     }
 }
