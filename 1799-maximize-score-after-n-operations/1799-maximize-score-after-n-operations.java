6ba0843ff8426d234c317a803c1d32919ba66510
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 16:31:49 -0400

    Time: 1118 ms (9.46%), Space: 54.8 MB (5.55%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.java
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.java
@@ -1,35 +1,43 @@
 class Solution {
+    Map<String, Integer> dp = new HashMap<>();
+    int len;
     public int maxScore(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> gcdVal = new HashMap<>();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                gcdVal.put((1 << i) + (1 << j), gcd(nums[i], nums[j]));
-            }
-        }
-        
-        int[] dp = new int[1 << n];
-        
-        for (int i = 0; i < (1 << n); ++i) {
-            int bits = Integer.bitCount(i); // how many numbers are used
-            if (bits % 2 != 0) // odd numbers, skip it
-                continue;
-            for (int k : gcdVal.keySet()) {
-                if ((k & i) != 0) // overlapping used numbers
-                    continue;
-                dp[i ^ k] = Math.max(dp[i ^ k], dp[i] + gcdVal.get(k) * (bits / 2 + 1));
-            }
-        }
-        
-        return dp[(1 << n) - 1];
+        len = nums.length;
+        char[] mask = new char[len];
+        for (int i = 0; i < len; i++) mask[i] = '0';
+        return dfs(mask, 0, nums);
     }
-    
-    public int gcd(int a, int b) {
-        if (b == 0)   
-            return a;     
-        return gcd(b, a % b);   
+
+    private int dfs(char[] mask, int multi, int[] nums) {
+        String key = new String(mask);
+        if (dp.containsKey(key)) return dp.get(key);
+        if (multi * 2 == len) return 0; // you picked all nums;
+        int res = 0;
+        List<Integer> unVisited = getZeros(mask);
+        for (int i = 0; i < unVisited.size() - 1; i++) {
+            for (int j = i + 1; j < unVisited.size(); j++) {
+                int a = unVisited.get(i), b = unVisited.get(j);
+                char[] newMask = mask.clone();
+                newMask[a] = '1';
+                newMask[b] = '1';
+                res = Math.max(res, dfs(newMask, multi + 1, nums) + gcd(nums[a], nums[b]) * (multi + 1));
+            }
+        }
+        dp.put(key, res);
+        return res;
+    }
+
+    private List<Integer> getZeros(char[] msk) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            if (msk[i] == '0') list.add(i);
+        }
+        return list;
+    }
+
+    private int gcd(int a, int b) {
+        if (a == b) return a;
+        if (a > b) return gcd(b, a - b);
+        return gcd(a, b - a);
     }
 }
-
-// Time: O(2^n * n^2)
-// Space: O(2 ^ n)
