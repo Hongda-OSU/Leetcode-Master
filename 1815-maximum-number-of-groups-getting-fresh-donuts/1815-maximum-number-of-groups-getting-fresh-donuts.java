f204c9c38aee0eda8a1b85e7e581c1c93bed30a0
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 16:50:47 -0400

    Time: 3 ms (100.00%), Space: 41.3 MB (94.44%) - LeetHub

--- 1815-maximum-number-of-groups-getting-fresh-donuts/1815-maximum-number-of-groups-getting-fresh-donuts.java
+++ 1815-maximum-number-of-groups-getting-fresh-donuts/1815-maximum-number-of-groups-getting-fresh-donuts.java
@@ -1,85 +1,65 @@
 class Solution {
-    
-    HashMap<Long, Integer> map = new HashMap<>();
-    
-    class State {
-        long mask = 0;
-        
-        State(long v) {
-            mask = v;
-        }
-        
-        public boolean isEmpty(int i) {
-            return ((mask >> (i*5)) & 31L) == 0;
-        }
-        
-        public void add(int i, long v) {
-            mask += v << (i*5);
-        }
-        
-        public void minus1(int i) {
-            mask -= 1L << (i*5);
-        }
-    }
+    private HashMap<Long, Integer> map = new HashMap<>();
     
     public int maxHappyGroups(int batchSize, int[] groups) {
-        int[] cnt = new int[batchSize];
-        for (int g : groups) {
-            cnt[g % batchSize]++;
-        }
-        
-        // optimization
-        // try to combine groups to pairs
+        int[] count = new int[batchSize];
+        for (int group : groups) 
+            count[group % batchSize]++;
         for (int i = 1; i < batchSize; i++) {
-            if (cnt[i] == 0) {
+            if (count[i] == 0)
                 continue;
-            }
-            
             int search = batchSize - i;
-            
             if (search == i) {
-                cnt[0] += cnt[i] / 2;
-                cnt[i] = cnt[i] % 2;
+                count[0] += count[i] / 2;
+                count[i] = count[i] % 2;
                 continue;
             }
-            
-            int min = Math.min(cnt[i], cnt[search]);
-            cnt[0] += min;
-            cnt[i] -= min;
-            cnt[search] -= min;
+            int min = Math.min(count[i], count[search]);
+            count[0] += min;
+            count[i] -= min;
+            count[search] -= min;
         }
-        
         State state = new State(0);
-        for (int i = 1; i < batchSize; i++) {
-            state.add(i, cnt[i]);
-        }
-        
-        return dfs(0, batchSize, state) + cnt[0];
+        for (int i = 1; i < batchSize; i++) 
+            state.add(i, count[i]);
+        return dfs(0, batchSize, state) + count[0];
     }
     
-    public int dfs(int rem, int batchSize, State state) {
-        if (state.mask == 0) {
+    private int dfs(int num, int batchSize, State state) {
+        if (state.mask == 0)
             return 0;
-        }
-        if (map.containsKey(state.mask)) {
+        if (map.containsKey(state.mask))
             return map.get(state.mask);
-        }
-        
         int max = 0;
-        
         for (int i = 1; i < batchSize; i++) {
-            if (state.isEmpty(i)) {
+            if (state.isEmpty(i))
                 continue;
-            }
             State newState = new State(state.mask);
-            newState.minus1(i);
-            max = Math.max(max, dfs((batchSize-i+rem)%batchSize, batchSize, newState));
+            newState.minus(i);
+            max = Math.max(max, dfs((batchSize - i + num) % batchSize, batchSize, newState));
         }
-        
-        max += (rem == 0) ? 1 : 0;
-        
+        max += (num == 0) ? 1 : 0;
         map.put(state.mask, max);
-        
         return max;
+    }
+}
+
+class State {
+    public long mask;
+    
+    public State(long mask) {
+        this.mask = mask;
+    }
+    
+    public boolean isEmpty(int i) {
+        return ((mask >> (i * 5)) & 31L) == 0;
+    } 
+    
+    public void add(int i, long v) {
+        mask += v << (i * 5);
+    }
+    
+    public void minus(int i) {
+        mask -= 1L << (i * 5);
     }
 }
