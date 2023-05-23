d527067690d009090214af4bc9e0416387a49bc1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 16:18:04 -0400

    Time: 8 ms (98.02%), Space: 43.3 MB (16.83%) - LeetHub

--- 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.java
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.java
@@ -1,26 +1,27 @@
 class Solution {
-     public int numberWays(List<List<Integer>> hats) {
-        int n = hats.size();
-        List<Integer>[] h2p = new List[41]; // h2p[i] indicates the list of people who can wear i_th hat
-        for (int i = 1; i <= 40; i++) h2p[i] = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            for (int hat : hats.get(i))
-                h2p[hat].add(i);
-        Integer[][] dp = new Integer[41][1024];
-        return dfs(h2p, (1 << n) - 1, 1, 0, dp);
-    }
-    // dfs(...hat, assignedPeople...) number of ways to assign up to `hat` different hats to n people keep in mind that `assignedPeople` is
-    //     the mask of list of people were assigned hat
-    int dfs(List<Integer>[] h2p, int allMask, int hat, int assignedPeople, Integer[][] dp) {
-        if (assignedPeople == allMask) return 1; // Check if assigned different hats to all people
-        if (hat > 40) return 0; // no more hats to process
-        if (dp[hat][assignedPeople] != null) return dp[hat][assignedPeople];
-        int ans = dfs(h2p, allMask, hat + 1, assignedPeople, dp); // Don't wear this hat
-        for (int p : h2p[hat]) {
-            if (((assignedPeople >> p) & 1) == 1) continue; // Skip if person `p` was assigned hat
-            ans += dfs(h2p, allMask, hat + 1, assignedPeople | (1 << p), dp); // Wear this hat for p_th person
-            ans %= 1_000_000_007;
+    public int numberWays(List<List<Integer>> hats) {
+        int n = hats.size(), sCnt = 2<<(n-1);
+        int[] dp = new int[sCnt];
+        dp[0] = 1;
+        List<Integer>[] h2p= new List[40];
+        Arrays.setAll(h2p, d -> new ArrayList<>());
+        for(int i = 0; i < n; ++i){
+            for(int h: hats.get(i)){
+                h2p[h-1].add(i);
+            }
         }
-        return dp[hat][assignedPeople] = ans;
+
+        for(int i = 0; i < 40; ++i){
+            int[] newDp = Arrays.copyOf(dp, sCnt);
+            for(int p: h2p[i]){
+                for(int j =0; j < sCnt; ++j){
+                    if(((1<<p) & j) == 0){
+                        newDp[j + (1<<p)] = (newDp[j + (1<<p)]+dp[j]) % 1000000007;
+                    }
+                }   
+            }
+            dp = newDp;
+        }
+        return dp[sCnt-1];
     }
 }
