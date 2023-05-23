f92e6726d420a93fc75efd3b2e2c46c3e23dc5c0
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 17:10:11 -0400

    Time: 0 ms (100.00%), Space: 39 MB (95.86%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,46 +1,24 @@
-class Solution {
+public class Solution {
+    int count = 0;
     public int totalNQueens(int n) {
-        char board[][] = new char[n][n];
-        for(char i[] : board)
-            Arrays.fill(i, '.');
-        return dfs(0, board);
-    }
-    public int dfs(int col, char board[][]){
-        if(col == board.length) return 1;
-        int count = 0;
-        for(int row = 0; row < board.length; row++){
-            if(isSafe(board, row, col)){
-                board[row][col] = 'Q';
-                count += dfs(col + 1, board);
-                board[row][col] = '.';
-            }
-        }
+        boolean[] cols = new boolean[n];     // columns   |
+        boolean[] d1 = new boolean[2 * n];   // diagonals \
+        boolean[] d2 = new boolean[2 * n];   // diagonals /
+        backtracking(0, cols, d1, d2, n);
         return count;
     }
-    public boolean isSafe(char board[][], int row, int col){
-        int dupRow = row;
-        int dupCol = col;
-        
-        while(row >= 0 && col >= 0){
-            if(board[row][col] == 'Q') return false;
-            row--;
-            col--;
+    
+    public void backtracking(int row, boolean[] cols, boolean[] d1, boolean []d2, int n) {
+        if(row == n) count++;
+
+        for(int col = 0; col < n; col++) {
+            int id1 = col - row + n;
+            int id2 = col + row;
+            if(cols[col] || d1[id1] || d2[id2]) continue;
+            
+            cols[col] = true; d1[id1] = true; d2[id2] = true;
+            backtracking(row + 1, cols, d1, d2, n);
+            cols[col] = false; d1[id1] = false; d2[id2] = false;
         }
-        
-        row = dupRow;
-        col = dupCol;
-        while(col >= 0){
-            if(board[row][col] == 'Q') return false;
-            col--;
-        }
-        
-        row = dupRow;
-        col = dupCol;
-        while(col >= 0 && row < board.length){
-            if(board[row][col] == 'Q') return false;
-            row++;
-            col--;
-        }
-        return true;
     }
 }
