88f20425cea88cea8e03eace9f98c92cbb929311
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 16:32:56 -0400

    Time: 213 ms (34.32%), Space: 44.3 MB (13.64%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.java
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.java
@@ -1,43 +1,18 @@
 class Solution {
-    Map<String, Integer> dp = new HashMap<>();
-    int len;
-    public int maxScore(int[] nums) {
-        len = nums.length;
-        char[] mask = new char[len];
-        for (int i = 0; i < len; i++) mask[i] = '0';
-        return dfs(mask, 0, nums);
-    }
-
-    private int dfs(char[] mask, int multi, int[] nums) {
-        String key = new String(mask);
-        if (dp.containsKey(key)) return dp.get(key);
-        if (multi * 2 == len) return 0; // you picked all nums;
-        int res = 0;
-        List<Integer> unVisited = getZeros(mask);
-        for (int i = 0; i < unVisited.size() - 1; i++) {
-            for (int j = i + 1; j < unVisited.size(); j++) {
-                int a = unVisited.get(i), b = unVisited.get(j);
-                char[] newMask = mask.clone();
-                newMask[a] = '1';
-                newMask[b] = '1';
-                res = Math.max(res, dfs(newMask, multi + 1, nums) + gcd(nums[a], nums[b]) * (multi + 1));
+   int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int dfs(int[] n, int[][] dp, int i, int mask) {
+    if (i > n.length / 2)
+        return 0;
+    if (dp[i][mask] == 0)
+        for (int j = 0; j < n.length; ++j)
+            for (int k = j + 1; k < n.length; ++k) {
+                int new_mask = (1 << j) + (1 << k);
+                if ((mask & new_mask) == 0)
+                    dp[i][mask] = Math.max(dp[i][mask], i * gcd(n[j], n[k]) + dfs(n, dp, i + 1, mask + new_mask));
             }
-        }
-        dp.put(key, res);
-        return res;
-    }
-
-    private List<Integer> getZeros(char[] msk) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < len; i++) {
-            if (msk[i] == '0') list.add(i);
-        }
-        return list;
-    }
-
-    private int gcd(int a, int b) {
-        if (a == b) return a;
-        if (a > b) return gcd(b, a - b);
-        return gcd(a, b - a);
-    }
+    return dp[i][mask];
+}
+int maxScore(int[] n) {
+    return dfs(n, new int[n.length / 2 + 1][1 << n.length], 1, 0);
+}
 }
