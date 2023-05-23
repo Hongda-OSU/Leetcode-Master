ab0ca04b0278c99a7f3db5f3636ecf5bed1aad2e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 15:26:31 -0400

    Time: 29 ms (100.00%), Space: 44.9 MB (6.60%) - LeetHub

--- 0635-design-log-storage-system/0635-design-log-storage-system.java
+++ 0635-design-log-storage-system/0635-design-log-storage-system.java
@@ -1,44 +1,33 @@
-public class LogSystem {
-    Map<Integer,String> map=new HashMap<>();
+class LogSystem {
+    private Map<Integer, String> map;
+
     public LogSystem() {
-        
+        map = new HashMap<>();
     }
     
     public void put(int id, String timestamp) {
-        map.put(id,timestamp);
+        map.put(id, timestamp);      
     }
     
-    public List<Integer> retrieve(String s, String e, String gra) {
-        int x=0;
-        switch (gra){
-            case "Year":
-                x=4; 
-                break;
-            case "Month":
-                x=7;
-                break;
-            case "Day":
-                x=10;
-                break;
-            case "Hour":
-                x=13;
-                break;
-            case "Minute":
-                x=16;
-                break;
-            case "Second":
-                x=19;
-                break;
+    public List<Integer> retrieve(String start, String end, String granularity) {
+        int x = 0;
+        switch (granularity) {
+            case "Year" -> x = 4;
+            case "Month" -> x = 7;
+            case "Day" -> x = 10;
+            case "Hour" -> x = 13;
+            case "Minute" -> x = 16;
+            case "Second" -> x = 19;
         }
-        s=s.substring(0,x);
-        e=e.substring(0,x);
-        List<Integer> ans=new ArrayList<>();
-        for (Integer i:map.keySet())
-        {
-            String ss=map.get(i).substring(0,x);
-            if (ss.compareTo(s)>=0 && ss.compareTo(e)<=0) ans.add(i);
+        start = start.substring(0, x);
+        end = end.substring(0, x);
+        List<Integer> result = new ArrayList<>();
+        for (Integer i : map.keySet()) {
+            String str = map.get(i).substring(0, x);
+            if (str.compareTo(start) >= 0 && str.compareTo(end) <= 0)
+                result.add(i);
         }
-        return ans;
+        return result;
     }
 }
 
