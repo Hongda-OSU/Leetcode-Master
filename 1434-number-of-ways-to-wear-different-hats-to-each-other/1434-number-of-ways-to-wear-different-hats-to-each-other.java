d7d3a60bf0dae2c745e6d04a65b0efcb31d35a74
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 16:22:26 -0400

    Time: 8 ms (98.02%), Space: 43 MB (36.63%) - LeetHub

--- 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.java
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.java
@@ -1,27 +1,25 @@
 class Solution {
     public int numberWays(List<List<Integer>> hats) {
-        int n = hats.size(), sCnt = 2<<(n-1);
-        int[] dp = new int[sCnt];
+        int mod = 1_000_000_007;
+        int n = hats.size(), count = 2 << (n - 1);
+        int[] dp = new int[count];
         dp[0] = 1;
-        List<Integer>[] h2p= new List[40];
+        List<Integer>[] h2p = new List[40];
         Arrays.setAll(h2p, d -> new ArrayList<>());
-        for(int i = 0; i < n; ++i){
-            for(int h: hats.get(i)){
-                h2p[h-1].add(i);
-            }
+        for (int i = 0; i < n; i++) {
+            for (int h : hats.get(i)) 
+                h2p[h - 1].add(i);
         }
-
-        for(int i = 0; i < 40; ++i){
-            int[] newDp = Arrays.copyOf(dp, sCnt);
-            for(int p: h2p[i]){
-                for(int j =0; j < sCnt; ++j){
-                    if(((1<<p) & j) == 0){
-                        newDp[j + (1<<p)] = (newDp[j + (1<<p)]+dp[j]) % 1000000007;
-                    }
-                }   
+        for (int i = 0; i < 40; i++) {
+            int[] temp = Arrays.copyOf(dp, count);
+            for (int p : h2p[i]) {
+                for (int j = 0; j < count; j++) {
+                    if (((1 << p) & j) == 0) 
+                        temp[j + (1 << p)] = (temp[j + (1 << p)] + dp[j]) % mod;
+                }
             }
-            dp = newDp;
+            dp = temp;
         }
-        return dp[sCnt-1];
+        return dp[count - 1];
     }
 }
