055b3f93d20533582e27d21ebaf33767c504de81
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-23 15:39:26 -0400

    Time: 7 ms (90.32%), Space: 41.4 MB (98.92%) - LeetHub

--- 0631-design-excel-sum-formula/0631-design-excel-sum-formula.java
+++ 0631-design-excel-sum-formula/0631-design-excel-sum-formula.java
@@ -1,81 +1,88 @@
 public class Excel {
-    Formula[][] Formulas;
-    class Formula {
-        Formula(HashMap < String, Integer > c, int v) {
-            val = v;
-            cells = c;
-        }
-        HashMap < String, Integer > cells;
-        int val;
-    }
-    Stack < int[] > stack = new Stack < > ();
+    Cell[][] table;
+
     public Excel(int H, char W) {
-        Formulas = new Formula[H][(W - 'A') + 1];
+        table = new Cell[H+1][W-'A'+1];
     }
-
-    public int get(int r, char c) {
-        if (Formulas[r - 1][c - 'A'] == null)
-            return 0;
-        return Formulas[r - 1][c - 'A'].val;
-    }
+    
     public void set(int r, char c, int v) {
-        Formulas[r - 1][c - 'A'] = new Formula(new HashMap < String, Integer > (), v);
-        topologicalSort(r - 1, c - 'A');
-        execute_stack();
+        if(table[r][c-'A'] == null) table[r][c-'A'] = new Cell (v); 
+        else table[r][c-'A'].setValue(v); 
     }
-
+    
+    public int get(int r, char c) {
+        if( table[r][c-'A'] == null) return 0;
+        else return table[r][c-'A'].getValue();  
+    }
+    
     public int sum(int r, char c, String[] strs) {
-        HashMap < String, Integer > cells = convert(strs);
-        int summ = calculate_sum(r - 1, c - 'A', cells);
-        set(r, c, summ);
-        Formulas[r - 1][c - 'A'] = new Formula(cells, summ);
-        return summ;
-    }
-
-    public void topologicalSort(int r, int c) {
-        for (int i = 0; i < Formulas.length; i++)
-            for (int j = 0; j < Formulas[0].length; j++)
-                if (Formulas[i][j] != null && Formulas[i][j].cells.containsKey("" + (char)('A' + c) + (r + 1))) {
-                    topologicalSort(i, j);
-                }
-        stack.push(new int[] {r,c});
-    }
-
-    public void execute_stack() {
-        while (!stack.isEmpty()) {
-            int[] top = stack.pop();
-            if (Formulas[top[0]][top[1]].cells.size() > 0)
-                calculate_sum(top[0], top[1], Formulas[top[0]][top[1]].cells);
+        if (table[r][c-'A'] == null) {
+            table[r][c-'A'] = new Cell(strs);
+        } else {
+            table[r][c-'A'].setFormula(strs);
         }
+        
+        return table[r][c-'A'].getValue();
     }
-
-    public HashMap < String, Integer > convert(String[] strs) {
-        HashMap < String, Integer > res = new HashMap < > ();
-        for (String st: strs) {
-            if (st.indexOf(":") < 0)
-                res.put(st, res.getOrDefault(st, 0) + 1);
-            else {
-                String[] cells = st.split(":");
-                int si = Integer.parseInt(cells[0].substring(1)), ei = Integer.parseInt(cells[1].substring(1));
-                char sj = cells[0].charAt(0), ej = cells[1].charAt(0);
-                for (int i = si; i <= ei; i++) {
-                    for (char j = sj; j <= ej; j++) {
-                        res.put("" + j + i, res.getOrDefault("" + j + i, 0) + 1);
+    
+    
+    private class Cell{
+        int val=0;
+        HashMap<Cell, Integer> formula=new HashMap<>();//one cell might appear multiple times
+        
+        public Cell(int val){
+            setValue(val); 
+        }
+        public Cell(String[] formulaStr){
+            setFormula(formulaStr);
+        }
+        
+        public void setValue(int val) {           
+            formula.clear(); //will not be treated as a formula cell anymore        
+            this.val = val;
+        }
+        
+        public void setFormula(String[] formulaStr){
+            formula.clear();            
+            for(String str : formulaStr){
+                if (str.indexOf(":")<0){
+                    int[] pos = getPos(str);
+                    addFormulaCell(pos[0], pos[1]);
+                } else {
+                    String[] pos = str.split(":");
+                    int[] startPos = getPos(pos[0]);
+                    int[] endPos = getPos(pos[1]);
+                    for(int r = startPos[0]; r<=endPos[0]; r++){
+                        for(int c = startPos[1]; c<=endPos[1]; c++){
+                            addFormulaCell(r, c);
+                        }
                     }
                 }
             }
         }
-        return res;
-    }
-
-    public int calculate_sum(int r, int c, HashMap < String, Integer > cells) {
-        int sum = 0;
-        for (String s: cells.keySet()) {
-            int x = Integer.parseInt(s.substring(1)) - 1, y = s.charAt(0) - 'A';
-            sum += (Formulas[x][y] != null ? Formulas[x][y].val : 0) * cells.get(s);
+        
+        private int[] getPos(String str){
+            int[] pos = new int[2];
+            pos[1]=str.charAt(0)-'A';
+            pos[0]=Integer.parseInt(str.substring(1));
+            return pos;
         }
-        Formulas[r][c] = new Formula(cells, sum);
-        return sum;
+        
+        private void addFormulaCell(int r, int c){
+            if(table[r][c] == null) table[r][c] = new Cell(0);
+            Cell rangeCell = table[r][c];                            
+            formula.put(rangeCell, (formula.containsKey(rangeCell)? formula.get(rangeCell) : 0)+1);
+        }
+        
+        //recursive method
+        private int getValue(){
+            if(this.formula.isEmpty()) return this.val;
+            int sum = 0;
+            for(Cell cell : formula.keySet()){
+                sum+=cell.getValue()*formula.get(cell);
+            }
+            return sum;
+        }
     }
 }
 
