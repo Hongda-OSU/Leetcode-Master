d64ad6e9ab08c9642a15754bf5e0f6fcaa4d5c69
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-24 14:01:09 -0400

    Time: 99 ms (75.71%), Space: 72.6 MB (71.66%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,51 +1,46 @@
 class NumArray {
-    int[] fenwick;
-    
-    public NumArray(int[] nums) {
-        fenwick = new int[nums.length+1];
-        
-        for(int i = 1; i < fenwick.length; i++)
-            fenwick[i] = nums[i-1];
-        for(int i = 1; i < fenwick.length; i++){
-            int j = i + LSB(i);
-            if(j < fenwick.length)
-                fenwick[j] += fenwick[i];
-        }
-        
-    }
-    
-    public void update(int i, int val) {
-        int value = sumRange(i,i);
-        add(++i,val - value);
-    }
-    
-    private void add(int i, int val){
-        while(i < fenwick.length){
-            fenwick[i] += val;
-            i += LSB(i);
-        }
-    }
-    
-    public int sumRange(int i, int j) {
-        i++;
-        j++;
-        return preSum(j) - preSum(i-1);
-    }
-    
-    private int preSum(int i){
-        int sum = 0;
-        while(i != 0){
-            sum += fenwick[i];
-            i -= LSB(i);
-        }
-        
-        return sum;
-    }
-    
-    int LSB(int i){
-        return -i & i;
-    }
+   int[] nums;
+	int[] BIT;
+	int n;
+
+	public NumArray(int[] nums) {
+		this.nums = nums;
+
+		n = nums.length;
+		BIT = new int[n + 1];
+		for (int i = 0; i < n; i++)
+			init(i, nums[i]);
+	}
+
+	public void init(int i, int val) {
+		i++;
+		while (i <= n) {
+			BIT[i] += val;
+			i += (i & -i);
+		}
+	}
+
+	void update(int i, int val) {
+		int diff = val - nums[i];
+		nums[i] = val;
+		init(i, diff);
+	}
+
+	public int getSum(int i) {
+		int sum = 0;
+		i++;
+		while (i > 0) {
+			sum += BIT[i];
+			i -= (i & -i);
+		}
+		return sum;
+	}
+
+	public int sumRange(int i, int j) {
+		return getSum(j) - getSum(i - 1);
+	}
 }
+
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
