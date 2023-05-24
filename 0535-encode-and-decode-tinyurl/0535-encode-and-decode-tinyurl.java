3ab59ee97eaddf0e64b93a0bc8c88fa1386a7705
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-24 14:12:01 -0400

    Time: 3 ms (52.17%), Space: 42 MB (97.04%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,25 +1,19 @@
 public class Codec {
-    Map<String, String> codeDB = new HashMap<>(), urlDB = new HashMap<>();
-    static final String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-    private String getCode() {
-        char[] code = new char[6];
-        for (int i = 0; i < 6; i++) 
-            code[i] = chars.charAt((int)Math.random() * 62);
-        return "http://tinyurl.com/" + String.valueOf(code);
-    }
-    
+    Map<String, String> map = new HashMap<>();
+    StringBuilder sb = new StringBuilder();
+    // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        if (urlDB.containsKey(longUrl)) return urlDB.get(longUrl);
-        String code = getCode();
-        while (codeDB.containsKey(code)) code = getCode();
-        codeDB.put(code, longUrl);
-        urlDB.put(longUrl, code);
-        return code;
+        sb.append((char)Math.floor(Math.random() * 100));
+        while(map.containsKey(sb.toString())){
+            sb.append((char)Math.floor(Math.random() * 100));
+        }
+        map.put(sb.toString(), longUrl);
+        return sb.toString();
     }
 
+    // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return codeDB.get(shortUrl);
+        return map.get(shortUrl);
     }
 }
 
