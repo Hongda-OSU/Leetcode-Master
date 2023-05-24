263b268a0b311d4f2857b8c8cbaa30d633f8b026
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-24 14:00:32 -0400

    Time: 99 ms (75.71%), Space: 72.3 MB (75.41%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,56 +1,50 @@
 class NumArray {
-  static class BinaryIndexedTree {
-    int[] nums;
-    int[] BIT;
-    int n;
-
-    public BinaryIndexedTree(int[] nums) {
-      this.nums = nums;
-      this.n = nums.length;
-      BIT = new int[n + 1];
-      for (int i = 0; i < n; i++) {
-        init(i, nums[i]);
-      }
+    int[] fenwick;
+    
+    public NumArray(int[] nums) {
+        fenwick = new int[nums.length+1];
+        
+        for(int i = 1; i < fenwick.length; i++)
+            fenwick[i] = nums[i-1];
+        for(int i = 1; i < fenwick.length; i++){
+            int j = i + LSB(i);
+            if(j < fenwick.length)
+                fenwick[j] += fenwick[i];
+        }
+        
     }
-
-    void init(int i, int val) {
-      i++;
-      while (i <= n) {
-        BIT[i] += val;
-        i += (i & -i);
-      }
+    
+    public void update(int i, int val) {
+        int value = sumRange(i,i);
+        add(++i,val - value);
     }
-
-    void update(int i, int val) {
-      int diff = val - nums[i];
-      nums[i] = val;
-      init(i, diff);
+    
+    private void add(int i, int val){
+        while(i < fenwick.length){
+            fenwick[i] += val;
+            i += LSB(i);
+        }
     }
-
-    int getSum(int i) {
-      i++;
-      int ret = 0;
-      while (i > 0) {
-        ret += BIT[i];
-        i -= (i & -i);
-      }
-      return ret;
+    
+    public int sumRange(int i, int j) {
+        i++;
+        j++;
+        return preSum(j) - preSum(i-1);
     }
-  }
-
-  BinaryIndexedTree binaryIndexedTree;
-
-  public NumArray(int[] nums) {
-    binaryIndexedTree = new BinaryIndexedTree(nums);
-  }
-
-  public void update(int index, int val) {
-    binaryIndexedTree.update(index, val);
-  }
-
-  public int sumRange(int left, int right) {
-    return binaryIndexedTree.getSum(right) - binaryIndexedTree.getSum(left - 1);
-  }
+    
+    private int preSum(int i){
+        int sum = 0;
+        while(i != 0){
+            sum += fenwick[i];
+            i -= LSB(i);
+        }
+        
+        return sum;
+    }
+    
+    int LSB(int i){
+        return -i & i;
+    }
 }
 /**
  * Your NumArray object will be instantiated and called as such:
