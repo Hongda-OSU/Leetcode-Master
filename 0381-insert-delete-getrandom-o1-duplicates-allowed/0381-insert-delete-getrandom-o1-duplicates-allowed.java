df3a8b2d4b510ede1467bf3909d4dfacd926d1d3
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-25 14:57:04 -0400

    Time: 35 ms (98.15%), Space: 87.8 MB (36.24%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.java
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.java
@@ -1,83 +1,58 @@
-
 class RandomizedCollection {
+    private Map<Integer, Queue<Pair>> map;
+    private List<Pair> list;
+    private Random rand;
+    private int size;
 
-    class Pair {
-        int val;
-        int pos;
-        public Pair (int val, int pos) {
-            this.val = val;
-            this.pos = pos;
-        }
-    }
-    
-    Map<Integer, Queue<Pair>> map;
-    List<Pair> lists;
-    Random rand;
-    int size;
-    
-    /** Initialize your data structure here. */
     public RandomizedCollection() {
-        this.map = new HashMap<>();
-        this.lists = new ArrayList<>();
-        this.size = 0;
-        this.rand = new Random();
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
+        size = 0;
     }
     
-    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     public boolean insert(int val) {
-        if(!map.containsKey(val)) {
+        if (!map.containsKey(val)) 
             map.put(val, new LinkedList<>());
-        }
-        
-        Pair p = new Pair(val, this.size);
-        
-        if(lists.size() <= this.size) {
-            lists.add(p);
-        } else {
-            lists.set(size, p);
-        }
-        this.size++;
+        Pair p = new Pair(val, size);
+        if (list.size() <= size) 
+            list.add(p);
+        else 
+            list.set(size, p);
+        size++;
         map.get(val).add(p);
-        
         return map.get(val).size() == 1;
     }
     
-    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     public boolean remove(int val) {
-        if(! map.containsKey(val)) {
+        if (!map.containsKey(val))
             return false;
-        }
-        
-        Pair toRemove = this.map.get(val).poll();
-        
-        if (this.map.get(val).size() == 0) {
+        Pair remove = map.get(val).poll();
+        if (map.get(val).size() == 0)
             map.remove(val);
-        }
-        
-        Pair swapped = this.lists.get(this.size-1);
-        swapped.pos = toRemove.pos;
-        lists.set(toRemove.pos, swapped);
-        this.size--;
+        Pair swap = list.get(size - 1);
+        swap.pos = remove.pos;
+        list.set(remove.pos, swap);
+        size--;
         return true;
-        
     }
     
-    /** Get a random element from the collection. */
     public int getRandom() {
-        if(size == 0) return -1;
+        if (size == 0)
+            return -1;
         int pos = rand.nextInt(size);
-        return lists.get(pos).val;        
+        return list.get(pos).val;
     }
 }
 
-
-/**
- * Your RandomizedCollection object will be instantiated and called as such:
- * RandomizedCollection obj = new RandomizedCollection();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
+class Pair {
+    public int val, pos;
+    
+    public Pair(int val, int pos) {
+        this.val = val;
+        this.pos = pos;
+    }
+}
 
 /**
  * Your RandomizedCollection object will be instantiated and called as such:
