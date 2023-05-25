71930817ef015e969cacc5cc00ff70fd6d7a89d0
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-25 14:48:55 -0400

    Time: 36 ms (97.88%), Space: 87.6 MB (36.24%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.java
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.java
@@ -1,58 +1,83 @@
+
 class RandomizedCollection {
 
-    List<Integer> nums;
-    Map<Integer, Set<Integer>> idxMap;
-    Random random;
-
-    public RandomizedCollection() {
-        nums = new ArrayList<>();
-        idxMap = new HashMap<>();
-        random = new Random();
-    }
-
-    public boolean insert(int val) {
-        boolean response = !idxMap.containsKey(val);
-
-        if (response) {
-            idxMap.put(val, new HashSet<>());
+    class Pair {
+        int val;
+        int pos;
+        public Pair (int val, int pos) {
+            this.val = val;
+            this.pos = pos;
         }
-        idxMap.get(val).add(nums.size());
-        nums.add(val);
-
-        return response;
     }
-
+    
+    Map<Integer, Queue<Pair>> map;
+    List<Pair> lists;
+    Random rand;
+    int size;
+    
+    /** Initialize your data structure here. */
+    public RandomizedCollection() {
+        this.map = new HashMap<>();
+        this.lists = new ArrayList<>();
+        this.size = 0;
+        this.rand = new Random();
+    }
+    
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(!map.containsKey(val)) {
+            map.put(val, new LinkedList<>());
+        }
+        
+        Pair p = new Pair(val, this.size);
+        
+        if(lists.size() <= this.size) {
+            lists.add(p);
+        } else {
+            lists.set(size, p);
+        }
+        this.size++;
+        map.get(val).add(p);
+        
+        return map.get(val).size() == 1;
+    }
+    
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     public boolean remove(int val) {
-        if (!idxMap.containsKey(val)) {
+        if(! map.containsKey(val)) {
             return false;
         }
-
-        Set<Integer> idxSet = idxMap.get(val);
-        int idxToBeRemoved = idxSet.iterator().next();
-        if (idxSet.size() == 1) {
-            idxMap.remove(val);
-        } else {
-            idxSet.remove(idxToBeRemoved);
+        
+        Pair toRemove = this.map.get(val).poll();
+        
+        if (this.map.get(val).size() == 0) {
+            map.remove(val);
         }
-
-        int lastIdx = nums.size() - 1;
-        if (idxToBeRemoved != lastIdx) {
-            int lastVal = nums.get(lastIdx);
-            Set<Integer> lastIdxSet = idxMap.get(lastVal);
-            lastIdxSet.add(idxToBeRemoved);
-            lastIdxSet.remove(lastIdx);
-            nums.set(idxToBeRemoved, lastVal);
-        }
-
-        nums.remove(lastIdx);
-
+        
+        Pair swapped = this.lists.get(this.size-1);
+        swapped.pos = toRemove.pos;
+        lists.set(toRemove.pos, swapped);
+        this.size--;
         return true;
+        
     }
-
+    
+    /** Get a random element from the collection. */
     public int getRandom() {
-        return nums.get(random.nextInt(nums.size()));
+        if(size == 0) return -1;
+        int pos = rand.nextInt(size);
+        return lists.get(pos).val;        
     }
 }
+
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
 
 /**
  * Your RandomizedCollection object will be instantiated and called as such:
