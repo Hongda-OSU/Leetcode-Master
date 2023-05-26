0b9f918573bc50b1f1382f29af705a6e30932a06
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-26 14:56:37 -0400

    Time: 102 ms (67.34%), Space: 54.3 MB (95.75%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,29 +1,49 @@
 class UndergroundSystem {
-    Map<Integer, Pair<String, Integer>> checkInMap;
-    Map<String, Pair<Double, Integer>> routeMap;
-
+    private Map<String, Pair<Double, Double>> journeyData = new HashMap<>();
+    private Map<Integer, Pair<String, Integer>> checkInData = new HashMap<>();
+    
     public UndergroundSystem() {
-        checkInMap = new HashMap<>();
-        routeMap = new HashMap<>();
     }
     
     public void checkIn(int id, String stationName, int t) {
-        checkInMap.put(id, new Pair<>(stationName, t));
+        checkInData.put(id, new Pair<>(stationName, t));
     }
     
     public void checkOut(int id, String stationName, int t) {
-        Pair<String, Integer> checkIn = checkInMap.get(id);
-        checkInMap.remove(id);
-        String routeName = checkIn.getKey() + "_" + stationName;
-        int totalTime = t - checkIn.getValue();
-        Pair<Double, Integer> route = routeMap.getOrDefault(routeName, new Pair<>(0.0, 0));
-        routeMap.put(routeName, new Pair<>(route.getKey() + totalTime, route.getValue() + 1));
+        // Look up the check in station and check in time for this id.
+        // You could combine this "unpacking" into the other lines of code
+        // to have less lines of code overall, but we've chosen to be verbose
+        // here to make it easy for all learners to follow.
+        Pair<String, Integer> checkInDataForId = checkInData.get(id);
+        String startStation = checkInDataForId.getKey();
+        Integer checkInTime = checkInDataForId.getValue();
+        
+        // Lookup the current travel time data for this route.
+        String routeKey = stationsKey(startStation, stationName);
+        Pair<Double, Double> routeStats  = journeyData.getOrDefault(routeKey, new Pair<>(0.0, 0.0));
+        Double totalTripTime = routeStats.getKey();
+        Double totalTrips = routeStats.getValue();
+        
+        // Update the travel time data with this trip.
+        double tripTime = t - checkInTime;
+        journeyData.put(routeKey, new Pair<>(totalTripTime + tripTime, totalTrips + 1));
+        
+        // Remove check in data for this id.
+        // Note that this is optional, we'll talk about it in the space complexity analysis.
+        checkInData.remove(id);
     }
     
     public double getAverageTime(String startStation, String endStation) {
-        String routeName = startStation + "_" + endStation;
-        Pair<Double, Integer> trip = routeMap.get(routeName);
-        return trip.getKey() / trip.getValue();
+        // Lookup how many times this journey has been made, and the total time.
+        String routeKey = stationsKey(startStation, endStation);
+        Double totalTime = journeyData.get(routeKey).getKey();
+        Double totalTrips = journeyData.get(routeKey).getValue();
+        // The average is simply the total divided by the number of trips.
+        return totalTime / totalTrips;
+    }
+    
+    private String stationsKey(String startStation, String endStation) {
+        return startStation + "->" + endStation;
     }
 }
 
