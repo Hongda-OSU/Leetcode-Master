016da52c10f7799b2b91341ff54741994876ab54
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-26 15:41:27 -0400

    Time: 41 ms (99.66%), Space: 51.1 MB (34.48%) - LeetHub

--- 0715-range-module/0715-range-module.java
+++ 0715-range-module/0715-range-module.java
@@ -1,112 +1,104 @@
 class RangeModule {
+    private TreeNode root;
 
     public RangeModule() {
         
     }
-    TreeNode root;
     
-    class TreeNode
-    {
-        int start;
-        int end;
-        TreeNode right;
-        TreeNode left;
-        public TreeNode(int start,int end)
-        {
-            this.start=start;
-            this.end=end;
-        }
-    }
     public void addRange(int left, int right) {
-        root=addRange(root,left,right);
+        root = addRangeHelper(root, left, right);
     }
     
     public boolean queryRange(int left, int right) {
-        return queryRange(root,left,right);
+        return queryRangeHelper(root, left, right);
     }
     
     public void removeRange(int left, int right) {
-        root=removeRange(root,left,right);
+        root = removeRangeHelper(root, left, right);
     }
-    public TreeNode removeRange(TreeNode root,int start, int end) {
-       if(start>=end)
-       return root;
-       if(root==null)
-       return null;
-       if(start>=root.end)
-       root.right=removeRange(root.right,start,end);
-       else if(end<=root.start)
-        root.left=removeRange(root.left,start,end);
-        else
-        {
-        root.left=removeRange(root.left,start,root.start);
-        root.right=removeRange(root.right,root.end,end);
-        root.left=addRange(root.left,root.start,start);
-        root.left=addRange(root.left,end,root.end);
-        root=remove(root);
+    
+    private TreeNode addRangeHelper(TreeNode node, int start, int end) {
+        if (start >= end) 
+            return node;
+        if (node == null)
+            return new TreeNode(start, end);
+        if (start >= node.end) 
+            node.right = addRangeHelper(node.right, start, end);
+        else if (end <= node.start) 
+            node.left = addRangeHelper(node.left, start, end);
+        else {
+            node.left = addRangeHelper(node.left, start, node.start);
+            node.right = addRangeHelper(node.right, node.end, end);
         }
-        return root;
-
-
-    }
-    public TreeNode remove(TreeNode node)
-    {
-        if(node==null)
         return node;
-        if(node.left==null)
-        return node.right;
-        TreeNode leftLargest=getLargest(node.left,node);
-        leftLargest.left=node.left;
-        leftLargest.right=node.right;
+    }
+    
+    private boolean queryRangeHelper(TreeNode node, int start, int end) {
+        if (start >= end)
+            return true;
+        if (node == null)
+            return false;
+        if (start >= node.end)
+            return queryRangeHelper(node.right, start, end);
+        else if (end <= node.start)
+            return queryRangeHelper(node.left, start, end);
+        else if ((start >= node.start) && (end <= node.end))
+            return true;
+        return queryRangeHelper(node.left, start, node.start) && queryRangeHelper(node.right, node.end, end);
+    }
+    
+    private TreeNode removeRangeHelper(TreeNode node, int start, int end) {
+        if (start >= end)
+            return node;
+        if (node == null)
+            return null;
+        if (start >= node.end)
+            node.right = removeRangeHelper(node.right, start, end);
+        else if (end <= node.start)
+            node.left = removeRangeHelper(node.left, start, end);
+        else {
+            node.left = removeRangeHelper(node.left, start, node.start);
+            node.right = removeRangeHelper(node.right, node.end, end);
+            node.left = addRangeHelper(node.left, node.start, start);
+            node.left = addRangeHelper(node.left, end, node.end);
+            node = remove(node);
+        }
+        return node;
+    }
+    
+    private TreeNode remove(TreeNode node) {
+        if (node == null)
+            return node;
+        if (node.left == null)
+            return node.right;
+        TreeNode leftLargest = getLargest(node.left, node);
+        leftLargest.left = node.left;
+        leftLargest.right = node.right;
         return leftLargest;
     }
-    public TreeNode getLargest(TreeNode node,TreeNode parent )
-{
-    while(node.right!=null)
-    {
-        parent=node;
-        node=node.right;
-    }
-    if(node==parent.left)
-    parent.left=node.left;
-    if(node==parent.right)
-    parent.right=node.left;
-    node.left=null;
-    return node;
-}    
-public TreeNode addRange(TreeNode root,int start, int end) {
-    if(start>=end)
-    return root;
-    if(root==null)
-    return new TreeNode(start,end);
-    if(start>=root.end)
-    root.right=addRange(root.right,start,end);
-     else if(end<=root.start)
-    root.left=addRange(root.left,start,end);
-    else
-    {
-    root.left=addRange(root.left,start,root.start);
-    root.right=addRange(root.right,root.end,end);
-    }
-    return root;
-}
-public boolean queryRange(TreeNode root,int start, int end) {
-    if(start>=end)
-    return true;
-    if(root==null)
-    return false;
-    if(start>=root.end)
-   return queryRange(root.right,start,end);
-     else if(end<=root.start)
-    return queryRange(root.left,start,end);
-    else if((start>=root.start)&&(end<=root.end))
-    return true;
-
-    return queryRange(root.left,start,root.start) &&
-           queryRange(root.right,root.end,end);
     
-   
+    private TreeNode getLargest(TreeNode node, TreeNode parent) {
+        while (node.right != null) {
+            parent = node;
+            node = node.right;
+        }
+        if (node == parent.left)
+            parent.left = node.left;
+        if (node == parent.right)
+            parent.right = node.left;
+        node.left = null;
+        return node;
+    }
 }
+
+class TreeNode {
+    public int start, end;
+    public TreeNode left, right;
+    
+    public TreeNode(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
 }
 
 /**
