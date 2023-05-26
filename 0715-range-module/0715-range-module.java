e48ae7908184cf2c5bb07f805eb6cb8a49fa61a6
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-26 15:14:25 -0400

    Time: 40 ms (99.64%), Space: 51 MB (52.14%) - LeetHub

--- 0715-range-module/0715-range-module.java
+++ 0715-range-module/0715-range-module.java
@@ -1,61 +1,112 @@
 class RangeModule {
-    SegmentNode root;
-    int max = (int) Math.pow(10,9);
+
     public RangeModule() {
-        root = new SegmentNode(0,max,false);
+        
     }
-
+    TreeNode root;
+    
+    class TreeNode
+    {
+        int start;
+        int end;
+        TreeNode right;
+        TreeNode left;
+        public TreeNode(int start,int end)
+        {
+            this.start=start;
+            this.end=end;
+        }
+    }
     public void addRange(int left, int right) {
-        update(root,left,right,true);
+        root=addRange(root,left,right);
     }
-    private boolean update(SegmentNode node,int l,int r,boolean state){
-        if(l<=node.l && r>=node.r){
-            node.state = state;
-            node.left = null;
-            node.right = null;
-            return node.state;
-        }
-        if(l>=node.r || r<=node.l) return node.state;
-        int mid = node.l + (node.r - node.l) / 2;
-        if(node.left==null){
-            node.left = new SegmentNode(node.l,mid,node.state);
-            node.right = new SegmentNode(mid,node.r,node.state);
-        }
-        boolean left = update(node.left, l, r,state);
-        boolean right = update(node.right, l, r,state);
-        node.state = left && right;
-        return node.state;
-    }
-
+    
     public boolean queryRange(int left, int right) {
-        return query(root,left,right);
+        return queryRange(root,left,right);
     }
-    private boolean query(SegmentNode node,int l,int r){
-        if(l>=node.r || r<=node.l) return true;
-        if((l<=node.l && r>=node.r) || (node.left==null)) return node.state;
-        int mid = node.l + (node.r - node.l) / 2;
-        if(r<=mid){
-            return query(node.left,l,r);
-        }else if(l>=mid){
-            return query(node.right,l,r);
-        }else{
-            return query(node.left,l,r) && query(node.right,l,r);
-        }
-    }
-
+    
     public void removeRange(int left, int right) {
-        update(root,left,right,false);
+        root=removeRange(root,left,right);
     }
+    public TreeNode removeRange(TreeNode root,int start, int end) {
+       if(start>=end)
+       return root;
+       if(root==null)
+       return null;
+       if(start>=root.end)
+       root.right=removeRange(root.right,start,end);
+       else if(end<=root.start)
+        root.left=removeRange(root.left,start,end);
+        else
+        {
+        root.left=removeRange(root.left,start,root.start);
+        root.right=removeRange(root.right,root.end,end);
+        root.left=addRange(root.left,root.start,start);
+        root.left=addRange(root.left,end,root.end);
+        root=remove(root);
+        }
+        return root;
+
+
+    }
+    public TreeNode remove(TreeNode node)
+    {
+        if(node==null)
+        return node;
+        if(node.left==null)
+        return node.right;
+        TreeNode leftLargest=getLargest(node.left,node);
+        leftLargest.left=node.left;
+        leftLargest.right=node.right;
+        return leftLargest;
+    }
+    public TreeNode getLargest(TreeNode node,TreeNode parent )
+{
+    while(node.right!=null)
+    {
+        parent=node;
+        node=node.right;
+    }
+    if(node==parent.left)
+    parent.left=node.left;
+    if(node==parent.right)
+    parent.right=node.left;
+    node.left=null;
+    return node;
+}    
+public TreeNode addRange(TreeNode root,int start, int end) {
+    if(start>=end)
+    return root;
+    if(root==null)
+    return new TreeNode(start,end);
+    if(start>=root.end)
+    root.right=addRange(root.right,start,end);
+     else if(end<=root.start)
+    root.left=addRange(root.left,start,end);
+    else
+    {
+    root.left=addRange(root.left,start,root.start);
+    root.right=addRange(root.right,root.end,end);
+    }
+    return root;
 }
-class SegmentNode{
-    public int l, r;
-    public boolean state;
-    public SegmentNode left, right;
-    public SegmentNode(int l,int r,boolean state){
-        this.l = l;
-        this.r = r;
-        this.state = state;
-    }
+public boolean queryRange(TreeNode root,int start, int end) {
+    if(start>=end)
+    return true;
+    if(root==null)
+    return false;
+    if(start>=root.end)
+   return queryRange(root.right,start,end);
+     else if(end<=root.start)
+    return queryRange(root.left,start,end);
+    else if((start>=root.start)&&(end<=root.end))
+    return true;
+
+    return queryRange(root.left,start,root.start) &&
+           queryRange(root.right,root.end,end);
+    
+   
+}
 }
 
 /**
