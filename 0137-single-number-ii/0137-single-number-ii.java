1bcb4d391c870b1383b988fa6f20e082af2d3612
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-27 15:32:32 -0400

    Time: 0 ms (100.00%), Space: 44.6 MB (5.53%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,23 +1,10 @@
 class Solution {
-  public int singleNumber(int[] nums) {
-    int seenOnce = 0, seenTwice = 0;
-
-    for (int num : nums) {
-      // first appearence: 
-      // add num to seen_once 
-      // don't add to seen_twice because of presence in seen_once
-
-      // second appearance: 
-      // remove num from seen_once 
-      // add num to seen_twice
-
-      // third appearance: 
-      // don't add to seen_once because of presence in seen_twice
-      // remove num from seen_twice
-      seenOnce = ~seenTwice & (seenOnce ^ num);
-      seenTwice = ~seenOnce & (seenTwice ^ num);
+    public int singleNumber(int[] nums) {
+        int once = 0, twice = 0;
+        for (int num : nums) {
+            once = ~twice & (once ^ num);
+            twice = ~once & (twice ^ num);
+        }
+        return once;
     }
-
-    return seenOnce;
-  }
 }
