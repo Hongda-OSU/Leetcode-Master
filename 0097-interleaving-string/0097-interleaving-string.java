9a8801f44d736e036d8199a3628f9840bb36616c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-27 15:35:24 -0400

    Time: 5 ms (44.24%), Space: 40.7 MB (81.71%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -3,20 +3,20 @@ public class Solution {
         if (s3.length() != s1.length() + s2.length()) {
             return false;
         }
-        boolean dp[] = new boolean[s2.length() + 1];
+        boolean dp[][] = new boolean[s1.length() + 1][s2.length() + 1];
         for (int i = 0; i <= s1.length(); i++) {
             for (int j = 0; j <= s2.length(); j++) {
                 if (i == 0 && j == 0) {
-                    dp[j] = true;
+                    dp[i][j] = true;
                 } else if (i == 0) {
-                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
                 } else if (j == 0) {
-                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                    dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
                 } else {
-                    dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
                 }
             }
         }
-        return dp[s2.length()];
+        return dp[s1.length()][s2.length()];
     }
 }
