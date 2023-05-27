092503fb5888582fc77fade7445bbf7178659825
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-27 16:13:27 -0400

    Time: 1 ms (35.67%), Space: 39.5 MB (85.23%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,11 +1,11 @@
 class Solution {
-   public int trailingZeroes(int n) {
-    int zeroCount = 0;
-    long currentMultiple = 5;
-    while (n > 0) {
-        n /= 5;
-        zeroCount += n;
+    public int trailingZeroes(int n) {
+        int zero = 0;
+        long currentMultiple = 5;
+        while (n > 0) {
+            n /= 5;
+            zero += n;
+        }
+        return zero;
     }
-    return zeroCount;
-}
 }
