e0c4136aa2504a115275b4b8b22bbe441bca91a3
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-27 15:18:22 -0400

    Time: 20 ms (98.71%), Space: 55.9 MB (96.86%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,38 +1,31 @@
 class Solution {
-    public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
         int maxCapital = 0;
-        for (int i = 0; i < Capital.length; i++) {
-            maxCapital = Math.max(Capital[i], maxCapital);
-        }
-
-        if (W >= maxCapital) {
-            PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer a, Integer b) {
-                    return a - b;
-                }
-            });
-            for (int p : Profits) {
-                maxHeap.add(p);
-                if (maxHeap.size() > k) maxHeap.poll();
+        for (int i = 0; i < capital.length; i++)
+            maxCapital = Math.max(maxCapital, capital[i]);
+        if (w >= maxCapital) {
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> a - b);
+            for (int profit : profits) {
+                maxHeap.add(profit);
+                if (maxHeap.size() > k)
+                    maxHeap.poll();
             }
-            for (int h : maxHeap) W += h;
-            return W;
+            for (int h : maxHeap)
+                w += h;
+            return w;
         }
-
-        int index;
-        int n = Profits.length;
+        int index, n = profits.length;
         for (int i = 0; i < Math.min(k, n); i++) {
             index = -1;
-            for (int j = 0; j < n; ++j) {
-                if (W >= Capital[j] && (index == -1 || Profits[index] < Profits[j])) {
+            for (int j = 0; j < n; j++) {
+                if (w >= capital[j] && (index == -1 || profits[index] < profits[j]))
                     index = j;
-                }
             }
-            if (index == -1) break;
-            W += Profits[index];
-            Capital[index] = Integer.MAX_VALUE;
+            if (index == -1)
+                break;
+            w += profits[index];
+            capital[index] = Integer.MAX_VALUE;
         }
-        return W;
+        return w;
     }
 }
