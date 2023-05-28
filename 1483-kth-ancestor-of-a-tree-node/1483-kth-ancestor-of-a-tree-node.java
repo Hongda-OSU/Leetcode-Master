cbe785d9cbb64c3274ba1d9f8d0f1d4a021f893e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-28 15:04:23 -0400

    Time: 93 ms (37.50%), Space: 107.9 MB (9.38%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.java
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.java
@@ -1,34 +1,56 @@
 class TreeAncestor {
 
-  int[][] jump;
-    int maxPow;
-
+     
+    List<Integer>[] tree;       
+    List<List<Integer>> level;  // nodes in a level
+    int[] dfn;            // dfs number
+    int ndfn;             // dfs number starts wih 0
+    int[] depth;          // depth of each node
+    
     public TreeAncestor(int n, int[] parent) {
-        // log_base_2(n)
-        maxPow = (int) (Math.log(n) / Math.log(2)) + 1;
-        jump = new int[maxPow][n];
-        jump[0] = parent;
-        for (int p = 1; p < maxPow; p++) {
-            for (int j = 0; j < n; j++) {
-                int pre = jump[p - 1][j];
-                jump[p][j] = pre == -1 ? -1 : jump[p - 1][pre];
+        tree = new List[n];
+        for (int i = 0; i < n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+        level = new ArrayList<>();
+        dfn = new int[n];
+        ndfn = 0;
+        depth = new int[n];
+        
+        for (int i = 1; i < n; i++) {      // build tree
+            tree[parent[i]].add(i);
+        }
+        dfs(0, 0);
+    }
+    
+    private void dfs(int n, int dep) {
+        if (level.size() == dep) {
+            level.add(new ArrayList<>());
+        }
+        dfn[n] = ndfn++;                              // mark dfs number
+        depth[n] = dep;                               // save the depth  
+        level.get(dep).add(n);                    // save nodes into level
+        for (int child : tree[n]) {
+            dfs(child, dep + 1);
+        }
+    }
+    
+    public int getKthAncestor(int node, int k) {
+        int d = depth[node];
+        if (d - k < 0) return -1;
+        List<Integer> list = level.get(d - k);
+        int left = 0, right = list.size();
+        while (left < right) {                                       // binary search
+            int mid = left + (right - left) / 2;               
+            if (dfn[list.get(mid)] < dfn[node]) {        // find the first node larger than or equal to dfn[node]
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-    }
-
-    public int getKthAncestor(int node, int k) {
-        int maxPow = this.maxPow;
-        while (k > 0 && node > -1) {
-            if (k >= 1 << maxPow) {
-                node = jump[maxPow][node];
-                k -= 1 << maxPow;
-            } else
-                maxPow -= 1;
-        }
-        return node;
-    }
+        return list.get(left - 1);                             // anc
 }
-
+}
 /**
  * Your TreeAncestor object will be instantiated and called as such:
  * TreeAncestor obj = new TreeAncestor(n, parent);
