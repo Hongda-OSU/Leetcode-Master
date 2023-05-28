8ea7b7100eec971f8a1eaff068bf70eef3ff543a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-05-28 16:10:06 -0400

    Time: 77 ms (86.61%), Space: 88 MB (31.50%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.java
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.java
@@ -1,64 +1,57 @@
 class TreeAncestor {
+    private int[][] values;
+    private int[] index, offset;
 
-        int[][] values;
-
-        int[] reIndex;
-
-        int[] offset;
-
-        public TreeAncestor(int n, int[] parent) {
-            reIndex = new int[n];
-            offset = new int[n];
-            int[] t = new int[n];
-            int len = 0;
-            for (int i = 1; i < n; i++) {
-                if (t[parent[i]] == 0) {
-                    len++;
-                }
-                t[parent[i]]--;
-            }
-            len = n - len;
-            values = new int[len][0];
-            int[] temp = new int[n];
-            len = 0;
-            for (int i = 0; i < n; i++) {
-                if (t[i] == 0) {
-                    init(parent, temp, 0, len, i);
-                    len++;
-                }
-            }
+    public TreeAncestor(int n, int[] parent) {
+        index = new int[n];
+        offset = new int[n];
+        int[] t = new int[n];
+        int len = 0;
+        for (int i = 1; i < n; i++) {
+            if (t[parent[i]] == 0)
+                len++;
+            t[parent[i]]--;
         }
-
-        private void init(int[] parent, int[] temp, int index, int base, int cur) {
-            if (cur == -1) {
-                int[] nums = new int[index];
-                System.arraycopy(temp, 0, nums, 0, index);
-                values[base] = nums;
-                return;
+        values = new int[n - len][0];
+        int[] temp = new int[n];
+        len = 0;
+        for (int i = 0; i < n; i++) {
+            if (t[i] == 0) {
+                init(parent, temp, 0, len, i);
+                len++;
             }
-            if (offset[cur] > 0) {
-                int ol = values[reIndex[cur]].length - offset[cur];
-                int[] nums = new int[index + ol];
-                System.arraycopy(temp, 0, nums, 0, index);
-                System.arraycopy(values[reIndex[cur]], offset[cur], nums, index, ol);
-                values[base] = nums;
-                return;
-            }
-            offset[cur] = index;
-            reIndex[cur] = base;
-            temp[index] = cur;
-            init(parent, temp, index + 1, base, parent[cur]);
-        }
-
-        public int getKthAncestor(int node, int k) {
-            int index = reIndex[node];
-            int off = offset[node];
-            if (off + k >= values[index].length) {
-                return -1;
-            }
-            return values[index][off + k];
         }
     }
+    
+    public int getKthAncestor(int node, int k) {
+        int idx = index[node];
+        int off = offset[node];
+        if (off + k >= values[idx].length)
+            return -1;
+        return values[idx][off + k];
+    }
+    
+    private void init(int[] parent, int[] temp, int idx, int base, int curr) {
+        if (curr == -1) {
+            int[] nums = new int[idx];
+            System.arraycopy(temp, 0, nums, 0, idx);
+            values[base] = nums;
+            return;
+        }
+        if (offset[curr] > 0) {
+            int ol = values[index[curr]].length - offset[curr];
+            int[] nums = new int[idx + ol];
+            System.arraycopy(temp, 0, nums, 0, idx);
+            System.arraycopy(values[index[curr]], offset[curr], nums, idx, ol);
+            values[base] = nums;
+            return;
+        }
+        offset[curr] = idx;
+        index[curr] = base;
+        temp[idx] = curr;
+        init(parent, temp, idx + 1, base, parent[curr]);
+    }
+}
 
 /**
  * Your TreeAncestor object will be instantiated and called as such:
