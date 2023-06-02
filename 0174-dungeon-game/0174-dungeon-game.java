0619126c26c56b270ddfff73d64e46aaa9c29ab9
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-06-02 02:29:19 -0400

    Time: 2 ms (63.65%), Space: 44.1 MB (8.19%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,19 +1,30 @@
 class Solution {
-    public int calculateMinimumHP(int[][] dungeon) {
-        int m = dungeon.length, n = dungeon[0].length;
-        int[][] health = new int[m][n];
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (i == m - 1 && j == n - 1)
-                    health[i][j] = Math.max(1, 1 - dungeon[i][j]);
-                else if (i == m - 1) 
-                    health[i][j] = Math.max(1, health[i][j + 1] - dungeon[i][j]);
-                else if (j == n - 1)
-                    health[i][j] = Math.max(1, health[i + 1][j] - dungeon[i][j]);
-                else
-                    health[i][j] = Math.max(1, Math.min(health[i + 1][j], health[i][j + 1]) - dungeon[i][j]);
-            }
-        }
-        return health[0][0];
+   public int calculateMinimumHP(int[][] dungeon) {
+    if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0) return 0;
+    
+    int m = dungeon.length;
+    int n = dungeon[0].length;
+    
+    int[][] health = new int[m][n];
+
+    health[m - 1][n - 1] = Math.max(1 - dungeon[m - 1][n - 1], 1);
+
+    for (int i = m - 2; i >= 0; i--) {            
+        health[i][n - 1] = Math.max(health[i + 1][n - 1] - dungeon[i][n - 1], 1);
     }
+
+    for (int j = n - 2; j >= 0; j--) {
+        health[m - 1][j] = Math.max(health[m - 1][j + 1] - dungeon[m - 1][j], 1);
+    }
+
+    for (int i = m - 2; i >= 0; i--) {
+        for (int j = n - 2; j >= 0; j--) {
+            int down = Math.max(health[i + 1][j] - dungeon[i][j], 1);
+            int right = Math.max(health[i][j + 1] - dungeon[i][j], 1);
+            health[i][j] = Math.min(right, down);
+        }
+    }
+
+    return health[0][0];
+}
 }
