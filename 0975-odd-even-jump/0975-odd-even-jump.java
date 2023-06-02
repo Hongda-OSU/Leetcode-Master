125191a6510c2bb4d6f00fe50df4d2e7c9f003cf
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-06-02 00:50:29 -0400

    Time: 35 ms (99.57%), Space: 44.9 MB (96.98%) - LeetHub

--- 0975-odd-even-jump/0975-odd-even-jump.java
+++ 0975-odd-even-jump/0975-odd-even-jump.java
@@ -1,29 +1,58 @@
 class Solution {
-    public int oddEvenJumps(int[] A) {
-        int n = A.length;
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        boolean[][] dp = new boolean[n][2];
-        dp[n - 1][0] = true;
-        dp[n - 1][1] = true;
-        map.put(A[n - 1], n - 1);
-        int res = 1;
-
-        for (int i = n - 2; i >= 0; i--) {
-            // Odd step
-            Integer nextGreater = map.ceilingKey(A[i]);
-            if (nextGreater != null) {
-                dp[i][0] = dp[map.get(nextGreater)][1];
+    public int oddEvenJumps(int[] arr) {
+        
+        int[] count = new int[100001];
+        boolean[][] res = new boolean[2][arr.length];
+        res[0][arr.length - 1] = true;
+        res[1][arr.length - 1] = true;
+        count[arr[arr.length - 1]] = arr.length;
+        int min = arr[arr.length - 1], max = arr[arr.length - 1];
+        int result = 1;
+        for(int i = arr.length - 2; i >= 0; i--) {
+            int nextSmallIndex = findNextSmall(count, min, max, arr[i]);
+            int nextLargeIndex = findNextLarge(count, min, max, arr[i]);
+            if(nextSmallIndex == -1) {
+                res[0][i] = false;
+            } else {
+                res[0][i] = res[1][nextSmallIndex];
             }
-            // Even step
-            Integer nextSmaller = map.floorKey(A[i]);
-            if (nextSmaller != null) {
-                dp[i][1] = dp[map.get(nextSmaller)][0];
-            }
-            map.put(A[i], i);
 
-            res += dp[i][0] ? 1 : 0;
+            if(nextLargeIndex == -1) {
+                res[1][i] = false;
+            } else {
+                res[1][i] = res[0][nextLargeIndex];
+            }
+
+            count[arr[i]] = i + 1;
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+            // System.out.println(arr[i] + " " + nextSmallIndex + " " + nextLargeIndex + " " + i + " " + res[0][i] + " " + res[1][i]);
+            if(res[0][i]) {
+                result++;
+            }
         }
 
-        return res;
+        return result;
+
+    }
+
+    int findNextSmall(int[] count, int min, int max, int val) {
+
+        for(int i=val; i <= max; i++) {
+            if(count[i] != 0) {
+                return count[i]-1;
+            }
+        }
+        return -1;
+    }
+
+    int findNextLarge(int[] count, int min, int max, int val) {
+
+        for(int i=val; i >= min; i--) {
+            if(count[i] != 0) {
+                return count[i]-1;
+            }
+        }
+        return -1;
     }
 }
