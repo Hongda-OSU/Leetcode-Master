85967afff90d527a67f042c0d6f49ab20b8067bf
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-06-02 00:32:50 -0400

    Time: 45 ms (67.86%), Space: 57.3 MB (54.46%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.java
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.java
@@ -1,13 +1,14 @@
 class Solution {
    public int constrainedSubsetSum(int[] arr, int k) {
         int n = arr.length;
+        int[] dp = new int[n];
         Deque<Integer> deque = new LinkedList<>();
         int ans = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-			int max = Math.max(0, deque.isEmpty() ? 0 : arr[deque.peekFirst()]);
-            arr[i] += max;
-            ans = Math.max(ans, arr[i]);
-            while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()]) { // If dp[i] >= deque.peekLast() -> Can discard the tail since it's useless
+            int max = Math.max(0, deque.isEmpty() ? 0 : dp[deque.peekFirst()]);
+            dp[i] = arr[i] + max;
+            ans = Math.max(ans, dp[i]);
+            while (!deque.isEmpty() && dp[i] >= dp[deque.peekLast()]) { // If dp[i] >= deque.peekLast() -> Can discard the tail since it's useless
                 deque.pollLast();
             }
             deque.addLast(i);
