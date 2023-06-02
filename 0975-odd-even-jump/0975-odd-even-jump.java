e056a70c1725c121cee61c5f10420d05bc6f034c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-06-02 02:12:56 -0400

    Time: 35 ms (99.57%), Space: 44.6 MB (98.28%) - LeetHub

--- 0975-odd-even-jump/0975-odd-even-jump.java
+++ 0975-odd-even-jump/0975-odd-even-jump.java
@@ -1,41 +1,45 @@
 class Solution {
     public int oddEvenJumps(int[] arr) {
-        
-        int len = arr.length;
-        int minjmp[] = new int[len];
-        int maxjmp[] = new int[len];
-        
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        int evjmp, oddjmp;
-        for(int i = len-1; i>=0; i--) 
-        {
-            Integer minpos = map.floorKey(arr[i]);
-            evjmp = (minpos != null)?map.get(minpos):len; //default len, to show not possible
-            
-            if(evjmp != len && (evjmp == len-1 || maxjmp[evjmp] == len-1))
-                evjmp = len-1; //check the last pos reachability
-            
-            Integer maxpos = map.ceilingKey(arr[i]);
-            oddjmp = (maxpos != null) ? map.get(maxpos):len;
-            
-            if(oddjmp != len && (oddjmp == len-1 || minjmp[oddjmp] == len-1))
-                oddjmp = len-1;//check the last pos reachability
-            
-            minjmp[i] = evjmp; //specify possible jump path, if not possible assign len
-            maxjmp[i] = oddjmp;//specify possible jump path, if not possible assign len
-            
-            map.put(arr[i],i); //put the current index
-        }
-        
-        int res = 0;
-        
-        for(int i = 0; i< len-1; i++) {
-            
-            if(maxjmp[i] == len-1)
+        int[] count = new int[100001];
+        boolean[][] result = new boolean[2][arr.length];
+        result[0][arr.length - 1] = true;
+        result[1][arr.length - 1] = true;
+        count[arr[arr.length - 1]] = arr.length;
+        int min = arr[arr.length - 1], max = arr[arr.length - 1];
+        int res = 1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            int nextSmallIdx = findNextSmall(count, min, max, arr[i]);
+            int nextLargeIdx = findNextLarge(count, min, max, arr[i]);
+            if (nextSmallIdx == -1)
+                result[0][i] = false;
+            else 
+                result[0][i] = result[1][nextSmallIdx];
+            if (nextLargeIdx == -1)
+                result[1][i] = false;
+            else 
+                result[1][i] = result[0][nextLargeIdx];
+            count[arr[i]] = i + 1;
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+            if (result[0][i])
                 res++;
         }
-        
-        return res+1; //since last position will always be the answer
+        return res;
     }
     
+    private int findNextSmall(int[] count, int min, int max, int val) {
+        for (int i = val; i <= max; i++) {
+            if (count[i] != 0)
+                return count[i] - 1;
+        }
+        return -1;
+    }
+    
+    private int findNextLarge(int[] count, int min, int max, int val) {
+        for (int i = val; i >= min; i--) {
+            if (count[i] != 0)
+                return count[i] - 1;
+        }
+        return -1;
+    }
 }
