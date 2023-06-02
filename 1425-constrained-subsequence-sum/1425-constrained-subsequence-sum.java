101a3fc2ecdcb5f24ba58eba778ca938b5214a0a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-06-02 00:32:42 -0400

    Time: 47 ms (45.54%), Space: 57.4 MB (54.46%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.java
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.java
@@ -1,17 +1,20 @@
 class Solution {
-   public int constrainedSubsetSum(int[] A, int k) {
-        int res = A[0];
-        Deque<Integer> q = new ArrayDeque<>();
-        for (int i = 0; i < A.length; ++i) {
-            A[i] += !q.isEmpty() ? q.peek() : 0;
-            res = Math.max(res, A[i]);
-            while (!q.isEmpty() && A[i] > q.peekLast())
-                q.pollLast();
-            if (A[i] > 0)
-                q.offer(A[i]);
-            if (i >= k && !q.isEmpty() && q.peek() == A[i - k])
-                q.poll();
+   public int constrainedSubsetSum(int[] arr, int k) {
+        int n = arr.length;
+        Deque<Integer> deque = new LinkedList<>();
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+			int max = Math.max(0, deque.isEmpty() ? 0 : arr[deque.peekFirst()]);
+            arr[i] += max;
+            ans = Math.max(ans, arr[i]);
+            while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()]) { // If dp[i] >= deque.peekLast() -> Can discard the tail since it's useless
+                deque.pollLast();
+            }
+            deque.addLast(i);
+            if (i - deque.peekFirst() + 1 > k) { // remove the last element of range k
+                deque.removeFirst();
+            }
         }
-        return res;
+        return ans;
     }
 }
