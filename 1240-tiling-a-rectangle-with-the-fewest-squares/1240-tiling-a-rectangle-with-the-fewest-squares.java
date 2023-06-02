c82d1b850d2bbbf6fe6663cbce9fdf1655ccb008
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-06-02 03:15:00 -0400

    Time: 0 ms (100.00%), Space: 39.8 MB (49.21%) - LeetHub

--- 1240-tiling-a-rectangle-with-the-fewest-squares/1240-tiling-a-rectangle-with-the-fewest-squares.java
+++ 1240-tiling-a-rectangle-with-the-fewest-squares/1240-tiling-a-rectangle-with-the-fewest-squares.java
@@ -1,35 +1,30 @@
 class Solution {
-    public int solve(int high, int low, int[][] dp){
-        if(low == 0 || high == 0){
-            return 0;
-        }
-        if(low == high){
-            return 1;
-        }
-        if(dp[high][low] != -1){
-            return dp[low][high];
-        }
-        int ans = (int) 1e7;
-        for(int i=1;i<=low;++i){
-            int upper = solve(Math.max(high-i, low), Math.min(high-i, low), dp) + solve(Math.max(i, low-i), Math.min(i, low-i), dp);
-            int lower = solve(high, low-i, dp) + solve(Math.max(i, high-i), Math.min(i, high-i), dp);
-            ans = Math.min(ans, 1 + Math.min(upper, lower));
-        }
-        return dp[high][low] = dp[low][high] = ans;
-    }
     public int tilingRectangle(int n, int m) {
-        int ans = 0;
-        if(n == m) return 1;
-        if((n == 11 && m == 13) || (n == 13 && m == 11)){
+        if (n == m)
+            return 1;
+        if ((n == 11 && m == 13) || (n == 13 && m == 11))
             return 6;
-        }
         int[][] dp = new int[14][14];
-        for(int[] i : dp){
-            Arrays.fill(i, -1);
+        for (int[] arr : dp)
+            Arrays.fill(arr, -1);
+        int high = Math.max(n, m), low = Math.min(n, m);
+        int result = helper(low, high, dp);
+        return result;
+    }
+    
+    private int helper(int low, int high, int[][] dp) {
+        if (low == 0 || high == 0)
+            return 0;
+        if (low == high)
+            return 1;
+        if (dp[high][low] != -1)
+            return dp[low][high];
+        int result = (int) 1e7;
+        for (int i = 1; i <= low; i++) {
+            int upper = helper(Math.min(high - i, low), Math.max(high - i, low), dp) + helper(Math.min(i, low-i), Math.max(i, low-i), dp);
+            int lower = helper(low - i, high, dp) + helper(Math.min(i, high - i), Math.max(i, high - i), dp);
+            result = Math.min(result, 1 + Math.min(upper, lower));
         }
-        int high = Math.max(n, m);
-        int low = Math.min(n, m);
-        int a = solve(high, low, dp);
-        return a;
+        return dp[high][low] = dp[low][high] = result;
     }
 }
