615ca7b19f228e11c2c0344b34f10b281631b5cb
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-01 14:33:12 -0500

    Time: 5 ms (100.00%), Space: 65.2 MB (13.96%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -1,16 +1,20 @@
-public class Solution {
-    public int maxDistance(List<List<Integer>> arrays) {
-        int result = Integer.MIN_VALUE;
-        int max = arrays.get(0).get(arrays.get(0).size() - 1);
-        int min = arrays.get(0).get(0);
-        
-        for (int i = 1; i < arrays.size(); i++) {
-            result = Math.max(result, Math.abs(arrays.get(i).get(0) - max));
-            result = Math.max(result, Math.abs(arrays.get(i).get(arrays.get(i).size() - 1) - min));
-            max = Math.max(max, arrays.get(i).get(arrays.get(i).size() - 1));
-            min = Math.min(min, arrays.get(i).get(0));
-        }
-        
-        return result;
+class Solution {
+     public int maxDistance(List<List<Integer>> arrays) {
+    int res = 0;
+    //running min and max, use constrained values
+    int min = 10001, max = -10001;
+    
+    for (int i = 0; i < arrays.size(); i++) {
+      List<Integer> cur = arrays.get(i);
+      //min and max elements for the current array
+      int minEl = cur.get(0), maxEl = cur.get(cur.size() - 1);
+      //get running max result among this max and prev min and prev max and
+      //current min
+      res = Math.max(res, Math.max(maxEl - min, max - minEl));
+      //update running min/max
+      min = Math.min(min, minEl);
+      max = Math.max(max, maxEl);
     }
+    return res;
+  }
 }
