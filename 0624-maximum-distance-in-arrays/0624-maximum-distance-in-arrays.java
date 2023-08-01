1feebb02e1e83b1d1126f17c1a158a556397f964
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-01 14:39:01 -0500

    Time: 5 ms (100.00%), Space: 62.3 MB (58.24%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -1,20 +1,14 @@
 class Solution {
-     public int maxDistance(List<List<Integer>> arrays) {
-    int res = 0;
-    //running min and max, use constrained values
-    int min = 10001, max = -10001;
-    
-    for (int i = 0; i < arrays.size(); i++) {
-      List<Integer> cur = arrays.get(i);
-      //min and max elements for the current array
-      int minEl = cur.get(0), maxEl = cur.get(cur.size() - 1);
-      //get running max result among this max and prev min and prev max and
-      //current min
-      res = Math.max(res, Math.max(maxEl - min, max - minEl));
-      //update running min/max
-      min = Math.min(min, minEl);
-      max = Math.max(max, maxEl);
+    public int maxDistance(List<List<Integer>> arrays) {
+        int result = 0;
+        int MIN = 10001, MAX = -10001;
+        for (int i = 0; i < arrays.size(); i++) {
+            List<Integer> curr = arrays.get(i);
+            int min = curr.get(0), max = curr.get(curr.size() - 1);
+            result = Math.max(result, Math.max(max - MIN, MAX - min));
+            MIN = Math.min(MIN, min);
+            MAX = Math.max(MAX, max);
+        }
+        return result;
     }
-    return res;
-  }
 }
