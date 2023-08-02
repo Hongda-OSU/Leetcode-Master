5ebcd88cacaf07da37a6b1f8044afa57ef6ee9b6
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-01 22:32:50 -0500

    Time: 0 ms (100.00%), Space: 41.2 MB (48.70%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.java
+++ 0161-one-edit-distance/0161-one-edit-distance.java
@@ -1,29 +1,18 @@
 class Solution {
-  public boolean isOneEditDistance(String s, String t) {
-    int ns = s.length();
-    int nt = t.length();
-
-    // Ensure that s is shorter than t.
-    if (ns > nt)
-      return isOneEditDistance(t, s);
-
-    // The strings are NOT one edit away distance  
-    // if the length diff is more than 1.
-    if (nt - ns > 1)
-      return false;
-
-    for (int i = 0; i < ns; i++)
-      if (s.charAt(i) != t.charAt(i))
-        // if strings have the same length
-        if (ns == nt)
-          return s.substring(i + 1).equals(t.substring(i + 1));
-        // if strings have different lengths
-        else
-          return s.substring(i).equals(t.substring(i + 1));
-
-    // If there is no diffs on ns distance
-    // the strings are one edit away only if
-    // t has one more character. 
-    return (ns + 1 == nt);
-  }
+    public boolean isOneEditDistance(String s, String t) {
+        int sLen = s.length(), tLen = t.length();
+        if (sLen > tLen)
+            return isOneEditDistance(t, s);
+        if (tLen - sLen > 1)
+            return false;
+        for (int i = 0; i < sLen; i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                if (sLen == tLen)
+                    return s.substring(i + 1).equals(t.substring(i + 1));
+                else
+                    return s.substring(i).equals(t.substring(i + 1));
+            }
+        }
+        return (sLen + 1 == tLen);
+    }
 }
