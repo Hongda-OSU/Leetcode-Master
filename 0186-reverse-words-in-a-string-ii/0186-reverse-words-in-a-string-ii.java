a873b677b7a09bd2ec5683f6ed96ca1ab1d27884
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-01 23:11:30 -0500

    Time: 1 ms (99.65%), Space: 48 MB (65.96%) - LeetHub

--- 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
+++ 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
@@ -1,32 +1,28 @@
 class Solution {
-    public void reverse(char[] s, int left, int right) {
-        while (left < right) {
-            char tmp = s[left];
-            s[left++] = s[right];
-            s[right--] = tmp;
-        }
-    }
-
-    public void reverseEachWord(char[] s) {
-        int n = s.length;
-        int start = 0, end = 0;
-
-        while (start < n) {
-            // go to the end of the word
-            while (end < n && s[end] != ' ') ++end;
-            // reverse the word
-            reverse(s, start, end - 1);
-            // move to the next word
-            start = end + 1;
-            ++end;
-        }
-    }
-
     public void reverseWords(char[] s) {
-        // reverse the whole string
-        reverse(s, 0, s.length - 1);
-
-        // reverse each word
-        reverseEachWord(s);
+    // Three step to reverse
+    // 1, reverse the whole sentence
+    reverse(s, 0, s.length - 1);
+    // 2, reverse each word
+    int start = 0;
+    int end = -1;
+    for (int i = 0; i < s.length; i++) {
+        if (s[i] == ' ') {
+            reverse(s, start, i - 1);
+            start = i + 1;
+        }
     }
+    // 3, reverse the last word, if there is only one word this will solve the corner case
+    reverse(s, start, s.length - 1);
+}
+
+public void reverse(char[] s, int start, int end) {
+    while (start < end) {
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+}
 }
