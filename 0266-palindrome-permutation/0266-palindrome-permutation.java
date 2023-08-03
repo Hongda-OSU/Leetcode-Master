645b4ef0da13544361b63f5fab2ebec8a1ae9640
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-03 01:08:00 -0500

    Time: 1 ms (60.03%), Space: 40.6 MB (19.75%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.java
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -1,10 +1,12 @@
 public class Solution {
     public boolean canPermutePalindrome(String s) {
-        Set < Character > set = new HashSet < > ();
-        for (int i = 0; i < s.length(); i++) {
-            if (!set.add(s.charAt(i)))
+        Set<Character> set=new HashSet<Character>();
+        for(int i=0; i<s.length(); ++i){
+            if (!set.contains(s.charAt(i)))
+                set.add(s.charAt(i));
+            else 
                 set.remove(s.charAt(i));
         }
-        return set.size() <= 1;
+        return set.size()==0 || set.size()==1;
     }
 }
