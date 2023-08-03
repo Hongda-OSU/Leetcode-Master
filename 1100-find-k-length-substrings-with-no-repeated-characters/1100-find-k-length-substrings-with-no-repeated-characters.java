b0ab7f3b521ce1862cd0eeb0af4b6c0f5a60d61d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-02 22:24:27 -0500

    Time: 3 ms (98.59%), Space: 40.8 MB (87.04%) - LeetHub

--- 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.java
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.java
@@ -1,46 +1,32 @@
 class Solution {
     public int numKLenSubstrNoRepeats(String s, int k) {
-        // We can reuse the condition from the first approach
-        // as for k > 26, there can be no substrings with only unique characters
-        if (k > 26)
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        if (n < k)
             return 0;
-        
-        int answer = 0;
-        int n = s.length();
-        
-        // Initializing the left and right pointers
-        int left = 0, right = 0;
-        // Initializing an empty frequency array
-        int freq[] = new int[26];
-        
-        while (right < n) {
-            
-            // Add the current character in the frequency array
-            freq[s.charAt(right) - 'a']++;
-            
-            // If the current character appears more than once in the frequency array
-            // keep contracting the window and removing characters from the
-            // frequency array till the frequency of the current character becomes 1.
-            while (freq[s.charAt(right) - 'a'] > 1) {
-                freq[s.charAt(left) - 'a']--;
-                left++;
-            }
-            
-            
-            // Check if the length of the current unique substring is equal to k
-            if (right - left + 1 == k) {
-                answer++;
-                
-                // Contract the window and remove the leftmost character from the
-                // frequency array
-                freq[s.charAt(left) - 'a']--;
-                left++;
-            }
-            
-            // Expand the window
-            right++;
+        int repeated = 0;
+        int[] count = new int[26];
+        // check first k characters 
+        for (int i = 0; i < k; i++) {
+            count[arr[i] - 'a']++;
+            if (count[arr[i] - 'a'] == 2) 
+                repeated++;
         }
+        // if first k charcters have no repeat, then result is 1
+        int result = repeated > 0 ? 0 : 1;
         
-        return answer;
+        for (int i = k; i < n; i++) {
+            // remove old character
+            count[arr[i - k] - 'a']--;
+            if (count[arr[i - k] - 'a'] == 1)
+                repeated--;
+            // add new character
+            count[arr[i] - 'a']++;
+            if (count[arr[i] - 'a'] == 2)
+                repeated++;
+            if (repeated == 0)
+                result++;
+        }
+        return result;
     }
 }
