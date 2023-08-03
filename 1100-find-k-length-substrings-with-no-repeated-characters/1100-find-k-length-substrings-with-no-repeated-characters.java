ba4abe695a3243bb8ec8a7c477985fa9682c8e72
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-02 22:07:36 -0500

    Time: 10 ms (63.38%), Space: 43.9 MB (6.76%) - LeetHub

--- 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.java
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.java
@@ -1,46 +1,13 @@
 class Solution {
-    public int numKLenSubstrNoRepeats(String s, int k) {
-        // We can reuse the condition from the first approach
-        // as for k > 26, there can be no substrings with only unique characters
-        if (k > 26)
-            return 0;
-        
-        int answer = 0;
-        int n = s.length();
-        
-        // Initializing the left and right pointers
-        int left = 0, right = 0;
-        // Initializing an empty frequency array
-        int freq[] = new int[26];
-        
-        while (right < n) {
-            
-            // Add the current character in the frequency array
-            freq[s.charAt(right) - 'a']++;
-            
-            // If the current character appears more than once in the frequency array
-            // keep contracting the window and removing characters from the
-            // frequency array till the frequency of the current character becomes 1.
-            while (freq[s.charAt(right) - 'a'] > 1) {
-                freq[s.charAt(left) - 'a']--;
-                left++;
-            }
-            
-            
-            // Check if the length of the current unique substring is equal to k
-            if (right - left + 1 == k) {
-                answer++;
-                
-                // Contract the window and remove the leftmost character from the
-                // frequency array
-                freq[s.charAt(left) - 'a']--;
-                left++;
-            }
-            
-            // Expand the window
-            right++;
+    public int numKLenSubstrNoRepeats(String S, int K) {
+        HashSet<Character> cur = new HashSet<>();
+        int res = 0, i = 0;
+        for (int j = 0; j < S.length(); ++j) {
+            while (cur.contains(S.charAt(j)))
+                cur.remove(S.charAt(i++));
+            cur.add(S.charAt(j));
+            res += j - i + 1 >= K ? 1 : 0;
         }
-        
-        return answer;
+        return res;
     }
 }
