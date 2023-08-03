f243090c4dfbc13cf631f7b77c7ac77c772ac5f5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-02 22:08:22 -0500

    Time: 12 ms (57.75%), Space: 43.3 MB (30.42%) - LeetHub

--- 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.java
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.java
@@ -1,13 +1,20 @@
 class Solution {
     public int numKLenSubstrNoRepeats(String S, int K) {
-        HashSet<Character> cur = new HashSet<>();
-        int res = 0, i = 0;
-        for (int j = 0; j < S.length(); ++j) {
-            while (cur.contains(S.charAt(j)))
-                cur.remove(S.charAt(i++));
-            cur.add(S.charAt(j));
-            res += j - i + 1 >= K ? 1 : 0;
+        int ans = 0;
+        Set<Character> set = new HashSet<>();
+        int i = 0;
+        
+        for (int j = 0; j < S.length(); j++) {
+            while (set.contains(S.charAt(j))) {
+                set.remove(S.charAt(i++));
+            }
+            set.add(S.charAt(j));
+            
+            if (j - i + 1 == K) {
+                ans++;
+                set.remove(S.charAt(i++));
+            }
         }
-        return res;
+        return ans;
     }
 }
