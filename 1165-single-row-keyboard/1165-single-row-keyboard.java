9abfbf182b419aabb2720086ea015077512c78d2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-04 16:41:39 -0500

    Time: 4 ms (99.24%), Space: 41.2 MB (82.62%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.java
+++ 1165-single-row-keyboard/1165-single-row-keyboard.java
@@ -1,24 +1,23 @@
 class Solution {
-    public int calculateTime(String keyboard, String word) {
-        int[] keyIndices = new int[26];
-
-        // Get the index for each key.
-        for (int i = 0; i < keyboard.length(); i++)
-            keyIndices[keyboard.charAt(i) - 'a'] = i;
-
-        // Initialize previous index as starting index = 0.
-        int prev = 0;
-        int result = 0;
-
-        // Calculate the total time.
-        for (char c : word.toCharArray()) {
-            // Add the distance from previous index
-            // to current letter's index to the result.
-            result += Math.abs(prev - keyIndices[c - 'a']);
-
-            // Update the previous index to current index for next iteration.
-            prev = keyIndices[c - 'a'];
+public int calculateTime(String keyboard, String word) {
+        //fill mapping for the keyboard chars and indexes
+        int[] map = new int[26];
+        int j = 0;
+        for (char ch : keyboard.toCharArray()) {
+            map[ch - 'a'] = j++;
         }
-        return result;
+        
+        int res = 0;
+        //reuse j as a placeholder for the previously typed char. Starting from 0
+        j = 0;
+        for (char ch : word.toCharArray()) {
+            //find position of the current char as per keyboard mapping
+            int idx = map[ch - 'a'];
+            //add difference to the result
+            res += Math.abs(j - idx);
+            //save current as previous position for the next iteration
+            j = idx;
+        }
+        return res;
     }
 }
