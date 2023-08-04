504a809d15b00a47b0793c17cb9940f6cc9f9afe
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-04 17:12:27 -0500

    Time: 1 ms (100.00%), Space: 42.7 MB (50.69%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -1,29 +1,24 @@
 class Solution {
     public List<List<String>> groupStrings(String[] strings) {
-        Map<String,List<String>> group_map = new HashMap<>();
-        for(String s: strings){
-            StringBuilder key  = new StringBuilder();
-            for(int i = 0; i < s.length();i++){
-                int relative_distance = (s.charAt(i) - s.charAt(0) + 26) % 26;
-                // in case [0,1,11] [0,11,1], so i add "." to key.
-                key.append(".");
-                key.append(Integer.toString(relative_distance));
-            }
-            String k = key.toString();
-            if(!group_map.containsKey(k)){
-                List<String> value = new ArrayList<>();
-                group_map.put(k,value);
-            }
-            group_map.get(k).add(s);
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strings) {
+            String hashKey = getHash(str);
+            if (!map.containsKey(hashKey)) 
+                map.put(hashKey, new ArrayList<>());
+            map.get(hashKey).add(str);
         }
-        System.out.println(group_map);
-        
-        List<List<String>> output = new ArrayList<>();
-        for(String key: group_map.keySet()){
-            List<String> value = new ArrayList<>();
-            value = group_map.get(key);
-            output.add(value);
+        List<List<String>> groups = new ArrayList<>();
+        for (List<String> group : map.values()) 
+            groups.add(group);
+        return groups;
+    }
+    
+    private String getHash(String str) {
+        char[] arr = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < arr.length; i++) {
+            sb.append((char)((arr[i] - arr[i - 1] + 26) % 26 + 'a'));
         }
-        return output;
+        return sb.toString();
     }
 }
