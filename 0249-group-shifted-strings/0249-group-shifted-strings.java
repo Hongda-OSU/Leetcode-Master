0051e84f4b0cf8682417101c9f4ff0022ac7743a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-04 17:07:18 -0500

    Time: 6 ms (29.10%), Space: 44.2 MB (9.95%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -1,36 +1,29 @@
 class Solution {
-    // Create a hash value
-    String getHash(String s) {
-        char[] chars = s.toCharArray();
-        StringBuilder hashKey = new StringBuilder();
-        
-        for (int i = 1; i < chars.length; i++) {
-            hashKey.append((char) ((chars[i] - chars[i - 1] + 26) % 26 + 'a'));
-        }
-        
-        return hashKey.toString();
-    }
-    
     public List<List<String>> groupStrings(String[] strings) {
-        Map<String, List<String>> mapHashToList = new HashMap<>();
-        
-        // Create a hash_value (hashKey) for each string and append the string
-        // to the list of hash values i.e. mapHashToList["cd"] = ["acf", "gil", "xzc"]
-        for (String str : strings ) {
-            String hashKey = getHash(str);
-            if (mapHashToList.get(hashKey) == null) {
-                mapHashToList.put(hashKey, new ArrayList<>());
-            } 
-            mapHashToList.get(hashKey).add(str);
+        Map<String,List<String>> group_map = new HashMap<>();
+        for(String s: strings){
+            StringBuilder key  = new StringBuilder();
+            for(int i = 0; i < s.length();i++){
+                int relative_distance = (s.charAt(i) - s.charAt(0) + 26) % 26;
+                // in case [0,1,11] [0,11,1], so i add "." to key.
+                key.append(".");
+                key.append(Integer.toString(relative_distance));
+            }
+            String k = key.toString();
+            if(!group_map.containsKey(k)){
+                List<String> value = new ArrayList<>();
+                group_map.put(k,value);
+            }
+            group_map.get(k).add(s);
         }
+        System.out.println(group_map);
         
-        // Iterate over the map, and add the values to groups
-        List<List<String>> groups = new ArrayList<>();
-        for (List<String> group : mapHashToList.values()) {
-            groups.add(group);
+        List<List<String>> output = new ArrayList<>();
+        for(String key: group_map.keySet()){
+            List<String> value = new ArrayList<>();
+            value = group_map.get(key);
+            output.add(value);
         }
-        
-        // Return a list of all of the grouped strings
-        return groups;
+        return output;
     }
 }
