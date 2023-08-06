1700ddaa70fb9867740457c89130dc49d197f47f
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-06 02:10:04 -0500

    Time: 0 ms (100.00%), Space: 44.4 MB (59.01%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
@@ -1,17 +1,18 @@
-public class Solution {
-    public int[][] multiply(int[][] A, int[][] B) {
-        int m = A.length, n = A[0].length, nB = B[0].length;
-        int[][] C = new int[m][nB];
-
-        for(int i = 0; i < m; i++) {
-            for(int k = 0; k < n; k++) {
-                if (A[i][k] != 0) {
-                    for (int j = 0; j < nB; j++) {
-                        if (B[k][j] != 0) C[i][j] += A[i][k] * B[k][j];
+class Solution {
+    public int[][] multiply(int[][] mat1, int[][] mat2) {
+        int m = mat1.length, n = mat1[0].length, n2 = mat2[0].length;
+        int[][] result = new int[m][n2];
+        
+        for (int i = 0; i < m; i++) {
+            for (int k = 0; k < n; k++) {
+                if (mat1[i][k] != 0) {
+                    for (int j = 0; j < n2; j++) {
+                        if (mat2[k][j] != 0)
+                            result[i][j] += mat1[i][k] * mat2[k][j];
                     }
                 }
             }
         }
-        return C;   
+        return result;
     }
 }
