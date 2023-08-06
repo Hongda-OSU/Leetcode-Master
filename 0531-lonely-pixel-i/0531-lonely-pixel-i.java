789c6de0d42df7c7d6dcb1912c30e23d1b95baf2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-06 01:13:14 -0500

    Time: 3 ms (40.09%), Space: 45 MB (89.64%) - LeetHub

--- 0531-lonely-pixel-i/0531-lonely-pixel-i.java
+++ 0531-lonely-pixel-i/0531-lonely-pixel-i.java
@@ -1,24 +1,22 @@
 class Solution {
     public int findLonelyPixel(char[][] picture) {
-    int n = picture.length, m = picture[0].length;
-    
-    int firstRowCount = 0;
-    for (int i=0;i<n;i++) 
-        for (int j=0;j<m;j++) 
-            if (picture[i][j] == 'B') {   
-                if (picture[0][j] < 'Y' && picture[0][j] != 'V') picture[0][j]++;
-                if (i == 0) firstRowCount++;
-                else if (picture[i][0] < 'Y' && picture[i][0] != 'V') picture[i][0]++;
+        int m = picture.length, n = picture[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                row[i] += picture[i][j] == 'W' ? 1 : 0;
+                col[j] += picture[i][j] == 'W' ? 1 : 0;
             }
-
-    int count = 0;
-    for (int i=0;i<n;i++) 
-        for (int j=0;j<m;j++) 
-            if (picture[i][j] < 'W' && (picture[0][j] == 'C' || picture[0][j] == 'X')) { 
-                if (i == 0) count += firstRowCount == 1 ? 1 : 0;
-                else if (picture[i][0] == 'C' || picture[i][0] == 'X') count++;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] == 'B' && row[i] == n - 1 && col[j] == m - 1) {
+                    res++;
+                }
             }
-                
-    return count;
-}
+        }
+        return res;
+    }
 }
