8d0ed98769d4a19c9b68cb3316b75573b6b531a5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-06 01:12:38 -0500

    Time: 2 ms (95.95%), Space: 45.3 MB (52.25%) - LeetHub

--- 0531-lonely-pixel-i/0531-lonely-pixel-i.java
+++ 0531-lonely-pixel-i/0531-lonely-pixel-i.java
@@ -1,64 +1,24 @@
 class Solution {
-    // Returns true if the cell at (x, y) is lonely.
-    // There should not be any other black cell 
-    // In the first row and column except (x, y) itself.
-    boolean check(char[][] picture, int x, int y) {
-        int n = picture.length;
-        int m = picture[0].length;
-        
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            cnt += (picture[i][y] == 'B' ? 1 : 0);
-        }
-        
-        for (int j = 0; j < m; j++) {
-            // avoid double count (x, y)
-            if (j != y) cnt += (picture[x][j] == 'B' ? 1 : 0);
-        }
-        return picture[x][y] == 'B' && cnt == 1;
-    }
-    
     public int findLonelyPixel(char[][] picture) {
-        int n = picture.length;
-        int m = picture[0].length;
-        
-        int answer = 0;
-        for (int j = 0; j < m; j++) {
-            answer += check(picture, 0, j) ? 1 : 0;
-        }
-        for (int i = 1; i < n; i++) {
-            answer += check(picture, i, 0) ? 1 : 0;
-        }
+    int n = picture.length, m = picture[0].length;
+    
+    int firstRowCount = 0;
+    for (int i=0;i<n;i++) 
+        for (int j=0;j<m;j++) 
+            if (picture[i][j] == 'B') {   
+                if (picture[0][j] < 'Y' && picture[0][j] != 'V') picture[0][j]++;
+                if (i == 0) firstRowCount++;
+                else if (picture[i][0] < 'Y' && picture[i][0] != 'V') picture[i][0]++;
+            }
 
-        // Convert cell 'B' to '1' and 'W' to '0'
-        for (int j = 0; j < m; j++) {
-            picture[0][j] = (picture[0][j] == 'B' ? '1' : '0');
-        }
-        
-        for (int i = 0; i < n; i++) {
-            picture[i][0] = (picture[i][0] == 'B' ? '1' : '0');
-        }
-        
-        // If the cell is black increment the count of corresponding row and column by 1
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (picture[i][j] == 'B') {
-                    picture[i][0]++;
-                    picture[0][j]++;
-                }
+    int count = 0;
+    for (int i=0;i<n;i++) 
+        for (int j=0;j<m;j++) 
+            if (picture[i][j] < 'W' && (picture[0][j] == 'C' || picture[0][j] == 'X')) { 
+                if (i == 0) count += firstRowCount == 1 ? 1 : 0;
+                else if (picture[i][0] == 'C' || picture[i][0] == 'X') count++;
             }
-        }
-        
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (picture[i][j] == 'B') {
-                    if (picture[0][j] == '1' && picture[i][0] == '1') {
-                        answer++;
-                    }
-                }
-            }
-        }
-        
-        return answer;
-    }
+                
+    return count;
+}
 }
