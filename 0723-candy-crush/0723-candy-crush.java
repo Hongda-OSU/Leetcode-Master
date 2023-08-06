118d8cef8dca6bbbdeb12338afa731815eb4a455
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-06 03:10:43 -0500

    Time: 6 ms (94.29%), Space: 44.1 MB (22.50%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.java
+++ 0723-candy-crush/0723-candy-crush.java
@@ -1,34 +1,37 @@
 class Solution {
     public int[][] candyCrush(int[][] board) {
-        int N = board.length, M = board[0].length;
+        int m = board.length, n = board[0].length;
         boolean found = true;
         while (found) {
             found = false;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     int val = Math.abs(board[i][j]);
-                    if (val == 0) continue;
-                    if (j < M - 2 && Math.abs(board[i][j + 1]) == val && Math.abs(board[i][j + 2]) == val) {
+                    if (val == 0)
+                        continue;
+                    if (j < n - 2 && Math.abs(board[i][j + 1]) == val && Math.abs(board[i][j + 2]) == val) {
                         found = true;
-                        int ind = j;
-                        while (ind < M && Math.abs(board[i][ind]) == val) board[i][ind++] = -val;
+                        int index = j;
+                        while (index < n && Math.abs(board[i][index]) == val)
+                            board[i][index++] = -val;
                     }
-                    if (i < N - 2 && Math.abs(board[i + 1][j]) == val && Math.abs(board[i + 2][j]) == val) {
+                    if (i < m - 2 && Math.abs(board[i + 1][j]) == val && Math.abs(board[i + 2][j]) == val) {
                         found = true;
-                        int ind = i;
-                        while (ind < N && Math.abs(board[ind][j]) == val) board[ind++][j] = -val;           
+                        int index = i;
+                        while (index < m && Math.abs(board[index][j]) == val)
+                            board[index++][j] = -val;
                     }
                 }
             }
-            if (found) { // move positive values to the bottom, then set the rest to 0
-                for (int j = 0; j < M; j++) {
-                    int storeInd = N - 1;
-                    for (int i = N - 1; i >= 0; i--) {
-                        if (board[i][j] > 0) {
-                            board[storeInd--][j] = board[i][j];
-                        }
+            if (found) {
+                for (int j = 0; j < n; j++) {
+                    int index = m - 1;
+                    for (int i = m - 1; i >= 0; i--) {
+                        if (board[i][j] > 0)
+                            board[index--][j] = board[i][j];
                     }
-                    for (int k = storeInd; k >= 0; k--) board[k][j] = 0;
+                    for (int k = index; k >= 0; k--)
+                    board[k][j] = 0;
                 }
             }
         }
