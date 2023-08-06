3bd1aac0d1da7a16f41e1351536674a33efb95e6
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-06 01:18:07 -0500

    Time: 2 ms (95.95%), Space: 44.8 MB (94.14%) - LeetHub

--- 0531-lonely-pixel-i/0531-lonely-pixel-i.java
+++ 0531-lonely-pixel-i/0531-lonely-pixel-i.java
@@ -1,27 +1,23 @@
 class Solution {
     public int findLonelyPixel(char[][] picture) {
-
-        //track the only one 'B''s column
-        int[] row = new int[picture.length];
-        //count the number of 'B' in column
-        int[] column = new int[picture[0].length];
-
-        //Time: O(M * N)
+        int[] row = new int[picture.length], col = new int[picture[0].length];
         for (int i = 0; i < picture.length; i++) {
-            int c = 0; int x = -1;
+            int count = 0, index = -1;
             for (int j = 0; j < picture[0].length; j++) {
                 if (picture[i][j] == 'B') {
-                    c++; x = j; column[j]++;
+                    count++;
+                    index = j;
+                    col[j]++;
                 }
             }
-            //if there is only one 'B' in the row, then we track it's column
-            if (c == 1) row[i] = x + 1;
+            if (count == 1)
+                row[i] = index + 1;
         }
-
-        //Time: O(M)
-        int res = 0;
-        for (int r: row)
-            if (r > 0  && column[r - 1] == 1) res++;
-        return res;
+        int result = 0;
+        for (int r : row) {
+            if (r > 0 && col[r - 1] == 1)
+                result++;
+        }
+        return result;
     }
 }
