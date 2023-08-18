1a14ae1b8c78c30cc262d0b2d4f10fd97a68e668
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-17 20:57:40 -0500

    Time: 9 ms (59.63%), Space: 44.2 MB (13.42%) - LeetHub

--- 0439-ternary-expression-parser/0439-ternary-expression-parser.java
+++ 0439-ternary-expression-parser/0439-ternary-expression-parser.java
@@ -1,24 +1,35 @@
 class Solution {
     public String parseTernary(String expression) {
-    if (expression == null || expression.length() == 0) return "";
-    Deque<Character> stack = new LinkedList<>();
-
-    for (int i = expression.length() - 1; i >= 0; i--) {
-        char c = expression.charAt(i);
-        if (!stack.isEmpty() && stack.peek() == '?') {
-
-            stack.pop(); //pop '?'
-            char first = stack.pop();
-            stack.pop(); //pop ':'
-            char second = stack.pop();
-
-            if (c == 'T') stack.push(first);
-            else stack.push(second);
-        } else {
-            stack.push(c);
+        Deque<Character> s1 = new LinkedList<>();
+        Deque<Character> s2 = new LinkedList<>();
+        
+        // Judge form right to left. So we add all char to s1.
+        for(char exp : expression.toCharArray()) {
+            // We don't need ":" due to one-digit number.
+            if(exp != ':') {
+                s1.push(exp);
+            }
         }
+        
+        // If meet "?", we should check which digit needs to be kept.
+        while(s1.size() + s2.size() > 1) {
+            char nowC = s1.pop();
+            
+            if(nowC == '?') {
+                char nextC = s1.pop();
+                
+                if(nextC == 'T') {
+                    char temp = s2.pop();
+                    s2.pop();
+                    s2.push(temp);
+                } else{
+                    s2.pop();
+                }
+            } else {
+                s2.push(nowC);
+            }
+        }
+        
+        return s2.pop().toString();
     }
-
-    return String.valueOf(stack.peek());
-}
 }
