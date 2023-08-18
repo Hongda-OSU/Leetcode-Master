85a91b453f0c67b97b6be752172bad6c4a64d42a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-17 20:29:46 -0500

    Time: 9 ms (78.30%), Space: 53.1 MB (51.89%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.java
+++ 1272-remove-interval/1272-remove-interval.java
@@ -1,33 +1,16 @@
 class Solution {
-    public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
-
-        List<List<Integer>> ans = new ArrayList();
-       for(int i = 0; i < intervals.length; i++){
-
-            int[] in = intervals[i];
-
-            if(in[1] < toBeRemoved[0] || in[0] >= toBeRemoved[1]){
-                List<Integer> innerList = new ArrayList();
-                innerList.add(in[0]); innerList.add(in[1]);
-                ans.add(innerList);
-            }else{
-
-                if(in[1] > toBeRemoved[0] && in[0] < toBeRemoved[0]){
-                    List<Integer> innerList = new ArrayList();
-                    innerList.add(in[0]); innerList.add(toBeRemoved[0]);
-                    ans.add(innerList);
-                }
-                
-                if(in[1] > toBeRemoved[1] && in[0] < toBeRemoved[1]){
-                    List<Integer> innerList = new ArrayList();
-                    innerList.add(toBeRemoved[1]); innerList.add(in[1]); 
-                    ans.add(innerList);
-
-                }
+   public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int[] i : intervals) {
+            if (i[1] <= toBeRemoved[0] || i[0] >= toBeRemoved[1]) { // no overlap.
+                ans.add(Arrays.asList(i[0], i[1]));
+            }else { // i[1] > toBeRemoved[0] && i[0] < toBeRemoved[1].
+                if(i[0] < toBeRemoved[0]) // left end no overlap.
+                    ans.add(Arrays.asList(i[0], toBeRemoved[0]));
+                if (i[1] > toBeRemoved[1]) // right end no overlap.
+                    ans.add(Arrays.asList(toBeRemoved[1], i[1]));
             }
-       }
-
-       return ans;
-        
+        }
+        return ans;
     }
 }
