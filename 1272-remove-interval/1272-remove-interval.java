f31cc3d520bf6e28effdda93eb7a09452133a8ce
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-17 20:29:12 -0500

    Time: 10 ms (60.38%), Space: 55.5 MB (6.13%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.java
+++ 1272-remove-interval/1272-remove-interval.java
@@ -1,21 +1,33 @@
 class Solution {
     public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
-        List<List<Integer>> result = new ArrayList<>();
-        for (int[] interval : intervals) {
-            // If there are no overlaps, add the interval to the list as is.
-            if (interval[0] > toBeRemoved[1] || interval[1] < toBeRemoved[0]) {
-                result.add(Arrays.asList(interval[0], interval[1]));
-            } else {
-                // Is there a left interval we need to keep?
-                if (interval[0] < toBeRemoved[0]) {
-                    result.add(Arrays.asList(interval[0], toBeRemoved[0]));
+
+        List<List<Integer>> ans = new ArrayList();
+       for(int i = 0; i < intervals.length; i++){
+
+            int[] in = intervals[i];
+
+            if(in[1] < toBeRemoved[0] || in[0] >= toBeRemoved[1]){
+                List<Integer> innerList = new ArrayList();
+                innerList.add(in[0]); innerList.add(in[1]);
+                ans.add(innerList);
+            }else{
+
+                if(in[1] > toBeRemoved[0] && in[0] < toBeRemoved[0]){
+                    List<Integer> innerList = new ArrayList();
+                    innerList.add(in[0]); innerList.add(toBeRemoved[0]);
+                    ans.add(innerList);
                 }
-                // Is there a right interval we need to keep?
-                if (interval[1] > toBeRemoved[1]) {
-                    result.add(Arrays.asList(toBeRemoved[1], interval[1]));
+                
+                if(in[1] > toBeRemoved[1] && in[0] < toBeRemoved[1]){
+                    List<Integer> innerList = new ArrayList();
+                    innerList.add(toBeRemoved[1]); innerList.add(in[1]); 
+                    ans.add(innerList);
+
                 }
             }
-        }
-        return result;
+       }
+
+       return ans;
+        
     }
 }
