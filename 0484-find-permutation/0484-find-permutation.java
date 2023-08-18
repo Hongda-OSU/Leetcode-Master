6cab65d52a9e183c0525aa3b2c786e3c4af004ed
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-18 18:15:24 -0500

    Time: 9 ms (52.90%), Space: 45.1 MB (75.48%) - LeetHub

--- 0484-find-permutation/0484-find-permutation.java
+++ 0484-find-permutation/0484-find-permutation.java
@@ -1,19 +1,24 @@
-public class Solution {
+class Solution {
     public int[] findPermutation(String s) {
-        int[] res = new int[s.length() + 1];
-        Stack < Integer > stack = new Stack < > ();
+        int n = s.length();
+        int[] result = new int[n + 1];
+        Stack<Integer> stack = new Stack<>();
+        
         int j = 0;
-        for (int i = 1; i <= s.length(); i++) {
+        for (int i = 1; i <= n; i++) {
             if (s.charAt(i - 1) == 'I') {
                 stack.push(i);
-                while (!stack.isEmpty())
-                    res[j++] = stack.pop();
-            } else
+                while (!stack.isEmpty()) {
+                    result[j++] = stack.pop();
+                }
+            } else {
                 stack.push(i);
+            }
         }
-        stack.push(s.length() + 1);
-        while (!stack.isEmpty())
-            res[j++] = stack.pop();
-        return res;
+        stack.push(n + 1);
+        
+        while (!stack.isEmpty()) 
+            result[j++] = stack.pop();
+        return result;
     }
 }
