79d41af976c2549e20fe63ceb5e6c5ed45e1a329
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-18 18:43:59 -0500

    Time: 15 ms (33.37%), Space: 41.9 MB (50.92%) - LeetHub

--- 0439-ternary-expression-parser/0439-ternary-expression-parser.java
+++ 0439-ternary-expression-parser/0439-ternary-expression-parser.java
@@ -1,25 +1,27 @@
 class Solution {
     public String parseTernary(String expression) {
-        int i = 0, n = expression.length();
+        Stack<Character> stack = new Stack<>();
         
-        for (; i < n;) {
-            if (expression.charAt(i) != 'T' && expression.charAt(i) != 'F' 
-                || i == n - 1 || expression.charAt(i + 1) == ':') 
-                break;
+        // Traverse the expression from right to left
+        for (int i = expression.length() - 1; i >= 0; i--) {
             
-            if (expression.charAt(i) == 'T') {
-                i += 2;
-            } else {
-                int count;
-                for (count = 1, i += 2; count != 0; i++) {
-                    if (expression.charAt(i) == ':') {
-                        count--;
-                    } else if (expression.charAt(i) == '?') {
-                        count++;
-                    }
-                }
+            // If stack top is ?, then replace next four characters
+            // with E1 or E2 depending on the value of B
+            if (!stack.isEmpty() && stack.peek() == '?') {
+                stack.pop();
+                char onTrue = stack.pop();
+                stack.pop();
+                char onFalse = stack.pop();
+                stack.push(expression.charAt(i) == 'T' ? onTrue : onFalse);
+            }
+            
+            // Otherwise, push this character
+            else {
+                stack.push(expression.charAt(i));
             }
         }
-        return expression.substring(i, i + 1);
+        
+        // Return the final character
+        return String.valueOf(stack.peek());
     }
 }
