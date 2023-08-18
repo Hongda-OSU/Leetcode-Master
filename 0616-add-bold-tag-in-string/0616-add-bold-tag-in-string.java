8a7ab1186d000d9e83e2ecf6996201f0fc34e814
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-17 19:21:17 -0500

    Time: 5 ms (98.13%), Space: 43.6 MB (53.75%) - LeetHub

--- 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.java
+++ 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.java
@@ -1,91 +1,27 @@
 class Solution {
-    class TrieNode {
-        Map<Character, TrieNode> children;
-        boolean isWord;
-        
-        TrieNode() {
-            children = new HashMap<>();
-        }
-        
-        public String toString() {
-            return children.toString() + " - " + isWord;
-        }
-    }
-    
-    class Trie {
-        TrieNode root;
-        
-        Trie() {
-            root = new TrieNode();
-        }
-        
-        public void add(String word) {
-            int len = word.length();
-            TrieNode ptr = root;
-            for (int i = 0; i < len; ++ i) {
-                char c = word.charAt(i);
-                if (!ptr.children.containsKey(c)) {
-                    ptr.children.put(c, new TrieNode());
-                }
-                ptr = ptr.children.get(c);
-            }
-            ptr.isWord = true;
-        }
-        
-        public int search(String s, int i) {
-            int end = -1, len = s.length();
-            TrieNode ptr = root;
-            while (i < len) {
-                char c = s.charAt(i);
-                if (!ptr.children.containsKey(c)) {
-                    return end;
-                }
-                
-                ptr = ptr.children.get(c);
-                if (ptr.isWord) {
-                    end = i;
-                }
-                
-                ++ i;
-                
-            }
-            
-            return end;
-        }
-    }
-    
     public String addBoldTag(String s, String[] words) {
-        Trie trie = new Trie();
-        for (String word: words) {
-            trie.add(word);
-        }
-        
-        int len = s.length(), maxEnd = -1;
-        boolean[] isBold = new boolean[len];
-        
-        for (int i = 0; i < len; ++ i) {
-            int end = trie.search(s, i);
-            maxEnd = Math.max(maxEnd, end);
-            isBold[i] = maxEnd >= i;
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        boolean state = false;
-        for (int i = 0; i < len; ++ i) {
-            if (isBold[i] != state) {
-                state = !state;
-                if (state) {
-                    sb.append("<b>");
-                } else {
-                    sb.append("</b>");
+        int n = s.length();
+        boolean[] bold = new boolean[n];
+        for (String word : words) {
+            int start = s.indexOf(word);
+            while (start != -1) {
+                for (int i = start; i < start + word.length(); i++) {
+                    bold[i] = true;
                 }
+                start = s.indexOf(word, start + 1);
             }
-            sb.append(s.charAt(i));
-        }
-        if (state) {
-            sb.append("</b>");
         }
         
+        String openTag = "<b>", closeTag = "</b>";
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+            if (bold[i] && (i == 0 || !bold[i - 1]))
+                sb.append(openTag);
+            sb.append(s.charAt(i));
+            if (bold[i] && (i == n - 1 || !bold[i + 1])) 
+                sb.append(closeTag);
+        }
         return sb.toString();
     }
 }
