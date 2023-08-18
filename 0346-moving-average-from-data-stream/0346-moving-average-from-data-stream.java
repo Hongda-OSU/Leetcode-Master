124ed64690cd5ccd3cf0575ffdb8de0cf3c9a6c1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-18 18:37:08 -0500

    Time: 43 ms (75.60%), Space: 48.3 MB (79.65%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.java
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.java
@@ -1,21 +1,18 @@
 class MovingAverage {
-  int size, windowSum = 0, count = 0;
-  Deque queue = new ArrayDeque<Integer>();
+    int size, windowSum = 0, count = 0;
+    Deque<Integer> queue = new ArrayDeque<>();
 
-  public MovingAverage(int size) {
-    this.size = size;
-  }
-
-  public double next(int val) {
-    ++count;
-    // calculate the new sum by shifting the window
-    queue.add(val);
-    int tail = count > size ? (int)queue.poll() : 0;
-
-    windowSum = windowSum - tail + val;
-
-    return windowSum * 1.0 / Math.min(size, count);
-  }
+    public MovingAverage(int size) {
+        this.size = size;
+    }
+    
+    public double next(int val) {
+        count++;
+        queue.add(val);
+        int tail = count > size ? (int)queue.poll() : 0;
+        windowSum = windowSum - tail + val;
+        return windowSum * 1.0 / Math.min(size, count);
+    }
 }
 
 /**
