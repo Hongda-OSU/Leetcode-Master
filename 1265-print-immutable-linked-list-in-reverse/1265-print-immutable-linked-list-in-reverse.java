9ce40e93cb90e4e7f58e88c1cfeb1f26227f235a
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-23 14:21:52 -0500

    Time: 1 ms (70.91%), Space: 41.1 MB (84.55%) - LeetHub

--- 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.java
+++ 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.java
@@ -8,17 +8,28 @@
  */
 
 class Solution {
-    public void printLinkedListInReverse(ImmutableListNode head) {
-        ImmutableListNode curr;
-        ImmutableListNode end = null;
-
-        while (head != end) {
-            curr = head;
-            while (curr.getNext() != end) {
-                curr = curr.getNext();
-            }
-            curr.printValue();
-            end = curr;
+    private void helper(ImmutableListNode start, ImmutableListNode end) {
+        if (start == null || start == end) {
+            return;
         }
+        if (start.getNext() == end) {
+            start.printValue();
+            return;
+        }
+
+        ImmutableListNode slow = start;
+        ImmutableListNode fast = start;
+
+        while (fast != end && fast.getNext() != end) {
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+        }
+
+        helper(slow, end);
+        helper(start, slow);
+    }
+
+    public void printLinkedListInReverse(ImmutableListNode head) {
+        helper(head, null);
     }
 }
