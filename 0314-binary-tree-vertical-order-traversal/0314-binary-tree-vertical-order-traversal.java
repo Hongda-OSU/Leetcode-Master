1b8e52f214439946c885970f3130f045bc467dcc
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-23 18:27:23 -0500

    Time: 3 ms (53.45%), Space: 42.3 MB (11.40%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
@@ -13,47 +13,49 @@
  *     }
  * }
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
+  Map<Integer, ArrayList<Pair<Integer, Integer>>> columnTable = new HashMap();
+  int minColumn = 0, maxColumn = 0;
+
+  private void DFS(TreeNode node, Integer row, Integer column) {
+    if (node == null)
+      return;
+
+    if (!columnTable.containsKey(column)) {
+      this.columnTable.put(column, new ArrayList<Pair<Integer, Integer>>());
+    }
+
+    this.columnTable.get(column).add(new Pair<Integer, Integer>(row, node.val));
+    this.minColumn = Math.min(minColumn, column);
+    this.maxColumn = Math.max(maxColumn, column);
+    // preorder DFS traversal
+    this.DFS(node.left, row + 1, column - 1);
+    this.DFS(node.right, row + 1, column + 1);
+  }
+
   public List<List<Integer>> verticalOrder(TreeNode root) {
     List<List<Integer>> output = new ArrayList();
     if (root == null) {
       return output;
     }
 
-    Map<Integer, ArrayList> columnTable = new HashMap();
-    Queue<Pair<TreeNode, Integer>> queue = new ArrayDeque();
-    int column = 0;
-    queue.offer(new Pair(root, column));
+    this.DFS(root, 0, 0);
 
-    while (!queue.isEmpty()) {
-      Pair<TreeNode, Integer> p = queue.poll();
-      root = p.getKey();
-      column = p.getValue();
+    // Retrieve the resuts, by ordering by column and sorting by row
+    for (int i = minColumn; i < maxColumn + 1; ++i) {
 
-      if (root != null) {
-        if (!columnTable.containsKey(column)) {
-          columnTable.put(column, new ArrayList<Integer>());
+      Collections.sort(columnTable.get(i), new Comparator<Pair<Integer, Integer>>() {
+        @Override
+        public int compare(Pair<Integer, Integer> p1, Pair<Integer, Integer> p2) {
+          return p1.getKey() - p2.getKey();
         }
-        columnTable.get(column).add(root.val);
+      });
 
-        queue.offer(new Pair(root.left, column - 1));
-        queue.offer(new Pair(root.right, column + 1));
+      List<Integer> sortedColumn = new ArrayList();
+      for (Pair<Integer, Integer> p : columnTable.get(i)) {
+        sortedColumn.add(p.getValue());
       }
-    }
-
-    List<Integer> sortedKeys = new ArrayList<Integer>(columnTable.keySet());
-    Collections.sort(sortedKeys);
-    for(int k : sortedKeys) {
-      output.add(columnTable.get(k));
+      output.add(sortedColumn);
     }
 
     return output;
