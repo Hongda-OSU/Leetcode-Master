3093dbc87a3adc878808db231b884bc719b80ecd
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-25 17:19:23 -0500

    Time: 17 ms (80.28%), Space: 44 MB (35.06%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.java
+++ 0277-find-the-celebrity/0277-find-the-celebrity.java
@@ -2,17 +2,7 @@
       boolean knows(int a, int b); */
 
 public class Solution extends Relation {
-    
     private int numberOfPeople;
-    private Map<Pair<Integer, Integer>, Boolean> cache = new HashMap<>(); 
-    
-    @Override
-    public boolean knows(int a, int b) {
-        if (!cache.containsKey(new Pair(a, b))) {
-            cache.put(new Pair(a, b), super.knows(a, b));
-        }
-        return cache.get(new Pair(a, b));
-    }
     
     public int findCelebrity(int n) {
         numberOfPeople = n;
@@ -22,18 +12,17 @@ public class Solution extends Relation {
                 celebrityCandidate = i;
             }
         }
-        if (isCelebrity(celebrityCandidate)) {
+        if (isCelebrity(celebrityCandidate))
             return celebrityCandidate;
-        }
         return -1;
     }
     
-    private boolean isCelebrity(int i) {
-        for (int j = 0; j < numberOfPeople; j++) {
-            if (i == j) continue; // Don't ask if they know themselves.
-            if (knows(i, j) || !knows(j, i)) {
+    private boolean isCelebrity(int k) {
+        for (int i = 0; i < numberOfPeople; i++) {
+            if (i == k)
+                continue;
+            if (knows(k, i) || !knows(i, k))
                 return false;
-            }
         }
         return true;
     }
