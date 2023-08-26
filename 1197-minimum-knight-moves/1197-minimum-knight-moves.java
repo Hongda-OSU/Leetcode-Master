e8ae915b57bc63d1a4c9d9c8f26139363914a480
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-25 22:37:47 -0500

    Time: 612 ms (27.10%), Space: 54 MB (37.81%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.java
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.java
@@ -1,27 +1,32 @@
 class Solution {
+    private final int[][] DIRECTIONS = new int[][]
+        {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+    
     public int minKnightMoves(int x, int y) {
-        final int[][] MOVE = new int[][] {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
         x = Math.abs(x);
         y = Math.abs(y);
-        final int sizeX = x + 4, sizeY = y+4;
-        boolean [][] board = new boolean[sizeX][sizeY];
-        
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{0,0,0});
-        board[2][2] = true;
-        while (!q.isEmpty()){
-            int[] n = q.poll();
-            int count = n[2];
-            if (n[0] == x && n[1] == y)
-                return count;
-            ++count;
-            for (int[] move: MOVE){
-                int i = n[0] + move[0]+ 2, j = n[1]+move[1] + 2;
-                if (i <0 || j <0 || i>=sizeX ||j >=sizeY || board[i][j])
-                    continue;
-                board[i][j] = true;
-                q.add(new int[]{i-2, j-2, count});
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+        Set<String> visited = new HashSet<>();
+        visited.add("0,0");
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.remove();
+                int currX = curr[0], currY = curr[1];
+                if (currX == x && currY == y)
+                    return result;
+                for (int[] dir : DIRECTIONS) {
+                    int newX = currX + dir[0];
+                    int newY = currY + dir[1];
+                    if (!visited.contains(newX + "," + newY) && newX >= -1 && newY >= -1) {
+                        queue.add(new int[]{newX, newY});
+                        visited.add(newX + "," + newY);
+                    }
+                }
             }
+            result++;
         }
         return -1;
     }
