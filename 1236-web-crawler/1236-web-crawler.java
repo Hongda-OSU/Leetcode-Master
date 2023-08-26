c8c7399233c7efaf3b69ca0b36b4a6ba66010e1d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-25 21:18:50 -0500

    Time: 5 ms (84.58%), Space: 47.8 MB (44.28%) - LeetHub

--- 1236-web-crawler/1236-web-crawler.java
+++ 1236-web-crawler/1236-web-crawler.java
@@ -7,27 +7,29 @@
  */
 
 class Solution {
-    private String getHostname(String url) {
-        // split url by slashes
-        // for instance, "http://example.org/foo/bar" will be split into
-        // "http:", "", "example.org", "foo", "bar"
-        // the hostname is the 2-nd (0-indexed) element
-        return url.split("/")[2];
-    }
-
     public List<String> crawl(String startUrl, HtmlParser htmlParser) {
-        String startHostname = getHostname(startUrl);
-        Queue<String> q = new LinkedList<String>(Arrays.asList(startUrl));
-        HashSet<String> visited = new HashSet<String>(Arrays.asList(startUrl));
-        while (!q.isEmpty()) {
-            String url = q.remove();
-            for (String nextUrl : htmlParser.getUrls(url)) {
-                if (getHostname(nextUrl).equals(startHostname) && !visited.contains(nextUrl)) {
-                    q.add(nextUrl);
-                    visited.add(nextUrl);
+        Set<String> set = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        String hostname = getHostname(startUrl);
+        
+        queue.offer(startUrl);
+        set.add(startUrl);
+        
+        while (!queue.isEmpty()) {
+            String currentUrl = queue.poll();
+            for (String url : htmlParser.getUrls(currentUrl)) {
+                if (url.contains(hostname) && !set.contains(url)) {
+                    queue.offer(url);
+                    set.add(url);
                 }
             }
         }
-        return new ArrayList<>(visited);
+        
+        return new ArrayList<String>(set);
+    }
+    
+    private String getHostname(String Url) {
+        String[] ss = Url.split("/");
+        return ss[2];
     }
 }
