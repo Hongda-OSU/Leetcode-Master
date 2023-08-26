dd3860d350a6a9adc4fa14fe34d878340156ccdd
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-25 22:30:44 -0500

    Time: 14 ms (97.42%), Space: 43.4 MB (89.29%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.java
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.java
@@ -1,38 +1,25 @@
 class Solution {
-    private final int[][] DIRECTIONS = new int[][] {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
-    
-    public int minKnightMoves(int x, int y) {
-        x = Math.abs(x);
-        y = Math.abs(y);
-        
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {0, 0});
-        
-        Set<String> visited = new HashSet<>();
-        visited.add("0,0");
-        
-        int result = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int[] cur = queue.remove();
-                int curX = cur[0];
-                int curY = cur[1];
-                if (curX == x && curY == y) {
-                    return result;
-                }
-                
-                for (int[] d : DIRECTIONS) {
-                    int newX = curX + d[0];
-                    int newY = curY + d[1];
-                    if (!visited.contains(newX + "," + newY) && newX >= -1 && newY >= -1) {
-                        queue.add(new int[] {newX, newY});
-                        visited.add(newX + "," + newY);
-                    }
-                }
-            }
-            result++;
+    private Map<String, Integer> memo = new HashMap<>();
+
+    private int dfs(int x, int y) {
+        String key = x + "," + y;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
         }
-        return -1;
+
+        if (x + y == 0) {
+            return 0;
+        } else if (x + y == 2) {
+            return 2;
+        } else {
+            Integer ret = Math.min(dfs(Math.abs(x - 1), Math.abs(y - 2)),
+                    dfs(Math.abs(x - 2), Math.abs(y - 1))) + 1;
+            memo.put(key, ret);
+            return ret;
+        }
+    }
+
+    public int minKnightMoves(int x, int y) {
+        return dfs(Math.abs(x), Math.abs(y));
     }
 }
