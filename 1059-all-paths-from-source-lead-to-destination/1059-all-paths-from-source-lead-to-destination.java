ce984273a25bc3077155b584a6f7c0327a7a1f2e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-25 20:53:26 -0500

    Time: 5 ms (97.65%), Space: 45.8 MB (59.24%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.java
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.java
@@ -1,30 +1,32 @@
 class Solution {
-    enum State { PROCESSING, PROCESSED }
+    private enum State {PROCESSING, PROCESSED};
     
-    public boolean leadsToDestination(int n, int[][] edges, int src, int dest) {
-        List<Integer>[] graph = buildDigraph(n, edges);
-        return leadsToDest(graph, src, dest, new State[n]);
+    public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
+        List<Integer>[] graph = buildGraph(n, edges);
+        return leadsToDest(graph, source, destination, new State[n]);
+    }
+    
+    private List<Integer>[] buildGraph(int n, int[][] edges) {
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) 
+            graph[i] = new ArrayList<>();
+        for (int[] edge : edges) 
+            graph[edge[0]].add(edge[1]);
+        return graph;
     }
     
     private boolean leadsToDest(List<Integer>[] graph, int node, int dest, State[] states) {
-        if (states[node] != null) return states[node] == State.PROCESSED;
-        if (graph[node].isEmpty()) return node == dest;
+        if (states[node] != null)
+            return states[node] == State.PROCESSED;
+        if (graph[node].isEmpty())
+            return node == dest;
         states[node] = State.PROCESSING;
         for (int next : graph[node]) {
-            if (!leadsToDest(graph, next, dest, states)) return false;
+            if (!leadsToDest(graph, next, dest, states)) {
+                return false;
+            }
         }
         states[node] = State.PROCESSED;
         return true;
-    }
-    
-    private List<Integer>[] buildDigraph(int n, int[][] edges) {
-        List<Integer>[] graph = new List[n];
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        for (int[] edge : edges) {
-            graph[edge[0]].add(edge[1]);
-        }
-        return graph;
-    }
+    }  
 }
