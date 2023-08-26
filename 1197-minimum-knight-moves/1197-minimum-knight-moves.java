b694bdcb8d5c2fd655d4a349a25e7d0255f93fdc
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-25 22:29:39 -0500

    Time: 615 ms (26.97%), Space: 54.3 MB (25.68%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.java
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.java
@@ -1,40 +1,38 @@
 class Solution {
+    private final int[][] DIRECTIONS = new int[][] {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+    
     public int minKnightMoves(int x, int y) {
-        // the offsets in the eight directions
-        int[][] offsets = {{1, 2}, {2, 1}, {2, -1}, {1, -2},
-                {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
-
-        // - Rather than using the inefficient HashSet, we use the bitmap
-        //     otherwise we would run out of time for the test cases.
-        // - We create a bitmap that is sufficient to cover all the possible
-        //     inputs, according to the description of the problem.
-        boolean[][] visited = new boolean[607][607];
-
-        Deque<int[]> queue = new LinkedList<>();
-        queue.addLast(new int[]{0, 0});
-        int steps = 0;
-
-        while (queue.size() > 0) {
-            int currLevelSize = queue.size();
-            // iterate through the current level
-            for (int i = 0; i < currLevelSize; i++) {
-                int[] curr = queue.removeFirst();
-                if (curr[0] == x && curr[1] == y) {
-                    return steps;
+        x = Math.abs(x);
+        y = Math.abs(y);
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {0, 0});
+        
+        Set<String> visited = new HashSet<>();
+        visited.add("0,0");
+        
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.remove();
+                int curX = cur[0];
+                int curY = cur[1];
+                if (curX == x && curY == y) {
+                    return result;
                 }
-
-                for (int[] offset : offsets) {
-                    int[] next = new int[]{curr[0] + offset[0], curr[1] + offset[1]};
-                    // align the coordinate to the bitmap
-                    if (!visited[next[0] + 302][next[1] + 302]) {
-                        visited[next[0] + 302][next[1] + 302] = true;
-                        queue.addLast(next);
+                
+                for (int[] d : DIRECTIONS) {
+                    int newX = curX + d[0];
+                    int newY = curY + d[1];
+                    if (!visited.contains(newX + "," + newY) && newX >= -1 && newY >= -1) {
+                        queue.add(new int[] {newX, newY});
+                        visited.add(newX + "," + newY);
                     }
                 }
             }
-            steps++;
+            result++;
         }
-        // move on to the next level
-        return steps;
+        return -1;
     }
 }
