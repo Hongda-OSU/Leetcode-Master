8af726912817ff266e955739a6d47dd87fd8af3b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-28 17:21:52 -0500

    Time: 4 ms (94.63%), Space: 43.8 MB (80.30%) - LeetHub

--- 1086-high-five/1086-high-five.java
+++ 1086-high-five/1086-high-five.java
@@ -1,28 +1,33 @@
 class Solution {
+
     private int K;
 
     public int[][] highFive(int[][] items) {
         this.K = 5;
-        TreeMap<Integer, Queue<Integer>> allScores = new TreeMap<>();
-        for (int[] item : items) {
-            int id = item[0];
-            int score = item[1];
-            if (!allScores.containsKey(id))
-                allScores.put(id, new PriorityQueue<>());
-            // insert the score in the min heap
-            allScores.get(id).add(score);
-            // remove the minimum element from the min heap in case the size of the min heap exceeds 5 
-            if (allScores.get(id).size() > this.K)
-                allScores.get(id).poll();
-        }
-
+        Arrays.sort(
+                items,
+                new Comparator<int[]>() {
+                    @Override
+                    public int compare(int[] a, int[] b) {
+                        if (a[0] != b[0])
+                            // item with lower id goes first
+                            return a[0] - b[0];
+                        // in case of tie for ids, item with higher score goes first
+                        return b[1] - a[1];
+                    }
+                });
         List<int[]> solution = new ArrayList<>();
-
-        for (int id : allScores.keySet()) {
+        int n = items.length;
+        int i = 0;
+        while (i < n) {
+            int id = items[i][0];
             int sum = 0;
-            // min heap contains the top 5 scores
-            for (int i = 0; i < this.K; ++i)
-                sum += allScores.get(id).poll();
+            // obtain total using the top 5 scores
+            for (int k = i; k < i + this.K; ++k)
+                sum += items[k][1];
+            // ignore all the other scores for the same id
+            while (i < n && items[i][0] == id)
+                i++;
             solution.add(new int[] {id, sum / this.K});
         }
         int[][] solutionArray = new int[solution.size()][];
