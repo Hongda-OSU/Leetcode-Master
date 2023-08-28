a4ef8431ad112b1bc81e0912aaa1cba5922e07b1
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-28 17:26:26 -0500

    Time: 4 ms (94.63%), Space: 43.8 MB (80.30%) - LeetHub

--- 1086-high-five/1086-high-five.java
+++ 1086-high-five/1086-high-five.java
@@ -1,36 +1,25 @@
 class Solution {
-
-    private int K;
-
     public int[][] highFive(int[][] items) {
-        this.K = 5;
-        Arrays.sort(
-                items,
-                new Comparator<int[]>() {
-                    @Override
-                    public int compare(int[] a, int[] b) {
-                        if (a[0] != b[0])
-                            // item with lower id goes first
-                            return a[0] - b[0];
-                        // in case of tie for ids, item with higher score goes first
-                        return b[1] - a[1];
-                    }
-                });
-        List<int[]> solution = new ArrayList<>();
-        int n = items.length;
-        int i = 0;
-        while (i < n) {
-            int id = items[i][0];
-            int sum = 0;
-            // obtain total using the top 5 scores
-            for (int k = i; k < i + this.K; ++k)
-                sum += items[k][1];
-            // ignore all the other scores for the same id
-            while (i < n && items[i][0] == id)
-                i++;
-            solution.add(new int[] {id, sum / this.K});
+        Arrays.sort(items, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        List<int[]> list = new ArrayList<>();
+        int count = 0, sum = 0, prevId = -1;
+        for (int[] item : items) {
+            if (item[0] != prevId) {
+                count = 1;
+                prevId = item[0];
+                sum = item[1];
+            } else {
+                count++;
+                sum += item[1];
+            }
+            if (count == 5) 
+                list.add(new int[]{item[0], sum / 5});
         }
-        int[][] solutionArray = new int[solution.size()][];
-        return solution.toArray(solutionArray);
+        int[][] result = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) {
+            result[i][0] = list.get(i)[0];
+            result[i][1] = list.get(i)[1];
+        }
+        return result;
     }
 }
