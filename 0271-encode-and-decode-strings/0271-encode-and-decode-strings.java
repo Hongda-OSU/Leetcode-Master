76db04d469f92a9a1eb9fb53c5aef42b046ed55e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-30 17:31:02 -0500

    Time: 4 ms (95.40%), Space: 44.7 MB (41.44%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -1,62 +1,30 @@
 public class Codec {
-
-    // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        // Initialize a StringBuilder to hold the encoded strings
+        // Initialize a StringBuilder to hold the encoded string.
         StringBuilder encodedString = new StringBuilder();
-
-        // Iterate over each string in the input list
         for (String s : strs) {
-            // Replace each occurrence of '/' with '//'
-            // This is our way of "escaping" the slash character
-            // Then add our delimiter '/:' to the end
-            encodedString.append(s.replace("/", "//")).append("/:");
+            // Append the length, the delimiter, and the string itself.
+            encodedString.append(s.length()).append("/:").append(s);
         }
-
-        // Return the final encoded string
         return encodedString.toString();
     }
 
-    // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        // Initialize a List to hold the decoded strings
+        // Initialize a list to hold the decoded strings.
         List<String> decodedStrings = new ArrayList<>();
-
-        // Initialize a StringBuilder to hold the current string being built
-        StringBuilder currentString = new StringBuilder();
-
-        // Initialize an index 'i' to start of the string
         int i = 0;
-
-        // Iterate while 'i' is less than the length of the encoded string
         while (i < s.length()) {
-            // If we encounter the delimiter '/:'
-            if (i + 1 < s.length() && s.charAt(i) == '/' && s.charAt(i + 1) == ':') {
-                // Add the currentString to the list of decodedStrings
-                decodedStrings.add(currentString.toString());
-
-                // Clear currentString for the next string
-                currentString = new StringBuilder();
-
-                // Move the index 2 steps forward to skip the delimiter
-                i += 2;
-            }
-            // If we encounter an escaped slash '//'
-            else if (i + 1 < s.length() && s.charAt(i) == '/' && s.charAt(i + 1) == '/') {
-                // Add a single slash to the currentString
-                currentString.append('/');
-
-                // Move the index 2 steps forward to skip the escaped slash
-                i += 2;
-            }
-            // Otherwise, just add the character to currentString and move the index 1 step forward.
-            else {
-                currentString.append(s.charAt(i));
-                i++;
-            }
+            // Find the delimiter.
+            int delim = s.indexOf("/:", i);
+            // Get the length, which is before the delimiter.
+            int length = Integer.parseInt(s.substring(i, delim));
+            // Get the string, which is of 'length' length after the delimiter.
+            String str = s.substring(delim + 2, delim + 2 + length);
+            // Add the string to the list.
+            decodedStrings.add(str);
+            // Move the index to the start of the next length.
+            i = delim + 2 + length;
         }
-
-        // Return the list of decoded strings
         return decodedStrings;
     }
 }
