48b1223acedf70ce6c4702acf8062e848c1af77e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-30 17:28:23 -0500

    Time: 13 ms (56.55%), Space: 43.9 MB (45.83%) - LeetHub

--- 0604-design-compressed-string-iterator/0604-design-compressed-string-iterator.java
+++ 0604-design-compressed-string-iterator/0604-design-compressed-string-iterator.java
@@ -1,28 +1,31 @@
-public class StringIterator {
-    
+class StringIterator {
     Queue<int[]> queue = new LinkedList<>();
-    
+
     public StringIterator(String s) {
         int i = 0, n = s.length();
         while (i < n) {
-            int j = i+1;
-            while (j < n && s.charAt(j) - 'A' < 0) j++;
-            queue.add(new int[]{s.charAt(i) - 'A',  Integer.parseInt(s.substring(i+1, j))});
+            int j = i + 1;
+            while (j < n && s.charAt(j) - 'A' < 0) {
+                j++;
+            }
+            queue.add(new int[]{s.charAt(i) - 'A', Integer.parseInt(s.substring(i + 1, j))});
             i = j;
         }
     }
     
     public char next() {
-        if (queue.isEmpty()) return ' ';
+        if (queue.isEmpty())
+            return ' ';
         int[] top = queue.peek();
-        if (--top[1] == 0) queue.poll();
+        if (--top[1] == 0)
+            queue.poll();
         return (char) ('A' + top[0]);
+            
     }
     
     public boolean hasNext() {
         return !queue.isEmpty();
     }
-
 }
 
 /**
