f9b5c079fc873a7548aadd2fd3a2ff49ad9082e8
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-30 17:36:43 -0500

    Time: 4 ms (95.40%), Space: 44.6 MB (67.14%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -1,31 +1,26 @@
 public class Codec {
+
+    // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        // Initialize a StringBuilder to hold the encoded string.
-        StringBuilder encodedString = new StringBuilder();
-        for (String s : strs) {
-            // Append the length, the delimiter, and the string itself.
-            encodedString.append(s.length()).append("/:").append(s);
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length()).append("/:").append(str);
         }
-        return encodedString.toString();
+        return sb.toString();
     }
 
+    // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        // Initialize a list to hold the decoded strings.
-        List<String> decodedStrings = new ArrayList<>();
+        List<String> decoded = new ArrayList<>();
         int i = 0;
         while (i < s.length()) {
-            // Find the delimiter.
             int delim = s.indexOf("/:", i);
-            // Get the length, which is before the delimiter.
             int length = Integer.parseInt(s.substring(i, delim));
-            // Get the string, which is of 'length' length after the delimiter.
             String str = s.substring(delim + 2, delim + 2 + length);
-            // Add the string to the list.
-            decodedStrings.add(str);
-            // Move the index to the start of the next length.
+            decoded.add(str);
             i = delim + 2 + length;
         }
-        return decodedStrings;
+        return decoded;
     }
 }
 
