932a2606196640fdc0be51ccdb3b8133711eff0c
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-30 18:10:28 -0500

    Time: 2 ms (96.86%), Space: 45.7 MB (75.26%) - LeetHub

--- 0428-serialize-and-deserialize-n-ary-tree/0428-serialize-and-deserialize-n-ary-tree.java
+++ 0428-serialize-and-deserialize-n-ary-tree/0428-serialize-and-deserialize-n-ary-tree.java
@@ -19,12 +19,21 @@ class Node {
 
 class Codec {
 
+    class WrappableInt {
+        private int value;
+        public WrappableInt(int x) {
+            this.value = x;
+        }
+        public int getValue() {
+            return this.value;
+        }
+        public void increment() {
+            this.value++;
+        }
+    }
+    
     // Encodes a tree to a single string.
     public String serialize(Node root) {
-        
-        if (root == null) {
-            return "";
-        }
         
         StringBuilder sb = new StringBuilder();
         this._serializeHelper(root, sb);
@@ -33,104 +42,50 @@ class Codec {
     
     private void _serializeHelper(Node root, StringBuilder sb) {
         
-        // Queue to perform a level order traversal of the tree
-        Queue<Node> q = new LinkedList<Node>();
-        
-        // Two dummy nodes that will help us in serialization string formation.
-        // We insert the "endNode" whenever a level ends and the "childNode"
-        // whenever a node's children are added to the queue and we are about
-        // to switch over to the next node.
-        Node endNode = new Node();
-        Node childNode = new Node();
-        q.add(root);
-        q.add(endNode);
-        
-        while (!q.isEmpty()) {
-            
-            // Pop a node
-            Node node = q.poll();
-            
-            // If this is an "endNode", we need to add another one
-            // to mark the end of the current level unless this
-            // was the last level.
-            if (node == endNode) {
-                
-                // We add a sentinal value of "#" here
-                sb.append('#');
-                if (!q.isEmpty()) {
-                    q.add(endNode);  
-                }
-            } else if (node == childNode) {
-                
-                // Add a sentinal value of "$" here to mark the switch to a
-                // different parent.
-                sb.append('$');
-            } else {
-                
-                // Add value of the current node and add all of it's
-                // children nodes to the queue. Note how we convert
-                // the integers to their corresponding ASCII counterparts.
-                sb.append((char) (node.val + '0'));
-                for (Node child : node.children) {
-                    q.add(child);
-                }
-                
-                // If this not is NOT the last one on the current level, 
-                // add a childNode as well since we move on to processing
-                // the next node.
-                if (q.peek() != endNode) {
-                    q.add(childNode);
-                }
-            }
+        if (root == null) {
+            return;
         }
+        
+        // Add the value of the node
+        sb.append((char) (root.val + '0'));
+        
+        // Recurse on the subtrees and build the 
+        // string accordingly
+        for (Node child : root.children) {
+            this._serializeHelper(child, sb);
+        }
+        
+        // Add the sentinel to indicate that all the children
+        // for the current node have been processed
+        sb.append('#');
     }
 
     // Decodes your encoded data to tree.
     public Node deserialize(String data) {
-        if (data.isEmpty()) {
+        if(data.isEmpty())
             return null;
-        }
-            
-        Node rootNode = new Node(data.charAt(0) - '0', new ArrayList<Node>());
-        this._deserializeHelper(data, rootNode);
-        return rootNode;
+        
+        return this._deserializeHelper(data, new WrappableInt(0));
     }
     
-    private void _deserializeHelper(String data, Node rootNode) {  
+    private Node _deserializeHelper(String data, WrappableInt index) {  
         
-        // We move one level at a time and at every level, we need access
-        // to the nodes on the previous level as well so that we can form
-        // the children arrays properly. Hence two arrays.
-        LinkedList<Node> currentLevel = new LinkedList<Node>();
-        LinkedList<Node> prevLevel = new LinkedList<Node>();
-        currentLevel.add(rootNode);
-        Node parentNode = rootNode;
-        
-        // Process the characters in the string one at a time.
-        for (int i = 1; i < data.length(); i++) {
-            char d = data.charAt(i);
-            if (d == '#') {
-                // Special processing for end of level. We need to swap the
-                // array lists. Here, we simply re-initialize the "currentLevel"
-                // arraylist rather than clearing it.
-                prevLevel = currentLevel;
-                currentLevel = new LinkedList<Node>();
-                
-                // Since we move one level down, we take the parent as the first
-                // node on the current level.
-                parentNode = prevLevel.poll();
-            } else {
-                if (d == '$') {
-                    
-                    // Special handling for change in parent on the same level
-                    parentNode = prevLevel.poll();
-                } else {
-                    Node childNode = new Node(d - '0', new ArrayList<Node>());    
-                    currentLevel.add(childNode);
-                    parentNode.children.add(childNode);
-                }
-            }
+        if (index.getValue() == data.length()) {
+            return null;
         }
+        
+        Node node = new Node(data.charAt(index.getValue()) - '0', new ArrayList<Node>());
+        index.increment();
+        while (data.charAt(index.getValue()) != '#') {
+            node.children.add(this._deserializeHelper(data, index));
+        }
+        
+        // Discard the sentinel. Note that this also moves us
+        // forward in the input string. So, we don't have the index
+        // progressing inside the above while loop!
+        index.increment();
+        
+        return node;
     }
 }
 
