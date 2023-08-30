2edd32f65c89289143a3a9d55356ccb65eb6f9d2
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-30 18:13:55 -0500

    Time: 7 ms (77.35%), Space: 46.3 MB (5.92%) - LeetHub

--- 0428-serialize-and-deserialize-n-ary-tree/0428-serialize-and-deserialize-n-ary-tree.java
+++ 0428-serialize-and-deserialize-n-ary-tree/0428-serialize-and-deserialize-n-ary-tree.java
@@ -19,45 +19,42 @@ class Node {
 
 class Codec {
 
-    // Encodes a tree to a single string.
-    public String serialize(Node root) {
-        List<String> list=new LinkedList<>();
-        serializeHelper(root,list);
-        return String.join(",",list);
-    }
-    
-    private void serializeHelper(Node root, List<String> list){
-        if(root==null){
-            return;
-        }else{
-            list.add(String.valueOf(root.val));
-            list.add(String.valueOf(root.children.size()));
-            for(Node child:root.children){
-                serializeHelper(child,list);
-            }
+// Encodes a tree to a single string.
+public String serialize(Node root) {
+    StringBuilder sb = new StringBuilder();
+    serialize(root, sb);
+    return sb.toString();
+}
+ 
+private void serialize(Node root, StringBuilder sb){
+    if(root == null) {
+      sb.append("#").append(",");   
+    }else{
+        sb.append(root.val).append(",");
+        sb.append(root.children.size()).append(",");
+        for(Node child : root.children){
+            serialize(child, sb);
         }
-    }
-
-    // Decodes your encoded data to tree.
-    public Node deserialize(String data) {
-        if(data.isEmpty())
-            return null;
-        
-        String[] ss=data.split(",");
-        Queue<String> q=new LinkedList<>(Arrays.asList(ss));
-        return deserializeHelper(q);
-    }
+    }      
+}
+// Decodes your encoded data to tree.
+public Node deserialize(String data) {
+    Queue<String> q = new LinkedList<>(Arrays.asList(data.split(",")));
+    return deserialize(q);
+}
+private Node deserialize(Queue<String> q){
+    String s = q.poll();
+    if(s.equals("#")) return null;
     
-    private Node deserializeHelper(Queue<String> q){
-        Node root=new Node();
-        root.val=Integer.parseInt(q.poll());
-        int size=Integer.parseInt(q.poll());
-        root.children=new ArrayList<Node>(size);
-        for(int i=0;i<size;i++){
-            root.children.add(deserializeHelper(q));
-        }
-        return root;
+    Node root = new Node(Integer.valueOf(s));
+    int children = Integer.valueOf(q.poll());
+    
+    root.children = new ArrayList<>();
+    for(int i=0; i< children; i++){
+        root.children.add(deserialize(q));
     }
+    return root;
+}
 }
 
 // Your Codec object will be instantiated and called as such:
