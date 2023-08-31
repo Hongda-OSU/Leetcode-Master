a3044906ee65737d31332c6582a93e53fda009e4
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-31 16:32:24 -0500

    Time: 3 ms (100.00%), Space: 54.7 MB (71.35%) - LeetHub

--- 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.java
+++ 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.java
@@ -1,24 +1,42 @@
 class Solution {
+    private List<String> result;
+    
     public List<String> findStrobogrammatic(int n) {
-        return findStrobogrammaticHelper(n, n);
+        result = new ArrayList<>();
+        findStrobogrammaticHelper(new char[n], 0, n - 1);
+        return result;
     }
     
-    private List<String> findStrobogrammaticHelper(int m, int n) {
-        if (m == 0)
-            return new ArrayList<>(Arrays.asList(""));
-        if (m == 1) 
-            return new ArrayList<>(Arrays.asList("0", "1", "8"));
-        List<String> list = findStrobogrammaticHelper(m - 2, n);
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            String str = list.get(i);
-            if (m != n)
-                result.add("0" + str + "0");
-            result.add("1" + str + "1");
-            result.add("6" + str + "9");
-            result.add("8" + str + "8");
-            result.add("9" + str + "6");
+    private void findStrobogrammaticHelper(char[] arr, int left, int right) {
+        if (left > right) {
+            result.add(new String(arr));
+            return;
         }
-        return result;
+        if (left == right) {
+            arr[left] = '0';
+            result.add(new String(arr));
+            arr[left] = '1';
+            result.add(new String(arr));
+            arr[left] = '8';
+            result.add(new String(arr));
+            return;
+        }
+        if (left != 0) {
+            arr[left] = '0';
+            arr[right] = '0';
+            findStrobogrammaticHelper(arr, left + 1, right - 1);
+        }
+        arr[left] = '1';
+        arr[right] = '1';
+        findStrobogrammaticHelper(arr, left + 1, right - 1);
+        arr[left] = '8';
+        arr[right] = '8';
+        findStrobogrammaticHelper(arr, left + 1, right - 1);
+        arr[left] = '6';
+        arr[right] = '9';
+        findStrobogrammaticHelper(arr, left + 1, right - 1);
+        arr[left] = '9';
+        arr[right] = '6';
+        findStrobogrammaticHelper(arr, left + 1, right - 1);
     }
 }
