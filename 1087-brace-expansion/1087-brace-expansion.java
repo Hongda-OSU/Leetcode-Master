0311b7909cdab98dfac884a56177d274a45616b3
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-08-31 17:03:59 -0500

    Time: 3 ms (69.77%), Space: 43.5 MB (83.72%) - LeetHub

--- 1087-brace-expansion/1087-brace-expansion.java
+++ 1087-brace-expansion/1087-brace-expansion.java
@@ -1,39 +1,41 @@
 class Solution {
-    public String[] expand(String S) {
-      List<String> res = new ArrayList<>();
-      dfs(S, 0, new StringBuilder(), res);   
-      
-      String[] out = new String[res.size()];
-      for (int i = 0; i < res.size(); i++) { out[i] = res.get(i); }
-      return out;
-  }
-  
-  private void dfs(String s, int index, StringBuilder sb, List<String> res) {
-      if (index == s.length()) {
-          if (sb.length() > 0) { res.add(sb.toString()); }
-          return;
-      }
-      
-      char c = s.charAt(index);
-      int position = sb.length();
-      if (c == '{') {
-          List<Character> charList = new ArrayList<>();
-          int endIndex = index + 1;
-          while (endIndex < s.length() && s.charAt(endIndex) != '}') { 
-              if (Character.isLetter(s.charAt(endIndex))) { charList.add(s.charAt(endIndex)); }
-              endIndex++; 
-          }    
-          
-          Collections.sort(charList);
-          for (char d : charList) {
-              sb.append(d);
-              dfs(s, endIndex + 1, sb, res);
-              sb.setLength(position);
-          }
-          
-      } else if (Character.isLetter(c)) {
-          sb.append(s.charAt(index));
-          dfs(s, index + 1, sb, res);    
-      }
-  }
+    public String[] expand(String s) {
+        List<String> list = new ArrayList<>();
+        dfs(s, 0, new StringBuilder(), list);
+        String[] result = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+    
+    private void dfs(String str, int index, StringBuilder sb, List<String> list) {
+        if (index == str.length()) {
+            if (sb.length() > 0) {
+                list.add(sb.toString());
+            }
+            return;
+        }
+        char ch = str.charAt(index);
+        int position = sb.length();
+        if (ch == '{') {
+            List<Character> charList = new ArrayList<>();
+            int endIndex = index + 1;
+            while (endIndex < str.length() && str.charAt(endIndex) != '}') {
+                if (Character.isLetter(str.charAt(endIndex))) {
+                    charList.add(str.charAt(endIndex));
+                }
+                endIndex++;
+            }
+            Collections.sort(charList);
+            for (char c : charList) {
+                sb.append(c);
+                dfs(str, endIndex + 1, sb, list);
+                sb.setLength(position);
+            }
+        } else if (Character.isLetter(ch)) {
+            sb.append(str.charAt(index));
+            dfs(str, index + 1, sb, list);
+        }
+    }
 }
