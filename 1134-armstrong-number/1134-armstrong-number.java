05f15816a55e22b8334efac6c918c9509f496a6e
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-09-01 19:43:52 -0500

    Time: 0 ms (100.00%), Space: 39.2 MB (60.99%) - LeetHub

--- 1134-armstrong-number/1134-armstrong-number.java
+++ 1134-armstrong-number/1134-armstrong-number.java
@@ -1,24 +1,15 @@
 class Solution {
-    public int getSumOfKthPowerOfDigits(int n, int k) {
-        // `result` stores the result of sum of k'th power of each digit.
-       int result = 0;
-
-       // Run until n is not 0
-       while (n != 0) {
-           // Modulo 10 gives us the last digit
-           // Add digit ^ k to the result
-           result += Math.pow(n % 10, k);
-
-           // Remove the last digit.
-           n /= 10;
-       }
-       return result;
-    }
     public boolean isArmstrong(int n) {
-        // Get length of the number by conveting to string.
         int length = String.valueOf(n).length();
-
-        // Return true if Sum of k'th power of digits equals original number.
         return getSumOfKthPowerOfDigits(n, length) == n;
+    }
+    
+    private int getSumOfKthPowerOfDigits(int n, int k) {
+        int result = 0;
+        while (n != 0) {
+            result += Math.pow(n % 10, k);
+            n /= 10;
+        }
+        return result;
     }
 }
