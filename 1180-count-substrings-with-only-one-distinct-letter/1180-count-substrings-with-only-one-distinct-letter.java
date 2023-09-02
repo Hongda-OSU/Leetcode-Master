3247abdf7f523d0960cd534f9d9eedb8ead88492
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-09-01 19:45:57 -0500

    Time: 1 ms (78.86%), Space: 40.5 MB (48.78%) - LeetHub

---
+++ 1180-count-substrings-with-only-one-distinct-letter/1180-count-substrings-with-only-one-distinct-letter.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countLetters(String S) {
+        int total = 1, count = 1;
+        for (int i = 1; i < S.length(); i++) {
+            if (S.charAt(i) == S.charAt(i-1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+            total += count;
+        }
+        return total;
+    }
+}
