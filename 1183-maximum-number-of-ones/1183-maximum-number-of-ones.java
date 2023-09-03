b0273cc50211ba3cbbc40cdb928d341b21d4e04b
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-09-02 21:41:05 -0500

    Time: 10 ms (66.12%), Space: 40.9 MB (23.53%) - LeetHub

--- 1183-maximum-number-of-ones/1183-maximum-number-of-ones.java
+++ 1183-maximum-number-of-ones/1183-maximum-number-of-ones.java
@@ -1,15 +1,16 @@
 class Solution {
-    public int maximumNumberOfOnes(int m, int n, int k, int max) {
-        int[][] store = new int[k*k][];
-        for(int i=0; i<store.length; ++i)
-            store[i] = new int[] {i, 0};
-        for(int i=0, x=0; i<m; ++i, x = (x == k-1 ? 0 : x+1))
-            for(int j=0, y=0; j<n; ++j, y = (y == k-1 ? 0 : y+1))
-                ++store[x*k + y][1];
-        Arrays.sort(store, (a, b) -> b[1] - a[1]);
-        int ans = 0;
-        for(int i=0; i<max; ++i)
-            ans += store[i][1];
-        return ans;
+    public int maximumNumberOfOnes(int width, int height, int sideLength, int maxOnes) {
+        List<Integer> count = new ArrayList<>();
+        for (int r = 0; r < sideLength; r++) {
+            for (int c = 0; c < sideLength; c++) {
+                count.add((1 + (width - 1 - c) / sideLength) * (1 + (height - 1 - r) / sideLength));
+            }
+        }
+        count.sort(Comparator.reverseOrder());
+        int result = 0;
+        for (int i = 0; i < maxOnes; i++) {
+            result += count.get(i);
+        }
+        return result;
     }
 }
